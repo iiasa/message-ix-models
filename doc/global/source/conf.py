8ab39962567e72f9f7375f00c28e4cf3e68721e3
Matthew Gidden <matthew.gidden@gmail.com>
2016-03-30 16:52:40 +0200

    Add the ability to cite things

--- doc/global/source/conf.py
+++ doc/global/source/conf.py
@@ -35,7 +35,8 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.mathjax'
+    'sphinx.ext.mathjax',
+    'sphinxcontrib.bibtex',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
