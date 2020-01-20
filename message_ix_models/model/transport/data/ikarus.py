94e39715eacfc6c059cbfb5388923d980442b537
Francesco Lovat <franlovat@gmail.com>
2020-01-20 11:30:34 +0100

    Update ikarus.py to import one tech from geam_trp_techinput.xlsx as a test

--- message_ix_models/model/transport/data/ikarus.py
+++ message_ix_models/model/transport/data/ikarus.py
@@ -1,28 +1,76 @@
-"""Prepare data from the IKARUS model via GEAM_TRP_techinput.xlsx."""
-from message_ix.model.transport.util import ScenarioInfo
+"""Prepare non-LDV data from the IKARUS model via GEAM_TRP_techinput.xlsx."""
+from openpyxl import load_workbook
+import pandas as pd
+import pint
+
+from message_data.model.transport.common import DATA_PATH
+from message_data.tools import ScenarioInfo
+
+
+FILE = 'GEAM_TRP_techinput.xlsx'
+
+# - Set up a pint.UnitRegistry
+u = pint.UnitRegistry()
+
+# Compute message_ix quantities *with* proper unit conversion:
+
+# - define all the units in UnitRegistry: i.e. EUR
+
+# Transport units
+u.define('vehicle = [vehicle] = v')
+u.define('passenger = [passenger] = p = pass')
+u.define('tonne_freight = [tonne_freight] = tf = tonnef')
+u.define('pkm = passenger * kilometer')
+u.define('tkm = tonne_freight * kilometer')
+u.define('@alias vkm = vkt = v km')
+u.define('@alias pkm = pkt = p km')
+u.define('@alias tkm = tkt = t km')
+# Currencies
+u.define('euro_2005 = [currency] = EUR_2005 = €_2005')
+# Based on Germany's GDP deflator, data from WorldBank
+u.define('euro_2000 = 0.94662 * EUR_2005 = EUR_2000 = €_2000'),
+# Exchange rate EUR/USD in 2005, data from WorldBank
+u.define('dollar_2005 = 1.2435 * EUR_2005 = USD_2005 = $_2005')
+
+# Based on units from excel sheet
+
+# input efficiency (~minp)
+# output efficiency (~moutp)
+# capacity_factor (~plf)
+# technical_lifetime (~pll)
+# inv (~inv)
+# fixed_cost (~fom)
+dict_units = {
+    'inv_cost': u('MEUR_2005 / vehicle'),
+    'fix_cost': u('kEUR_2005 / vehicle'),
+    'var_cost': u('EUR_2005 / hectokilometer / vehicle'),
+    'technical_lifetime': u('year'),
+    'availability': u('hectokilometer / vehicle / year'),
+    'input_electricity': u('GJ / hectokilometer / vehicle'),
+    'output': u('1')
+}
 
 
 def main(scenario):
     s_info = ScenarioInfo(scenario)
-    # Open the GEAM_TRP_techinput.xls file using openpyxl
-
+    # Open *GEAM_TRP_techinput.xlsx* using openpyxl
+    wb = load_workbook(DATA_PATH / FILE, read_only=True, data_only=True)
     # Open the 'updateTRPdata' sheet
-
+    sheet = wb['updateTRPdata']
     # Read values from table for e.g. "regional train electric efficient"
     # (= rail_pub)
+    data = pd.DataFrame(list(sheet['C103':'I109']),
+                        index=dict_units.keys()).applymap(lambda c: c.value). \
+                        applymap('{:,.2f}'.format)
+    data.columns = [2000, 2005, 2010, 2015, 2020, 2025, 2030]
 
-    # Compute message_ix quantities *with* proper unit conversion:
+    # Assign units to rows of *data*
+    for idx, unit in enumerate(dict_units):
+        data.iloc[[idx]] = (data.iloc[[idx]]) * dict_units[unit]
 
-    # - Set up a pint.UnitRegistry
-    # - define all the units: EUR
-
-    s_info.N  # list of nodes e.g. for node_loc column of parameters
-    s_info.Y  # list of years e.g. for year_vtg column of parameters
-
-    # moutp
-    # capacity_factor (~plf)
-    # technical_lifetime (~pll)
-    # inv (~inv)
-    # fixed_cost (~fom)
+    nodes = s_info.N  # list of nodes e.g. for node_loc column of parameters
+    years = s_info.Y  # list of years e.g. for year_vtg column of parameters
 
     # Write the resulting data to temporary files: 1 per parameter.
+    wb.save("temp_one_parameter.xlsx")
+
