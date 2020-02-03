bf0c7145b409f89d61a084d03dbc294147b34daf
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-02-03 16:39:16 +0100

    Expand ldv data import

--- message_ix_models/model/transport/data/ldv.py
+++ message_ix_models/model/transport/data/ldv.py
@@ -3,30 +3,50 @@ from openpyxl import load_workbook
 import pandas as pd
 
 from message_data.model.transport.common import DATA_PATH, UNITS
+from message_data.tools import ScenarioInfo
 
 
 FILE = 'LDV_costs_efficiencies_US-TIMES_MA3T.xlsx'
 
-units = {
-    'efficiency': UNITS('10^9 v km / GW / year'),
-    'inv_cost': UNITS('USD / vehicle'),
-    'fix_cost': UNITS('USD / vehicle'),
-}
+TABLES = [
+    ('efficiency', slice('B3', 'Q15'), UNITS('10^9 v km / GW h / year')),
+    ('inv_cost', slice('B33', 'Q45'), UNITS('USD / vehicle')),
+    ('fix_cost', slice('B62', 'Q74'), UNITS('USD / vehicle')),
+]
 
 
 def get_ldv_data(scenario):
+    s_info = ScenarioInfo(scenario)
+
     wb = load_workbook(DATA_PATH / FILE, read_only=True, data_only=True)
 
-    # Efficiency table
-    unit = UNITS('10^9 v km / GW / year')
+    # Tables
+    dfs = []
 
-    sheet = wb['MESSAGE_LDV_nam']
-    data = pd.DataFrame(sheet['B3':'Q15']) \
-             .applymap(lambda c: c.value)
-    data.columns = data.loc[0, :]
-    data = data.iloc[1:, :] \
-               .drop('Description', axis=1) \
-               .melt(id_vars=data.columns[:2], var_name='year') \
-               .astype({'year': int})
+    # Iterate over regions/nodes
+    for node in s_info.N:
+        if node == 'World':
+            continue
+        else:
+            sheet_node = node.split('_')[-1].lower()
 
-    return data
+        # Worksheet for this region
+        sheet = wb[f'MESSAGE_LDV_{sheet_node}']
+
+        # Read tables for efficiency, investment, and fixed cost
+        for name, cells, unit in TABLES:
+            df = pd.DataFrame(sheet[cells]) \
+                     .applymap(lambda c: c.value)
+            # Make the first row the headers
+            df.columns = df.loc[0, :]
+            # Remaining rows: rearrange columns, period as column.
+            df = df.iloc[1:, :] \
+                   .drop(['Technology', 'Description'], axis=1) \
+                   .rename(columns={'MESSAGE name': 'technology'}) \
+                   .melt(id_vars=['technology'], var_name='year') \
+                   .astype({'year': int}) \
+                   .assign(node=node, name=name)
+
+            dfs.append(df)
+
+    return pd.concat(dfs, ignore_index=True)
