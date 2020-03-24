b7e745158a635a819a714cd08250682ae1391345
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-03-24 17:26:27 +0100

    Add unit, replace_vars args for reporting IAMC conversion

--- message_ix_models/report/core.py
+++ message_ix_models/report/core.py
@@ -54,6 +54,9 @@ def prepare_reporter(scenario, config, key, output_path):
     with open(config, 'r') as f:
         config = yaml.safe_load(f)
 
+    # Variable name replacement: dict, not list of entries
+    rep.add('iamc variable names', config.get('iamc variable names', {}))
+
     # Mapping of file sections to handlers
     sections = (
         ('aggregate', add_aggregate),
@@ -206,71 +209,91 @@ def add_combination(rep, info):
 def add_iamc_table(rep, info):
     """Add one entry from the 'iamc:' section of a config file.
 
-    Each entry uses :meth:`.Reporter.convert_pyam`, plus extra computations, to
-    format data from the internal :class:`.Quantity` into a
+    Each entry uses :meth:`.Reporter.convert_pyam` (plus extra computations) to
+    reformat data from the internal :class:`.Quantity` data structure into a
     :class:`pyam.IamDataFrame`.
 
     The entry *info* must contain:
 
-    - ``variable`` (:class:`str`): variable name. This is used two ways: it
-      is placed in the 'Variable' column of the resulting IamDataFrame; and the
+    - **variable** (:class:`str`): variable name. This is used two ways: it
+      is placed in 'Variable' column of the resulting IamDataFrame; and the
       reporting key to :meth:`~.Reporter.get` the data frame is
       ``<variable>:iamc``.
-    - ``format``: dict controlling :meth:`.convert_pyam`; see the
-      documentation of that method. It contains:
+    - **base** (:class:`str`): key for the quantity to convert.
+    - **select** (:class:`dict`, optional): keword arguments to
+      :meth:`~.Quantity.sel`.
+    - **group_sum** (2-:class:`tuple`, optional): `group` and `sum` arguments
+      to :func:`.group_sum`.
+    - **year_time_dim** (:class:`str`, optional): Dimension to use for the IAMC
+      'Year' or 'Time' column. Default 'ya'. (Passed to
+      :meth:`~message_ix.reporting.Reporter.convert_pyam`.)
+    - **drop** (:class:`list` of :class:`str`, optional): Dimensions to drop
+      (→ convert_pyam).
+    - **unit** (:class:`str`, optional): Force output in these units (→
+      convert_pyam).
 
-      - ``year_time_dim`` (:class:`str`, optional): Dimension to use for the
-        'Year' or 'Time' column. Default 'ya'.
-      - ``drop`` (:class:`list` of :class:`str`, optional): Dimensions to drop.
-      - Other entries: passed as keyword arguments to :func:`.collapse`, which
-        is then supplied as the `collapse` callback for :meth:`.convert_pyam`.
-        :func:`.collapse` formats the 'Variable' column of the IamDataFrame.
+    Additional entries are passed as keyword arguments to :func:`.collapse`,
+    which is then given as the `collapse` callback for :meth:`.convert_pyam`.
 
+    :func:`.collapse` formats the 'Variable' column of the IamDataFrame.
+    The variable name replacements from the 'iamc variable names:' section of
+    the config file are applied to all variables.
     """
     # For each quantity, use a chain of computations to prepare it
-    name = info['variable']
+    name = info.pop('variable')
 
     # Chain of keys produced: first entry is the key for the base quantity
-    base = Key.from_str_or_key(info['base'])
+    base = Key.from_str_or_key(info.pop('base'))
     keys = [base]
 
     # Second entry is a simple rename
     keys.append(rep.add(Key(name, base.dims, base.tag), base))
 
-    if 'select' in info:
-        # Select a subset of data from the base quantity
+    # Optionally select a subset of data from the base quantity
+    try:
+        sel = info.pop('select')
+    except KeyError:
+        pass
+    else:
         key = keys[-1].add_tag('sel')
-        rep.add(key, (Quantity.sel, keys[-1], info['select']), strict=True)
+        rep.add(key, (Quantity.sel, keys[-1], sel), strict=True)
         keys.append(key)
 
-    if 'group_sum' in info:
-        # Aggregate data by groups
-        args = dict(group=info['group_sum'][0], sum=info['group_sum'][1])
+    # Optionally aggregate data by groups
+    try:
+        gs = info.pop('group_sum')
+    except KeyError:
+        pass
+    else:
         key = keys[-1].add_tag('agg')
-        rep.add(key, (partial(group_sum, **args), keys[-1]), strict=True)
+        task = (partial(group_sum, group=gs[0], sum=gs[1]), keys[-1])
+        rep.add(key, task, strict=True)
         keys.append(key)
 
-    # 'format' section: convert the data to a pyam data structure
+    # Arguments for Reporter.convert_pyam()
+    args = dict(
+        # Use 'ya' for the IAMC 'Year' column; unless YAML reporting config
+        # includes a different dim under format/year_time_dim.
+        year_time_dim=info.pop('year_time_dim', 'ya'),
+        drop=set(info.pop('drop', [])) & set(keys[-1].dims),
+        replace_vars='iamc variable names',
+    )
 
-    # Copy for destructive .pop() operation
-    args = copy(info['format'])
-    args['var_name'] = name
+    # Optionally convert units
+    try:
+        args['unit'] = info.pop('unit')
+    except KeyError:
+        pass
 
-    # Use 'ya' for the IAMC 'Year' column; unless YAML reporting config
-    # includes a different dim under format/year_time_dim.
-    year_time_dim = args.pop('year_time_dim', 'ya')
+    # Remaining arguments are for the collapse() callback
+    args['collapse'] = partial(collapse, var_name=name, **info)
 
-    drop = set(args.pop('drop', [])) & set(keys[-1].dims)
-
-    # Use the built-in message_ix.Reporter method to add the coversion step
-    iamc_keys = rep.convert_pyam(keys[-1], year_time_dim, drop=drop,
-                                 collapse=partial(collapse, **args))
+    # Use the message_ix.Reporter method to add the coversion step
+    iamc_keys = rep.convert_pyam(keys[-1], **args)
     keys.extend(iamc_keys)
 
-    # Revise the 'message:default' report to include the last key in
-    # the chain
-    rep.add('message:default',
-            rep.graph['message:default'] + (keys[-1],))
+    # Revise the 'message:default' report to include the last key in the chain
+    rep.add('message:default', rep.graph['message:default'] + (keys[-1],))
 
     log.info(f'Add {keys[-1]!r} from {keys[0]!r}')
     log.debug(f'  {len(keys)} keys total')
