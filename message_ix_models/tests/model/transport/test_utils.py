3e3e9119c06c9e005a4af1077f7b1af188bf32ac
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-04-19 14:08:25 +0200

    Adjust cli, test fixtures to clean up contexts

--- message_ix_models/tests/model/transport/test_utils.py
+++ message_ix_models/tests/model/transport/test_utils.py
@@ -4,26 +4,28 @@ from message_data.model.transport.utils import consumer_groups, read_config
 
 
 def test_read_config(test_context):
-    read_config(test_context)
+    # read_config() returns a reference to the current context
+    context = read_config()
+    assert context is test_context
 
     # Data table is loaded
-    assert 'mer_to_ppp' in test_context.data
+    assert 'mer_to_ppp' in context.data
 
     # Scalar parameters are loaded
-    assert 'scaling' in test_context.data
-    assert test_context.data['whours'] == 200 * 8
+    assert 'scaling' in context.data
+    assert context.data['whours'] == 200 * 8
 
 
 def test_consumer_groups(test_context):
     # Returns a list of codes
-    result = consumer_groups(test_context, rtype='code')
+    result = consumer_groups(rtype='code')
     assert 'RUEAA' in result
 
     # Returns tuples of (code, description)
-    result = consumer_groups(test_context, rtype='description')
+    result = consumer_groups(rtype='description')
     assert ('RUEAA', 'rural, or “outside msa”, early adopter, average') \
         in result
 
     # Returns xarray objects for indexing
-    result = consumer_groups(test_context, rtype='indexers')
+    result = consumer_groups(rtype='indexers')
     assert all(isinstance(da, xr.DataArray) for da in result.values())
