8e261f316bd65aff58c05f9ed665865362ed48c7
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-04-19 15:35:41 +0200

    Add tests…transport…test_solve_bare_res()

--- message_ix_models/tests/model/transport/test_build.py
+++ message_ix_models/tests/model/transport/test_build.py
@@ -1,6 +1,25 @@
 from message_data.model.transport.build import main as build
+# from message_data.reporting.core import prepare_reporter
 
 
 def test_build_bare_res(bare_res):
     """Test that model.transport.build works on the MESSAGEix-GLOBIOM RES."""
     build(bare_res)
+
+
+def test_solve_bare_res(bare_res):
+    """Test that MESSAGE-Transport built on the bare RES will solve."""
+    build(bare_res)
+
+    bare_res.solve()
+
+    # Report the results
+    #
+    # rep = prepare_reporter(bare_res)
+    #
+    # rep.set_filters(t=[
+    #     'transport freight load factor',
+    #     'transport pax load factor',
+    # ])
+    #
+    # print(rep.get('ACT'))
