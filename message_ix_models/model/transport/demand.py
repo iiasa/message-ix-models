409c567620815ddda03005cc20e1832dcd436ceb
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-04-19 15:33:19 +0200

    New submodule transport.demand

---
+++ message_ix_models/model/transport/demand.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+
+from message_data.tools import broadcast, make_df
+
+
+def demand(info):
+    """Return transport demands.
+
+    Parameters
+    ----------
+    info : .ScenarioInfo
+    """
+    return dict(demand=dummy(info))
+
+
+def dummy(info):
+    """Dummy demands.
+
+    Parameters
+    ----------
+    info : .ScenarioInfo
+    """
+    data = dict(
+        year=info.Y,
+        value=np.arange(len(info.Y)) + 0.1,
+        level='useful',
+        commodity='transport freight',
+        unit='t km',
+        time='year',
+    )
+
+    # - Assemble into a message_ix-ready DataFrame
+    # - Broadcast over all nodes
+    data = make_df('demand', **data)\
+        .pipe(broadcast, node=info.N[1:])
+
+    # Original data plus a copy
+    return pd.concat([
+        data,
+        data.assign(commodity='transport pax', unit='km'),
+    ])
