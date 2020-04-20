c785f66c1b09b9d9165dc7029edc7a67d6e5d344
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-04-20 16:32:58 +0200

    Parametrize test of transport.build

--- message_ix_models/tests/model/transport/test_build.py
+++ message_ix_models/tests/model/transport/test_build.py
@@ -1,10 +1,26 @@
+import pytest
+
+from message_data.model.transport.utils import read_config
 from message_data.model.transport.build import main as build
 from message_data.model.transport.check import check
 
+from message_data.tests import binary_data_available
 
-def test_build_bare_res(bare_res):
+
+@pytest.mark.parametrize('nonldv_data_source', [
+    None,
+    pytest.param('IKARUS', marks=binary_data_available),
+])
+def test_build_bare_res(bare_res, nonldv_data_source):
     """Test that model.transport.build works on the MESSAGEix-GLOBIOM RES."""
-    build(bare_res)
+    # Pre-load transport config/metadata
+    context = read_config()
+
+    # Manually modify some of the configuration per test parameters
+    context['transport config']['data source']['non-LDV'] = nonldv_data_source
+
+    # Build succeeds without error
+    build(bare_res, fast=True)
 
 
 def test_solve_bare_res(bare_res):
