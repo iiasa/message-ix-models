264943e0cd0b332fbbedf7aa9766c7327a6ddbd7
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-05-26 22:10:57 +0200

    Add basic cli for solving the materials model

--- message_ix_models/model/material/__init__.py
+++ message_ix_models/model/material/__init__.py
@@ -1,5 +1,7 @@
 from typing import Mapping
 
+import click
+
 from message_data.model.build import apply_spec
 from message_data.tools import ScenarioInfo
 from .data import gen_data
@@ -13,6 +15,8 @@ def build(scenario):
 
     # Apply to the base scenario
     apply_spec(scenario, spec, gen_data)
+
+    return scenario
 
 
 def get_spec() -> Mapping[str, ScenarioInfo]:
@@ -32,3 +36,38 @@ def get_spec() -> Mapping[str, ScenarioInfo]:
         add.set[set_name].extend(config.get("add", []))
 
     return dict(require=require, remove=ScenarioInfo(), add=add)
+
+
+# Group to allow for multiple CLI subcommands under "material"
+@click.group("material")
+def cli():
+    """Model with materials accounting."""
+
+
+@cli.command()
+@click.pass_obj
+def solve(context):
+    """Build and solve model.
+
+    Use the --url option to specify the base scenario.
+    """
+    # Determine the output scenario name based on the --url CLI option. If the
+    # user did not give a recognized value, this raises an error.
+    output_scenario_name = {
+        "baseline": "NoPolicy",
+        "NPi2020-con-prim-dir-ncr": "NPi",
+        "NPi2020_1000-con-prim-dir-ncr": "NPi2020_1000",
+        "NPi2020_400-con-prim-dir-ncr": "NPi2020_400",
+    }.get(context.scenario_info["scenario"])
+
+    if context.scenario_info["model"] != "CD_Links_SSP2":
+        print("WARNING: this code is not tested with this base scenario!")
+
+    # Clone and set up
+    scenario = build(
+        context.get_scenario()
+        .clone(model="JM_GLB_NITRO", scenario=output_scenario_name)
+    )
+
+    # Solve
+    scenario.solve()
