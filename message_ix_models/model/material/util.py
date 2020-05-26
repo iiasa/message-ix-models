0421e25b3dbe786586f68081d318dfac205d9117
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-05-26 20:18:08 +0200

    Move material.read_config() to new material.util submodule

---
+++ message_ix_models/model/material/util.py
@@ -0,0 +1,30 @@
+from message_data.tools import as_codes, get_context
+
+
+def read_config():
+    """Read configuration from material.yaml."""
+    # TODO this is similar to transport.utils.read_config; make a common
+    #      function so it doesn't need to be in this file.
+    context = get_context()
+
+    try:
+        # Check if the configuration was already loaded
+        context["material"]["set"]
+    except KeyError:
+        # Not yet loaded
+        pass
+    else:
+        # Already loaded
+        return context
+
+    # Read material.yaml
+    context.load_config("material")
+
+    # Convert some values to Code objects
+    for set_name, info in context["material"]["set"].items():
+        try:
+            info["add"] = as_codes(info["add"])
+        except KeyError:
+            pass
+
+    return context
