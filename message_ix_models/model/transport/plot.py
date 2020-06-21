5247ed3093099bca4b74d536ca94101cb7e99459
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-06-21 16:07:17 +0200

    Add transport plots

---
+++ message_ix_models/model/transport/plot.py
@@ -0,0 +1,56 @@
+import matplotlib
+import plotnine as p9
+
+matplotlib.use("cairo")
+
+
+class Plot:
+    save_args = dict(height=210, width=297, units="mm", verbose=False)
+    inputs = []
+    name = ""
+
+    def __call__(self, *args):
+        filename = f"{self.name}.pdf"
+        self.generate(*args).save(filename, **self.save_args)
+        return filename
+
+    def generate(*args):
+        return p9.ggplot(*args)
+
+
+class ModeShare0(Plot):
+    name = "mode-share"
+    inputs = ["out:nl-t-ya:transport"]
+
+    def generate(self, data):
+        # Select a subset of technologies
+        techs = list(filter(lambda n: "usage" in n, data.coords["t"].values))
+        df = data.sel(t=techs).to_series().rename("value").reset_index()
+
+        return (
+            p9.ggplot(df, p9.aes(x="ya", y="value", fill="t"))
+            + p9.facet_wrap(["nl"], ncol=2)
+            + p9.geom_bar(stat="identity")
+        )
+
+
+class ModeShare1(Plot):
+    name = "mode-share-cg-WEU"
+    inputs = ["out:nl-t-ya-c:transport"]
+
+    def generate(self, data):
+        # Select a subset of technologies
+        techs = list(filter(lambda n: "usage" in n, data.coords["t"].values))
+        df = (
+            data.sel(t=techs, nl="R11_WEU")
+            .to_series().rename("value").reset_index()
+        )
+
+        return (
+            p9.ggplot(df, p9.aes(x="ya", y="value", fill="t"))
+            + p9.facet_wrap(["c"], ncol=5)
+            + p9.geom_bar(stat="identity")
+        )
+
+
+PLOTS = [ModeShare0, ModeShare1]
