3dc49be6fc141f04a35d89bcb97a3a312e82a054
GamzeUnlu95 <gamze199556@gmail.com>
2020-07-16 16:46:35 +0200

    Read and generate data functions for aluminum

--- message_ix_models/model/material/data.py
+++ message_ix_models/model/material/data.py
@@ -11,6 +11,174 @@ from .util import read_config
 
 log = logging.getLogger(__name__)
 
+def read_data():
+    """Read and clean data from :file:`n-fertilizer_techno-economic.xlsx`."""
+    # Ensure config is loaded, get the context
+    context = read_config()
+
+    # Shorter access to sets configuration
+    sets = context["material"]["set"]
+
+    # Read the file
+    data = pd.read_excel(
+        context.get_path("material", "n-fertilizer_techno-economic.xlsx"),
+        sheet_name="Sheet1",
+    )
+
+    # Prepare contents for the "parameter" and "technology" columns
+    # FIXME put these in the file itself to avoid ambiguity/error
+
+    # "Variable" column contains different values selected to match each of
+    # these parameters, per technology
+    params = [
+        "inv_cost",
+        "fix_cost",
+        "var_cost",
+        "technical_lifetime",
+        "input_fuel",
+        "input_elec",
+        "input_water",
+        "output_NH3",
+        "output_water",
+        "output_heat",
+        "emissions",
+        "capacity_factor",
+    ]
+
+    param_values = []
+    tech_values = []
+    for t in sets["technology"]["add"]:
+        param_values.extend(params)
+        tech_values.extend([t.id] * len(params))
+
+    # Clean the data
+    data = (
+        # Insert "technology" and "parameter" columns
+        data.assign(technology=tech_values, parameter=param_values)
+        # Drop columns that don't contain useful information
+        .drop(["Model", "Scenario", "Region"], axis=1)
+        # Set the data frame index for selection
+        .set_index(["parameter", "technology"])
+    )
+
+    # TODO convert units for some parameters, per LoadParams.py
+
+    return data
+
+# This function can also be generic for all materials
+def read_data_aluminum():
+    """Read and clean data from :file:`aluminum_techno_economic.xlsx`."""
+
+    # Ensure config is loaded, get the context
+    context = read_config()
+
+    # Shorter access to sets configuration
+    sets = context["material"]["aluminum"]
+
+    # Read the file
+    data_aluminum = pd.read_excel(
+        context.get_path("material", "aluminum_techno_economic.xlsx"),
+        sheet_name="aluminum",
+    )
+
+    # Clean the data
+
+    data_aluminum= data_aluminum.drop(['Region', 'Source', 'Description'], axis = 1)
+
+    # Unit conversion
+
+    # At the moment this is done in the excel file, can be also done here
+    # To make sure we use the same units
+
+    return data_aluminum
+
+def gen_data_aluminum(scenario, dry_run=False):
+    """Generate data for materials representation of aluminum."""
+    # Load configuration
+
+    config = read_config()["material"]["aluminum"]
+
+    # Information about scenario, e.g. node, year
+    s_info = ScenarioInfo(scenario)
+
+    # Techno-economic assumptions
+    data_aluminum = read_data()
+
+    # List of data frames, to be concatenated together at end
+    results = defaultdict(list)
+
+    # TODO: If there are differnet values for the years...
+    # TODO: Adding the active years to the tables
+
+    # Iterate over technologies
+
+    for t in technology_add:
+
+        params = data_aluminum.loc[(data_aluminum["technology"] == t),\
+        "parameter"].values.tolist()
+
+        # Iterate over parameters
+
+        for par in params:
+
+            # Obtain the parameter names, commodity,level,emission
+
+            split = par.split("|")
+            param_name = split[0]
+
+            # Obtain the scalar value for the parameter
+
+            val = data_aluminum.loc[((data_aluminum["technology"] == t) \
+            & (data_aluminum["parameter"] == par)),2015].values[0]
+
+            common = dict(
+            year_vtg= years,
+            mode="M1",
+            time="year",
+            time_origin="year",
+            time_dest="year",)
+
+            # For the parameters which inlcudes index names
+            if len(split)> 1:
+
+                if (param_name == "input")|(param_name == "output"):
+
+                    # Assign commodity and level names
+                    com = split[1]
+                    lev = split[2]
+
+                    df = (make_df(param_name, technology=t, commodity=com, \
+                    level=lev, value=val, unit='t', **common)\
+                    .pipe(broadcast, node_loc=nodes).pipe(same_node))
+
+                    results[param_name].append(df)
+
+                elif param_name == "emission_factor":
+
+                    # Assign the emisson type
+                    emi = split[1]
+
+                    df = (make_df(param_name, technology=t,value=val,\
+                    emission=emi, unit='t', **common).pipe(broadcast, \
+                    node_loc=nodes))
+                    results[param_name].append(df)
+
+            # Parameters with only parameter name
+
+            else:
+                df = (make_df(param_name, technology=t, value=val,unit='t', \
+                **common).pipe(broadcast, node_loc=nodes))
+                results[param_name].append(df)
+
+    # Concatenate to one data frame per parameter
+    results = {par_name: pd.concat(dfs) for par_name, dfs in results.items()}
+
+    # Temporary: return nothing, since the data frames are incomplete
+    return results
+
+def gen_data_generic(scenario, dry_run=False):
+    # For furnaces and boilers
+    # Export this data from excel file in the model .. ?
 
 def gen_data(scenario, dry_run=False):
     """Generate data for materials representation of nitrogen fertilizers.
@@ -73,59 +241,3 @@ def gen_data(scenario, dry_run=False):
     # Temporary: return nothing, since the data frames are incomplete
     return dict()
     # return result
-
-
-def read_data():
-    """Read and clean data from :file:`n-fertilizer_techno-economic.xlsx`."""
-    # Ensure config is loaded, get the context
-    context = read_config()
-
-    # Shorter access to sets configuration
-    sets = context["material"]["set"]
-
-    # Read the file
-    data = pd.read_excel(
-        context.get_path("material", "n-fertilizer_techno-economic.xlsx"),
-        sheet_name="Sheet1",
-        engine="openpyxl",
-    )
-
-    # Prepare contents for the "parameter" and "technology" columns
-    # FIXME put these in the file itself to avoid ambiguity/error
-
-    # "Variable" column contains different values selected to match each of
-    # these parameters, per technology
-    params = [
-        "inv_cost",
-        "fix_cost",
-        "var_cost",
-        "technical_lifetime",
-        "input_fuel",
-        "input_elec",
-        "input_water",
-        "output_NH3",
-        "output_water",
-        "output_heat",
-        "emissions",
-        "capacity_factor",
-    ]
-
-    param_values = []
-    tech_values = []
-    for t in sets["technology"]["add"]:
-        param_values.extend(params)
-        tech_values.extend([t.id] * len(params))
-
-    # Clean the data
-    data = (
-        # Insert "technology" and "parameter" columns
-        data.assign(technology=tech_values, parameter=param_values)
-        # Drop columns that don't contain useful information
-        .drop(["Model", "Scenario", "Region"], axis=1)
-        # Set the data frame index for selection
-        .set_index(["parameter", "technology"])
-    )
-
-    # TODO convert units for some parameters, per LoadParams.py
-
-    return data
