1e7ddd014ce5e9083e01450d55b1d40092f97c54
Jihoon <grcosmos@gmail.com>
2020-08-12 14:35:34 +0200

    Revise read_config() definition for set.yaml

--- message_ix_models/model/material/util.py
+++ message_ix_models/model/material/util.py
@@ -2,8 +2,8 @@ from message_ix_models import Context
 from message_ix_models.util import as_codes, load_package_data
 
 
-def read_config(context=None):
-    """Read configuration from material.yaml."""
+def read_config():
+    """Read configuration from set.yaml."""
     # TODO this is similar to transport.utils.read_config; make a common
     #      function so it doesn't need to be in this file.
     context = context or Context.get_instance(0)
@@ -18,14 +18,23 @@ def read_config(context=None):
         # Already loaded
         return context
 
-    # Read material.yaml, store with a shorter name
-    context["material"] = load_package_data("material", "config")
+    # Read material.yaml
+    context.load_config("material", "set")
+
+    # Use a shorter name
+    context["material"] = context["material set"]
+
+    # Merge technology.yaml with set.yaml
+    # context["material"]["steel"]["technology"]["add"] = (
+    #     context.pop("transport technology")
+    # )
 
     # Convert some values to Code objects
-    for set_name, info in context["material"]["set"].items():
-        try:
-            info["add"] = as_codes(info["add"])
-        except KeyError:
-            pass
+    # JM: Ask what this is
+    # for set_name, info in context["material"]["common"].items():
+    #     try:
+    #         info["add"] = as_codes(info["add"])
+    #     except KeyError:
+    #         pass
 
     return context
