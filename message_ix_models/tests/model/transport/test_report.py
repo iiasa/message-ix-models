41d0b475f42da998bd074b7334d3d82b871715c7
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-08-28 01:16:15 +0200

    Add "transport all" reporting key; output plots in target dir

--- message_ix_models/tests/model/transport/test_report.py
+++ message_ix_models/tests/model/transport/test_report.py
@@ -6,19 +6,25 @@ def test_register_cb():
     register(callback)
 
 
-def test_report_bare(solved_bare_res_transport, session_context):
+def test_report_bare(solved_bare_res_transport, session_context, tmp_path):
     """Run MESSAGE-Transport–specific reporting."""
     register(callback)
 
+    session_context["output dir"] = tmp_path
+
     rep, key = prepare_reporter(
         solved_bare_res_transport,
-        session_context.get_config_file('report', 'global'),
-        'out::transport',
+        session_context.get_config_file("report", "global"),
+        "out::transport",
     )
 
-    # The key is added, can be computed and retrieved, and has a specific
-    # aggregate calculated
-    rep.get(key).sel(t='freight truck')
+    # The key is added, can be computed and written to file
+    path = tmp_path / "out::transport.xlsx"
+    rep.write(key, path)
+    # print(path)
+
+    # The result has a specific aggregate calculated
+    rep.get(key).sel(t="freight truck")
 
     # Get the catch-all key, including plots etc.
-    rep.get("transport")
+    rep.get("transport all")
