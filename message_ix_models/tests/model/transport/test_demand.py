8ee6eca7c772cd165bbc710f551311772848682b
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-09-22 01:11:01 +0200

    Update transport demand and reporting tests

--- message_ix_models/tests/model/transport/test_demand.py
+++ message_ix_models/tests/model/transport/test_demand.py
@@ -3,7 +3,7 @@ import message_ix
 import pytest
 
 from message_data.model.bare import get_spec
-from message_data.model.transport import demand
+from message_data.model.transport import demand, plot
 
 
 def test_demand_dummy(test_context):
@@ -14,23 +14,39 @@ def test_demand_dummy(test_context):
     assert any(demand.dummy(info)["commodity"] == "transport pax URLMM")
 
 
-def test_from_external_data(test_context):
+def test_from_external_data(test_context, tmp_path):
     test_context.regions = "R11"
     info = get_spec(test_context)["add"]
 
     rep = demand.from_external_data(info)
 
-    # These following units are implied by the test of "transport pdt:…:mode":
+    # These units are implied by the test of "transport pdt:*:mode":
     # "GDP PPP:n-y" → "MUSD / year"
     # "GDP PPP per capita:n-y" → "kUSD / passenger / year"
     # "transport pdt:n-y") → "Mm / year"
 
-    # Total demand by mode: can be computed
-    result = rep.get("transport pdt:n-y-t:mode")
+    # Total demand by mode
+    key = "transport pdt:n-y-t:mode"
+
+    # Graph structure can be visualized
+    import dask
+    from dask.optimization import cull
+
+    dsk, deps = cull(rep.graph, key)
+    dask.visualize(dsk, filename=str(tmp_path / "demand-graph.pdf"))
+
+    # Can be computed
+    result = rep.get(key)
     # Has correct units: km / year / capita
     assert (
         registry.Quantity(1, result.attrs["_unit"]) == registry("1 km / year")
     )
+
+    # Can be plotted
+    key = "transport pdt plot"
+    p = plot.ModeShare2
+    rep.add(key, tuple([p(), "config"] + p.inputs))
+    rep.get(key)
 
 
 @pytest.mark.skip(reason="Requires user's context")
@@ -38,6 +54,4 @@ def test_from_scenario(user_context):
     url = "ixmp://reporting/CD_Links_SSP2_v2.1_clean/baseline"
     scenario, mp = message_ix.Scenario.from_url(url)
 
-    result = demand.from_scenario(scenario)
-    print(result)
-    pytest.fail()
+    demand.from_scenario(scenario)
