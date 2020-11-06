32f8d6d4877375611cb0f5f771264bf4d64951a7
Jihoon <grcosmos@gmail.com>
2020-11-06 15:40:55 +0100

    Minor removal of unnecessary lines + new config

--- message_ix_models/model/material/material_testscript.py
+++ message_ix_models/model/material/material_testscript.py
@@ -13,7 +13,6 @@ import message_data.model.material.data as dt
 from message_data.model.material.plotting import Plots
 import pyam
 
-# from message_data.model.material import bare
 
 from message_data.tools import Context
 
@@ -25,71 +24,10 @@ from message_data.tools import (
     set_info,
 )
 
-from message_data.model.bare import create_res
-# from message_data.model.create import create_res
+from message_data.model.create import create_res
 from message_data.model.material import build, get_spec
 
 from message_data.model.material.util import read_config
-
-#%% Main test run from bare
-
-from message_data.model.bare import create_res
-# Create Context obj
-Context._instance = []
-ctx = Context()
-
-# Set default scenario/model names
-ctx.scenario_info.setdefault('model', 'Material_test')
-ctx.scenario_info.setdefault('scenario', 'baseline')
-ctx['period_start'] = 2020
-ctx['regions'] = 'China'
-ctx['datafile'] = 'China_steel_standalone - test.xlsx'
-
-# Use general code to create a Scenario with some stuff in it
-scen = create_res(context = ctx)
-
-# Use material-specific code to add certain stuff
-a = build(scen)
-
-# Solve the model
-scen.solve()
-
-p = Plots(scen, 'China', firstyear=2020)
-p.plot_activity(baseyear=False, subset=['bf_steel', 'bof_steel', 'dri_steel', 'eaf_steel'])
-p.plot_activity(baseyear=False, subset=['bof_steel', 'eaf_steel'])
-p.plot_capacity(baseyear=True, subset=['bf_steel', 'bof_steel', 'dri_steel', 'eaf_steel'])
-p.plot_new_capacity(baseyear=True, subset=['bf_steel', 'bof_steel', 'dri_steel', 'eaf_steel'])
-p.plot_activity(baseyear=True, subset=['manuf_steel', 'prep_secondary_steel'])
-
-
-#%% run with NPi prices (bare)
-from message_data.model.bare import create_res
-Context._instance = []
-ctx = Context()
-
-# Set default scenario/model names
-ctx.scenario_info.setdefault('model', 'Material_test')
-ctx.scenario_info.setdefault('scenario', 'NPi400')
-ctx['period_start'] = 2020
-ctx['regions'] = 'China'
-ctx['datafile'] = 'China_steel_standalone - test.xlsx'
-
-# Use general code to create a Scenario with some stuff in it
-scen_np = create_res(context = ctx)
-
-# Use material-specific code to add certain stuff
-a = build(scen_np)
-
-# Solve the model
-scen_np.solve()
-
-p = Plots(scen_np, 'China', firstyear=2020)
-p.plot_activity(baseyear=True, subset=['bf_steel', 'bof_steel', 'dri_steel', \
-                                       'eaf_steel'])
-p.plot_capacity(baseyear=True, subset=['bf_steel', 'bof_steel', 'dri_steel', \
-                                       'eaf_steel'])
-p.plot_new_capacity(baseyear=True, subset=['bf_steel', 'bof_steel', \
-                                           'dri_steel', 'eaf_steel'])
 
 
 #%% Main test run based on a MESSAGE scenario
