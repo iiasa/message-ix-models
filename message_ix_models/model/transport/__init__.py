340b275f4e99c0a5e7073ec919bc0d741cae2c22
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-01-14 15:17:22 +0100

    Remove hard-coded .model.transport.scenario_info; blacken .transport.cli

--- message_ix_models/model/transport/__init__.py
+++ message_ix_models/model/transport/__init__.py
@@ -1,11 +1,3 @@
 from .utils import read_config
 
-__all__ = [
-    "read_config",
-]
-
-
-SCENARIO_INFO = dict(
-    model='MESSAGEix-Transport',
-    scenario='baseline',
-)
+__all__ = ["read_config"]
