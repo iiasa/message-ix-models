a95c53b16b34dc64314d46cebfe5c980b458d280
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-01-14 15:17:22 +0100

    Remove hard-coded .model.transport.scenario_info; blacken .transport.cli

--- message_ix_models/model/transport/cli.py
+++ message_ix_models/model/transport/cli.py
@@ -1,35 +1,44 @@
 from pathlib import Path
 
 import click
-import message_ix
 
-from . import SCENARIO_INFO
 from message_data.tools.cli import clone_to_dest, common_params
 
 
-@click.group('transport')
+@click.group("transport")
 def cli():
     """MESSAGE-Transport model."""
     pass
 
 
 @cli.command()
-@click.option('--version', default='geam_ADV3TRAr2_BaseX2_0',
-              metavar='VERSION', help='Model version to read.')
-@click.option('--check-base/--no-check-base', is_flag=True,
-              help='Check properties of the base scenario (default: no).')
-@click.option('--parse/--no-parse', is_flag=True,
-              help='(Re)parse MESSAGE V data files (default: no).')
-@click.option('--region', default='', metavar='REGIONS',
-              help='Comma-separated region(s).')
-@click.argument('SOURCE_PATH', required=False,
-                default=Path('reference', 'data'))
+@common_params("dest")
+@click.option(
+    "--version",
+    default="geam_ADV3TRAr2_BaseX2_0",
+    metavar="VERSION",
+    help="Model version to read.",
+)
+@click.option(
+    "--check-base/--no-check-base",
+    is_flag=True,
+    help="Check properties of the base scenario (default: no).",
+)
+@click.option(
+    "--parse/--no-parse",
+    is_flag=True,
+    help="(Re)parse MESSAGE V data files (default: no).",
+)
+@click.option(
+    "--region", default="", metavar="REGIONS", help="Comma-separated region(s)."
+)
+@click.argument("SOURCE_PATH", required=False, default=Path("reference", "data"))
 @click.pass_obj
-def migrate(context, version, check_base, parse, region, source_path):
+def migrate(context, version, check_base, parse, region, source_path, dest):
     """Migrate data from MESSAGE(V)-Transport.
 
-    If --parse is given, data from .chn, .dic, and .inp files is read from
-    SOURCE_PATH for VERSION. Values are extracted and cached.
+    If --parse is given, data from .chn, .dic, and .inp files is read from SOURCE_PATH
+    for VERSION. Values are extracted and cached.
 
     Data is transformed to be suitable for the target scenario, and stored in
     migrate/VERSION/*.csv.
@@ -41,20 +50,21 @@ def migrate(context, version, check_base, parse, region, source_path):
 
     # Load the target scenario from database
     mp = context.get_platform()
-    s_target = message_ix.Scenario(mp, **SCENARIO_INFO)
+    s_target = dest
     info = ScenarioInfo(s_target)
 
     # Check that it has the required features
     if check_base:
         with silence_log():
             build(s_target, dry_run=True)
-            print(f'Scenario {s_target} is a valid target for building '
-                  'MESSAGE-Transport.')
+            print(
+                f"Scenario {s_target} is a valid target for building "
+                "MESSAGEix-Transport."
+            )
 
     if parse:
         # Parse raw data
-        data = import_all(source_path, nodes=region.split(','),
-                          version=version)
+        data = import_all(source_path, nodes=region.split(","), version=version)
     else:
         # Load cached data
         data = load_all(version=version)
@@ -65,14 +75,15 @@ def migrate(context, version, check_base, parse, region, source_path):
 
 @cli.command("build")
 @common_params("dest dry_run quiet")
-@click.option("--fast", is_flag=True,
-              help="Skip removing data for removed set elements.")
+@click.option(
+    "--fast", is_flag=True, help="Skip removing data for removed set elements."
+)
 @click.pass_obj
 def build_cmd(context, dest, **options):
     """Prepare the model."""
     from .build import main
 
-    scenario, platform = clone_to_dest(context, defaults=SCENARIO_INFO)
+    scenario, platform = clone_to_dest(context)
 
     main(scenario, **options)
 
@@ -90,7 +101,8 @@ def solve(context, macro):
 
     if macro:
         from .callback import main as callback
-        args['callback'] = callback
+
+        args["callback"] = callback
 
     scenario.solve(**args)
     scenario.commit()
