7539db61888b95d888ee3905abf69e5a672b75f9
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-01-15 00:41:13 +0100

    Use message_data.testing in transport tests; blacken

--- message_ix_models/tests/test_report.py
+++ message_ix_models/tests/test_report.py
@@ -4,15 +4,17 @@ from functools import partial
 from ixmp.reporting.quantity import Quantity
 from message_ix.reporting import Reporter
 import pandas as pd
+import pytest
 
+from message_data import testing
 from message_data.reporting import prepare_reporter
 from message_data.reporting.computations import combine
 
 
 # Minimal reporting configuration for testing
 MIN_CONFIG = {
-    'units': {
-        'replace': {'???': ''},
+    "units": {
+        "replace": {"???": ""},
     },
 }
 
@@ -28,33 +30,37 @@ def test_computation_combine():
     rep = Reporter()
 
     # Add data to the Reporter
-    foo = ['foo1', 'foo2']
-    bar = ['bar1', 'bar2']
+    foo = ["foo1", "foo2"]
+    bar = ["bar1", "bar2"]
 
     a = pd.Series(
         [1, 2, 3, 4],
-        index=pd.MultiIndex.from_product([foo, bar], names=['foo', 'bar']),
+        index=pd.MultiIndex.from_product([foo, bar], names=["foo", "bar"]),
     )
     b = pd.Series(
         [10, 20, 30, 40],
-        index=pd.MultiIndex.from_product([bar, foo], names=['bar', 'foo']),
+        index=pd.MultiIndex.from_product([bar, foo], names=["bar", "foo"]),
     )
-    c = pd.Series([100, 200], index=pd.Index(foo, name='foo'))
+    c = pd.Series([100, 200], index=pd.Index(foo, name="foo"))
 
-    rep.add('a', Quantity(a))
-    rep.add('b', Quantity(b))
-    rep.add('c', Quantity(c))
+    rep.add("a", Quantity(a))
+    rep.add("b", Quantity(b))
+    rep.add("c", Quantity(c))
 
-    rep.add('d', (partial(combine, weights=[0.5, 1, 2]), 'a', 'b', 'c'))
+    rep.add("d", (partial(combine, weights=[0.5, 1, 2]), "a", "b", "c"))
 
-    assert rep.get('d').loc[('foo2', 'bar1')] == 3 * 0.5 + 20 * 1 + 200 * 2
+    assert rep.get("d").loc[("foo2", "bar1")] == 3 * 0.5 + 20 * 1 + 200 * 2
 
 
-def test_report_bare_res(solved_res, session_context):
+def test_report_bare_res(request, session_context):
     """Prepare and run the standard MESSAGE-GLOBIOM reporting on a bare RES."""
+    ctx = session_context
+
+    scenario = testing.bare_res(request, ctx, solved=True)
+
     # Prepare the reporter
     reporter, key = prepare_reporter(
-        solved_res,
+        scenario,
         config=session_context.get_config_file("report", "global"),
         key="message:default",
     )
@@ -66,85 +72,93 @@ def test_report_bare_res(solved_res, session_context):
 
 
 # Common data for tests
-DATA_INV_COST = pd.DataFrame([
-    ['R11_NAM', 'coal_ppl', '2010', 10.5, 'USD'],
-    ['R11_LAM', 'coal_ppl', '2010',  9.5, 'USD'],
-    ], columns='node_loc technology year_vtg value unit'.split())
+DATA_INV_COST = pd.DataFrame(
+    [
+        ["R11_NAM", "coal_ppl", "2010", 10.5, "USD"],
+        ["R11_LAM", "coal_ppl", "2010", 9.5, "USD"],
+    ],
+    columns="node_loc technology year_vtg value unit".split(),
+)
 
 IAMC_INV_COST = dict(
-    variable='Investment Cost',
-    base='inv_cost:nl-t-yv',
-    year_time_dim='yv',
-    var=['t'],
-    unit='EUR_2005',
+    variable="Investment Cost",
+    base="inv_cost:nl-t-yv",
+    year_time_dim="yv",
+    var=["t"],
+    unit="EUR_2005",
 )
 
 
-def test_apply_units(bare_res):
-    qty = 'inv_cost'
+@pytest.mark.parametrize("regions", ["R11"])
+def test_apply_units(request, test_context, regions):
+    test_context.regions = regions
+    bare_res = testing.bare_res(request, test_context, solved=True)
+
+    qty = "inv_cost"
 
     # Create a temporary config dict
     config = MIN_CONFIG.copy()
 
     # Prepare the reporter
-    bare_res.solve()
     reporter, key = prepare_reporter(bare_res, config=config, key=qty)
 
     # Add some data to the scenario
     inv_cost = DATA_INV_COST.copy()
     bare_res.remove_solution()
     bare_res.check_out()
-    bare_res.add_par('inv_cost', inv_cost)
-    bare_res.commit('')
+    bare_res.add_par("inv_cost", inv_cost)
+    bare_res.commit("")
     bare_res.solve()
 
     # Units are retrieved
-    USD_2005 = reporter.unit_registry.Unit('USD_2005')
-    assert reporter.get(key).attrs['_unit'] == USD_2005
+    USD_2005 = reporter.unit_registry.Unit("USD_2005")
+    assert reporter.get(key).attrs["_unit"] == USD_2005
 
     # Add data with units that will be discarded
-    inv_cost['unit'] = ['USD', 'kg']
+    inv_cost["unit"] = ["USD", "kg"]
     bare_res.remove_solution()
     bare_res.check_out()
-    bare_res.add_par('inv_cost', inv_cost)
+    bare_res.add_par("inv_cost", inv_cost)
 
     # Units are discarded
-    assert str(reporter.get(key).attrs['_unit']) == 'dimensionless'
+    assert str(reporter.get(key).attrs["_unit"]) == "dimensionless"
 
     # Update configuration, re-create the reporter
-    config['units']['apply'] = {'inv_cost': 'USD'}
-    bare_res.commit('')
+    config["units"]["apply"] = {"inv_cost": "USD"}
+    bare_res.commit("")
     bare_res.solve()
     reporter, key = prepare_reporter(bare_res, config=config, key=qty)
 
     # Units are applied
-    assert str(reporter.get(key).attrs['_unit']) == USD_2005
+    assert str(reporter.get(key).attrs["_unit"]) == USD_2005
 
     # Update configuration, re-create the reporter
-    config['iamc'] = [IAMC_INV_COST]
+    config["iamc"] = [IAMC_INV_COST]
     reporter, key = prepare_reporter(bare_res, config=config, key=qty)
 
     # Units are converted
-    df = reporter.get('Investment Cost:iamc').as_pandas()
-    assert set(df['unit']) == {'EUR_2005'}
+    df = reporter.get("Investment Cost:iamc").as_pandas()
+    assert set(df["unit"]) == {"EUR_2005"}
 
 
-def test_iamc_replace_vars(bare_res):
+@pytest.mark.parametrize("regions", ["R11"])
+def test_iamc_replace_vars(request, test_context, regions):
     """Test the 'iamc variable names' reporting configuration."""
-    scen = bare_res
+    test_context.regions = regions
+    scen = testing.bare_res(request, test_context)
 
-    qty = 'inv_cost'
+    qty = "inv_cost"
     config = {
-        'iamc': [IAMC_INV_COST],
-        'iamc variable names': {
-            'Investment Cost|Coal_Ppl': 'Investment Cost|Coal',
-        }
+        "iamc": [IAMC_INV_COST],
+        "iamc variable names": {
+            "Investment Cost|Coal_Ppl": "Investment Cost|Coal",
+        },
     }
     scen.check_out()
-    scen.add_par('inv_cost', DATA_INV_COST)
-    scen.commit('')
+    scen.add_par("inv_cost", DATA_INV_COST)
+    scen.commit("")
     scen.solve()
 
     reporter, key = prepare_reporter(scen, config=config, key=qty)
-    df = reporter.get('Investment Cost:iamc').as_pandas()
-    assert set(df['variable']) == {'Investment Cost|Coal'}
+    df = reporter.get("Investment Cost:iamc").as_pandas()
+    assert set(df["variable"]) == {"Investment Cost|Coal"}
