9469c981182ef991bc92b14bf5bd97096263d513
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-01-15 00:14:05 +0100

    Simplify tests.model.transport fixtures

--- message_ix_models/tests/model/transport/conftest.py
+++ message_ix_models/tests/model/transport/conftest.py
@@ -1,34 +1,20 @@
+from copy import deepcopy
+
 import pytest
 
-from message_data.model.bare import get_spec
 from message_data.model.transport.utils import read_config
-from message_data.model.transport.build import main as build
-
-
-@pytest.fixture(
-    scope="session",
-    params=[pytest.param(None, marks=pytest.mark.gams_license)],
-    )
-def solved_bare_res_transport(_bare_res):
-    # Pre-load transport config/metadata
-    context = read_config()
-
-    context['transport config']['data source']['LDV'] = 'US-TIMES MA3T'
-    context['transport config']['data source']['non-LDV'] = 'IKARUS'
-
-    scen = _bare_res.clone()
-    build(scen, fast=True, quiet=False)
-    scen.solve(solve_options=dict(lpmethod=4))
-
-    yield scen
 
 
 @pytest.fixture(scope="session")
-def res_info(session_context):
-    ctx = session_context
+def transport_context(session_context):
+    """A context with the MESSAGEix-Transport configuration loaded."""
+    read_config()
+    yield session_context
 
-    # TODO this alters state that may be depended on by other tests; avoid
-    ctx.regions = "R11"
-    spec = get_spec(ctx)["add"]
 
-    yield spec
+@pytest.fixture(scope="function")
+def transport_context_f(transport_context):
+    """A modifiable copy of :func:`transport_context`, scoped to one test function."""
+    ctx = deepcopy(transport_context)
+    yield ctx
+    ctx.delete()
