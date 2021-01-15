5476f29032ca2c13bad44f06b27e5266d5335736
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-01-15 17:59:23 +0100

    Check compatibility in .transport.demand.prepare_reporter(); test

--- message_ix_models/tests/model/transport/test_build.py
+++ message_ix_models/tests/model/transport/test_build.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest import param, mark
 
 from message_data import testing
 from message_data.model.transport import build, report
@@ -24,21 +25,26 @@ def test_get_spec(transport_context_f, regions_arg, regions_exp):
     assert spec["require"].set["node"] == exp
 
 
+_NIE = pytest.mark.xfail(raises=NotImplementedError)
+
+
 @pytest.mark.parametrize(
-    "ldv, nonldv, solve",
+    "regions, ldv, nonldv, solve",
     [
-        (None, None, False),
-        (None, None, True),
-        ("US-TIMES MA3T", "IKARUS", False),
-        ("US-TIMES MA3T", "IKARUS", True),
+        ("R11", None, None, False),  # 31 s
+        param("R11", None, None, True, marks=mark.slow),  # 44 s
+        param("R11", "US-TIMES MA3T", "IKARUS", False, marks=mark.slow),  # 43 s
+        param("R11", "US-TIMES MA3T", "IKARUS", True, marks=mark.slow),  # 74 s
+        # Non-R11 configurations currently fail
+        param("R14", None, None, False, marks=_NIE),
+        param("ISR", None, None, False, marks=_NIE),
     ],
 )
-@pytest.mark.parametrize("regions", ["R11"])
-def test_build_bare_res(request, transport_context_f, ldv, nonldv, solve, regions):
+def test_build_bare_res(request, transport_context_f, regions, ldv, nonldv, solve):
     """Test that model.transport.build works on the bare RES, and the model solves."""
     # Pre-load transport config/metadata
     ctx = transport_context_f
-    ctx.regions = "R11"
+    ctx.regions = regions
 
     # Manually modify some of the configuration per test parameters
     ctx["transport config"]["data source"]["LDV"] = ldv
