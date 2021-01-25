df5b474453b8d497ca7a0ac3582e5a92020483d4
Jihoon <min@iiasa.ac.at>
2021-01-25 16:19:44 +0100

    Debug the data interfact (fix level/comm names)

--- message_ix_models/model/material/data_buildings.py
+++ message_ix_models/model/material/data_buildings.py
@@ -110,11 +110,12 @@ def gen_data_buildings(scenario, dry_run=False):
     context = read_config()
     config = context["material"]["buildings"]
 
-    lev = config['level']['add'][0]
-    comm = config['commodity']['add'][0]
-    tec = config['technology']['add'][0] # "buildings"
+    # New element names for buildings integrations
+    lev_new = config['level']['add'][0]
+    comm_new = config['commodity']['add'][0]
+    tec_new = config['technology']['add'][0] # "buildings"
 
-    print(lev, comm, tec, type(tec))
+    print(lev_new, comm_new, tec_new, type(tec_new))
 
     # Information about scenario, e.g. node, year
     s_info = ScenarioInfo(scenario)
@@ -138,22 +139,20 @@ def gen_data_buildings(scenario, dry_run=False):
     # Read field values from the buildings input data
     regions = list(set(data_buildings.node))
     comms = list(set(data_buildings.commodity))
-    types = list(set(data_buildings.type)) #
-    # ['R11_AFR', 'R11_CPA', 'R11_EEU', 'R11_FSU', 'R11_LAM', \
-    #     'R11_MEA', 'R11_NAM', 'R11_PAO', 'R11_PAS', 'R11_SAS', 'R11_WEU']
+    types = list(set(data_buildings.type))
 
     common = dict(
         time="year",
         time_origin="year",
         time_dest="year",
         mode="M1")
-    
+
     # Filter only the years in the base scenario
     data_buildings['year'] = data_buildings['year'].astype(int)
     data_buildings_demand['year'] = data_buildings_demand['year'].astype(int)
     data_buildings = data_buildings[data_buildings['year'].isin(modelyears)]
     data_buildings_demand = data_buildings_demand[data_buildings_demand['year'].isin(modelyears)]
-            
+
     for rg in regions:
         for comm in comms:
             # for typ in types:
@@ -164,10 +163,9 @@ def gen_data_buildings(scenario, dry_run=False):
             val_scr = data_buildings.loc[(data_buildings["type"] == types[1]) \
                 & (data_buildings["commodity"] == comm)\
                 & (data_buildings["node"] == rg), ]
-            
 
             # Material input to buildings
-            df = make_df('input', technology=tec, commodity=comm, \
+            df = make_df('input', technology=tec_new, commodity=comm, \
                 level="product", year_vtg = val_mat.year, \
                 value=val_mat.value, unit='t', \
                 node_loc = rg, **common)\
@@ -176,7 +174,7 @@ def gen_data_buildings(scenario, dry_run=False):
             results['input'].append(df)
 
             # Scrap output back to industry
-            df = make_df('output', technology=tec, commodity=comm, \
+            df = make_df('output', technology=tec_new, commodity=comm, \
                 level='old_scrap', year_vtg = val_scr.year, \
                 value=val_scr.value, unit='t', \
                 node_loc = rg, **common)\
@@ -185,8 +183,8 @@ def gen_data_buildings(scenario, dry_run=False):
             results['output'].append(df)
 
         # Service output to buildings demand
-        df = make_df('output', technology=tec, commodity='floor_area', \
-            level=lev, year_vtg = val_mat.year, \
+        df = make_df('output', technology=tec_new, commodity=comm_new, \
+            level='demand', year_vtg = val_mat.year, \
             value=1, unit='t', \
             node_loc=rg, **common)\
             .pipe(same_node)\
@@ -196,200 +194,8 @@ def gen_data_buildings(scenario, dry_run=False):
     # Create external demand param
     parname = 'demand'
     demand = data_buildings_demand
-    df = make_df(parname, level='demand', commodity='steel', value=demand.value, unit='t', \
+    df = make_df(parname, level='demand', commodity=comm_new, value=demand.value, unit='t', \
         year=demand.year, time='year', node=demand.node)
-    results[parname].append(df)
-
-    # Concatenate to one data frame per parameter
-    results = {par_name: pd.concat(dfs) for par_name, dfs in results.items()}
-
-    return results
-
-
-
-
-
-
-
-
-
-    # for t in s_info.set['technology']:
-    for t in config['technology']['add']:
-
-        params = data_steel.loc[(data_steel["technology"] == t),\
-            "parameter"].values.tolist()
-
-        # Special treatment for time-varying params
-        if t in tec_ts:
-            common = dict(
-                time="year",
-                time_origin="year",
-                time_dest="year",)
-
-            param_name = data_steel_ts.loc[(data_steel_ts["technology"] == t), 'parameter']
-
-            for p in set(param_name):
-                val = data_steel_ts.loc[(data_steel_ts["technology"] == t) \
-                    & (data_steel_ts["parameter"] == p), 'value']
-                units = data_steel_ts.loc[(data_steel_ts["technology"] == t) \
-                    & (data_steel_ts["parameter"] == p), 'units'].values[0]
-                mod = data_steel_ts.loc[(data_steel_ts["technology"] == t) \
-                    & (data_steel_ts["parameter"] == p), 'mode']
-                yr = data_steel_ts.loc[(data_steel_ts["technology"] == t) \
-                    & (data_steel_ts["parameter"] == p), 'year']
-
-                if p=="var_cost":
-                    df = (make_df(p, technology=t, value=val,\
-                    unit='t', year_vtg=yr, year_act=yr, mode=mod, **common).pipe(broadcast, \
-                    node_loc=nodes))
-                else:
-                    rg = data_steel_ts.loc[(data_steel_ts["technology"] == t) \
-                        & (data_steel_ts["parameter"] == p), 'region']
-                    df = make_df(p, technology=t, value=val,\
-                    unit='t', year_vtg=yr, year_act=yr, mode=mod, node_loc=rg, **common)
-
-                results[p].append(df)
-
-        # Iterate over parameters
-        for par in params:
-
-            # Obtain the parameter names, commodity,level,emission
-            split = par.split("|")
-            print(split)
-            param_name = split[0]
-            # Obtain the scalar value for the parameter
-            val = data_steel.loc[((data_steel["technology"] == t) \
-                & (data_steel["parameter"] == par)),'value']#.values
-            regions = data_steel.loc[((data_steel["technology"] == t) \
-                & (data_steel["parameter"] == par)),'region']#.values
-
-            common = dict(
-                year_vtg= yv_ya.year_vtg,
-                year_act= yv_ya.year_act,
-                # mode="M1",
-                time="year",
-                time_origin="year",
-                time_dest="year",)
-
-            for rg in regions:
-
-                # For the parameters which inlcudes index names
-                if len(split)> 1:
-
-                    print('1.param_name:', param_name, t)
-                    if (param_name == "input")|(param_name == "output"):
-
-                        # Assign commodity and level names
-                        com = split[1]
-                        lev = split[2]
-                        mod = split[3]
-                        print(rg, par, lev)
-
-                        if (param_name == "input") and (lev == "import"):
-                            df = make_df(param_name, technology=t, commodity=com, \
-                            level=lev, \
-                            value=val[regions[regions==rg].index[0]], mode=mod, unit='t', \
-                            node_loc=rg, node_origin="R11_GLB", **common)
-                        elif (param_name == "output") and (lev == "export"):
-                            df = make_df(param_name, technology=t, commodity=com, \
-                            level=lev, \
-                            value=val[regions[regions==rg].index[0]], mode=mod, unit='t', \
-                            node_loc=rg, node_dest="R11_GLB", **common)
-                        else:
-                            df = (make_df(param_name, technology=t, commodity=com, \
-                            level=lev, \
-                            value=val[regions[regions==rg].index[0]], mode=mod, unit='t', \
-                            node_loc=rg, **common)\
-                            .pipe(same_node))
-
-                        # Copy parameters to all regions, when node_loc is not GLB
-                        if (len(regions) == 1) and (rg != "R11_GLB"):
-                            print("copying to all R11", rg, lev)
-                            df['node_loc'] = None
-                            df = df.pipe(broadcast, node_loc=nodes)#.pipe(same_node)
-                            # Use same_node only for non-trade technologies
-                            if (lev != "import") and (lev != "export"):
-                                df = df.pipe(same_node)
-
-                    elif param_name == "emission_factor":
-
-                        # Assign the emisson type
-                        emi = split[1]
-                        mod = split[2]
-
-                        df = make_df(param_name, technology=t, \
-                        value=val[regions[regions==rg].index[0]],\
-                        emission=emi, mode=mod, unit='t', \
-                        node_loc=rg, **common)
-
-                    else: # time-independent var_cost
-                        mod = split[1]
-                        df = make_df(param_name, technology=t, \
-                        value=val[regions[regions==rg].index[0]], \
-                        mode=mod, unit='t', node_loc=rg, \
-                        **common)
-
-                # Parameters with only parameter name
-                else:
-                    print('2.param_name:', param_name)
-                    df = make_df(param_name, technology=t, \
-                    value=val[regions[regions==rg].index[0]], unit='t', \
-                    node_loc=rg, **common)
-
-                # Copy parameters to all regions
-                if len(set(df['node_loc'])) == 1 and list(set(df['node_loc']))[0]!='R11_GLB':
-                    print("Copying to all R11")
-                    df['node_loc'] = None
-                    df = df.pipe(broadcast, node_loc=nodes)
-
-                results[param_name].append(df)
-
-    # Add relations for scrap grades and availability
-
-    for r in config['relation']['add']:
-
-        params = set(data_steel_rel.loc[(data_steel_rel["relation"] == r),\
-            "parameter"].values)
-
-        common_rel = dict(
-            year_rel = modelyears,
-            year_act = modelyears,
-            mode = 'M1',
-            relation = r,)
-
-        for par_name in params:
-            if par_name == "relation_activity":
-
-                val = data_steel_rel.loc[((data_steel_rel["relation"] == r) \
-                    & (data_steel_rel["parameter"] == par_name)),'value'].values
-                tec = data_steel_rel.loc[((data_steel_rel["relation"] == r) \
-                    & (data_steel_rel["parameter"] == par_name)),'technology'].values
-
-                print(par_name, "val", val, "tec", tec)
-
-                df = (make_df(par_name, technology=tec, \
-                            value=val, unit='-', mode = 'M1', relation = r)\
-                    .pipe(broadcast, node_rel=nodes, \
-                            node_loc=nodes, year_rel = modelyears))\
-                    .assign(year_act=copy_column('year_rel'))
-
-                results[par_name].append(df)
-
-            elif par_name == "relation_upper":
-
-                val = data_steel_rel.loc[((data_steel_rel["relation"] == r) \
-                    & (data_steel_rel["parameter"] == par_name)),'value'].values[0]
-
-                df = (make_df(par_name, value=val, unit='-',\
-                **common_rel).pipe(broadcast, node_rel=nodes))
-
-                results[par_name].append(df)
-
-    # Create external demand param
-    parname = 'demand'
-    demand = gen_mock_demand_steel(scenario)
-    df = make_df(parname, level='demand', commodity='steel', value=demand.value, unit='t', \
-        year=demand.year, time='year', node=demand.node)#.pipe(broadcast, node=nodes)
     results[parname].append(df)
 
     # Concatenate to one data frame per parameter
