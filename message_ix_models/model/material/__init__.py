88ac2fe831fc428a484be17a9038bd99dfda41f4
Jihoon <min@iiasa.ac.at>
2021-01-26 17:15:45 +0100

    Move a functino from data.py to avoid circular import

--- message_ix_models/model/material/__init__.py
+++ message_ix_models/model/material/__init__.py
@@ -117,3 +117,42 @@ def solve(context, datafile):
 
     # Solve
     scenario.solve()
+
+import logging
+log = logging.getLogger(__name__)
+
+from .data_cement import gen_data_cement
+from .data_steel import gen_data_steel
+from .data_aluminum import gen_data_aluminum
+from .data_generic import gen_data_generic
+from .data_petro import gen_data_petro_chemicals
+from .data_buildings import gen_data_buildings
+from message_data.tools import add_par_data
+
+DATA_FUNCTIONS = [
+    gen_data_buildings,
+    gen_data_steel,
+    gen_data_cement,
+    gen_data_aluminum,
+    gen_data_petro_chemicals,
+    gen_data_generic
+]
+
+# Try to handle multiple data input functions from different materials
+def add_data(scenario, dry_run=False):
+    """Populate `scenario` with MESSAGE-Transport data."""
+    # Information about `scenario`
+    info = ScenarioInfo(scenario)
+
+    # Check for two "node" values for global data, e.g. in
+    # ixmp://ene-ixmp/CD_Links_SSP2_v2.1_clean/baseline
+    if {"World", "R11_GLB"} < set(info.set["node"]):
+        log.warning("Remove 'R11_GLB' from node list for data generation")
+        info.set["node"].remove("R11_GLB")
+
+    for func in DATA_FUNCTIONS:
+        # Generate or load the data; add to the Scenario
+        log.info(f'from {func.__name__}()')
+        add_par_data(scenario, func(scenario), dry_run=dry_run)
+
+    log.info('done')
