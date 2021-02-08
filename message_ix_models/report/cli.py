fd623be7df09995f527ea3c1a2ddcff27d2c7f32
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-02-08 14:11:16 +0100

    Blacken .reporting.{cli,core}

--- message_ix_models/report/cli.py
+++ message_ix_models/report/cli.py
@@ -13,41 +13,44 @@ from message_data.tools.cli import mark_time
 log = logging.getLogger(__name__)
 
 
-@click.command(name='report')
+@click.command(name="report")
 @click.pass_obj
-@click.option('--config', 'config_file', default='global', show_default=True,
-              help='Path or stem for reporting config file.')
-@click.option("--module", "-m", metavar="MODULES",
-              help="Add extra reporting for MODULES.")
-@click.option('-o', '--output', 'output_path', type=Path,
-              help='Write output to file instead of console.')
-@click.option('--from-file', type=click.Path(exists=True, dir_okay=False),
-              help='Report multiple Scenarios listed in FILE.')
-@click.option('--verbose', is_flag=True, help='Set log level to DEBUG.')
-@click.option('--dry-run', '-n', is_flag=True,
-              help='Only show what would be done.')
-@click.argument('key', default='message:default')
-def cli(
-    context,
-    config_file,
-    module,
-    output_path,
-    from_file,
-    verbose,
-    dry_run,
-    key
-):
+@click.option(
+    "--config",
+    "config_file",
+    default="global",
+    show_default=True,
+    help="Path or stem for reporting config file.",
+)
+@click.option(
+    "--module", "-m", metavar="MODULES", help="Add extra reporting for MODULES."
+)
+@click.option(
+    "-o",
+    "--output",
+    "output_path",
+    type=Path,
+    help="Write output to file instead of console.",
+)
+@click.option(
+    "--from-file",
+    type=click.Path(exists=True, dir_okay=False),
+    help="Report multiple Scenarios listed in FILE.",
+)
+@click.option("--verbose", is_flag=True, help="Set log level to DEBUG.")
+@click.option("--dry-run", "-n", is_flag=True, help="Only show what would be done.")
+@click.argument("key", default="message:default")
+def cli(context, config_file, module, output_path, from_file, verbose, dry_run, key):
     """Postprocess results.
 
-    KEY defaults to the comprehensive report 'message:default', but may also
-    be the name of a specific model quantity, e.g. 'output'.
+    KEY defaults to the comprehensive report 'message:default', but may alsobe the name
+    of a specific model quantity, e.g. 'output'.
 
-    --config can give either the absolute path to a reporting configuration
-    file, or the stem (i.e. name without .yaml extension) of a file in
-    data/report.
+    --config can give either the absolute path to a reporting configuration file, or
+    the stem (i.e. name without .yaml extension) of a file in data/report.
 
-    With --from-file, read multiple Scenario identifiers from FILE, and report
-    each one. In this usage, --output-path may only be a directory.
+    With --from-file, read multiple Scenario identifiers from FILE, and report each one.
+    In this usage, --output-path may only be a directory.
     """
     mark_time(quiet=True)
 
@@ -55,15 +58,15 @@ def cli(
     config = Path(config_file)
     if not config.exists():
         # Path doesn't exist; treat it as a stem in the metadata dir
-        config = context.get_config_file('report', config_file)
+        config = context.get_config_file("report", config_file)
 
     if not config.exists():
         # Can't find the file
-        raise click.BadOptionUsage(f'--config={config_file} not found')
+        raise click.BadOptionUsage(f"--config={config_file} not found")
 
     if verbose:
-        log.setLevel('DEBUG')
-        logging.getLogger('ixmp').setLevel('DEBUG')
+        log.setLevel("DEBUG")
+        logging.getLogger("ixmp").setLevel("DEBUG")
 
     # Load modules
     module = module or ""
@@ -71,6 +74,7 @@ def cli(
         name = f"message_data.{name}.report"
         __import__(name)
         register(sys.modules[name].callback)
+        print(f"Registered reporting config from {name}")
 
     # Prepare a list of Context objects, each referring to one Scenario
     contexts = []
@@ -80,7 +84,7 @@ def cli(
         if not output_path:
             output_path = Path.cwd()
         if not output_path.is_dir():
-            msg = '--output-path must be directory with --from-file'
+            msg = "--output-path must be directory with --from-file"
             raise click.BadOptionUsage(msg)
 
         for item in yaml.safe_load(open(from_file)):
@@ -93,10 +97,14 @@ def cli(
             # Construct an output path from the parsed info/URL
             ctx.output_path = Path(
                 output_path,
-                '_'.join([ctx.platform_info['name'],
-                          ctx.scenario_info['model'],
-                         ctx.scenario_info['scenario']]),
-                ).with_suffix('.xlsx')
+                "_".join(
+                    [
+                        ctx.platform_info["name"],
+                        ctx.scenario_info["model"],
+                        ctx.scenario_info["scenario"],
+                    ]
+                ),
+            ).with_suffix(".xlsx")
 
             contexts.append(ctx)
     else:
