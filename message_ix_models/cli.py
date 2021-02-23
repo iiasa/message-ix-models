bdc3cd9e8aa4e89c5e136a351130b2a3eed45856
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-02-23 15:52:07 +0100

    Add .cli and tests

---
+++ message_ix_models/cli.py
@@ -0,0 +1,12 @@
+import click
+
+from message_ix.cli import main
+
+
+@click.group("model")
+@click.pass_obj
+def model_group(context):
+    """MESSAGEix-GLOBIOM tools."""
+
+
+main.add_command(model_group)
