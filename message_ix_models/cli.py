6a0fb675bee788bb6c0779c327cb5952a5954644
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-02-23 22:05:45 +0100

    Incorporate message_data.cli commands in message_ix_models.cli

--- message_ix_models/cli.py
+++ message_ix_models/cli.py
@@ -1,3 +1,5 @@
+import sys
+
 import click
 
 
@@ -9,3 +11,19 @@ def main():
 @main.command()
 def model():
     """Commands for models."""
+
+
+try:
+    from message_data.cli import modules_with_cli as message_data_modules_with_cli
+except ImportError:
+    message_data_modules_with_cli = []
+
+for name in message_data_modules_with_cli:  # pragma: no cover
+    name = "message_data." + name
+    __import__(name)
+    main.add_command(sys.modules[name].cli)
+
+    # TODO use this in the future
+    # name = f"message_data.{name}.cli"
+    # __import__(name)
+    # main.add_command(sys.modules[name].main)
