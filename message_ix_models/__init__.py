cc2c9292d1ddc9dfe4a0316735a26599d8afa526
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-03-03 12:54:10 +0100

    Provide message_ix_models.__version__

--- message_ix_models/__init__.py
+++ message_ix_models/__init__.py
@@ -4,9 +4,17 @@ from message_ix_models.util.context import Context
 from message_ix_models.util.importlib import MessageDataFinder
 from message_ix_models.util.logging import setup as setup_logging
 from message_ix_models.util.scenarioinfo import ScenarioInfo
+from pkg_resources import DistributionNotFound, get_distribution
 
 # Expose utility classes
 __all__ = ["Context", "ScenarioInfo"]
+
+try:
+    # Version string for reference in other code
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # Package is not installed
+    __version__ = "999"
 
 # No logging to stdout (console) by default
 setup_logging(console=False)
