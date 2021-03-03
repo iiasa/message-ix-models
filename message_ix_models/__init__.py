59e08235483172a487c4046ce61459b6d570542a
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-03-03 12:46:38 +0100

    Expose ScenarioInfo as a top-level object

--- message_ix_models/__init__.py
+++ message_ix_models/__init__.py
@@ -3,6 +3,10 @@ import sys
 from message_ix_models.util.context import Context
 from message_ix_models.util.importlib import MessageDataFinder
 from message_ix_models.util.logging import setup as setup_logging
+from message_ix_models.util.scenarioinfo import ScenarioInfo
+
+# Expose utility classes
+__all__ = ["Context", "ScenarioInfo"]
 
 # No logging to stdout (console) by default
 setup_logging(console=False)
