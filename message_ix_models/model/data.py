64ee544b9ca2eee8c199561f8d3176c8e35f3e8b
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-03-03 12:44:24 +0100

    Add stub .model.data

---
+++ message_ix_models/model/data.py
@@ -0,0 +1,12 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def get_data(scenario, context, spec, **options):
+    """Data for the bare RES."""
+    if context.res_with_dummies:
+        log.warning("get_dummy_data() not migrated")
+        # return get_dummy_data(scenario, spec)
+    else:
+        return dict()
