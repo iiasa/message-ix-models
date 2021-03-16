53fb260f61d34965fc977940da7f726a495a5d82
Unknown <krey@iiasa.ac.at>
2021-03-16 14:36:05 +0100

    Adjusted code for compatibility with structure in python
    (not everything tested)

--- message_ix_models/model/material/material_intensity/data_material_intensities_rpy2.py
+++ message_ix_models/model/material/material_intensity/data_material_intensities_rpy2.py
@@ -21,6 +21,12 @@ data_path = "C:/Users/krey/Documents/git/message_data/data/material"
 # import MESSAGEix
 import ixmp
 import message_ix
+from message_data.tools import ScenarioInfo
+
+#def gen_data_material_intensities(scenario, dry_run=False):
+#    """Generate data for endogenous materials demand of power sector.
+
+#    """
 
 # launch the IX modeling platform using the local default databases
 mp = ixmp.Platform(name='default', jvmargs=['-Xmx12G'])
@@ -32,12 +38,15 @@ scen = "NoPolicy"
 # load existing scenario
 scenario = message_ix.Scenario(mp, model, scen)
 
+# information about scenario, e.g. node, year
+s_info = ScenarioInfo(scenario)
+
 # read node, technology, commodity and level from existing scenario
-node = scenario.set('node')
-year = scenario.set('year')
-technology = scenario.set('technology')
-commodity = scenario.set('commodity')
-level = scenario.set('level')
+node = s_info.N
+year = s_info.set['year'] #s_info.Y is only for modeling years
+technology = s_info.set['technology']
+commodity = s_info.set['commodity']
+level = s_info.set['level']
 # read inv.cost data
 inv_cost = scenario.par('inv_cost')
 
@@ -45,11 +54,23 @@ inv_cost = scenario.par('inv_cost')
 unit = mp.units()
 #if (!('t/kW' %in% unit$.)) mp$add_unit('t/kW', 'tonnes (of commodity) per kW of capacity')
 
+# List of data frames, to be concatenated together at end
+results = defaultdict(list)
+
+param_name = ["input_cap_new", "input_cap_ret", "output_cap_ret",]
 # source R code
 r=ro.r
 r.source(rcode_path+"ADVANCE_lca_coefficients_embedded.R")
 
 # call R function with type conversion
-with localconverter(ro.default_converter + pandas2ri.converter):
-    p=r.read_material_intensities(data_path, node, year, technology, commodity, level, inv_cost)
 
+for p in set(param_name):
+    with localconverter(ro.default_converter + pandas2ri.converter):
+        df = r.read_material_intensities(p, data_path, node, year, technology, commodity, level, inv_cost)
+
+
+results[parname].append(df)
+
+    # Concatenate to one data frame per parameter
+    results = {par_name: pd.concat(dfs) for par_name, dfs in results.items()}
+    return results
