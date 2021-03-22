fa1c96861b479d1f4641201eaa55a9decbb31988
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-03-22 18:53:04 +0100

    Sort imports

--- message_ix_models/__init__.py
+++ message_ix_models/__init__.py
@@ -2,9 +2,9 @@ import sys
 
 from pkg_resources import DistributionNotFound, get_distribution
 
+from message_ix_models.util._logging import setup as setup_logging
 from message_ix_models.util.context import Context
 from message_ix_models.util.importlib import MessageDataFinder
-from message_ix_models.util._logging import setup as setup_logging
 from message_ix_models.util.scenarioinfo import ScenarioInfo
 
 # Expose utility classes
