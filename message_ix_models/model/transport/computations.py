afa8c70c696179be81ecdca3b29477ba03e17262
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-03-22 14:32:18 +0100

    Add .transport.computations.load_transport_file

--- message_ix_models/model/transport/computations.py
+++ message_ix_models/model/transport/computations.py
@@ -1,6 +1,34 @@
 import pandas as pd
 
+from genno import Quantity, computations
+from ixmp.reporting import RENAME_DIMS
+from message_ix_models.util import private_data_path
+
 from message_data.tools import ScenarioInfo
+
+
+def load_transport_file(basename: str, units=None, name: str = None) -> Quantity:
+    """Load transport calibration data from a CSV file.
+
+    Wrapper around :func:`genno.computations.load_file`.
+
+    Parameters
+    ----------
+    basename : str
+        Base name of the file, excluding the :file:`.csv` suffix and the path. The
+        full path is constructed automatically using :func:`.private_data_path`.
+    units : str or pint.Unit, optional
+        Units to assign the the resulting
+    name : str, optional
+        Name to assign.
+    """
+
+    return computations.load_file(
+        path=private_data_path("transport", basename).with_suffix(".csv"),
+        dims=RENAME_DIMS,
+        units=units,
+        name=name,
+    )
 
 
 def transport_check(scenario, ACT):
