a777b5b1a2418f3cf0623086ad1c7c3f6cac8c11
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-03-23 15:54:54 +0100

    Update test_create_res()

--- message_ix_models/tests/model/test_bare.py
+++ message_ix_models/tests/model/test_bare.py
@@ -9,7 +9,7 @@ SET_SIZE = dict(
     level=6,
     technology=377,
     node=14 + 1,  # R14 is default, and 'World' exists automatically
-    year=11,  # Default: 2010, 2020, ..., 2110
+    year=17,  # YB is default: 2020, 2025, ..., 2050, 2060, ..., 2110
 )
 
 
@@ -25,8 +25,8 @@ SET_SIZE = dict(
         # MESSAGE-IL
         (dict(regions="ISR"), dict(node=1 + 1)),
         #
-        # Different time discretizations
-        (dict(time_step=5), dict(year=21)),  # 2010, 2015, ..., 2110
+        # Different time periods
+        (dict(year="A"), dict(year=13)),  # 2010, 2020, ..., 2110
         #
         # Option to add a dummy technology/commodity so the model solves
         (
@@ -59,8 +59,3 @@ def test_create_res(request, test_context, settings, expected):
 
     # Contains the correct periods
     year = scenario.set("year").tolist()
-    assert year[0] == test_context.period_start
-    assert year[-1] == test_context.period_end
-    assert year[-1] - year[-2] == test_context.time_step
-
-    # TODO Contains the correct duration_period values
