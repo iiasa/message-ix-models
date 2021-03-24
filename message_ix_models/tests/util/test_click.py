d6d346b9604072bd35d0a8162d7ac0bcef8e084f
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-03-24 14:47:37 +0100

    Simplify test_click.py using a context manager

--- message_ix_models/tests/util/test_click.py
+++ message_ix_models/tests/util/test_click.py
@@ -1,51 +1,58 @@
 """Basic tests of the command line."""
+from contextlib import contextmanager
+
 import click
-from click.testing import CliRunner
 
 from message_ix_models.cli import main
 from message_ix_models.util.click import common_params
 
 
-def test_default_path_cb(session_context):
+@contextmanager
+def temporary_command(group, func):
+    """Context manager that temporarily attaches command `func` to `group`."""
+    try:
+        group.add_command(func)
+        yield
+    finally:
+        group.commands.pop(func.name)
+
+
+def test_default_path_cb(session_context, mix_models_cli):
     """Test :func:`.default_path_cb`."""
     # Create a hidden command and attach it to the CLI
-    name = "_test_default_path_cb"
-
-    @main.command(name=name, hidden=True)
+    @click.command(name="_test_default_path_cb", hidden=True)
     @common_params("rep_out_path")
     @click.pass_obj
-    def _(ctx, rep_out_path):
+    def func(ctx, rep_out_path):
         print(ctx["rep_out_path"])  # Print the value stored on the Context object
 
     # Command parameters: --local-data gives the local data path, but the --rep-out-path
     # option is *not* given
-    cmd = [f"--local-data={session_context.local_data}", name]
+    cmd = [f"--local-data={session_context.local_data}", func.name]
 
     # …so default_path_cb() should supply "{local_data}/reporting_output".
     expected = session_context.local_data / "reporting_output"
 
     # Run the command
-    result = CliRunner().invoke(main, cmd)
+    with temporary_command(main, func):
+        result = mix_models_cli.assert_exit_0(cmd)
 
     # The value was stored on, and retrieved from, `ctx`
-    assert 0 == result.exit_code, result.exc_info
     assert f"{expected}\n" == result.output
 
 
-def test_store_context():
+def test_store_context(mix_models_cli):
     """Test :func:`.store_context`."""
     # Create a hidden command and attach it to the CLI
-    name = "_test_store_context"
-
-    @main.command(name=name, hidden=True)
+    @click.command(name="_test_store_context", hidden=True)
     @common_params("ssp")
     @click.pass_obj
-    def _(ctx, ssp):
+    def func(ctx, ssp):
         print(ctx["ssp"])  # Print the value stored on the Context object
 
     # Run the command with a valid value
-    result = CliRunner().invoke(main, [name, "SSP2"])
+    with temporary_command(main, func):
+        result = mix_models_cli.assert_exit_0([func.name, "SSP2"])
 
     # The value was stored on, and retrieved from, `ctx`
-    assert 0 == result.exit_code, result.exc_info
     assert "SSP2\n" == result.output
