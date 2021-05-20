3efd4f04287763aef2bc08d34c7a215ccc4e385c
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-05-20 08:57:42 +0200

    Move transport.data.freight() to its own submodule

--- message_ix_models/model/transport/data/__init__.py
+++ message_ix_models/model/transport/data/__init__.py
@@ -14,16 +14,16 @@ from message_ix_models.util import (
     same_node,
 )
 
-from message_data.model.transport.utils import input_commodity_level
-
 from .ldv import get_ldv_data
 from .non_ldv import get_non_ldv_data
+from .freight import get_freight_data
 
 log = logging.getLogger(__name__)
 
 DATA_FUNCTIONS = [
     get_ldv_data,
     get_non_ldv_data,
+    get_freight_data,
 ]
 
 
@@ -153,50 +153,6 @@ def conversion(context):
 
 
 DATA_FUNCTIONS.append(conversion)
-
-
-def freight(context):
-    """Data for freight technologies."""
-    codes = context["transport set"]["technology"]["add"]
-    freight_truck = codes[codes.index("freight truck")]
-    info = context["transport build info"]
-
-    common = dict(
-        year_vtg=info.Y,
-        year_act=info.Y,
-        mode="all",
-        time="year",  # no subannual detail
-        time_dest="year",
-        time_origin="year",
-    )
-
-    data = defaultdict(list)
-    for tech in freight_truck.child:
-        i_o = make_io(
-            src=(None, None, "GWa"),
-            dest=("transport freight vehicle", "useful", "km"),
-            efficiency=1.0,
-            on="input",
-            technology=tech.id,
-            **common,
-        )
-
-        i_o["input"] = input_commodity_level(i_o["input"], "final")
-
-        for par, df in i_o.items():
-            data[par].append(df.pipe(broadcast, node_loc=info.N[1:]).pipe(same_node))
-
-    data = {par: pd.concat(dfs) for par, dfs in data.items()}
-
-    data.update(
-        make_matched_dfs(
-            base=data["input"],
-            capacity_factor=1,
-            technical_lifetime=10,
-        )
-    )
-
-    return data
 
 
 DATA_FUNCTIONS.append(freight)
