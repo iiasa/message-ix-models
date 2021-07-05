8f0b5300b819d27b9c7fc3bd7f37f103adab4e60
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-07-05 15:49:07 +0200

    Shorten debug logging of data frames in .util.add_par_data()

--- message_ix_models/util/__init__.py
+++ message_ix_models/util/__init__.py
@@ -61,7 +61,7 @@ def add_par_data(
     for par_name, values in data.items():
         N = values.shape[0]
         log.info(f"{N} rows in {repr(par_name)}")
-        log.debug(str(values))
+        log.debug(values.to_string(max_rows=5))
 
         total += N
 
