95849e94eb7de99f9bb9f103e01cea91d4784308
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-07-19 18:16:46 +0200

    Simplify .transport.demand.add_exogenous_data() using genno
    - move dummy_prices() to .computations.
    - add .computations.rename()
    - type hint .transport.computations

--- message_ix_models/model/transport/computations.py
+++ message_ix_models/model/transport/computations.py
@@ -1,19 +1,33 @@
 """Reporting computations for MESSAGEix-Transport."""
+from typing import Hashable, Mapping, Union
+
+import numpy as np
 import pandas as pd
+import xarray as xr
 from genno import Quantity, computations
-from ixmp.reporting import RENAME_DIMS
+from ixmp import Scenario
 from message_ix_models import ScenarioInfo
-from message_ix_models.util import private_data_path
 
 
-def as_quantity(info):
+def as_quantity(info: dict) -> Quantity:
     dim = info.pop("_dim")
     unit = info.pop("_unit")
 
     return Quantity(pd.Series(info).rename_axis(dim), units=unit)
 
 
-def ldv_distance(config):
+def dummy_prices(gdp: Quantity) -> Quantity:
+    # Commodity prices: all equal to 0.1
+
+    # Same coords/shape as `gdp`, but with c="transport"
+    coords = [(dim, item.data) for dim, item in gdp.coords.items()]
+    coords.append(("c", ["transport"]))
+    shape = list(len(c[1]) for c in coords)
+
+    return Quantity(xr.DataArray(np.full(shape, 0.1), coords=coords), units="USD / km")
+
+
+def ldv_distance(config: dict) -> Quantity:
     """Return annual driving distance per LDV.
 
     - Regions other than R11_NAM have M/F values in same proportion to their A value as
@@ -30,7 +44,19 @@ def ldv_distance(config):
     return result
 
 
-def transport_check(scenario, ACT):
+def rename(
+    qty: Quantity,
+    new_name_or_name_dict: Union[Hashable, Mapping[Hashable, Hashable]] = None,
+    **names: Hashable
+) -> Quantity:
+    """Like :meth:`xarray.DataArray.rename`.
+
+    .. todo:: Upstream to :mod:`genno`.
+    """
+    return qty.rename(new_name_or_name_dict, **names)
+
+
+def transport_check(scenario: Scenario, ACT: Quantity) -> pd.Series:
     """Reporting computation for :func:`check`.
 
     Imported into :mod:`.reporting.computations`.
