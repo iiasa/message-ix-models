cddee83ca4c215e83ba2ea5cf14955640f1928cf
Muhammad Awais <awais307@gmail.com>
2021-07-30 17:49:19 +0200

    Fix errors in overall workflow

--- message_ix_models/model/water/data/__init__.py
+++ message_ix_models/model/water/data/__init__.py
@@ -11,11 +11,10 @@ log = logging.getLogger(__name__)
 
 DATA_FUNCTIONS = [
     cool_tech,  # Water & parasitic_electricity requirements for cooling technologies
-    non_cooling_tec,
-    demands
+    non_cooling_tec
+    # demands
 ]
 
-data_funcs = [cool_tech,non_cooling_tec()] if context.nexus_set = 'cooling' else DATA_FUNCTIONS
 
 def add_data(scenario, context, dry_run=False):
     """Populate `scenario` with MESSAGEix-Nexus data."""
@@ -23,20 +22,13 @@ def add_data(scenario, context, dry_run=False):
     info = ScenarioInfo(scenario)
     context["water build info"] = info
 
-    for func in DATA_FUNCTIONS:
-        # Generate or load the data; add to the Scenario
-        log.info(f"from {func.__name__}()")
-        add_par_data(scenario, func(context), dry_run=dry_run)
+    data_funcs = (
+        [cool_tech, non_cooling_tec]
+        if context.nexus_set == "cooling"
+        else DATA_FUNCTIONS
+    )
 
-    log.info("done")
-
-def add_cooling_data(scenario, context, dry_run=False):
-    """Populate `scenario` with cooling technology data."""
-
-    info = ScenarioInfo(scenario)
-    context["water build info"] = info
-
-    for func in DATA_FUNCTIONS:
+    for func in data_funcs:
         # Generate or load the data; add to the Scenario
         log.info(f"from {func.__name__}()")
         add_par_data(scenario, func(context), dry_run=dry_run)
