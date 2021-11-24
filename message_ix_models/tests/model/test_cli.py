69039ad9efa822e548d57b09488b0050c7e695af
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-11-24 13:44:02 +0100

    Use .structure.codelists() instead of hard-coded lists

--- message_ix_models/tests/model/test_cli.py
+++ message_ix_models/tests/model/test_cli.py
@@ -2,4 +2,4 @@ def test_create_bare(mix_models_cli):
     """The ``res create-bare`` CLI command can be invoked."""
     # --regions is not a required option, but we give it anyway to test the CLI code
     # that handles it
-    mix_models_cli.assert_exit_0(["res", "create-bare", "--regions=R11"])
+    mix_models_cli.assert_exit_0(["res", "create-bare", "--nodes=R11"])
