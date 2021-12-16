c0a1422dc834ac18ee0c341d75ef425be64bafdc
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-12-16 14:43:22 +0100

    Add Context.__repr__()

--- message_ix_models/util/context.py
+++ message_ix_models/util/context.py
@@ -91,6 +91,9 @@ class Context(dict):
 
         return result
 
+    def __repr__(self):
+        return f"<{self.__class__.__name__} object at {id(self)} with {len(self)} keys"
+
     def delete(self):
         """Hide the current Context from future :meth:`.get_instance` calls."""
         # Index of the *last* matching instance
