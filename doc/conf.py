6921f0d0a1ad354d0ebbf20e498e747697fadb2a
Laura Wienpahl <57132039+LauWien@users.noreply.github.com>
2022-05-04 06:45:17 +0200

    Use alphabetical order for html output

--- doc/conf.py
+++ doc/conf.py
@@ -51,14 +51,10 @@ def setup(app: "sphinx.application.Sphinx") -> None:
 
 # -- Options for HTML output -----------------------------------------------------------
 
-html_favicon = "_static/favicon.svg"
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for a list of
-# builtin themes.
-html_theme = "sphinx_rtd_theme"
-
 # A list of CSS files.
 html_css_files = ["custom.css"]
+
+html_favicon = "_static/favicon.svg"
 
 # The name of an image file (relative to this directory) to place at the top of the
 # sidebar.
@@ -68,6 +64,10 @@ html_logo = "_static/combined-logo-white.png"
 # to this directory. They are copied after the builtin static files, so a file named
 # "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for a list of
+# builtin themes.
+html_theme = "sphinx_rtd_theme"
 
 # -- Options for sphinx.ext.autosummary ------------------------------------------------
 
