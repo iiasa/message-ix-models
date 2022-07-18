bff51591d3e07ed358a0147bf3e86400903e5862
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-07-18 18:39:52 +0200

    Tidy add_tax_emission() 1
    - Remove non-functional if __name__ == "__main__" block.
    - Use existing ScenarioInfo features to retrieve years.
    - Chain pd.DataFrame.assign() instead of multiple assignments.
    - Reflow docstring.

--- message_ix_models/model/emissions.py
+++ message_ix_models/model/emissions.py
@@ -1,28 +1,25 @@
-from .get_optimization_years import main as get_optimization_years  # type: ignore
+from message_ix_models import ScenarioInfo
 
 
-def main(scen, price, conversion_factor=44 / 12):
+def add_tax_emission(scen, price, conversion_factor=44 / 12):
     """Adds a global CO2 price to a scenario.
 
-    A global carbon price is implemented with an annual growth rate
-    equal to the discount rate.
+    A global carbon price is implemented with an annual growth rate equal to the
+    discount rate.
 
     Parameters
     ----------
     scen : :class:`message_ix.Scenario`
-        Scenario for which a carbon price should be added
-
+        Scenario for which a carbon price should be added.
     price : int
-        Carbon price which should be added to the model.  This value
-        will be applied from the 'firstmodelyear' onwards.
-
+        Carbon price which should be added to the model. This value will be applied from
+        the 'firstmodelyear' onwards.
     conversion_factor : float
-        The conversion_factor with which the input value is multiplied.
-        The default assumption assumes that the price is specified in
-        US$2005/tCO2, hence it is converted to US$2005/tC as required
-        by the model.
+        The conversion_factor with which the input value is multiplied. The default
+        assumption assumes that the price is specified in US$2005/tCO2, hence it is
+        converted to US$2005/tC as required by the model.
     """
-    years = get_optimization_years(scen)
+    years = ScenarioInfo(scen).Y
     df = (
         scen.par("duration_period", filters={"year": years})
         .drop(["unit"], axis=1)
@@ -42,16 +39,14 @@ def main(scen, price, conversion_factor=44 / 12):
         df.reset_index()
         .drop(["duration"], axis=1)
         .rename(columns={"year": "type_year"})
+        .assign(
+            node="World",
+            type_emission="TCE",
+            type_tec="all",
+            unit="USD/tC"
+        )
     )
-    df.loc[:, "node"] = "World"
-    df.loc[:, "type_emission"] = "TCE"
-    df.loc[:, "type_tec"] = "all"
-    df.loc[:, "unit"] = "USD/tC"
 
     scen.check_out()
     scen.add_par("tax_emission", df)
     scen.commit("Added carbon price")
-
-
-if __name__ == "__main__":
-    main("test", "test")
