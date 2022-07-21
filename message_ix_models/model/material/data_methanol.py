98b6c87262891f68d56d7195e1ad44ff69e5f728
Florian Maczek <macflo@gmx.at>
2022-07-21 23:31:27 +0200

    Fix par_dict merging + add sets

--- message_ix_models/model/material/data_methanol.py
+++ message_ix_models/model/material/data_methanol.py
@@ -9,11 +9,20 @@ import matplotlib.pyplot as plt
 from message_ix_models import ScenarioInfo
 from message_ix import make_df
 from message_ix_models.util import broadcast, same_node
-from util import read_config
+from .util import read_config
 
 
 def gen_data_methanol(scenario):
-    return {**gen_data_meth_h2(scenario), **gen_data_meth_bio(scenario)}
+    dict1 = gen_data_meth_h2(scenario)
+    dict2 = gen_data_meth_bio(scenario)
+    keys = set(list(dict1.keys())+list(dict2.keys()))
+    new_dict = {}
+    for i in keys:
+        if (i in dict2.keys()) & (i in dict1.keys()):
+            new_dict[i] = dict1[i].append(dict2[i])
+        else:
+            new_dict[i] = dict1[i]
+    return new_dict
 
 
 def gen_data_meth_h2(scenario):
