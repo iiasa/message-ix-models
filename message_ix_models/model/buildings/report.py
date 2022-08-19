7d23b6341c36eb5253d4880f016544569eaa9e37
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-08-19 16:53:59 +0200

    Use more helper functions in .buildings.report

--- message_ix_models/model/buildings/report.py
+++ message_ix_models/model/buildings/report.py
@@ -92,20 +92,11 @@ def callback(rep: message_ix.Reporter, context: Context) -> None:
     rep.add("buildings iamc store", [])  # Does nothing
 
 
-def buildings_filters(all_techs: List[str], years: List) -> Dict:
-    """Return filters for buildings reporting."""
-    # Regular expression to match technology IDs relevant for buildings reporting
-    tech_re = re.compile("(resid|comm).*(apps|cool|cook|heat|hotwater)")
-    return dict(
-        technology=list(
-            filter(lambda t: tech_re.search(t) or t == "biomass_nc", all_techs)
-        ),
-        year_act=years,
-    )
+# Helper functions
 
 
-def _add_global_total(df):
-    """Add a global total (across the "node" dimension to `df`."""
+def add_global_total(df: pd.DataFrame) -> pd.DataFrame:
+    """Add a global total (across the "node" dimension) to `df`."""
     assert set(df.columns) == set(COLS)
 
     total = (
@@ -118,6 +109,42 @@ def _add_global_total(df):
         pd.concat([df, total], ignore_index=True)
         .sort_values(["node", "variable", "year"])
         .reset_index(drop=True)
+    )
+
+
+def fuel_sector_from_commodity(df: pd.DataFrame) -> pd.DataFrame:
+    """Extract "fuel" and "sector" from "commodity" in `df`; apply `NAME_MAP`."""
+    f_s = df["commodity"].str.rsplit("_", 1, expand=True)
+    return df.assign(fuel=f_s[0], sector=f_s[1]).replace(NAME_MAP)
+
+
+def sum_on(df: pd.DataFrame, *columns) -> pd.DataFrame:
+    """Compute a sum on `df`, grouped by `columns`."""
+    return df.groupby(list(columns)).sum().reset_index()
+
+
+def var_name(df: pd.DataFrame, expr: str) -> pd.DataFrame:
+    """Format the "variable" column of `df` given `expr`.
+
+    `expr` should be like "Some text {other_col} text {different_col}", referencing
+    existing columns of `df`.
+    """
+    # Prepend 0. to replacement groups in `expr` so it is suitable for use with apply()
+    return df.assign(variable=df.apply(expr.replace("{", "{0.").format, axis=1))
+
+
+# Reporting computations/atomic steps
+
+
+def buildings_filters(all_techs: List[str], years: List) -> Dict:
+    """Return filters for buildings reporting."""
+    # Regular expression to match technology IDs relevant for buildings reporting
+    tech_re = re.compile("(resid|comm).*(apps|cool|cook|heat|hotwater)")
+    return dict(
+        technology=list(
+            filter(lambda t: tech_re.search(t) or t == "biomass_nc", all_techs)
+        ),
+        year_act=years,
     )
 
 
@@ -142,7 +169,8 @@ def report0(scenario: message_ix.Scenario, filters: dict) -> pd.DataFrame:
     # Fix for non commercial biomass to be consistent with MESSAGE's original numbers
     # which go directly from primary to useful. So, we are "de-usefulizing" here using
     # our conversion factor
-    FE_rep.loc[FE_rep["technology"] == "biomass_nc", "value"] /= 0.15
+    mask = FE_rep["technology"] == "biomass_nc"
+    FE_rep.loc[mask, "value"] /= 0.15
 
     # - Rename "biomass_nc" to "biomass_nc_resid_cook"
     # - Duplicate data as "biomass_resid_cook"
@@ -150,57 +178,46 @@ def report0(scenario: message_ix.Scenario, filters: dict) -> pd.DataFrame:
     FE_rep = pd.concat(
         [
             FE_rep.replace(dict(technology={"biomass_nc": "biomass_nc_resid_cook"})),
-            FE_rep.query("technology == 'biomass_nc'").assign(
-                technology="biomass_resid_cook"
-            ),
+            FE_rep[mask].assign(technology="biomass_resid_cook"),
         ]
     ).assign(commodity=lambda df: df["technology"].str.rsplit("_", 1, expand=True)[0])
 
     # - Calculate totals by (commodity, node, year)
-    # NB(PNK) genno will do this automatically if FE_rep is described with sums=True
+    #   NB(PNK) genno will do this automatically if FE_rep is described with sums=True
+    # - Extract fuel and sector from commodity labels.
+    # - Adjust sector and fuel names.
+    # - Construct a variable label.
     FE_rep = (
         FE_rep[["node", "commodity", "year", "value"]]
-        .groupby(["node", "commodity", "year"])
-        .sum()
-        .reset_index()
+        .pipe(sum_on, "node", "commodity", "year")
+        .pipe(fuel_sector_from_commodity)
+        .pipe(var_name, "Final Energy|{sector}|{fuel}")
     )
 
-    # Extract fuel and sector from commodity labels
-    FE_rep[["fuel", "sector"]] = FE_rep["commodity"].str.rsplit("_", 1, expand=True)
-
-    # Adjust sector and fuel names
-    FE_rep.replace(NAME_MAP, inplace=True)
-
-    # Construct a variable label
-    FE_rep["variable"] = "Final Energy|" + FE_rep["sector"] + "|" + FE_rep["fuel"]
-
     # Convert from internal ACT GWa to EJ
+    # TODO(PNK) provide a common function similar to
+    #           message_ix_models.util.convert_units() for this kind of operation
     units_to = "EJ/yr"
     converted = registry.Quantity(FE_rep["value"].values, "GWa/year").to("EJ/yr")
     FE_rep = FE_rep.assign(value=converted.magnitude, unit=units_to)
 
     # Sum commercial and residential by fuel type
-    FE_rep_tot = (
-        FE_rep.groupby(["node", "fuel", "unit", "year"])
-        .sum()
-        .reset_index()
-        .assign(
-            variable=lambda df: "Final Energy|Residential and Commercial|" + df["fuel"]
-        )
+    FE_rep_tot = FE_rep.pipe(sum_on, "node", "fuel", "unit", "year").pipe(
+        var_name, "Final Energy|Residential and Commercial|{fuel}"
     )
 
-    FE_rep = pd.concat([FE_rep[COLS], FE_rep_tot[COLS]], ignore_index=True)
-
-    _add_global_total(FE_rep)
+    FE_rep = (
+        pd.concat([FE_rep[COLS], FE_rep_tot[COLS]], ignore_index=True)
+        .pipe(add_global_total)
+        .assign(fuel_type=lambda df: df["variable"].str.split("|", 2, expand=True)[1])
+    )
 
     # sum of the building related Final Energy by fuel types to get the variable
     # "Final Energy|Residential and Commercial",
     # "Final Energy|Residential", and "Final Energy|Commercial"
     # for FE_rep
 
-    FE_rep["fuel_type"] = FE_rep["variable"].str.split("|", 2, expand=True)[1]
-
-    var_list_drop = [
+    exclude = [
         "Final Energy|Residential and Commercial|Solids|Biomass|Traditional",
         "Final Energy|Residential|Solids|Biomass|Traditional",
         "Final Energy|Commercial|Solids|Biomass",
@@ -208,11 +225,10 @@ def report0(scenario: message_ix.Scenario, filters: dict) -> pd.DataFrame:
 
     # Sum of fuel types for different building sub-setors (R, C and R+C)
     FE_rep_tot_fuel = (
-        FE_rep[~FE_rep["variable"].isin(var_list_drop)]
-        .groupby(["node", "unit", "year", "fuel_type"])
-        .sum()
-        .reset_index()
-    ).assign(variable=lambda df: "Final Energy|" + df["fuel_type"])
+        FE_rep[~FE_rep["variable"].isin(exclude)]
+        .pipe(sum_on, "node", "unit", "year", "fuel_type")
+        .pipe(var_name, "Final Energy|{fuel_type}")
+    )
 
     FE_rep = pd.concat([FE_rep[COLS], FE_rep_tot_fuel[COLS]], ignore_index=True)
 
@@ -226,59 +242,44 @@ def report1(scenario: message_ix.Scenario, filters: dict) -> pd.DataFrame:
     act = scenario.var("ACT", filters=filters)
     emiss = scenario.par("relation_activity", filters=filters)
 
-    # Subset of emissions data where the relation name contains "Emission"
-    emiss = emiss.loc[emiss["relation"].str.contains("Emission")]
-
-    emiss = emiss.merge(act).rename(columns={"year_act": "year", "node_loc": "node"})
-
-    # Product of the "value" (from relation_activity) and "lvl" (from ACT)
-    # TODO move this product (relation_activity * lvl) upstream to message_ix.reporting
-    emiss["value"] = emiss["value"] * emiss["lvl"]
-
-    # Some fixes
-    emiss = emiss.assign(
-        technology=emiss["technology"].replace("biomass_nc", "biomass_nc_resid_cook"),
-        commodity=emiss["technology"].str.rsplit("_", 1, expand=True)[0],
-        emission=emiss["relation"].str.rsplit("_", 1, expand=True)[0],
-        unit=lambda df: "Mt " + df["emission"] + "/yr",
-    )[["node", "year", "commodity", "emission", "unit", "value"]]
-
-    # NB(PNK) genno will do this automatically if emiss is described with sums=True
+    # - Subset of emissions data where the relation name contains "Emission"
+    # - Merge ACT data
+    # - Rename columns.
+    # - Product of the "value" (from relation_activity) and "lvl" (from ACT)
+    #   TODO provider this product (relation_activity * lvl) from message_ix.reporting
+    # - Adjust technology, commodity, emission, and unit labels.
+    # - Select some columns.
+    # - Compute sums.
+    # - Extract fuel and sector from commodity label.
+    # - Adjust sector and fuel names.
+    # - Assemble variable names.
     emiss = (
-        emiss.groupby(["node", "year", "commodity", "emission", "unit"])
-        .sum()
-        .reset_index()
+        emiss[emiss["relation"].str.contains("Emission")]
+        .merge(act)
+        .rename(columns={"year_act": "year", "node_loc": "node"})
+        .assign(
+            value=lambda df: df["value"] * df["lvl"],
+            technology=lambda df: df["technology"].replace(
+                "biomass_nc", "biomass_nc_resid_cook"
+            ),
+            commodity=lambda df: df["technology"].str.rsplit("_", 1, expand=True)[0],
+            emission=lambda df: df["relation"].str.rsplit("_", 1, expand=True)[0],
+            unit=lambda df: "Mt " + df["emission"] + "/yr",
+        )[["node", "year", "commodity", "emission", "unit", "value"]]
+        .pipe(sum_on, "node", "year", "commodity", "emission", "unit")
+        .pipe(fuel_sector_from_commodity)
+        .pipe(var_name, "Emissions|{emission}|Energy|Demand|{sector}|{fuel}")
     )
 
-    emiss[["fuel", "sector"]] = emiss["commodity"].str.rsplit("_", 1, expand=True)
-
-    # Adjust sector and fuel names
-    emiss.replace(NAME_MAP, inplace=True)
-
-    # Assemble variable name
-    emiss["variable"] = (
-        "Emissions|"
-        + emiss["emission"]
-        + "|Energy|Demand|"
-        + emiss["sector"]
-        + "|"
-        + emiss["fuel"]
+    # - Compute a total across sector.
+    # - Construct variable names.
+    emiss_tot = emiss.pipe(sum_on, "node", "emission", "fuel", "unit", "year").pipe(
+        var_name, "Emissions|{emission}|Energy|Demand|Residential and Commercial|{fuel}"
     )
 
-    # Compute a total across sector
-    emiss_tot = (
-        emiss.groupby(["node", "emission", "fuel", "unit", "year"]).sum().reset_index()
-    ).assign(
-        variable=lambda df: "Emissions|"
-        + df["emission"]
-        + "|Energy|Demand|Residential and Commercial|"
-        + df["fuel"]
+    emiss = pd.concat([emiss[COLS], emiss_tot[COLS]], ignore_index=True).pipe(
+        add_global_total
     )
-
-    emiss = pd.concat([emiss[COLS], emiss_tot[COLS]], ignore_index=True)
-
-    # Global total
-    _add_global_total(emiss)
 
     return emiss
 
@@ -322,8 +323,8 @@ def report2(scenario: message_ix.Scenario, sturm_output_path: Path) -> pd.DataFr
 def report3(scenario: message_ix.Scenario, sturm_rep: pd.DataFrame) -> pd.DataFrame:
     """Manipulate variable names for `sturm_rep` and compute additional sums."""
 
-    # Variables to exclude
-    prefix = "Energy Service|Residential and Commercial|"
+    # Variables to exclude from the manipulations
+    prefix = "Energy Service|Residential and Commercial"
     exclude = [
         f"{prefix}|Commercial",
         f"{prefix}|Residential|Multi-family|Floor space",
@@ -331,10 +332,13 @@ def report3(scenario: message_ix.Scenario, sturm_rep: pd.DataFrame) -> pd.DataFr
         f"{prefix}|Residential|Slum|Floor space",
     ]
 
+    mask = sturm_rep["variable"].isin(exclude)
+
     # - Exclude certain variables.
     # - Convert a name like A|B|C|D to A|Residential and Commercial|D.
+    # - Sum, e.g. over the discarded B|C dimensions.
     data = (
-        sturm_rep[~sturm_rep["variable"].isin(exclude)]
+        sturm_rep[~mask]
         .reset_index(drop=True)
         .assign(
             variable=lambda df: df["variable"].str.replace(
@@ -343,13 +347,10 @@ def report3(scenario: message_ix.Scenario, sturm_rep: pd.DataFrame) -> pd.DataFr
                 regex=True,
             )
         )
+        .pipe(sum_on, "node", "variable", "unit", "year")
     )
 
-    # Sum of residential and commercial, i.e. omitting the discarded "B|C" dimensions
-    sum_1 = data.groupby(["node", "variable", "unit", "year"]).sum().reset_index()
-    data = pd.concat([data, sum_1], ignore_index=True)
-
-    # Global total, i.e. omitting "node"
-    _add_global_total(data)
+    # Reassemble; compute global total
+    data = pd.concat([sturm_rep, data], ignore_index=True).pipe(add_global_total)
 
     return data
