232046f75fabf1d21433d5d0e104817ced7dcb54
Florian Maczek <macflo@gmx.at>
2022-08-22 22:50:07 +0200

    Add methanol demand

--- message_ix_models/model/material/data_methanol.py
+++ message_ix_models/model/material/data_methanol.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from message_ix_models import ScenarioInfo
 from message_ix import make_df
 from message_ix_models.util import broadcast, same_node
-from .util import read_config
+from util import read_config
 
 
 def gen_data_methanol(scenario):
@@ -35,7 +35,15 @@ def gen_data_methanol(scenario):
             new_dict2[i] = dict3[i]
         if ~(i in dict3.keys()) & (i in new_dict.keys()):
             new_dict2[i] = new_dict[i]
-
+    # still contains ref_activity parameter! remove!
+    df = pd.read_excel(context.get_local_path("material", "methanol demand.xlsx"), sheet_name="methanol_demand")
+    df = df[(~df["Region"].isna()) & (df["Region"] != "World")]
+    df = df.dropna(axis=1)
+    df_melt = df.melt(id_vars=["Region"], value_vars=df.columns[5:], var_name="year")
+    df_final = message_ix.make_df("demand", unit="Mt", level="demand", value=df_melt.value, time="year",
+                                  commodity="methanol", year=df_melt.year, node=df_melt["Region"])
+    df_final["value"] = df_final["value"].apply(lambda x: x * 0.75)
+    new_dict2["demand"] = df_final
     return new_dict2
 
 
