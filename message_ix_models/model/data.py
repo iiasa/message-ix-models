e13322613f6b4f80ca29e83a214f92f12fd6218b
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-09-29 17:44:19 +0200

    Reference Config class settings in .model.bare

--- message_ix_models/model/data.py
+++ message_ix_models/model/data.py
@@ -5,7 +5,7 @@ log = logging.getLogger(__name__)
 
 def get_data(scenario, context, spec, **options):
     """Data for the bare RES."""
-    if context.res_with_dummies:
+    if context.model.res_with_dummies:
         log.warning("get_dummy_data() not migrated")
         # return get_dummy_data(scenario, spec)
     else:
