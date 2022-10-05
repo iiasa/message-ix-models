20afe3862058dadaadf2c50a83498a00a1ffc461
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-10-05 12:48:13 +0200

    Update .test_navigate.test_generate_workflow

--- message_ix_models/tests/project/test_navigate.py
+++ message_ix_models/tests/project/test_navigate.py
@@ -1,24 +1,39 @@
 import re
 
-from message_data.projects.navigate import workflow
 
+def test_generate_workflow(mix_models_cli):
+    """Test :func:`.navigate.workflow.generate` and associated CLI."""
 
-def test_generate_workflow(test_context):
-    # Code runs
-    wf = workflow.generate(test_context)
+    result = mix_models_cli.invoke(
+        [
+            "navigate",
+            "--scenario=NPi-act",
+            "run",
+            "--from=M built",
+            "--dry-run",
+            "report",
+        ]
+    )
 
     # Workflow has the expected scenarios in it
     assert re.match(
-        r"""'FINAL':
-- <Step <function report at [^>]*>>
-- 'MESSAGEix-GLOBIOM 1.1-BMT-R12 \(NAVIGATE\)/baseline':
-  - <Step <function build_buildings at [^>]*>>
-  - 'MESSAGEix-GLOBIOM 1.1-MT-R12 \(NAVIGATE\)/baseline':
-    - <Step <function build_transport at [^>]*>>
-    - 'MESSAGEix-Materials/baseline_DEFAULT_NAVIGATE_test':
-      - <Step <function build_materials at [^>]*>>
-      - 'Base/base':
-        - <Step <function base_scenario at [^>]*>>
+        r""".* Execute workflow:
+'report':
+- <Step report\(\)>
+- 'context':
+  - <Context object at \w+ with \d+ keys>
+- 'BMT solved':
+  - <Step build_buildings\(\) -> MESSAGEix-GLOBIOM 1.1-BMT-R12 \(NAVIGATE\)/NPi-act>
+  - 'context' \(above\)
+  - 'MT solved':
+    - <Step solve\(\)>
+    - 'context' \(above\)
+    - 'MT built':
+      - <Step build_transport\(\) -> MESSAGEix-GLOBIOM 1.1-MT-R12 \(NAVIGATE\)/NPi-act>
+      - 'context' \(above\)
+      - 'M built':
+        - <Step load -> MESSAGEix-Materials/baseline_DEFAULT_NAVIGATE>
+        - 'context' \(above\)
         - None""",
-        wf._computer.describe("FINAL"),
-    )
+        result.output,
+    ), result.output
