0128e7d82eb25101eb4ee615cbedfe4ff65210f8
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-10-06 13:52:09 +0200

    Generate all scenarios in the navigate workflow

--- message_ix_models/project/navigate/__init__.py
+++ message_ix_models/project/navigate/__init__.py
@@ -1,1 +1,12 @@
 """NAVIGATE project."""
+
+#: Codes for NAVIGATE T3.5 scenarios. All but "baseline" are abbreviated by removing
+#: "NAV_Dem-".
+SCENARIOS = [
+    "baseline",
+    "NPi-ref",
+    "NPi-act",
+    "NPi-tec",
+    "NPi-ele",
+    "NPi-all",
+]
