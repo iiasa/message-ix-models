56ea9f1a4bea4bcb76c264348fc8835a0aef62ce
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-10-09 17:44:40 +0200

    Update workflow tests

--- message_ix_models/tests/test_workflow.py
+++ message_ix_models/tests/test_workflow.py
@@ -1,15 +1,19 @@
+import re
+
+import pytest
 from message_ix import make_df
 
 from message_ix_models import Workflow, testing
+from message_ix_models.workflow import WorkflowStep, solve
 
 
-def changes_a(s):
+def changes_a(c, s):
     """Change a scenario by modifying structure data, but not data."""
     with s.transact():
         s.add_set("technology", "test_tech")
 
 
-def changes_b(s):
+def changes_b(c, s, value=None):
     """Change a scenario by modifying parameter data, but not structure."""
     with s.transact():
         s.add_par(
@@ -19,29 +23,36 @@ def changes_b(s):
                 node_loc=s.set("node")[0],
                 year_vtg=s.set("year")[0],
                 technology="test_tech",
-                value=100.0,
+                value=value,
                 unit="y",
             ),
         )
 
 
-def test_workflow(request, test_context):
-    def base_scenario(arg):
-        """Generate a base scenario."""
-        return testing.bare_res(request, test_context, solved=False)
+class TestWorkflowStep:
+    def test_repr(self):
+        assert "<Step load>" == repr(WorkflowStep(None))
+
+
+def test_workflow(caplog, request, test_context):
+    base_scenario = testing.bare_res(request, test_context, solved=False)
+    caplog.clear()
 
     # Create the workflow
     wf = Workflow(test_context)
 
     # Model/base is created from nothing by calling base_scenario
-    wf.add("Model/base", None, base_scenario)
+    wf.add_step("Model/base", None, target=base_scenario.url)
     # Model/A is created from Model/base by calling changes_a
-    wf.add("Model/A", "Model/base", changes_a)
+    wf.add_step("Model/A", "Model/base", changes_a)
     # Model/B is created from Model/A by calling changes_b
-    wf.add("Model/B", "Model/A", changes_b)
+    wf.add_step("Model/B", "Model/A", changes_b, value=100.0)
+
+    wf.add_step("B solved", "Model/B", solve)
 
     # Trigger the creation and solve of Model/B and all required precursor scenarios
-    s = wf.run("Model/B")
+    s = wf.run("B solved")
+    print(s)
 
     # Scenario contains changes from the first and second step
     assert "test_tech" in set(s.set("technology"))
@@ -49,14 +60,25 @@ def test_workflow(request, test_context):
     # Scenario was solved
     assert s.has_solution()
 
-    # Same test, except with solve=False
-    wf = Workflow(test_context, solve=False)
-    wf.add("Model/base", None, base_scenario)
-    wf.add("Model/A", "Model/base", changes_a)
-    wf.add("Model/B", "Model/A", changes_b)
-    s = wf.run("Model/B")
+    mp = "message-ix-models"
+    m = "MESSAGEix-GLOBIOM R14 YB"
+    messages = [
+        f"Loaded ixmp://{mp}/{m}/test_workflow#1",
+        f"Step runs on ixmp://{mp}/{m}/test_workflow#1",
+        "  with context.dest_scenario={}",
+        "Execute <function changes_a at [^>]*>",
+        f"…nothing returned, continue with {m}/test_workflow#1",
+        f"Step runs on ixmp://{mp}/{m}/test_workflow#1",
+        "  with context.dest_scenario={}",
+        "Execute <function changes_b at [^>]*>",
+        f"…nothing returned, continue with {m}/test_workflow#1",
+        f"Step runs on ixmp://{mp}/{m}/test_workflow#1",
+        "  with context.dest_scenario={}",
+        "Execute <function solve at [^>]*>",
+    ]
+    for expr, message in zip(messages, caplog.messages[1:]):
+        assert re.match(expr, message)
 
-    # Same assertions, except scenario was NOT solved
-    assert "test_tech" in set(s.set("technology"))
-    assert 1 == len(s.par("technical_lifetime"))
-    assert not s.has_solution()
+    # Now truncate the workflow at "Model/A"
+    with pytest.raises(RuntimeError, match="Unable to locate platform info for"):
+        wf.truncate("Model/A")
