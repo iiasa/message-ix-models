362ec0006f129e1eead8332b9fe7a53101f9a259
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-10-25 16:07:11 +0200

    Exclude "baseline" from navigate workflow; prefer npi-ref

--- message_ix_models/project/navigate/__init__.py
+++ message_ix_models/project/navigate/__init__.py
@@ -2,6 +2,9 @@
 
 #: Codes for NAVIGATE T3.5 scenarios. All but "baseline" are abbreviated by removing
 #: "NAV_Dem-".
+#:
+#: .. todo:: read these directly from the codelist from the official NAVIGATE workflow
+#:    repo
 SCENARIOS = [
     "baseline",
     "NPi-ref",
