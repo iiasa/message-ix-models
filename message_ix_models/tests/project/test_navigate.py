61e05c21c5e20328b381fa2f459c4f63712b69eb
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-11-09 14:55:56 +0100

    Update test of "mix-models navigate run"

--- message_ix_models/tests/project/test_navigate.py
+++ message_ix_models/tests/project/test_navigate.py
@@ -1,5 +1,59 @@
 import re
 
+# Chunks of text to look for in the --dry-run output. The text cannot be matched exactly
+# because the order of traversing the graph is non-deterministic, i.e. which step
+# displays "MT solved" and its subtree may vary.
+context = r"'context' \(above\)"
+BLOCKS = [
+    "Truncate workflow at 'M built'",
+    rf"""
+    - 'MT solved':
+      - <Step solve\(\)>
+      - {context}
+      - 'MT built':
+        - <Step build_transport\(\) -> MESSAGEix-GLOBIOM 1.1-MT-R12 \(NAVIGATE\)/baseline>
+        - {context}
+        - 'M built':
+          - <Step load -> MESSAGEix-Materials/baseline_DEFAULT_NAVIGATE>
+          - {context}
+          - None""",  # noqa: E501
+    r"""
+- 'report NPi-act':
+  - <Step report\(\)>
+  - 'context'.*
+  - 'BMT NPi-act solved':
+    - <Step build_buildings\(\) -> MESSAGEix-GLOBIOM 1.1-BMT-R12 \(NAVIGATE\)/NPi-act>
+    """,
+    r"""
+- 'report NPi-all':
+  - <Step report\(\)>
+  - 'context'.*
+  - 'BMT NPi-all solved':
+    - <Step build_buildings\(\) -> MESSAGEix-GLOBIOM 1.1-BMT-R12 \(NAVIGATE\)/NPi-all>
+    """,
+    r"""
+- 'report NPi-ele':
+  - <Step report\(\)>
+  - 'context'.*
+  - 'BMT NPi-ele solved':
+    - <Step build_buildings\(\) -> MESSAGEix-GLOBIOM 1.1-BMT-R12 \(NAVIGATE\)/NPi-ele>
+    """,
+    r"""
+- 'report NPi-ref':
+  - <Step report\(\)>
+  - 'context'.*
+  - 'BMT NPi-ref solved':
+    - <Step build_buildings\(\) -> MESSAGEix-GLOBIOM 1.1-BMT-R12 \(NAVIGATE\)/NPi-ref>
+    """,
+    r"""
+- 'report NPi-tec':
+  - <Step report\(\)>
+  - 'context'.*
+  - 'BMT NPi-tec solved':
+    - <Step build_buildings\(\) -> MESSAGEix-GLOBIOM 1.1-BMT-R12 \(NAVIGATE\)/NPi-tec>
+    """,
+]
+
 
 def test_generate_workflow(mix_models_cli):
     """Test :func:`.navigate.workflow.generate` and associated CLI."""
@@ -7,33 +61,13 @@ def test_generate_workflow(mix_models_cli):
     result = mix_models_cli.invoke(
         [
             "navigate",
-            "--scenario=NPi-act",
             "run",
             "--from=M built",
             "--dry-run",
-            "report",
+            "report all",
         ]
     )
 
     # Workflow has the expected scenarios in it
-    assert re.match(
-        r""".* Execute workflow:
-'report':
-- <Step report\(\)>
-- 'context':
-  - <Context object at \w+ with \d+ keys>
-- 'BMT solved':
-  - <Step build_buildings\(\) -> MESSAGEix-GLOBIOM 1.1-BMT-R12 \(NAVIGATE\)/NPi-act>
-  - 'context' \(above\)
-  - 'MT solved':
-    - <Step solve\(\)>
-    - 'context' \(above\)
-    - 'MT built':
-      - <Step build_transport\(\) -> MESSAGEix-GLOBIOM 1.1-MT-R12 \(NAVIGATE\)/NPi-act>
-      - 'context' \(above\)
-      - 'M built':
-        - <Step load -> MESSAGEix-Materials/baseline_DEFAULT_NAVIGATE>
-        - 'context' \(above\)
-        - None""",
-        result.output,
-    ), result.output
+    for b in BLOCKS:
+        assert re.search(b, result.output, flags=re.DOTALL), result.output
