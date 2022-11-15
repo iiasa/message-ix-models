fc6727193dd1fa69c0afde68be7e1eec3216a0c6
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-11-15 22:06:08 +0100

    Read navigate scenario info from workflow repo

--- message_ix_models/project/navigate/__init__.py
+++ message_ix_models/project/navigate/__init__.py
@@ -1,15 +1,67 @@
 """NAVIGATE project."""
+import logging
+from pathlib import Path
+from typing import Generator, Mapping, Optional
 
-#: Codes for NAVIGATE T3.5 scenarios. All but "baseline" are abbreviated by removing
-#: "NAV_Dem-".
-#:
-#: .. todo:: read these directly from the codelist from the official NAVIGATE workflow
-#:    repo
-SCENARIOS = [
-    "baseline",
-    "NPi-ref",
-    "NPi-act",
-    "NPi-tec",
-    "NPi-ele",
-    "NPi-all",
+import yaml
+from message_ix_models.util import as_codes
+from sdmx.model import Annotation, Code
+
+log = logging.getLogger(__name__)
+
+EXTRA_SCENARIOS = [
+    Code(
+        id="NAV_Dem-20C-ref",
+        annotations=[
+            Annotation(id="navigate-climate-policy", text="20C"),
+            Annotation(id="navigate-T3.5-policy", text=""),
+            Annotation(id="navigate-task", text="T3.5"),
+        ],
+    ),
 ]
+
+
+def iter_scenario_codes(
+    context, filters: Optional[Mapping] = None
+) -> Generator[Code, None, None]:
+    """Iterate over the scenarios defined in the ``navigate-workflow`` repository.
+
+    Parameters
+    ----------
+    filters : dict (str -> any), optional
+        Only codes with annotations that match these filters are returned.
+    """
+    filters = filters or dict()
+
+    workflow_dir = Path("~/vc/iiasa/navigate-workflow").expanduser()
+
+    # Use the particular path scheme of Scenario Explorer config repositories
+    path = workflow_dir.joinpath("definitions", "scenario", "scenarios.yaml")
+    log.info(f"Read scenarios from {path}")
+    with open(path) as f:
+        _content = yaml.safe_load(f)
+
+    # Transform into a form intelligible by as_codes()
+    content = {}
+    for item in _content:
+        if isinstance(item, str):
+            content[item] = Code(id=item, name=item)
+        else:
+            content.update(item)
+
+    # Transform to a list of SDMX codes
+    codes = as_codes(content)
+
+    # Yield codes whose annotations match `filters`
+    for code in codes + EXTRA_SCENARIOS:
+        match = True
+        for key, value in filters.items():
+            try:
+                if str(code.get_annotation(id=key).text) != value:
+                    match = False
+                    break
+            except KeyError:
+                match = False
+                break
+        if match:
+            yield code
