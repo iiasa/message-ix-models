425a2c5bb954732a1d16209a793715b1bb65a397
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2022-11-16 16:05:11 +0100

    Set barcrossalg=2 for navigate climate policy scenarios
    - move climate_policy from .navigate.workflow to .navigate.__init__

--- message_ix_models/project/navigate/__init__.py
+++ message_ix_models/project/navigate/__init__.py
@@ -7,13 +7,56 @@ import yaml
 from message_ix_models.util import as_codes
 from sdmx.model import Annotation, Code
 
+from message_data.projects.engage.workflow import PolicyConfig
+
 log = logging.getLogger(__name__)
+
+#: Values from engage/config.yaml, expressed as objects.
+#:
+#: .. todo:: The low_dem_scen values appear to form a "waterfall", with each
+#:    successively lower budget referencing the previous. Investigate how to run only
+#:    some budgets without the previous ones.
+_so = dict(model="MESSAGE", solve_options=dict(barcrossalg=2))
+CLIMATE_POLICY = {
+    pc.label: pc
+    for pc in (
+        # No climate policy
+        PolicyConfig("NPi", steps=[], solve=_so),
+        # Only step 1. This value does not appear in the official NAVIGATE scenarios
+        # list, but is used in EXTRA_SCENARIOS below.
+        PolicyConfig(
+            "1000 Gt",
+            steps=[1],
+            budget=2449,
+            low_dem_scen="EN_NPi2020_1200_step1",
+            solve=_so,
+        ),
+        # All steps 1–3
+        # From an entry labelled "1000" in engage/config.yaml
+        PolicyConfig(
+            "20C", budget=2449, low_dem_scen="EN_NPi2020_1200_step1", solve=_so
+        ),
+        # From an entry labelled "600" in engage/config.yaml
+        PolicyConfig(
+            "15C", budget=1288, low_dem_scen="EN_NPi2020_700_step1", solve=_so
+        ),
+    )
+}
+
 
 EXTRA_SCENARIOS = [
     Code(
         id="NAV_Dem-20C-ref",
         annotations=[
             Annotation(id="navigate-climate-policy", text="20C"),
+            Annotation(id="navigate-T3.5-policy", text=""),
+            Annotation(id="navigate-task", text="T3.5"),
+        ],
+    ),
+    Code(
+        id="NAV_Dem-1000 Gt-ref",
+        annotations=[
+            Annotation(id="navigate-climate-policy", text="1000 Gt"),
             Annotation(id="navigate-T3.5-policy", text=""),
             Annotation(id="navigate-task", text="T3.5"),
         ],
