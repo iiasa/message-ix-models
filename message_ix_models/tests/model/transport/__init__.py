09a5bf79a7a5f605b69a070d6e83e940fed88423
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-02-20 16:53:27 +0100

    Adjust tests of .transport.ldv

--- message_ix_models/tests/model/transport/__init__.py
+++ message_ix_models/tests/model/transport/__init__.py
@@ -4,6 +4,7 @@ from message_data.model.transport import Config, build
 
 
 def configure_build(context, regions, years):
+    # FIXME this overlaps with test_demand.demand_computer; combine
     context.update(regions=regions, years=years)
 
     Config.from_context(context)
