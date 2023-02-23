b1775f393af3d3a147722a234de26f5706c7e029
Florian Maczek <macflo@gmx.at>
2023-02-23 14:53:35 +0100

    Feat(utils): add dictionary merge util func

--- message_ix_models/model/material/util.py
+++ message_ix_models/model/material/util.py
@@ -1,6 +1,7 @@
 from message_ix_models import Context
 from pathlib import Path
 from message_ix_models.util import load_private_data
+import pandas as pd
 
 # Configuration files
 METADATA = [
@@ -59,3 +60,11 @@ def prepare_xlsx_for_explorer(filepath):
     df = df[~df["Region"].isna()]
     df["Region"] = df["Region"].map(add_R12)
     df.to_excel(filepath, index=False)
+
+
+def combine_df_dictionaries(*args):
+    keys = set([key for tup in args for key in tup])
+    comb_dict = {}
+    for i in keys:
+        comb_dict[i] = pd.concat([j.get(i) for j in args])
+    return comb_dict
