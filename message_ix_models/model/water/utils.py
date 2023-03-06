01b00469998b23c0a5250c4ae284925ce8a548ba
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-03-06 14:04:39 +0100

    Update .water.utils.map_yv_ya_lt with code from message_ix
    Silences mypy errors.

--- message_ix_models/model/water/utils.py
+++ message_ix_models/model/water/utils.py
@@ -1,3 +1,4 @@
+import logging
 from collections import defaultdict
 from functools import lru_cache
 from itertools import product
@@ -11,6 +12,8 @@ from sdmx.model import Code
 from message_ix_models import Context
 from message_ix_models.model.structure import get_codes
 from message_ix_models.util import load_private_data
+
+log = logging.getLogger(__name__)
 
 # Configuration files
 METADATA = [
@@ -133,29 +136,25 @@ def map_yv_ya_lt(
         matched set of labels for those dimensions.
     lt : int, lifetime
     """
-    # commented: currently unused, this does the same as the line below, using (start
-    # period, final period, uniform ``duration_period). The intermediate periods are
-    # inferred
-    # _s = slice(periods_or_start, end + 1, dp)
-    # data = np.mgrid[_s, _s]
-
     if not ya:
         ya = periods[0]
-        print(("first active year set as"), ya)
+        log.info(f"First active year set as {ya!r}")
     if not lt:
         raise ValueError("Add a fixed lifetime parameter 'lt'")
 
-    # Create a mesh grid using numpy built-ins
-    data = np.meshgrid(periods, periods, indexing="ij")
-    # Take the upper-triangular portion (setting the rest to 0), reshape
-    data = np.triu(data).reshape((2, -1))
+    # The following lines are the same as
+    # message_ix.tests.test_feature_vintage_and_active_years._generate_yv_ya
+
+    # - Create a mesh grid using numpy built-ins
+    # - Take the upper-triangular portion (setting the rest to 0)
+    # - Reshape
+    data = np.triu(np.meshgrid(periods, periods, indexing="ij")).reshape((2, -1))
     # Filter only non-zero pairs
     df = pd.DataFrame(
-        filter(lambda x: sum(x) > 0, list(zip(data[0, :], data[1, :]))),
+        filter(sum, zip(data[0, :], data[1, :])),
         columns=["year_vtg", "year_act"],
         dtype=np.int64,
     )
 
-    df = df[df.year_act >= ya]
-    df = df[(df.year_act - df.year_vtg) <= lt]
-    return df
+    # Select values using the `ya` and `lt` parameters
+    return df[(ya <= df.year_act) & (df.year_act - df.year_vtg <= lt)]
