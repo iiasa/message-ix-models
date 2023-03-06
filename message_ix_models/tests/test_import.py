a1181645f692e9f1d69508cddd436e88c3769c42
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-03-06 15:49:58 +0100

    Expand test_import.py, mirroring message_data

--- message_ix_models/tests/test_import.py
+++ message_ix_models/tests/test_import.py
@@ -1,3 +1,25 @@
-def test_import():
-    """Test that the package can be imported."""
-    import message_ix_models  # noqa: F401
+from importlib import import_module
+
+import pytest
+
+MODULES_WITHOUT_TESTS = [
+    None,
+    "model.water",
+    "model.water.build",
+    "model.water.data",
+    "model.water.reporting",
+]
+
+
+@pytest.mark.parametrize("name", MODULES_WITHOUT_TESTS)
+def test_import(name):
+    """Check that modules can be imported.
+
+    Modules **should** have specific tests that actually run code. Where those tests are
+    not yet written, this test checks that the modules can at least be imported without
+    error.
+
+    Once tests are written for each module, remove it from the above list.
+    """
+    full_name = ".".join(filter(None, ["message_ix_models", name]))
+    import_module(full_name)
