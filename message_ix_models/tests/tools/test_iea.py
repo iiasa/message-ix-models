ee19ef19026c997a22140f54cf4ce9e8f9cb95da
Measrainsey Meng <measrainsey@pm.me>
2023-05-12 14:20:43 +0200

    Rename function

--- message_ix_models/tests/tools/test_iea.py
+++ message_ix_models/tests/tools/test_iea.py
@@ -1,5 +1,5 @@
-from message_ix_models.tools.iea.weo import get_data
+from message_ix_models.tools.iea.weo import get_weo_data
 
 
-def test_get_data():
-    get_data()
+def test_get_weo_data():
+    get_weo_data()
