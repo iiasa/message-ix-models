7ea962552f1c6bc96848d569a09670fa913bb2df
Measrainsey Meng <measrainsey@pm.me>
2023-05-15 13:59:44 +0200

    Add dictionaries of weo regions and technologies

--- message_ix_models/tools/iea/weo.py
+++ message_ix_models/tools/iea/weo.py
@@ -28,7 +28,7 @@ def get_weo_data():
         "gas_turbine": ["Gas", 15],
         "ccgt_chp": ["Gas", 25],
         "fuel_cell": ["Gas", 35],
-        "coal_ccs": ["Fossil fuels equipped with CCUS", 5],
+        "pulverized_coal_ccs": ["Fossil fuels equipped with CCUS", 5],
         "igcc_ccs": ["Fossil fuels equipped with CCUS", 15],
         "ccgt_ccs": ["Fossil fuels equipped with CCUS", 25],
         "nuclear": ["Nuclear", 5],
@@ -92,3 +92,89 @@ def get_weo_data():
     all_cost_df = pd.concat(dfs_cost)
 
     return all_cost_df
+
+
+"""
+Match each R11 region with a WEO region
+"""
+dict_weo_r11 = {
+    "NAM": "United States",
+    "LAM": "Brazil",
+    "WEU": "Europe",
+    "EEU": "Russia",
+    "FSU": "Russia",
+    "AFR": "Africa",
+    "MEA": "Middle East",
+    "SAS": "India",
+    "CPA": "China",
+    "PAS": "India",
+    "PAO": "Japan",
+}
+
+
+"""
+Match each MESSAGEix technology with a WEO technology
+"""
+dict_weo_technologies = {
+    "coal_ppl": "steam_coal_subcritical",
+    "gas_ppl": "gas_turbine",
+    "gas_ct": "gas_turbine",
+    "gas_cc": "ccgt",
+    "bio_ppl": "bioenergy_large",
+    "coal_adv": "steam_coal_supercritical",
+    "igcc": "igcc",
+    "bio_istig": "igcc",
+    "coal_adv_ccs": "pulverized_coal_ccs",
+    "igcc_ccs": "igcc_ccs",
+    "gas_cc_ccs": "ccgt_ccs",
+    "bio_istig_ccs": "igcc_ccs",
+    "syn_liq": "igcc",
+    "meth_coal": "igcc",
+    "syn_liq_ccs": "igcc_ccs",
+    "meth_coal_ccs": "igcc_ccs",
+    "h2_coal": "igcc",
+    "h2_smr": "igcc",
+    "h2_bio": "igcc",
+    "h2_coal_ccs": "igcc_ccs",
+    "h2_smr_ccs": "igcc_ccs",
+    "h2_bio_ccs": "igcc_ccs",
+    "eth_bio": "igcc",
+    "eth_bio_ccs": "igcc_ccs",
+    "c_ppl_co2scr": "pulverized_coal_ccs",
+    "g_ppl_co2scr": "ccgt_ccs",
+    "bio_ppl_co2scr": "igcc_ccs",
+    "wind_ppl": "wind_onshore",
+    "wind_ppf": "wind_offshore",
+    "solar_th_ppl": "csp",
+    "solar_pv_I": "solarpv_buildings",
+    "solar_pv_RC": "solarpv_buildings",
+    "solar_pv_ppl": "solarpv_large",
+    "geo_ppl": "geothermal",
+    "hydro_lc": "hydropower_large",
+    "hydro_hc": "hydropower_small",
+    "meth_ng": "igcc",
+    "meth_ng_ccs": "igcc_ccs",
+    "coal_ppl_u": "steam_coal_subcritical",
+    "stor_ppl": "",
+    "h2_elec": "",
+    "liq_bio": "igcc",
+    "liq_bio_ccs": "igcc_ccs",
+    "coal_i": "ccgt_chp",
+    "foil_i": "ccgt_chp",
+    "loil_i": "ccgt_chp",
+    "gas_i": "ccgt_chp",
+    "biomass_i": "bioenergy_medium_chp",
+    "eth_i": "bioenergy_medium_chp",
+    "meth_i": "bioenergy_medium_chp",
+    "elec_i": "ccgt_chp",
+    "h2_i": "ccgt_chp",
+    "hp_el_i": "ccgt_chp",
+    "hp_gas_i": "ccgt_chp",
+    "solar_i": "solarpv_buildings",
+    "heat_i": "ccgt_chp",
+    "geo_hpl": "geothermal",
+    "nuc_lc": "nuclear",
+    "nuc_hc": "nuclear",
+    "csp_sm1_ppl": "csp",
+    "csp_sm3_ppl": "csp",
+}
