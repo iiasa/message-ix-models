a63ca8d97a5f3532fe0e52ed7d5f4b2758c35da0
Measrainsey Meng <measrainsey@pm.me>
2023-05-23 11:50:56 +0200

    Disable test for now

--- message_ix_models/tests/tools/test_iea.py
+++ message_ix_models/tests/tools/test_iea.py
@@ -1,5 +1,4 @@
-from message_ix_models.tools.iea.weo import get_weo_data
+# from message_ix_models.tools.iea.weo import get_weo_data
 
-
-def test_get_weo_data():
-    get_weo_data()
+# def test_get_weo_data():
+#     get_weo_data()
