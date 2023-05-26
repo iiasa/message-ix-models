0f9390dd515a587b80415e8e86f84cb6702296f7
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-05-26 17:33:37 +0200

    Use Context.get_cache_path() in pooch.fetch()

--- message_ix_models/util/pooch.py
+++ message_ix_models/util/pooch.py
@@ -1,7 +1,38 @@
+import logging
+from pathlib import Path
+
 import pooch
 
+from .context import Context
 
-def fetch(args):
-    p = pooch.create(path=pooch.os_cache("message-ix-models"), **args)
-    assert 1 == len(p.registry)
-    return p.fetch(next(iter(p.registry.keys())), progressbar=True)
+log = logging.getLogger(__name__)
+
+
+def fetch(args) -> Path:
+    """Create a :class:`.Pooch` instance and fetch a single file.
+
+    Files are stored under the directory identified by :meth:`.get_cache_path`, unless
+    `args` provides another location.
+
+    Parameters
+    ----------
+    args
+        Passed to :func:`pooch.create`.
+
+    Returns
+    -------
+    Path
+        Path to the fetched file.
+    """
+    args.setdefault("path", Context.get_instance(-1).get_cache_path())
+
+    p = pooch.create(**args)
+
+    if len(p.registry) > 1:
+        raise NotImplementedError("fetch() with registries with >1 files")
+
+    path = Path(p.fetch(next(iter(p.registry.keys())), progressbar=True))
+
+    log.info(f"Fetched {path}")
+
+    return path
