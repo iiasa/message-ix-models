24ec50c507238ea1c9f635fb7095f0429477997c
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-06-13 15:49:12 +0200

    Add .model.macro.generate()

---
+++ message_ix_models/model/macro.py
@@ -0,0 +1,108 @@
+"""MACRO utilities."""
+import logging
+from functools import lru_cache
+from itertools import product
+from typing import TYPE_CHECKING, List, Literal, Optional
+
+import pandas as pd
+from sdmx.model import Code
+
+from message_ix_models.model.bare import get_spec
+from message_ix_models.util import nodes_ex_world
+
+if TYPE_CHECKING:
+    from message_ix_models import Context
+
+log = logging.getLogger(__name__)
+
+#: Default set of commodities to include.
+COMMODITY = ["i_therm", "i_spec", "rc_spec", "rc_therm", "transport"]
+
+
+def generate(
+    parameter: Literal["aeei", "config", "depr", "drate", "lotol"],
+    context: "Context",
+    commodities: List[str] = COMMODITY,
+    value: Optional[float] = None,
+) -> pd.DataFrame:
+    """Generate uniform data for one :mod:`message_ix.macro` `parameter`.
+
+    :meth:`message_ix.Scenario.add_macro` expects as its `data` parameter a dictionary
+    mapping certain MACRO parameter names (or the special name "config") to
+    :class:`.pandas.DataFrame`. This function generates data for those data frames.
+
+    For the particular dimensions:
+
+    - "node": All nodes in the node code list given by :func:`.nodes_ex_world`.
+    - "year": All periods from the period *before* the first model year.
+    - "commodity": The elements of `commodities`.
+    - "sector": If each entry of `commodities` is a :class:`.Code` and has an annotation
+      with id="macro-sector", the given value. Otherwise, the same as `commodity`.
+
+    `value` supplies the parameter value, which is the same for all observations.
+    The labels level="useful" and unit="-" are fixed.
+
+    Parameters
+    ----------
+    parameter : str
+        MACRO parameter for which to generate data.
+    context
+        Used with :func:`.bare.get_spec`.
+    commodities : list of str or Code
+        Commodities to include in the MESSAGE-MACRO linkage.
+    value : float
+        Parameter value.
+
+    Returns
+    -------
+    pandas.DataFrame
+        The columns vary according to `parameter`:
+
+        - "aeei": node, year, sector, value, unit.
+        - "depr", "drate", or "lotol": node, value, unit.
+        - "config": node, sector, commodity, level, year.
+    """
+    spec = get_spec(context)
+
+    if isinstance(commodities[0], Code):
+        c_codes = commodities
+    else:
+        c_codes = spec.add.set["commodity"]
+
+    @lru_cache
+    def _sector(commodity: str) -> str:
+        try:
+            idx = c_codes.index(commodity)
+        except ValueError as e:
+            log.info(e)
+            result = commodity
+        else:
+            result = c_codes[idx].eval_annotation(id="macro-sector")
+        return result or commodity
+
+    # AEEI data must begin from the period before the first model period
+    ym1 = spec.add.set["year"].index(spec.add.y0) - 1
+    iterables = dict(
+        commodity=commodities,
+        level=["useful"],
+        node=nodes_ex_world(spec.add.N),
+        sector=map(_sector, commodities),
+        year=spec.add.set["year"][ym1:],
+    )
+
+    if parameter == "aeei":
+        dims = ["node", "year", "sector"]
+    elif parameter == "config":
+        dims = ["node", "sector", "commodity", "level", "year"]
+        assert value is None
+    elif parameter in ("depr", "drate", "lotol"):
+        dims = ["node"]
+    else:
+        raise NotImplementedError(f"generate(…) for MACRO parameter {parameter!r}")
+
+    result = pd.DataFrame(
+        [tuple(values) for values in product(*[iterables[d] for d in dims])],
+        columns=dims,
+    )
+
+    return result if parameter == "config" else result.assign(value=value, unit="-")
