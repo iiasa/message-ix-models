2f2b34f76654c9a5435472082c3655ed0e456a13
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-07-04 23:14:58 +0200

    Streamline transport plots using convenience methods

--- message_ix_models/model/transport/plot.py
+++ message_ix_models/model/transport/plot.py
@@ -34,131 +34,159 @@ class Plot(BasePlot):
     # Output goes in the "transport" subdirectory
     path = ["transport"]
 
+    #: 'Static' geoms: list of plotnine objects that are not dynamic
     static = [p9.theme(figure_size=(11.7, 8.3))]
 
-    def title(self, value):
+    #: Fixed plot title string. If not given, the first line of the class docstring is
+    #: used.
+    title = None
+
+    #: Units expression for plot title.
+    unit = None
+
+    def ggtitle(self, value=None):
         """Return :class:`plotnine.ggtitle` including the current date & time."""
-        return p9.ggtitle(f"{value} ({datetime.now().isoformat(timespec='minutes')})")
+        title_pieces = [
+            (self.title or self.__doc__).splitlines()[0].rstrip("."),
+            f"[{self.unit}]" if self.unit else None,
+            value,
+            f"({datetime.now().isoformat(timespec='minutes')})",
+        ]
+        return p9.ggtitle(" ".join(filter(None, title_pieces)))
+
+    def groupby_plot(self, data: pd.DataFrame, *args):
+        """Combination of groupby and ggplot().
+
+        Groups by `args` and yields a series of :class:`plotnine.ggplot` objects, one
+        per group, with :attr:`static` geoms and :func:`ggtitle` appended to each.
+        """
+        for group_key, group_df in data.groupby(*args):
+            yield group_key, (
+                p9.ggplot(group_df) + self.static + self.ggtitle(repr(group_key))
+            )
 
 
 class InvCost0(Plot):
+    """All transport investment cost."""
+
     basename = "inv-cost-transport"
     inputs = ["inv_cost:nl-t-yv:transport"]
-    _title_detail = "All transport"
+    static = Plot.static + [
+        p9.aes(x="yv", y="inv_cost", color="t"),
+        p9.geom_line(),
+        p9.geom_point(),
+    ]
 
     def generate(self, data):
         y_max = max(data["inv_cost"])
-        unit = data["unit"].unique()[0]
+        self.unit = data["unit"].unique()[0]
 
-        for nl, group_df in data.groupby("nl"):
-            yield (
-                p9.ggplot(p9.aes(x="yv", y="inv_cost", color="t"), group_df)
-                + p9.geom_line()
-                + p9.geom_point()
-                + p9.expand_limits(y=[0, y_max])
-                + self.title(f"{self._title_detail} investment cost [{unit}] {nl}")
-                + self.static
-            )
+        for _, ggplot in self.groupby_plot(data, "nl"):
+            yield ggplot + p9.expand_limits(y=[0, y_max])
 
 
 class InvCost1(InvCost0):
-    """Same as InvCost0, but for LDV techs only."""
+    """LDV transport investment cost.
+
+    Same as InvCost0, but for LDV techs only.
+    """
 
     basename = "inv-cost-ldv"
     inputs = ["inv_cost:nl-t-yv:ldv"]
-    _title_detail = "LDV transport"
 
 
 class InvCost2(InvCost0):
-    """Same as InvCost0, but for non-LDV techs only."""
+    """Non-LDV transport investment cost.
+
+    Same as InvCost0, but for non-LDV techs only.
+    """
 
     basename = "inv-cost-nonldv"
     inputs = ["inv_cost:nl-t-yv:nonldv"]
-    _title_detail = "Non-LDV transport"
 
 
 class FixCost(Plot):
+    """Fixed cost."""
+
     basename = "fix-cost"
     inputs = ["fix_cost:nl-t-yv-ya:transport all"]
+    static = Plot.static + [
+        p9.aes(x="ya", y="fix_cost", color="t", group="t"),
+        p9.geom_line(),
+        p9.geom_point(),
+    ]
 
     def generate(self, data):
         y_max = max(data["fix_cost"])
-        unit = data["unit"].unique()[0]
+        self.unit = data["unit"].unique()[0]
 
-        for nl, group_df in data.groupby("nl"):
-            yield (
-                p9.ggplot(p9.aes(x="ya", y="fix_cost", color="t", group="t"), group_df)
-                + p9.geom_line()
-                + p9.geom_point()
-                + p9.expand_limits(y=[0, y_max])
-                + self.title(f"Fixed cost [{unit}] {nl}")
-                + self.static
-            )
+        for _, ggplot in self.groupby_plot(data, "nl"):
+            yield ggplot + p9.expand_limits(y=[0, y_max])
 
 
 class VarCost(Plot):
+    """Variable cost."""
+
     basename = "var-cost"
     inputs = ["var_cost:nl-t-yv-ya:transport all"]
+    static = Plot.static + [
+        p9.aes(x="ya", y="var_cost", color="t", group="yv"),
+        p9.geom_line(),
+        p9.geom_point(),
+    ]
 
     def generate(self, data):
         y_max = max(data["var_cost"])
-        unit = data["unit"].unique()[0]
+        self.unit = data["unit"].unique()[0]
 
-        for nl, group_df in data.groupby("nl"):
-            yield (
-                p9.ggplot(p9.aes(x="ya", y="var_cost", color="t", group="yv"), group_df)
-                + p9.geom_line()
-                + p9.geom_point()
-                + p9.expand_limits(y=[0, y_max])
-                + self.title(f"Variable cost [{unit}] {nl}")
-                + self.static
-            )
+        for nl, ggplot in self.groupby_plot(data, "nl"):
+            yield ggplot + p9.expand_limits(y=[0, y_max])
 
 
 class LDV_IO(Plot):
+    """Input efficiency [GWa / km]."""
+
     basename = "ldv-efficiency"
     inputs = ["input:nl-t-yv-ya:transport all"]
+    static = Plot.static + [
+        p9.aes(x="ya", y="input", color="t"),
+        p9.facet_wrap(["nl"], ncol=2, labeller=LabelFirst("node: {}")),
+        p9.geom_line(),
+        p9.geom_point(),
+        p9.labs(x="Period", y="", color="LDV technology"),
+    ]
 
     def generate(self, data):
-        return (
-            p9.ggplot(data, p9.aes(x="ya", y="input", color="t"))
-            + p9.theme(figure_size=(11.7, 8.3))
-            + p9.facet_wrap(["nl"], ncol=2, labeller=LabelFirst("node: {}"))
-            + p9.geom_line()
-            + p9.geom_point()
-            + p9.labs(
-                x="Period",
-                y="Input efficiency [GWa / km]",
-                color="LDV technology",
-            )
-            + self.static
-        )
+        return p9.ggplot(data) + self.static + self.ggtitle()
 
 
 class LDVTechShare0(Plot):
+    """Activity [10⁹ km / y]."""
+
     basename = "ldv-tech-share"
     inputs = ["out:nl-t-ya:transport"]
+    static = Plot.static + [
+        p9.aes(x="ya", y="value", fill="t"),
+        p9.facet_wrap(["nl"], ncol=2, labeller=LabelFirst("node: {}")),
+        p9.geom_bar(stat="identity", width=4),
+        p9.labs(x="Period", y=None, fill="LDV technology"),
+    ]
 
     def generate(self, data):
-        # # DEBUG dump data
-        # data.to_csv(f"{self.basename}.csv")
-
-        return (
-            p9.ggplot(data, p9.aes(x="ya", y="value", fill="t"))
-            + p9.facet_wrap(["nl"], ncol=2, labeller=LabelFirst("node: {}"))
-            + p9.geom_bar(stat="identity", width=4)
-            + p9.labs(
-                x="Period",
-                y="Activity [10⁹ km / y]",
-                fill="LDV technology",
-            )
-            + self.static
-        )
+        return p9.ggplot(data) + self.static + self.ggtitle()
 
 
 class LDVTechShare1(Plot):
+    """Usage of LDV technologies by CG."""
+
     basename = "ldv-tech-share-by-cg"
     inputs = ["out:nl-t-ya-c", "consumer groups"]
+    static = Plot.static + [
+        p9.aes(x="ya", y="value", fill="t"),
+        p9.facet_wrap(["c"], ncol=5),
+        p9.geom_bar(stat="identity", width=4),
+        p9.labs(x="Period", y="Activity [10⁹ km / y]", fill="LDV technology"),
+    ]
 
     def generate(self, data, cg):
         # TODO do these operations in reporting for broader reuse
@@ -176,24 +204,8 @@ class LDVTechShare1(Plot):
             .query(f"c in {list(map(str, cg))}")
         )
 
-        # DEBUG dump data
-        data.to_csv(f"{self.basename}-1.csv")
-        log.info(f"Dumped data to {self.basename}.csv")
-
-        # Select a subset of technologies
-        for nl, group_df in data.groupby("nl"):
-            yield (
-                p9.ggplot(group_df, p9.aes(x="ya", y="value", fill="t"))
-                + p9.facet_wrap(["c"], ncol=5)
-                + p9.geom_bar(stat="identity", width=4)
-                + p9.labs(
-                    x="Period",
-                    y="Activity [10⁹ km / y]",
-                    fill="LDV technology",
-                )
-                + self.title(f"Usage of LDV technologies per CG, {nl}")
-                + self.static
-            )
+        for _, ggplot in self.groupby_plot(data, "nl"):
+            yield ggplot
 
 
 def c_group(df: pd.DataFrame, cg):
@@ -205,37 +217,39 @@ def c_group(df: pd.DataFrame, cg):
 
 
 class DemandCalibrated(Plot):
+    """Transport demand [pass · km / a]."""
+
     basename = "demand"
     inputs = ["demand:n-c-y", "c::transport", "cg"]
+    static = Plot.static + [
+        p9.aes(x="y", y="demand", fill="c_group"),
+        p9.geom_bar(stat="identity", width=4),
+        p9.labs(x="Period", y=None, fill="Transport mode group"),
+    ]
 
     def generate(self, data, commodities, cg):
         # Convert and select data
         data = data.query(f"c in {repr(list(map(str, commodities)))}").pipe(c_group, cg)
-        for node, node_data in data.groupby("n"):
-            yield (
-                p9.ggplot(node_data, p9.aes(x="y", y="demand", fill="c_group"))
-                + p9.geom_bar(stat="identity", width=4)
-                + p9.labs(x="Period", y=None, fill="Transport mode group")
-                + self.title(f"Transport demand [pass · km / a] {node}")
-                + self.static
-            )
+        for _, ggplot in self.groupby_plot(data, "n"):
+            yield ggplot
 
 
 class DemandCalibratedCap(Plot):
+    """Transport demand per capita [km / a]."""
+
     basename = "demand-capita"
     inputs = ["demand:n-c-y:capita", "c::transport", "cg"]
+    static = Plot.static + [
+        p9.aes(x="y", y="value", fill="c"),
+        p9.geom_bar(stat="identity", width=4),
+        p9.labs(x="Period", y=None, fill="Transport mode group"),
+    ]
 
     def generate(self, data, commodities, cg):
         # Convert and select data
         data = data.query(f"c in {repr(list(map(str, commodities)))}").pipe(c_group, cg)
-        for node, group_df in data.groupby("n"):
-            yield (
-                p9.ggplot(p9.aes(x="y", y="value", fill="c"), group_df)
-                + p9.geom_bar(stat="identity", width=4)
-                + p9.labs(x="Period", y=None, fill="Transport mode group")
-                + self.title(f"Transport demand per capita [km / a] {node}")
-                + self.static
-            )
+        for _, ggplot in self.groupby_plot(data, "n"):
+            yield ggplot
 
 
 def _reduce_units(df: pd.DataFrame, target_units) -> Tuple[pd.DataFrame, str]:
@@ -249,76 +263,72 @@ def _reduce_units(df: pd.DataFrame, target_units) -> Tuple[pd.DataFrame, str]:
 
 
 class DemandExo(Plot):
+    """Passenger transport activity."""
+
     basename = "demand-exo"
     inputs = ["pdt:n-y-t"]
+    static = Plot.static + [
+        p9.aes(x="y", y="value", fill="t"),
+        p9.geom_bar(stat="identity", width=4),
+        p9.labs(x="Period", y=None, fill="Mode (tech group)"),
+    ]
 
     def generate(self, data):
         # FIXME shouldn't need to change dtype here
         data = data.astype(dict(value=float))
-        data, unit = _reduce_units(data, "Gp km / a")
+        data, self.unit = _reduce_units(data, "Gp km / a")
         y_max = max(data["value"])
 
-        for n, group_df in data.groupby("n"):
-            yield (
-                p9.ggplot(p9.aes(x="y", y="value", fill="t"), group_df)
-                + p9.geom_bar(stat="identity", width=4)
-                + p9.expand_limits(y=[0, y_max])
-                + p9.labs(x="Period", y=None, fill="Mode (tech group)")
-                + self.title(f"Passenger transport activity [{unit}] {n}")
-                + self.static
-            )
+        for _, ggplot in self.groupby_plot(data, "n"):
+            yield ggplot + p9.expand_limits(y=[0, y_max])
 
 
 class DemandExoCap(Plot):
+    """Passenger transport activity per person."""
+
     basename = "demand-exo-capita"
     inputs = ["transport pdt:n-y-t:capita"]
+    static = Plot.static + [
+        p9.aes(x="y", y="value", fill="t"),
+        p9.geom_bar(stat="identity", width=4),
+        p9.labs(x="Period", y=None, fill="Mode (tech group)"),
+    ]
 
     def generate(self, data):
         # FIXME shouldn't need to change dtype here
         data = data.astype(dict(value=float))
-        data, unit = _reduce_units(data, "Mm / a")
+        data, self.unit = _reduce_units(data, "Mm / a")
         y_max = max(data["value"])
 
-        for n, group_df in data.groupby("n"):
-            yield (
-                p9.ggplot(p9.aes(x="y", y="value", fill="t"), group_df)
-                + p9.geom_bar(stat="identity", width=4)
-                + p9.expand_limits(y=[0, y_max])
-                + p9.labs(x="Period", y=None, fill="Mode (tech group)")
-                + self.title(f"Passenger transport activity per person [{unit}] {n}")
-                + self.static
-            )
+        for _, ggplot in self.groupby_plot(data, "n"):
+            yield ggplot + p9.expand_limits(y=[0, y_max])
 
 
 class EnergyCmdty(Plot):
+    """Energy input to transport [GWa]."""
+
     basename = "energy-by-cmdty"
     inputs = ["in:nl-ya-c:transport all"]
+    static = Plot.static + [
+        p9.aes(x="ya", y="value", fill="c"),
+        p9.geom_bar(stat="identity", width=5, color="black"),
+        p9.labs(x="Period", y="Energy", fill="Commodity"),
+    ]
 
     def generate(self, data):
         # Discard data for certain commodities
         data = data[~(data.c.str.startswith("transport") | (data.c == "disutility"))]
-        unit = "GWa"
 
-        for nl, group_df in data.groupby("nl"):
-            yield (
-                p9.ggplot(p9.aes(x="ya", y="value", fill="c"), group_df)
-                + p9.geom_bar(stat="identity", width=5, color="black")
-                + p9.labs(x="Period", y="Energy", fill="Commodity")
-                + self.title(f"Energy input to transport [{unit}] {nl}")
-                + self.static
-            )
-
-
-# class EmissionsTech(Plot):
-#     basename = "emissions-by-tech"
-#     inputs = ["emi:"]
+        for _, ggplot in self.groupby_plot(data, "nl"):
+            yield ggplot
 
 
 class Stock0(Plot):
+    """LDV transport vehicle stock."""
+
     basename = "stock-ldv"
     # Partial sum over driver_type dimension
     inputs = ["stock:nl-t-ya:ldv"]
-    _title_detail = "LDV transport vehicle stock"
     static = Plot.static + [
         p9.aes(x="ya", y="value", color="t"),
         p9.geom_line(),
@@ -328,40 +338,35 @@ class Stock0(Plot):
 
     def generate(self, data):
         y_max = max(data["value"])
-        unit = data["unit"].unique()[0]
+        self.unit = data["unit"].unique()[0]
 
-        for nl, group_df in data.groupby("nl"):
-            yield (
-                p9.ggplot(group_df)
-                + self.static
-                + p9.expand_limits(y=[0, y_max])
-                + self.title(f"{self._title_detail} [{unit}] {nl}")
-            )
+        for _, ggplot in self.groupby_plot(data, "nl"):
+            yield ggplot + p9.expand_limits(y=[0, y_max])
 
 
 class Stock1(Plot):
-    """Same as Stock0, but for non-LDV techs only."""
+    """Non-LDV transport vehicle stock.
+
+    Same as Stock0, but for non-LDV techs only.
+    """
 
     basename = "stock-non-ldv"
     inputs = ["stock:nl-t-ya:non-ldv"]
-    _title_detail = "Non-LDV transport"
+    static = Plot.static + [
+        p9.aes(x="yv", y="value", color="t"),
+        p9.geom_line(),
+        p9.geom_point(),
+    ]
 
     def generate(self, data):
         if not len(data):
             return
 
         y_max = max(data["value"])
-        unit = data["unit"].unique()[0]
+        self.unit = data["unit"].unique()[0]
 
-        for nl, group_df in data.groupby("nl"):
-            yield (
-                p9.ggplot(p9.aes(x="yv", y="value", color="t"), group_df)
-                + p9.geom_line()
-                + p9.geom_point()
-                + p9.expand_limits(y=[0, y_max])
-                + self.title(f"{self._title_detail} Vehicle stock [{unit}] {nl}")
-                + self.static
-            )
+        for _, ggplot in self.groupby_plot(data, "nl"):
+            yield ggplot + p9.expand_limits(y=[0, y_max])
 
 
 #: Plots of data from the built (and maybe solved) MESSAGEix-Transport scenario.
