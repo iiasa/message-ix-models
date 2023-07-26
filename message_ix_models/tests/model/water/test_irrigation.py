cec5340e6e20750ce6ae5c13a5160d9348f0214a
muhammad <39694989+awais307@users.noreply.github.com>
2023-07-26 16:23:44 -0700

    add tests (incomplete) for water

---
+++ message_ix_models/tests/model/water/test_irrigation.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from unittest.mock import patch
+
+from message_ix_models.model.water.data.irrigation import add_irr_structure
+
+def test_add_irr_structure():
+    # Mock the context
+    context = {
+        "water build info": {"Y": [2020, 2030, 2040]},
+        "type_reg": "country",
+        "regions": "test_region",
+        "map_ISO_c": {"test_region": "test_ISO"},
+    }
+    
+    # Mock the DataFrame read from CSV
+    df_node = pd.DataFrame({
+        "BCU_name": ["1", "2"],
+        "REGION": ["region1", "region2"]
+    })
+    
+    # Mock the function 'private_data_path' to return the mocked DataFrame
+    with patch('message_ix_models.util.private_data_path', return_value="path/to/file"), \
+         patch('pandas.read_csv', return_value=df_node):
+        
+        # Call the function to be tested
+        result = add_irr_structure(context)
+        
+        # Assert the results
+        assert isinstance(result, dict)
+        assert "input" in result
+        assert "output" in result
+        assert all(col in result["input"].columns for col in ["technology", "value", "unit", "level", "commodity", "mode", "time", "time_origin", "node_origin", "node_loc", "year_vtg", "year_act"])
+        assert all(col in result["output"].columns for col in ["technology", "value", "unit", "level", "commodity", "mode", "time", "time_dest", "node_loc", "node_dest", "year_vtg", "year_act"])
