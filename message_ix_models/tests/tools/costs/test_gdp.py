1d52c8335cb0cda35298dce08f28d1f41b8b95c6
Measrainsey Meng <measrainsey@pm.me>
2023-07-27 11:56:45 +0200

    Fix GDP tests

--- message_ix_models/tests/tools/costs/test_gdp.py
+++ message_ix_models/tests/tools/costs/test_gdp.py
@@ -4,7 +4,6 @@ from message_ix_models.tools.costs.gdp import (
     calculate_adjusted_region_cost_ratios,
     get_gdp_data,
     linearly_regress_tech_cost_vs_gdp_ratios,
-    project_gdp_converged_inv_costs,
 )
 from message_ix_models.tools.costs.learning import (
     get_cost_reduction_data,
@@ -69,37 +68,3 @@ def test_calculate_adjusted_region_cost_ratios():
 
     # Check that the adjusted cost ratios for NAM are equal to 1
     assert min(res.loc[res.r11_region == "NAM", "cost_ratio_adj"]) == 1.0
-
-
-# Test function to project GDP-converged investment costs
-def test_project_gdp_converged_inv_costs():
-    df_gdp = get_gdp_data()
-    df_weo = get_weo_data()
-    df_nam_orig_message = get_cost_assumption_data()
-    df_tech_cost_ratios = calculate_region_cost_ratios(df_weo)
-    df_linreg = linearly_regress_tech_cost_vs_gdp_ratios(df_gdp, df_tech_cost_ratios)
-    df_adj_cost_ratios = calculate_adjusted_region_cost_ratios(df_gdp, df_linreg)
-
-    df_region_diff = get_region_differentiated_costs(
-        df_weo, df_nam_orig_message, df_tech_cost_ratios
-    )
-
-    df_learning_rates = get_cost_reduction_data()
-    df_technology_first_year = get_technology_first_year_data()
-
-    df_nam_learning = project_NAM_inv_costs_using_learning_rates(
-        df_region_diff, df_learning_rates, df_technology_first_year
-    )
-
-    res = project_gdp_converged_inv_costs(df_nam_learning, df_adj_cost_ratios)
-
-    # Check SSP1, SSP2, and SSP3 are all present in the data
-    # TODO: this test won't be good once we make changing scenarios configurable
-    assert np.all(res.scenario.unique() == ["SSP1", "SSP2", "SSP3"])
-
-    # Check that the R11 regions are present
-    # TODO: this won't be a good test once we make changing regions configurable
-    assert np.all(
-        res.r11_region.unique()
-        == ["AFR", "CPA", "EEU", "FSU", "LAM", "MEA", "NAM", "PAO", "PAS", "SAS", "WEU"]
-    )
