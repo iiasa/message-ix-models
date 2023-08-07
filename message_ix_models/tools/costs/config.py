b5af19d735776ddb80b256652c9607347c51b4ea
Measrainsey Meng <measrainsey@pm.me>
2023-08-07 15:04:11 +0200

    Script with basic configurations

---
+++ message_ix_models/tools/costs/config.py
@@ -0,0 +1,8 @@
+BASE_YEAR = 2021
+FIRST_MODEL_YEAR = 2020
+LAST_MODEL_YEAR = 2100
+PRE_LAST_YEAR_RATE = 0.01
+
+# Conversion rate from 2021 USD to 2005 USD
+# Taken from https://www.officialdata.org/us/inflation/2021?endYear=2005&amount=1
+CONVERSION_2021_TO_2005_USD = 0.72
