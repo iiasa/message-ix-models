38e0f1c62c4bc3f0b8ff704f33e6e3bdd70cd47e
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-08-08 14:18:23 +0200

    Address pandas settingwithcopywarning in add_*_accounting

--- message_ix_models/tools/add_FFI_CO2_accounting.py
+++ message_ix_models/tools/add_FFI_CO2_accounting.py
@@ -20,26 +20,25 @@ def main(scen, relation_name, reg="R11_GLB", constraint_value=None):
     """
 
     if relation_name not in scen.set("relation").tolist():
-        scen.check_out()
-        scen.add_set("relation", relation_name)
-        scen.commit(
-            "relation {} for limiting".format(relation_name)
-            + " regional CO2 emissions at the global level added"
-        )
+        with scen.transact(
+            f"relation {relation_name!r} for limiting regional CO2 emissions at the "
+            "global level added"
+        ):
+            scen.add_set("relation", relation_name)
 
     if constraint_value:
         add_CO2_emission_constraint(
             scen, relation_name, constraint_value, type_rel="lower"
         )
 
-    df = scen.par(
-        "relation_activity",
-        filters={"relation": ["CO2_Emission", "CO2_shipping", "CO2_trade"]},
+    df = (
+        scen.par(
+            "relation_activity",
+            filters={"relation": ["CO2_Emission", "CO2_shipping", "CO2_trade"]},
+        )
+        .query("technology not in ['CO2_TCE', 'CO2t_TCE', 'CO2s_TCE']")
+        .assign(relation=relation_name, node_rel=reg)
     )
-    df = df.query("technology not in ['CO2_TCE', 'CO2t_TCE', 'CO2s_TCE']")
-    df.relation = relation_name
-    df.node_rel = reg
 
-    scen.check_out()
-    scen.add_par("relation_activity", df)
-    scen.commit("added new relation for accounting for FFI CO2 emissions")
+    with scen.transact("added new relation for accounting for FFI CO2 emissions"):
+        scen.add_par("relation_activity", df)
