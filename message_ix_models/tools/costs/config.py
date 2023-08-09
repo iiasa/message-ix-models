5c5b726156b22ab84bf3eba87c00121a3b0dc219
Measrainsey Meng <measrainsey@pm.me>
2023-08-09 08:44:43 +0200

    Add time steps specification to config file

--- message_ix_models/tools/costs/config.py
+++ message_ix_models/tools/costs/config.py
@@ -2,6 +2,7 @@ BASE_YEAR = 2021
 FIRST_MODEL_YEAR = 2020
 LAST_MODEL_YEAR = 2100
 PRE_LAST_YEAR_RATE = 0.01
+TIME_STEPS = 5
 
 # Conversion rate from 2021 USD to 2005 USD
 # Taken from https://www.officialdata.org/us/inflation/2021?endYear=2005&amount=1
