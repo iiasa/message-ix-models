b77aa6356da835c3dde501abfa69e6456c8ac7f8
Measrainsey Meng <measrainsey@pm.me>
2023-08-24 13:31:55 +0200

    Update projections functions to run each one depending on method selected

--- message_ix_models/tools/costs/projections.py
+++ message_ix_models/tools/costs/projections.py
@@ -3,37 +3,282 @@ from itertools import product
 import numpy as np
 import pandas as pd
 
-from message_ix_models.tools.costs.config import BASE_YEAR, LAST_MODEL_YEAR
+from message_ix_models.tools.costs.config import (
+    BASE_YEAR,
+    FIRST_MODEL_YEAR,
+    LAST_MODEL_YEAR,
+)
 from message_ix_models.tools.costs.gdp import calculate_gdp_adjusted_region_cost_ratios
 from message_ix_models.tools.costs.learning import (
     project_ref_region_inv_costs_using_learning_rates,
 )
-from message_ix_models.tools.costs.splines import (
-    get_final_inv_and_fom_costs,
-    project_all_inv_costs,
-)
+
+# from message_ix_models.tools.costs.splines import (
+#     get_final_inv_and_fom_costs,
+#     project_all_inv_costs,
+# )
 from message_ix_models.tools.costs.weo import get_weo_region_differentiated_costs
 
 
-# Function to get cost projections based on the following inputs:
-# - Spatial resolution
-# - Reference region
-# - Base year
-# - Scenario version (review or updated)
-# - SSP scenario
-# - Method (learning only, GDP adjusted, or convergence via spline projections)
-# - Convergence year (if applicable)
-# - Format (message or IAMC)
-def get_cost_projections(
+def create_projections_learning(in_node, in_ref_region, in_base_year, in_scenario):
+    print("Selected scenario: " + in_scenario)
+    print(
+        "For the learning method, only the SSP scenario(s) itself needs to be specified. \
+        No scenario version (previous vs. updated) is needed."
+    )
+
+    # If no scenario is specified, do not filter for scenario
+    # If it specified, then filter as below:
+    if in_scenario is not None:
+        if in_scenario == "all":
+            sel_scen = ["SSP1", "SSP2", "SSP3", "SSP4", "SSP5"]
+        else:
+            sel_scen = in_scenario.upper()
+
+    # Repeating to avoid linting error
+    sel_scen = sel_scen
+
+    df_region_diff = get_weo_region_differentiated_costs(
+        input_node=in_node,
+        input_ref_region=in_ref_region,
+        input_base_year=in_base_year,
+    )
+
+    df_ref_reg_learning = project_ref_region_inv_costs_using_learning_rates(
+        df_region_diff,
+        input_node=in_node,
+        input_ref_region=in_ref_region,
+        input_base_year=in_base_year,
+    )
+
+    if in_scenario is not None:
+        df_ref_reg_learning = df_ref_reg_learning.query("scenario == @sel_scen")
+
+    df_costs = (
+        df_region_diff.merge(df_ref_reg_learning, on="message_technology")
+        .assign(
+            inv_cost=lambda x: np.where(
+                x.year <= FIRST_MODEL_YEAR,
+                x.reg_cost_base_year,
+                x.inv_cost_ref_region_learning * x.reg_cost_ratio,
+            ),
+            fix_cost=lambda x: x.inv_cost * x.fix_to_inv_cost_ratio,
+        )
+        .reindex(
+            [
+                "scenario",
+                "message_technology",
+                "region",
+                "year",
+                "inv_cost",
+                "fix_cost",
+            ],
+            axis=1,
+        )
+    )
+
+    return df_costs
+
+
+def create_projections_gdp(
+    in_node, in_ref_region, in_base_year, in_scenario, in_scenario_version
+):
+    # Print selection of scenario version and scenario
+    print("Selected scenario: " + in_scenario)
+    print("Selected scenario version: " + in_scenario_version)
+
+    # If no scenario is specified, do not filter for scenario
+    # If it specified, then filter as below:
+    if in_scenario is not None:
+        if in_scenario == "all":
+            sel_scen = ["SSP1", "SSP2", "SSP3", "SSP4", "SSP5"]
+        else:
+            sel_scen = in_scenario.upper()
+
+    # If no scenario version is specified, do not filter for scenario version
+    # If it specified, then filter as below:
+    if in_scenario_version is not None:
+        if in_scenario_version == "all":
+            sel_scen_vers = ["Review (2023)", "Previous (2013)"]
+        elif in_scenario_version == "updated":
+            sel_scen_vers = ["Review (2023)"]
+        elif in_scenario_version == "original":
+            sel_scen_vers = ["Previous (2013)"]
+
+    # Repeating to avoid linting error
+    sel_scen = sel_scen
+    sel_scen_vers = sel_scen_vers
+
+    df_region_diff = get_weo_region_differentiated_costs(
+        input_node=in_node,
+        input_ref_region=in_ref_region,
+        input_base_year=in_base_year,
+    )
+
+    df_ref_reg_learning = project_ref_region_inv_costs_using_learning_rates(
+        df_region_diff,
+        input_node=in_node,
+        input_ref_region=in_ref_region,
+        input_base_year=in_base_year,
+    )
+
+    df_adj_cost_ratios = calculate_gdp_adjusted_region_cost_ratios(
+        df_region_diff,
+        input_node=in_node,
+        input_ref_region=in_ref_region,
+        input_base_year=in_base_year,
+    )
+
+    if in_scenario is not None:
+        df_ref_reg_learning = df_ref_reg_learning.query("scenario == @sel_scen")
+        df_adj_cost_ratios = df_adj_cost_ratios.query(
+            "scenario_version == @sel_scen_vers and scenario == @sel_scen"
+        )
+
+    df_costs = (
+        df_region_diff.merge(df_ref_reg_learning, on="message_technology")
+        .merge(
+            df_adj_cost_ratios, on=["scenario", "message_technology", "region", "year"]
+        )
+        .assign(
+            inv_cost=lambda x: np.where(
+                x.year <= FIRST_MODEL_YEAR,
+                x.reg_cost_base_year,
+                x.inv_cost_ref_region_learning * x.reg_cost_ratio_adj,
+            ),
+            fix_cost=lambda x: x.inv_cost * x.fix_to_inv_cost_ratio,
+        )
+        .reindex(
+            [
+                "scenario_version",
+                "scenario",
+                "message_technology",
+                "region",
+                "year",
+                "inv_cost",
+                "fix_cost",
+            ],
+            axis=1,
+        )
+    )
+
+    return df_costs
+
+
+def create_projections_converge(
+    in_node, in_ref_region, in_base_year, in_scenario, in_convergence_year
+):
+    print("Selected scenario: " + in_scenario)
+    print("Selected convergence year: " + str(in_convergence_year))
+    print(
+        "For the convergence method, only the SSP scenario(s) itself needs to be specified. \
+        No scenario version (previous vs. updated) is needed."
+    )
+
+    # If no scenario is specified, do not filter for scenario
+    # If it specified, then filter as below:
+    if in_scenario is not None:
+        if in_scenario == "all":
+            sel_scen = ["SSP1", "SSP2", "SSP3", "SSP4", "SSP5"]
+        else:
+            sel_scen = in_scenario.upper()
+
+    # Repeating to avoid linting error
+    sel_scen = sel_scen
+
+    df_region_diff = get_weo_region_differentiated_costs(
+        input_node=in_node,
+        input_ref_region=in_ref_region,
+        input_base_year=in_base_year,
+    )
+
+    df_ref_reg_learning = project_ref_region_inv_costs_using_learning_rates(
+        df_region_diff,
+        input_node=in_node,
+        input_ref_region=in_ref_region,
+        input_base_year=in_base_year,
+    )
+
+    if in_scenario is not None:
+        df_ref_reg_learning = df_ref_reg_learning.query("scenario == @sel_scen")
+
+    df_pre_costs = df_region_diff.merge(
+        df_ref_reg_learning, on="message_technology"
+    ).assign(
+        inv_cost_converge=lambda x: np.where(
+            x.year <= FIRST_MODEL_YEAR,
+            x.reg_cost_base_year,
+            np.where(
+                x.year < in_convergence_year,
+                x.inv_cost_ref_region_learning * x.reg_cost_ratio,
+                x.inv_cost_ref_region_learning,
+            ),
+        ),
+    )
+
+    df_splines = apply_splines_to_convergence(
+        df_pre_costs,
+        column_name="inv_cost_converge",
+        input_convergence_year=in_convergence_year,
+    )
+
+    df_costs = (
+        df_pre_costs.merge(
+            df_splines,
+            on=["scenario", "message_technology", "region", "year"],
+            how="outer",
+        )
+        .rename(columns={"inv_cost_splines": "inv_cost"})
+        .assign(fix_cost=lambda x: x.inv_cost * x.fix_to_inv_cost_ratio)
+        .reindex(
+            [
+                "scenario",
+                "message_technology",
+                "region",
+                "year",
+                "inv_cost",
+                "fix_cost",
+            ],
+            axis=1,
+        )
+    )
+
+    return df_costs
+
+
+def create_cost_projections(
     sel_node: str = "r12",
     sel_ref_region=None,
     sel_base_year: int = BASE_YEAR,
+    sel_method: str = "gdp",
     sel_scenario_version="updated",
     sel_scenario="all",
-    sel_method: str = "convergence",
     sel_convergence_year: int = 2050,
-    sel_format: str = "message",
 ):
+    """Get investment and fixed cost projections
+
+    Parameters
+    ----------
+    sel_node : str, optional
+        Spatial resolution, by default "r12". Options are "r11", "r12", and "r20"
+    sel_ref_region : str, optional
+        Reference region, by default R12_NAM for R12, R11_NAM for R11, and R20_NAM for R20
+    sel_base_year : int, optional
+        Base year, by default BASE_YEAR specified in the config file
+    sel_method : str, optional
+        Method to use, by default "gdp". Options are "learning", "gdp", and "convergence"
+    sel_scenario_version : str, optional
+        Scenario version, by default "updated". Options are "updated" and "original"
+    sel_scenario : str, optional
+        Scenario, by default "all"
+    sel_convergence_year : int, optional
+        Year to converge costs to, by default 2050
+
+    Returns
+    -------
+    pandas.DataFrame
+        Dataframe containing cost projections
+    """
     # Change node selection to upper case
     node_up = sel_node.upper()
 
@@ -61,42 +306,124 @@ def get_cost_projections(
         print("Selected reference region: " + sel_ref_region)
         print("Selected base year: " + str(sel_base_year))
 
-        # Print final selection of scenario version and scenario
-        print("Selected scenario version: " + sel_scenario_version)
-        print("Selected scenario: " + sel_scenario)
+        print("Selected method: " + sel_method)
 
-        df_region_diff = get_weo_region_differentiated_costs(
-            input_node=sel_node,
-            input_ref_region=sel_ref_region,
-            input_base_year=sel_base_year,
-        )
+        # If method is learning, then use the learning method
+        if sel_method == "learning":
+            df_costs = create_projections_learning(
+                in_node=node_up,
+                in_ref_region=sel_ref_region,
+                in_base_year=sel_base_year,
+                in_scenario=sel_scenario,
+            )
 
-        df_ref_reg_learning = project_ref_region_inv_costs_using_learning_rates(
-            df_region_diff,
-            input_node=sel_node,
-            input_ref_region=sel_ref_region,
-            input_base_year=sel_base_year,
-        )
+        # If method is GDP, then use the GDP method
+        if sel_method == "gdp":
+            df_costs = create_projections_gdp(
+                in_node=node_up,
+                in_ref_region=sel_ref_region,
+                in_base_year=sel_base_year,
+                in_scenario=sel_scenario,
+                in_scenario_version=sel_scenario_version,
+            )
 
-        df_adj_cost_ratios = calculate_gdp_adjusted_region_cost_ratios(
-            df_region_diff,
-            input_node=sel_node,
-            input_ref_region=sel_ref_region,
-            input_base_year=sel_base_year,
-        )
+        # If method is convergence, then use the convergence method
+        if sel_method == "convergence":
+            df_costs = create_projections_converge(
+                in_node=node_up,
+                in_ref_region=sel_ref_region,
+                in_base_year=sel_base_year,
+                in_scenario=sel_scenario,
+                in_convergence_year=sel_convergence_year,
+            )
 
-        df_all_inv = project_all_inv_costs(
-            df_region_diff,
-            df_ref_reg_learning,
-            df_adj_cost_ratios,
-            input_convergence_year=sel_convergence_year,
-            input_scenario_version=sel_scenario_version,
-            input_scenario=sel_scenario,
-        )
+        return df_costs
 
-        df_inv_fom = get_final_inv_and_fom_costs(df_all_inv, input_method=sel_method)
 
-        return df_inv_fom
+# Function to get cost projections based on the following inputs:
+# - Spatial resolution
+# - Reference region
+# - Base year
+# - Scenario version (review or updated)
+# - SSP scenario
+# - Method (learning only, GDP adjusted, or convergence via spline projections)
+# - Convergence year (if applicable)
+# - Format (message or IAMC)
+# def get_cost_projections(
+#     sel_node: str = "r12",
+#     sel_ref_region=None,
+#     sel_base_year: int = BASE_YEAR,
+#     sel_scenario_version="updated",
+#     sel_scenario="all",
+#     sel_method: str = "convergence",
+#     sel_convergence_year: int = 2050,
+#     sel_format: str = "message",
+# ):
+#     # Change node selection to upper case
+#     node_up = sel_node.upper()
+
+#     # Check if node selection is valid
+#     if node_up not in ["R11", "R12", "R20"]:
+#         return "Please select a valid spatial resolution: R11, R12, or R20"
+#     else:
+#         # Set default values for input arguments
+#         # If specified node is R11, then use R11_NAM as the reference region
+#         # If specified node is R12, then use R12_NAM as the reference region
+#         # If specified node is R20, then use R20_NAM as the reference region
+#         # However, if a reference region is specified, then use that instead
+#         if sel_ref_region is None:
+#             if node_up == "R11":
+#                 sel_ref_region = "R11_NAM"
+#             if node_up == "R12":
+#                 sel_ref_region = "R12_NAM"
+#             if node_up == "R20":
+#                 sel_ref_region = "R20_NAM"
+#         elif sel_ref_region is not None:
+#             sel_ref_region = sel_ref_region.upper()
+
+#         # Print final selection of regions, reference regions, and base year
+#         print("Selected node: " + node_up)
+#         print("Selected reference region: " + sel_ref_region)
+#         print("Selected base year: " + str(sel_base_year))
+
+#         print("Selected method: " + sel_method)
+
+#         # Print final selection of scenario version and scenario
+#         print("Selected scenario version: " + sel_scenario_version)
+#         print("Selected scenario: " + sel_scenario)
+
+#         df_region_diff = get_weo_region_differentiated_costs(
+#             input_node=sel_node,
+#             input_ref_region=sel_ref_region,
+#             input_base_year=sel_base_year,
+#         )
+
+#         df_ref_reg_learning = project_ref_region_inv_costs_using_learning_rates(
+#             df_region_diff,
+#             input_node=sel_node,
+#             input_ref_region=sel_ref_region,
+#             input_base_year=sel_base_year,
+#         )
+
+#         df_adj_cost_ratios = calculate_gdp_adjusted_region_cost_ratios(
+#             df_region_diff,
+#             input_node=sel_node,
+#             input_ref_region=sel_ref_region,
+#             input_base_year=sel_base_year,
+#         )
+
+#         df_all_inv = project_all_inv_costs(
+#             df_region_diff,
+#             df_ref_reg_learning,
+#             df_adj_cost_ratios,
+#             input_convergence_year=sel_convergence_year,
+#             input_scenario_version=sel_scenario_version,
+#             input_scenario=sel_scenario,
+#         )
+
+#         df_inv_fom = get_final_inv_and_fom_costs(df_all_inv, input_method=sel_method)
+
+#         return df_inv_fom
 
 
 def create_message_inputs(df_proj: pd.DataFrame):
