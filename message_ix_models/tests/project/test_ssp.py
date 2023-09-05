f205437eae62b96c1acc6dee3be80bdd33c5a540
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-09-05 14:22:47 +0200

    Add .ssp.parse(), field helper for dataclasses

--- message_ix_models/tests/project/test_ssp.py
+++ message_ix_models/tests/project/test_ssp.py
@@ -1,6 +1,13 @@
 import pytest
 
-from message_ix_models.project.ssp import SSP, SSP_2017, SSP_2024, generate
+from message_ix_models.project.ssp import (
+    SSP,
+    SSP_2017,
+    SSP_2024,
+    generate,
+    parse,
+    ssp_field,
+)
 
 
 def test_generate(tmp_path, test_context):
@@ -31,6 +38,45 @@ def test_enum():
     assert SSP_2017["1"] != SSP_2024["1"]
     assert SSP_2017["1"] is not SSP_2024["1"]
     assert SSP["1"] != SSP_2024["1"]
+
+
+@pytest.mark.parametrize(
+    "expected, value",
+    (
+        (SSP_2017["1"], SSP_2017["1"]),  # Literal value
+        (SSP_2017["1"], "1"),  # String ID as appears in the codelist
+        (SSP_2017["1"], "SSP1"),  # Prefixed by "SSP"
+        (SSP_2017["1"], 1),  # Integer
+    ),
+)
+def test_parse(value, expected):
+    assert expected == parse(value)
+
+
+def test_ssp_field():
+    from dataclasses import dataclass
+
+    @dataclass
+    class Foo:
+        bar: ssp_field = ssp_field(default=SSP_2017["1"])
+        baz: ssp_field = ssp_field(default=SSP_2024["5"])
+
+    # Can be instantiated with no arguments
+    f = Foo()
+    assert SSP_2017["1"] is f.bar
+    assert SSP_2024["5"] is f.baz
+
+    # Can be instantiated with different values
+    f = Foo(bar=SSP_2017["3"], baz=SSP_2024["3"])
+    assert SSP_2017["3"] is f.bar
+    assert SSP_2024["3"] is f.baz
+
+    # Values can be set and are passed through parse()
+    f.bar = "SSP2"
+    f.baz = "SSP4"
+
+    assert SSP_2017["2"] is f.bar
+    assert SSP_2017["4"] is f.baz
 
 
 def test_cli(mix_models_cli):
