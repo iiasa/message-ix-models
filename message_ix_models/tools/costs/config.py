13b2626f900ea5449257bc8a59e09be802b33e52
Measrainsey Meng <measrainsey@pm.me>
2023-09-06 15:35:12 +0200

    Add adjusted base year and horizon years

--- message_ix_models/tools/costs/config.py
+++ message_ix_models/tools/costs/config.py
@@ -1,8 +1,12 @@
 BASE_YEAR = 2021
+ADJ_BASE_YEAR = 2020
 FIRST_MODEL_YEAR = 2020
 LAST_MODEL_YEAR = 2100
 PRE_LAST_YEAR_RATE = 0.01
 TIME_STEPS = 5
+HORIZON_START = 1960
+HORIZON_END = 2110
+
 
 # Conversion rate from 2021 USD to 2005 USD
 # Taken from https://www.officialdata.org/us/inflation/2021?endYear=2005&amount=1
