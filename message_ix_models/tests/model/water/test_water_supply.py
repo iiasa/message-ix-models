e8d85962b3b3b1c457db36a08f07950667bbb62c
Muhammad Awais <awais307@gmail.com>
2023-09-27 16:21:47 -0700

    Apply code formatting

--- message_ix_models/tests/model/water/test_water_supply.py
+++ message_ix_models/tests/model/water/test_water_supply.py
@@ -1,7 +1,9 @@
-import pandas as pd
 from unittest.mock import patch
-from message_ix_models.model.water.data.water_supply import add_e_flow, add_water_supply, map_basin_region_wat
 
+import pandas as pd
+
+from message_ix_models.model.water.data.water_supply import (
+    add_e_flow, add_water_supply, map_basin_region_wat)
 
 
 def test_map_basin_region_wat():
@@ -17,26 +19,33 @@ def test_map_basin_region_wat():
     }
 
     # Mock the DataFrames read from CSV
-    df_x = pd.DataFrame({
-        "BCU_name": ["test_BCU"],
-    })
-    
-    df_sw = pd.DataFrame({
-        "Unnamed: 0": [0],
-        "BCU_name": ["test_BCU"],
-        "test_value": [1],
-    })
+    df_x = pd.DataFrame(
+        {
+            "BCU_name": ["test_BCU"],
+        }
+    )
+
+    df_sw = pd.DataFrame(
+        {
+            "Unnamed: 0": [0],
+            "BCU_name": ["test_BCU"],
+            "test_value": [1],
+        }
+    )
 
     # Mock the function 'private_data_path' to return the mocked DataFrame
-    with patch('message_ix_models.util.private_data_path', return_value="path/to/file"), \
-         patch('pandas.read_csv', return_value=df_sw):
-        
+    with patch(
+        "message_ix_models.util.private_data_path", return_value="path/to/file"
+    ), patch("pandas.read_csv", return_value=df_sw):
         # Call the function to be tested
         result = map_basin_region_wat(context)
-        
+
         # Assert the results
         assert isinstance(result, pd.DataFrame)
-        assert all(col in result.columns for col in ["region", "mode", "date", "MSGREG", "share", "year", "time"])
+        assert all(
+            col in result.columns
+            for col in ["region", "mode", "date", "MSGREG", "share", "year", "time"]
+        )
 
 
 def test_add_water_supply():
@@ -54,50 +63,54 @@ def test_add_water_supply():
     }
 
     # Mock the DataFrames read from CSV
-    df_node = pd.DataFrame({
-        "BCU_name": ["test_BCU"],
-        "REGION": ["test_REGION"]
-    })
+    df_node = pd.DataFrame({"BCU_name": ["test_BCU"], "REGION": ["test_REGION"]})
 
-    df_gwt = pd.DataFrame({
-        "REGION": ["test_REGION"],
-        "GW_per_km3_per_year": [1],
-    })
+    df_gwt = pd.DataFrame(
+        {
+            "REGION": ["test_REGION"],
+            "GW_per_km3_per_year": [1],
+        }
+    )
 
-    df_hist = pd.DataFrame({
-        "BCU_name": ["test_BCU"],
-        "hist_cap_sw_km3_year": [1],
-        "hist_cap_gw_km3_year": [1],
-    })
+    df_hist = pd.DataFrame(
+        {
+            "BCU_name": ["test_BCU"],
+            "hist_cap_sw_km3_year": [1],
+            "hist_cap_gw_km3_year": [1],
+        }
+    )
 
-    df_sw = pd.DataFrame({
-        "mode": ["test_mode"],
-        "MSGREG": ["test_MSGREG"],
-        "share": [1],
-        "year": [2020],
-        "time": ["year"],
-    })
+    df_sw = pd.DataFrame(
+        {
+            "mode": ["test_mode"],
+            "MSGREG": ["test_MSGREG"],
+            "share": [1],
+            "year": [2020],
+            "time": ["year"],
+        }
+    )
 
     # Mock the function 'private_data_path' to return the mocked DataFrame
-    with patch('message_ix_models.util.private_data_path', return_value="path/to/file"), \
-         patch('pandas.read_csv', return_value=df_node), \
-         patch('your_module.map_basin_region_wat', return_value=df_sw):
-
+    with patch(
+        "message_ix_models.util.private_data_path", return_value="path/to/file"
+    ), patch("pandas.read_csv", return_value=df_node), patch(
+        "your_module.map_basin_region_wat", return_value=df_sw
+    ):
         # Call the function to be tested
         result = add_water_supply(context)
 
         # Assert the results
         assert isinstance(result, dict)
-        assert 'input' in result
-        assert 'output' in result
-        assert 'var_cost' in result
-        assert 'technical_lifetime' in result
-        assert 'inv_cost' in result
+        assert "input" in result
+        assert "output" in result
+        assert "var_cost" in result
+        assert "technical_lifetime" in result
+        assert "inv_cost" in result
 
         for df in result.values():
             assert isinstance(df, pd.DataFrame)
-            
-            
+
+
 def test_add_e_flow():
     # Mock the context
     context = {
@@ -109,29 +122,26 @@ def test_add_e_flow():
     }
 
     # Mock the DataFrames read from CSV
-    df_sw = pd.DataFrame({
-        "Region": ["test_Region"],
-        "value": [1],
-        "year": [2020],
-        "time": ["year"]
-    })
+    df_sw = pd.DataFrame(
+        {"Region": ["test_Region"], "value": [1], "year": [2020], "time": ["year"]}
+    )
 
-    df_env = pd.DataFrame({
-        "Region": ["test_Region"],
-        "value": [1],
-        "year": [2020],
-        "time": ["year"]
-    })
+    df_env = pd.DataFrame(
+        {"Region": ["test_Region"], "value": [1], "year": [2020], "time": ["year"]}
+    )
 
     # Mock the function 'read_water_availability' to return the mocked DataFrame
-    with patch('your_module.read_water_availability', return_value=(df_sw, df_sw)), \
-         patch('message_ix_models.util.private_data_path', return_value="path/to/file"), \
-         patch('pandas.read_csv', return_value=df_sw):
-
+    with patch(
+        "your_module.read_water_availability", return_value=(df_sw, df_sw)
+    ), patch(
+        "message_ix_models.util.private_data_path", return_value="path/to/file"
+    ), patch(
+        "pandas.read_csv", return_value=df_sw
+    ):
         # Call the function to be tested
         result = add_e_flow(context)
 
         # Assert the results
         assert isinstance(result, dict)
-        assert 'bound_activity_lo' in result
-        assert isinstance(result['bound_activity_lo'], pd.DataFrame)
+        assert "bound_activity_lo" in result
+        assert isinstance(result["bound_activity_lo"], pd.DataFrame)
