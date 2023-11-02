6621a509824025a2dfe4e483b1cd9d6cfa1f79f8
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-11-02 13:35:45 +0100

    Test make_click_command()

--- message_ix_models/tests/test_workflow.py
+++ message_ix_models/tests/test_workflow.py
@@ -1,4 +1,5 @@
 import re
+from typing import Optional
 
 import ixmp
 import pytest
@@ -6,16 +7,18 @@ from genno import KeyExistsError
 from message_ix import make_df
 
 from message_ix_models import Workflow, testing
-from message_ix_models.workflow import WorkflowStep, solve
+from message_ix_models.workflow import WorkflowStep, make_click_command, solve
+
+# Functions for WorkflowSteps
 
 
-def changes_a(c, s):
+def changes_a(c, s) -> None:
     """Change a scenario by modifying structure data, but not data."""
     with s.transact():
         s.add_set("technology", "test_tech")
 
 
-def changes_b(c, s, value=None):
+def changes_b(c, s, value=None) -> None:
     """Change a scenario by modifying parameter data, but not structure."""
     with s.transact():
         s.add_par(
@@ -32,7 +35,7 @@ def changes_b(c, s, value=None):
 
 
 class TestWorkflowStep:
-    def test_call(self, test_context):
+    def test_call(self, test_context) -> None:
         def action(c, s):
             pass  # pragma: no cover
 
@@ -41,25 +44,31 @@ class TestWorkflowStep:
         with pytest.raises(RuntimeError):
             ws(test_context, None)
 
-    def test_repr(self):
+    def test_repr(self) -> None:
         assert "<Step load>" == repr(WorkflowStep(None))
 
 
-@pytest.mark.skipif(
-    condition=ixmp.__version__ < "3.5",
-    reason="ixmp.TimeSeries.url not available prior to ixmp 3.5.0",
-)
-def test_workflow(caplog, request, test_context):
-    # FIXME disentangle this to fewer tests of atomic behaviour
-    base_scenario = testing.bare_res(request, test_context, solved=False)
-    base_url = base_scenario.url
-    base_platform = base_scenario.platform.name
-    del base_scenario
+@pytest.fixture
+def wf(request, test_context) -> Workflow:
+    return _wf(test_context, request=request)
 
-    caplog.clear()
 
+def _wf(
+    context,
+    *,
+    base_url: Optional[str] = None,
+    base_platform: Optional[str] = None,
+    request=None,
+):
+    if base_url is base_platform is None:
+        base_scenario = testing.bare_res(request, context, solved=False)
+        base_platform = base_scenario.platform.name
+        base_url = f"ixmp://{base_platform}/{base_scenario.url}"
+        del base_scenario
+
+    """A function that generates a Workflow."""
     # Create the workflow
-    wf = Workflow(test_context)
+    wf = Workflow(context)
 
     # Model/base is created from nothing by calling base_scenario
     wf.add_step("base", None, target=base_url)
@@ -67,6 +76,59 @@ def test_workflow(caplog, request, test_context):
     wf.add_step("A", "base", changes_a)
     # Model/B is created from Model/A by calling changes_b
     wf.add_step("B", "A", changes_b, value=100.0)
+
+    # Store extra info
+    wf.graph.update({"_base_url": base_url, "_base_platform": base_platform})
+
+    return wf
+
+
+def test_make_click_command(monkeypatch: pytest.MonkeyPatch, mix_models_cli) -> None:
+    import click
+
+    import message_ix_models.cli
+
+    # make_click_command() runs and generates a command
+    cmd = make_click_command(f"{__name__}._wf", name="test", slug="test")
+    assert isinstance(cmd, click.Command)
+
+    # Monkey-patch this command into the CLI temporarily
+    _name = "_test-make-click-command"
+    monkeypatch.setitem(message_ix_models.cli.main.commands, _name, cmd)
+
+    # Invoke the command with various parameters
+    for params, output in (
+        (["--go", "B"], "nothing returned, workflow will continue with"),
+        (["--go", "--from=[AX]", "B"], "nothing returned, workflow will continue with"),
+        (["B"], "Workflow diagram written to"),
+    ):
+        # Command runs and exits with 0
+        result = mix_models_cli.assert_exit_0([_name] + params)
+        # Expected log messages or output were printed
+        assert output in result.output
+
+    # Invalid usage
+    for params, output in (
+        (["--go", "C"], "Error: No step(s) matched"),
+        (["--go"], "Error: No target step provided and no default for"),
+    ):
+        result = mix_models_cli.invoke([_name] + params)
+        assert 0 != result.exit_code
+        assert output in result.output
+
+
+@pytest.mark.skipif(
+    condition=ixmp.__version__ < "3.5",
+    reason="ixmp.TimeSeries.url not available prior to ixmp 3.5.0",
+)
+def test_workflow(caplog, request, test_context, wf) -> None:
+    # FIXME disentangle this to fewer tests of atomic behaviour
+
+    # Retrieve some information from the fixture
+    base_url = wf.graph.pop("_base_url")
+    base_platform = wf.graph.pop("_base_platform")
+
+    caplog.clear()
 
     # "B solved" is created from "Model/B" by clone and running solve()
     # clone=True without target= raises an exception
