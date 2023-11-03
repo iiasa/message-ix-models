1549da4d1b1411906678d8e3865a6475cef10805
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-11-03 19:58:34 +0100

    Replace .freight.iea_2017_t4 with an exodatasource

--- message_ix_models/model/transport/freight.py
+++ message_ix_models/model/transport/freight.py
@@ -1,70 +1,19 @@
 """Freight transport data."""
 from collections import defaultdict
-from typing import Dict, List
+from typing import TYPE_CHECKING, Dict, List
 
 import pandas as pd
-from genno import Computer, Quantity, computations
-from genno.operator import broadcast_map, load_file
+from genno import Computer
 from iam_units import registry
-from ixmp.reporting.computations import map_as_qty
 from message_ix_models import Context
 from message_ix_models.model.structure import get_codes
-from message_ix_models.util import (
-    broadcast,
-    make_io,
-    make_matched_dfs,
-    private_data_path,
-    same_node,
-)
+from message_ix_models.util import broadcast, make_io, make_matched_dfs, same_node
 from sdmx.model.v21 import Code
 
 from .util import input_commodity_level
 
-
-def iea_2017_t4(measure: int):
-    """Retrieve IEA “Future of Trucks” data.
-
-    Parameters
-    ----------
-    measure : int
-        One of:
-
-        1. energy intensity of vehicle distance travelled
-        2. load
-        3. energy intensity of freight service (mass × distance)
-    """
-    data = load_file(
-        private_data_path("transport", f"iea-2017-t4-{measure}.csv"),
-        dims={"node": "n", "technology": "t"},
-    )
-
-    # Broadcast to regions
-    m = pd.DataFrame(
-        [
-            ("R12_CHN", "CHN"),
-            ("R12_EEU", "EU28"),
-            ("R12_NAM", "USA"),
-            ("R12_SAS", "IND"),
-            ("R12_WEU", "EU28"),
-            # Assumed similarity
-            ("R12_AFR", "IND"),
-            ("R12_FSU", "CHN"),
-            ("R12_LAM", "USA"),
-            ("R12_MEA", "CHN"),
-            ("R12_PAO", "CHN"),
-            ("R12_PAS", "CHN"),
-            ("R12_RCPA", "CHN"),
-        ],
-        columns=["n2", "n"],
-    )
-    m = map_as_qty(m[["n", "n2"]], [])  # Expects columns in the from/to order
-    result = broadcast_map(data, m, rename={"n2": "n"})
-
-    # Share of freight activity; transcribed from figure 18, page 38
-    share = Quantity(
-        pd.Series([0.1, 0.3, 0.6], index=pd.Index("LCV MFT HFT".split(), name="t"))
-    )
-    return computations.sum(result, share, dimensions=["t"])
+if TYPE_CHECKING:
+    from genno import Quantity
 
 
 def prepare_computer(c: Computer):
@@ -74,7 +23,11 @@ def prepare_computer(c: Computer):
 
 
 def get_freight_data(
-    nodes: List[str], techs: List[Code], years: List[int], context: Context
+    efficiency: "Quantity",
+    nodes: List[str],
+    techs: List[Code],
+    years: List[int],
+    context: Context,
 ) -> Dict[str, pd.DataFrame]:
     """Data for freight technologies.
 
@@ -99,18 +52,16 @@ def get_freight_data(
     )
 
     # Efficiency information
-    efficiency = (
-        computations.convert_units(iea_2017_t4(1), "GWa / (Gv km)")
-        .to_series()
-        .reset_index()
-        .set_axis(["node_loc", "value"], axis=1)
+    efficiency_df = (
+        efficiency.to_series().reset_index().set_axis(["node_loc", "value"], axis=1)
     )
 
-    # NB currently unused. These values are in the range of 10–12 tonne / vehicle.
-    load_factor = (
-        iea_2017_t4(2).to_series().reset_index().set_axis(["node_loc", "value"], axis=1)
-    )
-    del load_factor
+    # # NB currently unused. These values are in the range of 10–12 tonne / vehicle.
+    # load_factor = (
+    #     iea_2017_t4(2).to_series().reset_index()
+    #     .set_axis(["node_loc", "value"], axis=1)
+    # )
+    # del load_factor
 
     data0: Dict[str, List] = defaultdict(list)
     for t in technologies:
@@ -127,7 +78,7 @@ def get_freight_data(
 
         data0["input"].append(
             input_commodity_level(context, i_o["input"], default_level="final")
-            .pipe(broadcast, efficiency)
+            .pipe(broadcast, efficiency_df)
             .pipe(same_node)
         )
         data0["output"].append(
