bc536d1640195a91d597a1d51c384fec405bdcd5
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2023-12-11 10:56:54 +0100

    Improve transport.workflow
    - use message_ix_models.report.report() directly, not the navigate
      report() step.
    - allow use with no base --url, only --platform.
    - use default reporting key from --key cli option.

--- message_ix_models/model/transport/workflow.py
+++ message_ix_models/model/transport/workflow.py
@@ -13,10 +13,11 @@ def generate(
     context: "message_ix_models.Context", *, report_key="transport all", **options
 ):
     from message_ix_models import Workflow
+    from message_ix_models.report import register, report
 
     from message_data.model.workflow import Config as SolveConfig
     from message_data.projects import navigate
-    from message_data.projects.navigate.workflow import report, solve
+    from message_data.projects.navigate.workflow import solve
 
     from . import build
     from .config import Config
@@ -24,19 +25,28 @@ def generate(
     # TODO respect dry-run
     # TODO respect quiet
 
+    options.pop("target_model_name")
+    options.pop("target_scenario_name")
+
     # Prepare transport configuration
     Config.from_context(context, options=options)
 
-    # Construct the base URL for scenarios
+    # Identify the base MESSAGEix-GLOBIOM scenario
+    base_url = context.url
+    if not base_url:
+        log.warning("No --url given; some workflow steps may not work")
+        base_url = f"ixmp://{context.platform_info['name']}/NONE/NONE"
+
+    # Construct a URL template for MESSAGEix-Transport scenarios
     if context.core.dest:
         # Value from --dest CLI option
-        base_url = context.dest + " {{}}"
+        url_template = context.dest + " {{}}"
     else:
         # Values from --model-extra, --scenario-extra CLI options
         m_extra = context.core.dest_scenario.pop("model", "")
         s_extra = context.core.dest_scenario.pop("scenario", "baseline")
 
-        base_url = "/".join(
+        url_template = "/".join(
             (
                 f"MESSAGEix-GLOBIOM 1.1-T-{context.model.regions} {m_extra}".rstrip(),
                 f"{{}} {s_extra}",
@@ -45,18 +55,19 @@ def generate(
 
     # Set values expected by workflow steps re-used from .projects.navigate
     context.navigate = navigate.Config(
-        scenario="baseline",
-        buildings=False,
-        material=False,
+        scenario="baseline", buildings=False, material=False
     )
     solve_config = SolveConfig(reserve_margin=False, solve=dict(model="MESSAGE"))
 
+    # Set the default key for ".* reported" steps
+    register("model.transport")
+    context.report.key = report_key
+
+    # Create the workflow
     wf = Workflow(context)
 
-    # Load base scenario
-    if not context.url:
-        raise RuntimeError("Must provide a --url")
-    wf.add_step("base", None, target=context.url)
+    # Load the base model scenario
+    wf.add_step("base", None, target=base_url)
 
     all_keys = []
     for ssp in SSP_2024:
@@ -66,7 +77,7 @@ def generate(
         label_full = f"SSP_2024.{ssp.name}"
 
         # Identify the target of this step
-        target = base_url.format(label_full)
+        target = url_template.format(label_full)
 
         # Build Transport on the scenario
         # TODO Add functionality like gen-activity
@@ -79,14 +90,7 @@ def generate(
         wf.add_step(f"{label} solved", f"{label} built", solve, config=solve_config)
 
         # Report
-        all_keys.append(
-            wf.add_step(
-                f"{label} reported",
-                f"{label} solved",
-                report,
-                use_legacy_reporting=False,
-            )
-        )
+        all_keys.append(wf.add_step(f"{label} reported", f"{label} solved", report))
 
     wf.add("all reported", all_keys)
     wf.default_key = "all reported"
