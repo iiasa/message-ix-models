756348504643a07f225629097e775fcede084873
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-01-19 13:08:13 +0100

    Reorganize .tools.iea.web and tests

--- message_ix_models/tests/tools/iea/test_web.py
+++ message_ix_models/tests/tools/iea/test_web.py
@@ -1,12 +1,14 @@
-"""Tests of :mod:`.tools`.
-
-.. todo:: As the module is expanded, split these tests to multiple files in a directory.
-"""
+"""Tests of :mod:`.tools`."""
 from shutil import copyfile
 
 import pandas as pd
 
-from message_ix_models.tools import iea_web
+from message_ix_models.tools.iea.web import (
+    FILE,
+    fuzz_data,
+    generate_code_lists,
+    load_data,
+)
 from message_ix_models.util import package_data_path
 
 
@@ -14,7 +16,7 @@ def test_iea_web(test_context, tmp_path):
     # Store in the temporary directory for this test
     test_context.cache_path = tmp_path.joinpath("cache")
 
-    result = iea_web.load_data()
+    result = load_data()
 
     # Result has the correct type
     assert isinstance(result, pd.DataFrame)
@@ -29,12 +31,12 @@ def test_iea_web(test_context, tmp_path):
 
 def test_generate_code_lists(test_context, tmp_path):
     # Copy the data file to a temporary directory
-    copyfile(package_data_path("iea", iea_web.FILE), tmp_path.joinpath(iea_web.FILE))
+    copyfile(package_data_path("iea", FILE), tmp_path.joinpath(FILE))
 
     # generate_code_lists() runs
-    iea_web.generate_code_lists(tmp_path)
+    generate_code_lists(tmp_path)
 
 
 def test_fuzz_data(test_context, tmp_path):
     # fuzz_data() runs
-    iea_web.fuzz_data(target_path=tmp_path)
+    fuzz_data(target_path=tmp_path)
