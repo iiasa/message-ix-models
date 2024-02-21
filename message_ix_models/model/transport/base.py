06a164f5c3f5ea86c20ca971f3ec9193f50b45ad
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-02-21 15:20:09 +0100

    Use where, not clip, on stage 1 scale factor

--- message_ix_models/model/transport/base.py
+++ message_ix_models/model/transport/base.py
@@ -2,6 +2,7 @@
 from functools import partial
 from typing import TYPE_CHECKING
 
+import numpy as np
 import pandas as pd
 
 if TYPE_CHECKING:
@@ -51,6 +52,16 @@ def prepare_reporter(rep: "message_ix.Reporter") -> str:
     # Final key
     targets = []
 
+    def _to_csv(base: "Key", name: str, hc):
+        """Helper to add computations to output data to CSV."""
+        # Some strings
+        csv, path, fn = f"{name} csv", f"{name} path", f"{name.replace(' ', '-')}.csv"
+        # Output path for this parameter
+        rep.add(path, "make_output_path", "config", "scenario", fn)
+        # Write to file
+        rep.add(csv, "write_report", base, path, hc)
+        targets.append(csv)
+
     e_iea = Key("energy:n-y-product-flow:iea")
     e_fnp = KeySeq(e_iea.drop("y"))
     e_cnlt = Key("energy:c-nl-t:iea+0")
@@ -77,20 +88,10 @@ def prepare_reporter(rep: "message_ix.Reporter") -> str:
     rep.add(s1[1], "convert_units", s1.base, units="1 / a")
     rep.add(s1[2], "mul", s1[1], Quantity(1.0, units="a"))
 
-    def _to_csv(base: "Key", name: str, hc):
-        """Helper to add computations to output data to CSV."""
-        # Some strings
-        csv, path, fn = f"{name} csv", f"{name} path", f"{name.replace(' ', '-')}.csv"
-        # Output path for this parameter
-        rep.add(path, "make_output_path", "config", "scenario", fn)
-        # Write to file
-        rep.add(csv, "write_report", base, path, hc)
-        targets.append(csv)
-
     _to_csv(s1[2], s1.name, dict(header_comment=SCALE_1_HEADER))
 
-    # Clip values to 1.0; this avoids x / 0 = inf
-    rep.add(s1[3], "clip", s1[2], lower=1.0)
+    # Replace 0 values to 1.0; this avoids x / 0 = inf
+    rep.add(s1[3], "where", s1[2], cond=lambda v: v > 1e-3 or v == np.inf, other=1.0)
     # Restore original "t" labels to scale-1
     rep.add(s1[4], "select", s1[3], "indexers::iea to transport")
     rep.add(s1[5], "rename_dims", s1[4], quote(dict(t_new="t")))
