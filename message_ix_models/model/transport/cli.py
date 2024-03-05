81a2108591b64447f25f6f55760185715301e888
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-03-05 16:48:59 +0100

    Combine transport gen-activity/debug with "run"

--- message_ix_models/model/transport/cli.py
+++ message_ix_models/model/transport/cli.py
@@ -21,14 +21,14 @@ Use :program:`mix-models transport --help` to show a help text like::
 
 Each individual command also has its own help text; try e.g. :program:`mix-models
 transport build --help`.
-
 """
+
 import logging
 from pathlib import Path
 
 import click
 from message_ix_models import ScenarioInfo
-from message_ix_models.util._logging import mark_time, silence_log
+from message_ix_models.util._logging import silence_log
 from message_ix_models.util.click import PARAMS, common_params, exec_cb
 from message_ix_models.workflow import make_click_command
 
@@ -77,9 +77,51 @@ cli.add_command(
                 ["--key", "report_key"], default="transport all", help="Key to report."
             ),
         ]
-        + [PARAMS[n] for n in "dest dry_run nodes quiet".split()],
+        + [PARAMS[n] for n in "dest nodes quiet".split()],
     )
 )
+
+
+@cli.command()
+@click.pass_obj
+def debug(context):
+    """Temporary code for development."""
+
+
+@cli.command("export-emi")
+@click.pass_obj
+@click.argument("path_stem")
+def export_emissions_factors(context, path_stem):
+    """Export emissions factors from base scenarios.
+
+    Only the subset corresponding to removed transport technologies are exported.
+    """
+    from datetime import datetime
+
+    from message_ix_models.util import private_data_path
+
+    # List of techs
+    techs = context.transport.set["technology"]["remove"]
+
+    # Load the targeted scenario
+    scenario = context.get_scenario()
+
+    # Output path
+    out_dir = private_data_path("transport", "emi")
+    out_dir.mkdir(exist_ok=True, parents=True)
+
+    for name in ("emission_factor", "relation_activity"):
+        df = scenario.par(name, filters=dict(technology=techs))
+
+        path = out_dir / f"{path_stem}-{name}.csv"
+        log.info(f"Write {len(df)} rows to {path}")
+
+        # Write header
+        path.write_text(
+            f"# Exported {datetime.today().isoformat()} from\n# {context.url}\n"
+        )
+        # Write data
+        df.to_csv(path, mode="a", index=False)
 
 
 @cli.command()
@@ -140,168 +182,3 @@ def migrate(context, version, check_base, parse, region, source_path, dest):
 
     # Transform the data
     transform(data, version, info)
-
-
-@cli.command("build")
-@common_params("dest dry_run regions quiet")
-@click.option("--future", "futures_scenario", help="Transport futures scenario")
-@click.option(
-    "--fast", is_flag=True, help="Skip removing data for removed set elements."
-)
-@click.option(
-    "--report",
-    "report_build",
-    is_flag=True,
-    help="Generate diagnostic reports of the built scenario.",
-)
-@click.pass_obj
-def build_cmd(context, report_build, **options):
-    """Prepare the model."""
-    from . import build
-
-    # Tidy options that were already handled by the top-level CLI
-    [options.pop(k) for k in "dest dry_run regions quiet".split()]
-
-    # Either clone from --dest, or create a new, bare RES
-    scenario = context.clone_to_dest()
-    platform = scenario.platform
-
-    # Build MESSAGEix-Transport
-    build.main(context, scenario, **options)
-
-    mark_time()
-
-    if report_build:
-        # Also output diagnostic reports
-        from message_ix_models.report import prepare_reporter, register
-
-        from . import report
-
-        register(report.callback)
-
-        rep, key = prepare_reporter(
-            scenario, context.get_config_file("report", "global")
-        )
-
-        # Add a catch-all key, including plots etc.
-        rep.add(
-            "_plots",
-            ["plot demand-exo", "plot var-cost", "plot fix-cost", "plot inv-cost"],
-        )
-
-        mark_time()
-
-        log.info(f"Report plots to {rep.graph['config']['output_dir']}")
-        log.debug(rep.describe("_plots"))
-
-        rep.get("_plots")
-
-        mark_time()
-
-    del platform
-
-
-@cli.command("gen-activity")
-@common_params("nodes years")
-@click.option("--ssp", type=click.Choice("12345"))
-@click.option("--ssp-update", type=click.Choice("12345"))
-@click.argument("output_dir", metavar="DIR", type=Path, required=False, default=None)
-@click.pass_obj
-def gen_activity(ctx, ssp, ssp_update, output_dir, **kwargs):
-    """Compute activity (demand) data and write to file in DIR."""
-    from message_ix_models.project.ssp import SSP_2017, SSP_2024
-
-    from . import build
-    from .plot import ComparePDT, ComparePDTCap
-
-    if ssp and ssp_update:
-        raise click.UsageError("--ssp is mutually exclusive with --ssp-update")
-    elif ssp:
-        options = {"ssp": SSP_2017[ssp]}
-        label = f"SSP_2017.{ssp}"
-    elif ssp_update:
-        options = {"ssp": SSP_2024[ssp_update]}
-        label = f"SSP_2024.{ssp_update}"
-    else:
-        raise click.UsageError("Must give one of --ssp or --ssp-update")
-
-    # Prepare a Computer instance for calculations
-    c = build.get_computer(ctx, options=options)
-
-    # Path to output file
-    output_dir = output_dir or ctx.get_local_path(
-        "transport", "gen-activity", f"{label}-{ctx.regions}-{ctx.years}"
-    )
-    c.configure(output_dir=output_dir)
-
-    # Compute total activity by mode
-    c.add("_1", "write_report", "pdt:n-y-t", output_dir.joinpath("pdt.csv"))
-    c.add(
-        "_2",
-        "write_report",
-        "transport pdt:n-y-t:capita",
-        output_dir.joinpath("pdt-cap.csv"),
-    )
-    c.add(
-        "_3", "write_report", "mode share:n-t-y", output_dir.joinpath("mode-share.csv")
-    )
-
-    key = c.add(
-        "gen-activity", ["_1", "_2", "_3", "plot demand-exo", "plot demand-exo-capita"]
-    )
-
-    log.info(f"Compute {repr(key)}")
-    log.info(c.describe(key))
-    output_dir.mkdir(exist_ok=True, parents=True)
-    c.get(key)
-    log.info(f"Wrote to {output_dir}")
-
-    # Compare data
-    for cls in ComparePDT, ComparePDTCap:
-        key = c.add(f"compare {cls.kind}", cls, output_dir.parent)
-        # FIXME on GitHub Actions—but *not* locally—the line above sets `key` to None.
-        #       Debug, fix, and then remove the line below.
-        key = f"compare {cls.kind}"
-        c.get(key)
-
-
-@cli.command()
-@click.pass_obj
-def debug(context):
-    """Temporary code for development."""
-
-
-@cli.command("export-emi")
-@click.pass_obj
-@click.argument("path_stem")
-def export_emissions_factors(context, path_stem):
-    """Export emissions factors from base scenarios.
-
-    Only the subset corresponding to removed transport technologies are exported.
-    """
-    from datetime import datetime
-
-    from message_ix_models.util import private_data_path
-
-    # List of techs
-    techs = context.transport.set["technology"]["remove"]
-
-    # Load the targeted scenario
-    scenario = context.get_scenario()
-
-    # Output path
-    out_dir = private_data_path("transport", "emi")
-    out_dir.mkdir(exist_ok=True, parents=True)
-
-    for name in ("emission_factor", "relation_activity"):
-        df = scenario.par(name, filters=dict(technology=techs))
-
-        path = out_dir / f"{path_stem}-{name}.csv"
-        log.info(f"Write {len(df)} rows to {path}")
-
-        # Write header
-        path.write_text(
-            f"# Exported {datetime.today().isoformat()} from\n# {context.url}\n"
-        )
-        # Write data
-        df.to_csv(path, mode="a", index=False)
