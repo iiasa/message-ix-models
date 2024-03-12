d3b370e62ab811ef6b5f3210e941d29500ea5944
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-03-12 03:57:19 +0100

    Use platformdirs.user_cache_path in @cached()

--- message_ix_models/util/cache.py
+++ message_ix_models/util/cache.py
@@ -4,24 +4,28 @@ This module extends :class:`genno.caching.Encoder` to handle classes common in
 :mod:`message_ix_models`, so these can be used as arguments to cached functions and
 included in the computed cache key:
 
-- :class:`sdmx.model.IdentifiableArtefact`, including :class:`.Code`: hashed as
-  their string representation / ID.
+- :class:`sdmx.model.IdentifiableArtefact`, including :class:`.Code`: hashed as their
+  string representation / ID.
 - :class:`ixmp.Platform`, :class:`xarray.Dataset`: ignored, with a warning logged.
 - :class:`ScenarioInfo`: only the :attr:`~ScenarioInfo.set` entries are hashed.
 
 """
+
 import json
 import logging
 from dataclasses import asdict, is_dataclass
-from typing import Callable
+from typing import TYPE_CHECKING, Callable, Set
 
 import genno.caching
 import ixmp
 import sdmx.model
 import xarray as xr
+from platformdirs import user_cache_path
 
-from .context import Context
 from .scenarioinfo import ScenarioInfo
+
+if TYPE_CHECKING:
+    from pathlib import Path
 
 log = logging.getLogger(__name__)
 
@@ -31,7 +35,7 @@ log = logging.getLogger(__name__)
 SKIP_CACHE = False
 
 # Paths already logged, to decrease verbosity
-PATHS_SEEN = set()
+PATHS_SEEN: Set["Path"] = set()
 
 
 # Show genno how to hash function arguments seen in message_ix_models
@@ -73,8 +77,7 @@ def cached(func: Callable) -> Callable:
     :doc:`genno:cache` in the :mod:`genno` documentation
     """
     # Determine and create the cache path
-    cache_path = Context.get_instance(-1).get_cache_path()
-    cache_path.mkdir(exist_ok=True, parents=True)
+    cache_path = user_cache_path("message-ix-models", ensure_exists=True)
 
     if cache_path not in PATHS_SEEN:
         log.debug(f"{func.__name__}() will cache in {cache_path}")
