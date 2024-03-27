fd5449a80f3986214c6a742cafc04c9c757b028d
Fridolin Glatter <glatter@iiasa.ac.at>
2024-03-27 11:14:14 +0100

    Use pytest.skip to skip untested snapshot

--- message_ix_models/tests/report/test_legacy.py
+++ message_ix_models/tests/report/test_legacy.py
@@ -17,10 +17,9 @@ log = logging.getLogger(__name__)
 @pytest.mark.snapshot
 def test_legacy_report(test_context, load_snapshot):
     # TODO This probably shouldn't be hardcoded
-    if load_snapshot.scenario == "baseline_v1":
-        scenario = load_snapshot
-    else:
-        return
+    if load_snapshot.scenario != "baseline_v1":
+        pytest.skip(reason="Test only latest version of public baseline snapshot.")
+    scenario = load_snapshot
 
     mp = test_context.get_platform()
 
