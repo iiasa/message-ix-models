3f6f204113dffc729528ea86a5e7f929339721c9
Fridolin Glatter <glatter@iiasa.ac.at>
2024-04-03 11:21:41 +0200

    Add missing regions to platform

--- message_ix_models/model/snapshot.py
+++ message_ix_models/model/snapshot.py
@@ -10,6 +10,7 @@ from message_ix.models import MACRO
 from tqdm import tqdm
 
 from message_ix_models import Spec
+from message_ix_models.report.legacy.get_nodes import get_nodes
 from message_ix_models.util import minimum_version
 from message_ix_models.util.pooch import SOURCE, fetch
 
@@ -117,6 +118,7 @@ def load(
     # Add units
     spec = Spec()
     spec.add.set["unit"] = get_codes(f"unit/snapshot-{snapshot_id}")
+    spec.add.set["node"] = get_nodes(scen=scenario)
     apply_spec(scenario, spec)
 
     # Initialize MACRO items
