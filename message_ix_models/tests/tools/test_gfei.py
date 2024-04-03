ba34560e56d69e1cb8f5bbcbe24245a4300fac8e
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-04-03 11:13:06 +0200

    Add .tools.gfei, tests
    - From message_data.tools.gfei_fuel_economy.
    - Converted to an ExoDataSource.

---
+++ message_ix_models/tests/tools/test_gfei.py
@@ -0,0 +1,23 @@
+import pytest
+from genno import Computer
+
+from message_ix_models.tools.exo_data import prepare_computer
+from message_ix_models.tools.gfei import GFEI  # noqa: F401
+
+
+class TestGFEI:
+    @pytest.mark.parametrize("regions, N_n", (("R12", 4),))
+    def test_prepare_computer(self, test_context, regions, N_n):
+        source = "GFEI"
+        source_kw = dict(plot=True)
+        test_context.model.regions = regions
+
+        c = Computer()
+
+        keys = prepare_computer(test_context, c, source, source_kw)
+
+        # Preparation of data and plotting runs successfully
+        result, *_ = c.get([keys[0], "plot GFEI debug"])
+
+        # Data have the expected dimensions
+        assert {"n", "t", "y"} == set(result.dims)
