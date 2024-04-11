980cdef5c6c4be5866b95c343caafd9c793fdb93
Fridolin Glatter <glatter@iiasa.ac.at>
2024-04-11 10:43:30 +0200

    Test on actual file, not monkeypatch

--- message_ix_models/tests/model/water/test_irrigation.py
+++ message_ix_models/tests/model/water/test_irrigation.py
@@ -1,6 +1,3 @@
-from unittest.mock import patch
-
-import pandas as pd
 from message_ix import Scenario
 
 from message_ix_models import ScenarioInfo
@@ -14,6 +11,7 @@ def test_add_irr_structure(test_context):
     # FIXME You probably want this to be part of a common setup rather than writing
     # something like this for every test
     context.type_reg = "country"
+    context.regions = "ZMB"
     nodes = get_codes(f"node/{context.regions}")
     nodes = list(map(str, nodes[nodes.index("World")].child))
     context.map_ISO_c = {context.regions: nodes[0]}
@@ -34,51 +32,44 @@ def test_add_irr_structure(test_context):
     # FIXME same as above
     context["water build info"] = ScenarioInfo(s)
 
-    # Mock the DataFrame read from CSV
-    df_node = pd.DataFrame({"BCU_name": ["1", "2"], "REGION": ["region1", "region2"]})
+    # Call the function to be tested
+    result = add_irr_structure(context)
 
-    # Mock the function 'private_data_path' to return the mocked DataFrame
-    with patch(
-        "message_ix_models.util.private_data_path", return_value="path/to/file"
-    ), patch("pandas.read_csv", return_value=df_node):
-        # Call the function to be tested
-        result = add_irr_structure(context)
-
-        # Assert the results
-        assert isinstance(result, dict)
-        assert "input" in result
-        assert "output" in result
-        assert all(
-            col in result["input"].columns
-            for col in [
-                "technology",
-                "value",
-                "unit",
-                "level",
-                "commodity",
-                "mode",
-                "time",
-                "time_origin",
-                "node_origin",
-                "node_loc",
-                "year_vtg",
-                "year_act",
-            ]
-        )
-        assert all(
-            col in result["output"].columns
-            for col in [
-                "technology",
-                "value",
-                "unit",
-                "level",
-                "commodity",
-                "mode",
-                "time",
-                "time_dest",
-                "node_loc",
-                "node_dest",
-                "year_vtg",
-                "year_act",
-            ]
-        )
+    # Assert the results
+    assert isinstance(result, dict)
+    assert "input" in result
+    assert "output" in result
+    assert all(
+        col in result["input"].columns
+        for col in [
+            "technology",
+            "value",
+            "unit",
+            "level",
+            "commodity",
+            "mode",
+            "time",
+            "time_origin",
+            "node_origin",
+            "node_loc",
+            "year_vtg",
+            "year_act",
+        ]
+    )
+    assert all(
+        col in result["output"].columns
+        for col in [
+            "technology",
+            "value",
+            "unit",
+            "level",
+            "commodity",
+            "mode",
+            "time",
+            "time_dest",
+            "node_loc",
+            "node_dest",
+            "year_vtg",
+            "year_act",
+        ]
+    )
