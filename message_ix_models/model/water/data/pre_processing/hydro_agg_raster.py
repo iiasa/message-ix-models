ee43a4ba2745952800dc161980aef7e9477e0864
Measrainsey Meng <measrainsey@pm.me>
2024-09-02 13:24:37 +0200

    Change to using `itertools.product` on selection of parameters
    - Removes nested for loops
    - Allow for running multiple selected parameters/variables
    - Reduces complexity of code

--- message_ix_models/model/water/data/pre_processing/hydro_agg_raster.py
+++ message_ix_models/model/water/data/pre_processing/hydro_agg_raster.py
@@ -14,97 +14,311 @@ import glob
 #  Import packages
 import os
 from datetime import datetime as dt
+from itertools import product
 
 import dask
-import numpy as np
 import xarray as xr
 
 start = dt.now()
 
-# TODO: set up path in a better way within the nexus/water module
-pdrive_path = "/Volumes/mengm.pdrv"
-# pdrive_path = "/pdrive/projects/"
+#: Specify user
+username = "meas"  # options: "meas", "mengm_unicc"
 
-# ene.model name
-enemodel = "ene.model"
-# enemodel = "ene.model3"  # unicc
+#: ISIMIP version
+isimipvers = "3b"
 
-# selections
-var = "qr"
-isimip = "3b"
-data = "future"
+#: Monthly scale
+monthlyscale = True
 
-# variable, for detailed symbols, refer to ISIMIP2b documentation
+#: Type of data
+#: Options: "historical", "future"
+datatype = ["future", "historical"]
+
+#: Water variables
+#: Options: "qtot", "dis", "qr", "qg"
+#: For detailed symbols, refer to ISIMIP2b documentation
+#: NOTE: "qg" (groundwater runoff) seems to be an option but
+#: no code has been written for aggregating this variable
 variables = [
     "qtot",  # total runoff
     "dis",  # discharge
-    "qg",  # groundwater runoff
-    "qr",
-]  # groudnwater recharge
+    "qr",  # groundwater recharge
+]
 
 
-if isimip == "2b":
-    climmodels = [
-        "gfdl-esm2m",
-        "hadgem2-es",
-        "ipsl-cm5a-lr",
-        "miroc5",
-    ]
+def set_scenarios(isimip):
+    if isimip == "2b":
+        climmodels = [
+            "gfdl-esm2m",
+            "hadgem2-es",
+            "ipsl-cm5a-lr",
+            "miroc5",
+        ]
 
-    scenarios = ["rcp26", "rcp60"]
-else:
-    climmodels = [
-        "gfdl-esm4",
-        "ipsl-cm6a-lr",
-        "mpi-esm1-2-hr",
-        "mri-esm2-0",
-        "ukesm1-0-ll",
-    ]
+        scenarios = ["rcp26", "rcp60"]
+    else:
+        climmodels = [
+            "gfdl-esm4",
+            "ipsl-cm6a-lr",
+            "mpi-esm1-2-hr",
+            "mri-esm2-0",
+            "ukesm1-0-ll",
+        ]
 
-    scenarios = ["ssp126", "ssp370"]
+        scenarios = ["ssp126", "ssp370"]
+    return climmodels, scenarios
 
 
-for cl in climmodels:
+def set_paths(user, isimip, cl, data):
+    # Specify path depending on user
+    if user == "meas":
+        pdrive_path = "/Volumes/mengm.pdrv"
+        enemodel = "ene.model"
+    elif user == "mengm_unicc":
+        pdrive_path = "/pdrive/projects/"
+        enemodel = "ene.model3"
+    else:
+        pdrive_path = "/pdrive/projects/"
+        enemodel = "ene.model"
+
+    # Set path depending on isimip scenario
+    if isimip == "2b":
+        path_hydrology = (
+            os.path.join(pdrive_path, enemodel, "NEST", "hydrology") + os.sep
+        )
+        path_water = (
+            os.path.join(
+                pdrive_path, "watxene", "ISIMIP", "ISIMIP2b", "output", "LPJmL"
+            )
+            + os.sep
+        )
+        path_isimip = (
+            os.path.join(pdrive_path, enemodel, "NEST", "hydrology", "isimip2b")
+            + os.sep
+        )
+    else:
+        path_hydrology = os.path.join(pdrive_path, enemodel, "NEST", "hydrology")
+        path_water = (
+            os.path.join(
+                pdrive_path,
+                "watxene",
+                "ISIMIP",
+                "ISIMIP3b",
+                "CWatM_results",
+                cl,
+                data,
+            )
+            + os.sep
+        )
+        path_isimip = (
+            os.path.join(
+                pdrive_path,
+                enemodel,
+                "NEST",
+                "hydrology",
+                "processed_nc4-isimip3b",
+            )
+            + os.sep
+        )
+    return path_hydrology, path_water, path_isimip
+
+
+def process_raster(user, isimip, cl, scen, var, data):
+    print(
+        "Processing ISIMIP "
+        + isimip
+        + " - "
+        + cl
+        + " - "
+        + scen
+        + " - "
+        + var
+        + " - "
+        + data
+    )
+
+    path_hydrology, path_water, path_isimip = set_paths(user, isimip, cl, data)
+
+    # define lat and long chunk for reducing computational load
+    latchunk = 120
+    lonchunk = 640
+
+    # NOTE: the previous version of the script had a spatialmethod parameter
+    # but this parameter is not used anywhere
+    # spatialmethod == "meansd" if var == "dis" else "sum"
+
+    # Open raster area file
+    # The file landareamaskmap0.nc can be found under
+    # P:\ene.model\NEST\delineation\data\delineated_basins_new
+    area = xr.open_dataarray(os.path.join(path_hydrology, "landareamaskmap0.nc"))
+
+    # TO AVOID ERROR WHEN OPENING AND SLICING INPUT DATA - CHECK!
+    dask.config.set({"array.slicing.split-large-chunks": False})
+
+    if data == "historical":
+        hydro_data = path_water + f"*{cl}*{var}*monthly*.nc"
+    elif data == "future":
+        hydro_data = path_water + f"*{cl}*{scen}*{var}*monthly*.nc"
+
+    files = glob.glob(hydro_data)
+    if var != "qr":
+        # Open hydrological data as a combined dataset
+        da = xr.open_mfdataset(files)
+    else:
+        # Open hydrological data as a combined dataset
+        da = xr.open_mfdataset(files)
+
+    if monthlyscale:
+        if var == "dis":
+            # # Converts the discharge  into km3/year
+            da = da * 0.031556952
+            da["dis"] = da.dis.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(da.dis.time)}
+            )
+
+            da.dis.attrs["unit"] = "km3/year"
+
+            # chunking reduces computational burden
+            da["dis"] = da.dis.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(da.dis.time)}
+            )
+
+            # saving the file here and reloading reduces disk space
+            da.to_netcdf(path_isimip + f"{var}_monthly__{cl}_{scen}.nc")
+        elif var == "qtot":
+            # Filter 5 year interval monthly time step
+            # da = da.sel(time=da.time.dt.year.isin(years))
+            # 1kg/m2/sec = 86400 mm/day
+            # 86400 mm/day X  Area (mm2) = 86400 mm3/day
+            # 86400 mm3/day = 86400 X  1000000 3.65 e-16 km3/year
+            da = da * 86400 * area * 3.65e-16 * 1000000
+            da.to_netcdf(path_isimip + f"{var}_monthly_{cl}_{scen}_{data}.nc")
+        elif var == "qr":
+            # Filter 5 year interval monthly time step
+            # da = da.sel(time=da.time.dt.year.isin(years))
+            # 1kg/m2/sec = 86400 mm/day
+            # 86400 mm/day X  Area (mm2) = 86400 mm3/day
+            # 86400 mm3/day = 86400 X  1000000 3.65 e-16 km3/year
+            da = da * 86400 * area * 3.65e-16 * 1000000
+            da.qr.attrs["unit"] = "km3/year"
+            da.to_netcdf(path_isimip + f"{var}_monthly_{cl}_{scen}_{data}.nc")
+
+    else:
+        if var == "dis":
+            da["dis"] = da.dis.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(da.dis.time)}
+            )
+            # Resample daily data to monthly (by summing daily values)
+            da = da.resample(time="Y").mean()
+
+            da["dis"] = da.dis.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(da.dis.time)}
+            )
+            # Take 20 year rolling average to make the time scale cosnistent
+            da = da.rolling(time=20).mean()
+
+            # Converts the discharge  into km3/year
+            da = da * 0.031556952
+            da.dis.attrs["unit"] = "km3/year"
+
+            # Long term Mean annual  discharge
+            davg = da.resample(time="30Y", loffset="4Y").mean()
+            davg["dis"] = davg.dis.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(davg.dis.time)}
+            )
+            davg.to_netcdf(path_isimip + f"{var}_90Y_avg_5y__{cl}_{scen}_temp_agg.nc")
+
+            # Now resample to an average value for each 5-year block, and
+            # offset by 2 years so that the value is centred to start 2101
+            da = da.resample(time="5Y", loffset="4Y").mean()
+
+            da.to_netcdf(path_isimip + f"{var}_5y__{cl}_{scen}_temp_agg.nc")
+            # read in saved data again to further process
+            da = xr.open_dataset(path_isimip + f"{var}_5y__{cl}_{scen}_temp_agg.nc")
+
+        elif var == "qtot":
+            da["qtot"] = da.qtot.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(da.qtot.time)}
+            )
+            # Resample daily data to monthly (by summing daily values)
+            da = da.resample(time="Y").mean()
+            da["qtot"] = da.qtot.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(da.qtot.time)}
+            )
+            da = da.rolling(time=20, min_periods=1).mean()
+            da["qtot"] = da.qtot.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(da.qtot.time)}
+            )
+            # Converts to total runoff per gs into km3/year
+            da = da * 86400 * area * 3.65e-16 * 1000000
+            da.qtot.attrs["unit"] = "km3/year"
+            da["qtot"] = da.qtot.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(da.qtot.time)}
+            )
+
+            # Now resample to an average value for each 5-year block
+            da = da.resample(time="5Y", loffset="4Y").mean()
+            da.to_netcdf(path_isimip + f"{var}_monthly__{cl}_{scen}_temp_agg.nc")
+
+        elif var == "qr":
+            da["qr"] = da.qr.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(da.qr.time)}
+            )
+            # Resample daily data to monthly (by summing daily values)
+            da = da.resample(time="Y").mean()
+            da["qr"] = da.qr.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(da.qr.time)}
+            )
+            da = da.rolling(time=20, min_periods=1).mean()
+            da["qr"] = da.qr.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(da.qr.time)}
+            )
+
+            # TODO check unit
+            # 1kg/m2/sec = 86400 mm/day
+            # 86400 mm/day X  Area (mm2) = 86400 mm3/day
+            # 86400 mm3/day = 86400 X  1000000 3.65 e-16 km3/year
+            da = da * 86400 * area * 3.65e-16 * 1000000
+            da.qr.attrs["unit"] = "km3/year"
+            da["qr"] = da.qr.chunk(
+                {"lat": latchunk, "lon": lonchunk, "time": len(da.qr.time)}
+            )
+
+            # Now resample to an average value for each 5-year block
+            da = da.resample(time="5Y", loffset="4Y").mean()
+
+            da.to_netcdf(path_isimip + f"{var}_5y__{cl}_{scen}.nc")
+
+    print(
+        "...Completed ISMIP "
+        + isimip
+        + " - "
+        + cl
+        + " - "
+        + scen
+        + " - "
+        + var
+        + " - "
+        + data
+    )
+
+
+if __name__ == "__main__":
+    # Select climate models and scenarios to run based on ISIMIP version
+    clim_models, scenarios = set_scenarios(isimipvers)
+
+    # Run the script for all combinations of user, ISIMIP version,
+    # climate model, scenario, variable, and data type
+    for user, isimip, cl, scen, var, data in product(
+        [username], [isimipvers], clim_models, scenarios, variables, datatype
+    ):
+        process_raster(user, isimip, cl, scen, var, data)
+
+
+for cl in clim_models:
     print(f"Processing {cl}...")
     for scen in scenarios:
         print(f"- Started {scen}...")
-        if isimip == "2b":
-            wd1 = os.path.join(pdrive_path, enemodel, "NEST", "hydrology") + os.sep
-            wd = (
-                os.path.join(
-                    pdrive_path, "watxene", "ISIMIP", "ISIMIP2b", "output", "LPJmL"
-                )
-                + os.sep
-            )
-            wd2 = (
-                os.path.join(pdrive_path, enemodel, "NEST", "hydrology", "isimip2b")
-                + os.sep
-            )
-        else:
-            wd1 = os.path.join(pdrive_path, enemodel, "NEST", "hydrology")
-            wd = (
-                os.path.join(
-                    pdrive_path,
-                    "watxene",
-                    "ISIMIP",
-                    "ISIMIP3b",
-                    "CWatM_results",
-                    f"{cl}",
-                    f"{data}",
-                )
-                + os.sep
-            )
-            wd2 = (
-                os.path.join(
-                    pdrive_path,
-                    enemodel,
-                    "NEST",
-                    "hydrology",
-                    "processed_nc4-isimip3b",
-                )
-                + os.sep
-            )
-
         # Define if monthly aggregation is required
         monthlyscale = True
         # define qauantile for statistical aggregation
@@ -127,15 +341,15 @@ for cl in climmodels:
         # Open raster area file
         # The file landareamaskmap0.nc can be found under
         # P:\ene.model\NEST\delineation\data\delineated_basins_new
-        area = xr.open_dataarray(os.path.join(wd1, "landareamaskmap0.nc"))
+        area = xr.open_dataarray(os.path.join(path_hydrology, "landareamaskmap0.nc"))
 
         # TO AVOID ERROR WHEN OPENING AND SLICING INPUT DATA - CHECK!
         dask.config.set({"array.slicing.split-large-chunks": False})
 
         if data == "historical":
-            hydro_data = wd + f"*{cl}*{var}*monthly*.nc"
+            hydro_data = path_water + f"*{cl}*{var}*monthly*.nc"
         elif data == "future":
-            hydro_data = wd + f"*{cl}*{scen}*{var}*monthly*.nc"
+            hydro_data = path_water + f"*{cl}*{scen}*{var}*monthly*.nc"
 
         files = glob.glob(hydro_data)
         if var != "qr":
@@ -145,14 +359,14 @@ for cl in climmodels:
             # Open hydrological data as a combined dataset
             da = xr.open_mfdataset(files)
 
-            # da.to_netcdf(wd+f'\{var}_memean_{scen}.nc')
+            # da.to_netcdf(path_water+f'\{var}_memean_{scen}.nc')
 
         if monthlyscale:
             years = np.arange(2010, 2105, 5)  # NOTE: same for true and false
         else:
             years = np.arange(2010, 2105, 5)
 
-        # ds = xr.open_dataset(wd+f'{var}_memean_{scen}.nc')
+        # ds = xr.open_dataset(path_water+f'{var}_memean_{scen}.nc')
         # Resample daily data to monthly by summing daily values
 
         # if var != "qr"\:
@@ -178,7 +392,7 @@ for cl in climmodels:
                     {"lat": latchunk, "lon": lonchunk, "time": len(da.dis.time)}
                 )
                 # saving the file here and reloading reduces disk space
-                da.to_netcdf(wd2 + f"{var}_monthly__{cl}_{scen}.nc")
+                da.to_netcdf(path_isimip + f"{var}_monthly__{cl}_{scen}.nc")
             elif var == "qtot":
                 # da["qtot"] = da.qtot.chunk(
                 #     {"lat": latchunk, "lon": lonchunk, "time": len(da.qtot.time)}
@@ -202,7 +416,7 @@ for cl in climmodels:
                 #     {"lat": latchunk, "lon": lonchunk, "time": len(da.qtot.time)}
                 # )
 
-                da.to_netcdf(wd2 + f"{var}_monthly_{cl}_{scen}_{data}.nc")
+                da.to_netcdf(path_isimip + f"{var}_monthly_{cl}_{scen}_{data}.nc")
             elif var == "qr":
                 # Filter 5 year interval monthly time step
                 # da = da.sel(time=da.time.dt.year.isin(years))
@@ -215,7 +429,7 @@ for cl in climmodels:
                 #     {"lat": latchunk, "lon": lonchunk, "time": len(da.qr.time)}
                 # )
 
-                da.to_netcdf(wd2 + f"{var}_monthly_{cl}_{scen}_{data}.nc")
+                da.to_netcdf(path_isimip + f"{var}_monthly_{cl}_{scen}_{data}.nc")
 
         else:
             if var == "dis":
@@ -239,15 +453,17 @@ for cl in climmodels:
                 davg["dis"] = davg.dis.chunk(
                     {"lat": latchunk, "lon": lonchunk, "time": len(davg.dis.time)}
                 )
-                davg.to_netcdf(wd2 + f"{var}_90Y_avg_5y__{cl}_{scen}_temp_agg.nc")
+                davg.to_netcdf(
+                    path_isimip + f"{var}_90Y_avg_5y__{cl}_{scen}_temp_agg.nc"
+                )
 
                 # Now resample to an average value for each 5-year block, and
                 # offset by 2 years so that the value is centred to start 2101
                 da = da.resample(time="5Y", loffset="4Y").mean()
 
-                da.to_netcdf(wd2 + f"{var}_5y__{cl}_{scen}_temp_agg.nc")
+                da.to_netcdf(path_isimip + f"{var}_5y__{cl}_{scen}_temp_agg.nc")
                 # read in saved data again to further process
-                da = xr.open_dataset(wd2 + f"{var}_5y__{cl}_{scen}_temp_agg.nc")
+                da = xr.open_dataset(path_isimip + f"{var}_5y__{cl}_{scen}_temp_agg.nc")
 
             elif var == "qtot":
                 da["qtot"] = da.qtot.chunk(
@@ -273,10 +489,10 @@ for cl in climmodels:
                 # da = da.rolling(time = 20).mean()
                 # Now resample to an average value for each 5-year block
                 da = da.resample(time="5Y", loffset="4Y").mean()
-                da.to_netcdf(wd2 + f"{var}_monthly__{cl}_{scen}_temp_agg.nc")
+                da.to_netcdf(path_isimip + f"{var}_monthly__{cl}_{scen}_temp_agg.nc")
                 # read in saved data again to further process
                 # da = xr.\
-                #    open_dataset( wd2+f'\output\{var}_5y__{climmodel}_{scen}_temp_agg.nc')
+                #    open_dataset( path_isimip+f'\output\{var}_5y__{climmodel}_{scen}_temp_agg.nc')
                 # da['qtot'] = da.qtot.chunk({'lat': latchunk, 'lon': lonchunk,\
                 #                                    'time': len(da.qtot.time)})
 
@@ -308,6 +524,6 @@ for cl in climmodels:
                 # Now resample to an average value for each 5-year block
                 da = da.resample(time="5Y", loffset="4Y").mean()
 
-                da.to_netcdf(wd2 + f"{var}_5y__{cl}_{scen}.nc")
+                da.to_netcdf(path_isimip + f"{var}_5y__{cl}_{scen}.nc")
 
         print("....Completed " + cl + " + " + scen)
