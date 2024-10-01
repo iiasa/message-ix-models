391afb92b75e81f0cd6f5f0baad0471c64ae66cb
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-10-01 16:29:37 +0200

    Implement postprocessing for aviation emissions

--- message_ix_models/tests/project/ssp/test_transport.py
+++ message_ix_models/tests/project/ssp/test_transport.py
@@ -1,8 +1,19 @@
 from message_ix_models.project.ssp.transport import main
+from message_ix_models.util import package_data_path
 
 
-def test_main(tmp_path):
+def test_main(tmp_path) -> None:
     # Locate a temporary data file
-    p = tmp_path.joinpath("input.xlsx")
+    path_in = package_data_path(
+        "test",
+        "report",
+        "SSP_dev_SSP2_v0.1_Blv0.18"
+        "_baseline_prep_lu_bkp_solved_materials_2025_macro.csv",
+    )
+    path_out = tmp_path.joinpath("output.csv")
 
-    main(p)
+    # Code runs
+    main(path_in=path_in, path_out=path_out)
+
+    # Output path exists
+    assert path_out.exists()
