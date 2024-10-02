803f545005fbc037f5aa9d6189a7bb96e68f0f64
Measrainsey Meng <measrainsey@pm.me>
2024-10-02 13:36:58 +0200

    Add script to modify Low scenarios

---
+++ message_ix_models/project/ssp/script/scenarios/low.py
@@ -0,0 +1,106 @@
+import ixmp  # type: ignore
+import message_ix  # type: ignore
+
+from message_ix_models.project.ssp.script.util.functions import (
+    add_balance_equality,
+    add_steel_sector_nze,
+    modify_steel_growth,
+    modify_steel_initial,
+    remove_bof_steel_lower,
+)
+
+# path_ue = "/Users/meas/iiasagit/scenariomip-review/scenariomip_review/data/ue-shares"
+path_ue = "/home/mengm/repo/scenariomip-review/scenariomip_review/data/ue-shares"
+
+# selections
+sel_scen = "SSP1"
+scen_suffix = ""
+# scen_vers = 1
+rem_bof_steel = True
+mod_growth_steel = True
+mod_initial_steel = True
+add_steel_target = False
+
+# parameters
+trp_year_start = 2035
+mult_price = 5.5
+rc_years = [2060, 2070, 2080, 2090, 2100, 2110]
+steel_years = [2030, 2035, 2040, 2045, 2050, 2055, 2060, 2070, 2080, 2090, 2100, 2110]
+steel_growth = 0.075
+steel_inital = 1.0
+nze_targets = [
+    4.2,
+    3.7,
+    3.2,
+    2.7,
+]
+
+# model and scenario names
+snames = {"SSP1": "SSP1 - Low Emissions", "SSP2": "SSP2 - Low Emissions"}
+svers = {"SSP1": 1, "SSP2": 1}
+model_orig = "SSP_" + sel_scen + "_v1.0"
+scenario_orig = snames[sel_scen]
+
+# add scen_suffic depending on remove_bof_steel, modify_lc_steel,
+# steel_scalar, and add_steel_target
+if rem_bof_steel:
+    scen_suffix += "_bof"
+if mod_growth_steel:
+    scen_suffix += "_growth"  # + str(steel_growth)
+if mod_initial_steel:
+    scen_suffix += "_initial"  # + str(steel_inital)
+if add_steel_target:
+    scen_suffix += "_nzsteel"
+
+model_target = "MM_ScenarioMIP"
+scenario_target = "Low_" + sel_scen + scen_suffix  # + "_v" + str(scen_vers)
+
+mp = ixmp.Platform("ixmp_dev")
+s_orig = message_ix.Scenario(
+    mp, model=model_orig, scenario=scenario_orig, version=svers[sel_scen]
+)
+s_tar = s_orig.clone(model_target, scenario_target, keep_solution=False)
+s_tar.set_as_default()
+
+
+# modify steel sector
+if rem_bof_steel:
+    remove_bof_steel_lower(s_tar, steel_years)
+
+if mod_growth_steel:
+    modify_steel_growth(
+        s_tar,
+        ["dri_gas_steel", "dri_h2_steel", "eaf_steel"],
+        steel_years,
+        steel_growth,
+    )
+
+if mod_initial_steel:
+    modify_steel_initial(
+        s_tar,
+        ["dri_gas_steel", "dri_h2_steel"],
+        steel_years,
+        steel_inital,
+    )
+
+if add_steel_target:
+    add_steel_sector_nze(s_tar, nze_targets)
+
+# add balance equality
+add_balance_equality(s_tar)
+
+# solve parameters
+# message_ix.models.DEFAULT_CPLEX_OPTIONS = {
+#     "advind": 0,
+#     "lpmethod": 4,
+#     "threads": 4,
+#     "epopt": 1e-6,
+#     "scaind": -1,
+# }
+
+solve_typ = "MESSAGE-MACRO"
+solve_args = dict(model=solve_typ)
+s_tar.solve(**solve_args)
+s_tar.set_as_default()
+
+mp.close_db()
