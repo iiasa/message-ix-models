abbe1cb03e20ed940a5f9e0c7d6089621d07a46c
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-10-02 15:41:34 +0200

    Implement CLI for .ssp.transport

--- message_ix_models/tests/project/ssp/test_transport.py
+++ message_ix_models/tests/project/ssp/test_transport.py
@@ -3,6 +3,7 @@ from message_ix_models.util import package_data_path
 
 
 def test_main(tmp_path) -> None:
+    """Code can be called from Python."""
     # Locate a temporary data file
     path_in = package_data_path(
         "test",
@@ -17,3 +18,36 @@ def test_main(tmp_path) -> None:
 
     # Output path exists
     assert path_out.exists()
+
+
+def test_cli(tmp_path, mix_models_cli) -> None:
+    """Code can be invoked from the command-line."""
+    from shutil import copyfile
+
+    # Locate a temporary data file
+    input_file = package_data_path(
+        "test",
+        "report",
+        "SSP_dev_SSP2_v0.1_Blv0.18"
+        "_baseline_prep_lu_bkp_solved_materials_2025_macro.xlsx",
+    )
+    path_in = tmp_path.joinpath(input_file.name)
+
+    # Copy the input file to the test data directory
+    copyfile(input_file, path_in)
+
+    # Code runs
+    result = mix_models_cli.invoke(["ssp", "transport", f"{path_in}"])
+    assert 0 == result.exit_code, result.output
+
+    # Output path was determined automatically and exists
+    path_out = tmp_path.joinpath(path_in.stem + "_out.xlsx")
+    assert path_out.exists()
+
+    # Messages were printed about file handling
+    for message in (
+        "Convert Excel input to ",
+        "No PATH_OUT given; write to ",
+        "Convert CSV output to ",
+    ):
+        assert message in result.output
