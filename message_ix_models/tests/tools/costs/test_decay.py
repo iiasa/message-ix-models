9501b402664d964a172c7de364c6f58ae095fd96
Measrainsey Meng <measrainsey@pm.me>
2024-12-02 17:10:14 +0100

    Add tests for `_get_module_cost_reduction()` and `_get_module_scenarios_reduction()`

--- message_ix_models/tests/tools/costs/test_decay.py
+++ message_ix_models/tests/tools/costs/test_decay.py
@@ -1,19 +1,89 @@
 from typing import Literal
 
+import pandas as pd
 import pytest
 
 from message_ix_models.tools.costs import Config
 from message_ix_models.tools.costs.decay import (
+    _get_module_cost_reduction,
+    _get_module_scenarios_reduction,
     get_technology_reduction_scenarios_data,
     project_ref_region_inv_costs_using_reduction_rates,
 )
 from message_ix_models.tools.costs.regional_differentiation import (
     apply_regional_differentiation,
+    get_raw_technology_mapping,
+    subset_module_map,
 )
 
 
+@pytest.mark.parametrize(
+    "module, t_exp",
+    (
+        ("energy", {"coal_ppl", "gas_ppl", "gas_cc", "solar_res1"}),
+        ("materials", {"biomass_NH3", "MTO_petro", "furnace_foil_steel"}),
+        ("cooling", {"coal_ppl__cl_fresh", "gas_cc__air", "nuc_lc__ot_fresh"}),
+    ),
+)
+def test_get_module_scenarios_reduction(
+    module: Literal["energy", "materials", "cooling"], t_exp
+) -> None:
+    tech_map = energy_map = get_raw_technology_mapping("energy")
+
+    # if module is not energy, run subset_module_map
+    if module != "energy":
+        module_map = get_raw_technology_mapping(module)
+        module_sub = subset_module_map(module_map)
+
+        # Remove energy technologies that exist in module mapping
+        energy_map = energy_map.query(
+            "message_technology not in @module_sub.message_technology"
+        )
+
+        tech_map = pd.concat([energy_map, module_sub], ignore_index=True)
+
+    result = _get_module_scenarios_reduction(module, energy_map, tech_map)
+
+    # Expected MESSAGEix-GLOBIOM technologies are present in the data
+    assert t_exp <= set(result.message_technology.unique())
+
+
+@pytest.mark.parametrize(
+    "module, t_exp",
+    (
+        ("energy", {"coal_ppl", "gas_ppl", "gas_cc", "solar_res1"}),
+        ("materials", {"biomass_NH3", "MTO_petro", "furnace_foil_steel"}),
+        ("cooling", {"coal_ppl__cl_fresh", "gas_cc__air", "nuc_lc__ot_fresh"}),
+    ),
+)
+def test_get_module_cost_reduction(
+    module: Literal["energy", "materials", "cooling"], t_exp
+) -> None:
+    tech_map = energy_map = get_raw_technology_mapping("energy")
+
+    # if module is not energy, run subset_module_map
+    if module != "energy":
+        module_map = get_raw_technology_mapping(module)
+        module_sub = subset_module_map(module_map)
+
+        # Remove energy technologies that exist in module mapping
+        energy_map = energy_map.query(
+            "message_technology not in @module_sub.message_technology"
+        )
+
+        tech_map = pd.concat([energy_map, module_sub], ignore_index=True)
+
+    # The function runs without error
+    result = _get_module_cost_reduction(module, energy_map, tech_map)
+
+    # Expected MESSAGEix-GLOBIOM technologies are present in the data
+    assert t_exp <= set(result.message_technology.unique())
+
+
 @pytest.mark.parametrize("module", ("energy", "materials", "cooling"))
-def test_get_technology_reduction_scenarios_data(module: str) -> None:
+def test_get_technology_reduction_scenarios_data(
+    module: Literal["energy", "materials", "cooling"],
+) -> None:
     config = Config()
     # The function runs without error
     result = get_technology_reduction_scenarios_data(config.y0, module=module)
