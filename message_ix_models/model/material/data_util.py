f958a24b579afac36aa7fd8fbb3dfdd3e5ddaa67
Florian Maczek <maczek@iiasa.ac.at>
2025-01-26 13:28:16 +0100

    Refactor aluminum input data
    - Move SSP differentiated parameters from input data to functions
    - Remove duplicated input files for SSPs
    - Change input data format from xlsx to csv
    - Move unformatted data files into subdirectory

--- message_ix_models/model/material/data_util.py
+++ message_ix_models/model/material/data_util.py
@@ -319,9 +319,12 @@ def read_timeseries(
     material = f"{material}/{ssp}" if ssp else material
     # Read the file
 
-    df = pd.read_excel(
-        package_data_path("material", material, filename), sheet_name=sheet_n
-    )
+    if filename.endswith(".csv"):
+        df = pd.read_csv(package_data_path("material", material, filename))
+    else:
+        df = pd.read_excel(
+            package_data_path("material", material, filename), sheet_name=sheet_n
+        )
 
     import numbers
 
@@ -379,10 +382,12 @@ def read_rel(
         sheet_n = "relations_R11"
     material = f"{material}/{ssp}" if ssp else material
     # Read the file
-    data_rel = pd.read_excel(
-        package_data_path("material", material, filename),
-        sheet_name=sheet_n,
-    )
+    if filename.endswith(".csv"):
+        data_rel = pd.read_csv(package_data_path("material", material, filename))
+    else:
+        data_rel = pd.read_excel(
+            package_data_path("material", material, filename), sheet_name=sheet_n
+        )
 
     return data_rel
 
@@ -1435,7 +1440,7 @@ def calculate_ini_new_cap(
             CLINKER_RATIO = 0.72
         if material == "steel":
             SCALER = 0.002
-            CLINKER_RATIO= 1
+            CLINKER_RATIO = 1
     elif ssp in ["SSP4", "SSP5"]:
         if material == "cement":
             SCALER = 0.002
