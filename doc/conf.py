178d5d064bd2e46e9c1f9177ecaa7cc1fbf0c834
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-01-28 21:38:20 +0100

    Update copyright year to 2025

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
 # -- Project information ---------------------------------------------------------------
 
 project = "message-ix-models"
-copyright = "2020–2024, IIASA Energy, Climate, and Environment (ECE) Program"
+copyright = "2020–%Y, IIASA Energy, Climate, and Environment (ECE) Program"
 author = "IIASA Energy, Climate, and Environment (ECE) Program"
 
 
