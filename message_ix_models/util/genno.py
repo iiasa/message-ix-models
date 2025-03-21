a9d50a5d67237bead4a46b3e9f4ec93dc65a5658
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-03-21 15:45:57 +0100

    Add .util.genno.Collector
    - Move merge_data() from .model.transport.operator to .report.operator.

--- message_ix_models/util/genno.py
+++ message_ix_models/util/genno.py
@@ -3,10 +3,12 @@
 Most code appearing here **should** be migrated upstream, to genno itself.
 """
 
+from collections.abc import Callable
 from typing import TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
-    from genno.types import AnyQuantity
+    from genno import Computer
+    from genno.types import AnyQuantity, KeyLike
 
 try:
     from genno.operator import as_quantity
@@ -44,6 +46,67 @@ except ImportError:
     Keys = SimpleNamespace  # type: ignore [assignment, misc]
 
 __all__ = [
+    "Collector",
     "Keys",
     "as_quantity",
 ]
+
+
+class Collector:
+    """Helper class to collect and merge data at a target key.
+
+    Example usage:
+
+    .. code-block:: python
+
+       # Create a Collector instance
+       collector = Collector(target="FOO", key_cb="{}::foo".format)
+
+       # Associate it with a particular Computer
+       c = collector.computer = Computer()
+
+       # Add a task
+       collect("bar", func, "k1", "k2", arg1="baz", arg2="qux")
+
+    These statements have the following effects:
+
+    1. Add to `c` a task with the key "FOO" that calls :func:`.merge_data` on 1 or more
+       inputs.
+    2. Construct a key "bar::foo" using the `key_cb`.
+    3. Add a task at "bar::foo" that calls :py:`func(k1, k2, arg1="baz", arg2="qux")`.
+    4. Add "bar::foo" (denoting the output of (3)) to the keys merged by (1).
+    """
+
+    __slots__ = ("_computer", "_key_cb", "_target")
+
+    def __init__(
+        self, target: "KeyLike", key_cb: Callable[["KeyLike"], "KeyLike"]
+    ) -> None:
+        self._target = target
+        self._key_cb = key_cb
+
+    @property
+    def computer(self) -> "Computer":
+        return self._computer
+
+    @computer.setter
+    def computer(self, c: "Computer") -> None:
+        from message_ix_models.report.operator import merge_data
+
+        self._computer = c
+        # Add the computation that merges data for this Collector
+        assert self._target not in self._computer
+        self._computer.graph[self._target] = (merge_data,)
+
+    def __call__(self, _target_name: str, *args, **kwargs) -> "KeyLike":
+        # Construct a key using the callback
+        key = self._key_cb(_target_name)
+
+        # Add a computation at `key` using the `args` and `kwargs`
+        c = self._computer
+        c.add(key, *args, **kwargs)
+
+        # Extend the keys to be collected with `key`
+        c.graph[self._target] = c.graph[self._target] + (key,)
+
+        return key
