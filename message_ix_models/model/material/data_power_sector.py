306c0592c3ce90644b13257bd0433edc4f3a2245
Florian Maczek <macflo@gmx.at>
2025-03-24 14:52:11 +0100

    Add power sector module to build

--- message_ix_models/model/material/data_power_sector.py
+++ message_ix_models/model/material/data_power_sector.py
@@ -494,12 +494,13 @@ def gen_data_power_sector(
 
     int_dict = read_material_intensities(str(data_path), inv_cost)
     for k, v in int_dict.items():
+        print(k, v.columns)
         results[k].append(v)
 
     # create new parameters input_cap_new, output_cap_new, input_cap_ret,
     # output_cap_ret, input_cap and output_cap if they don't exist
-    with scenario.transact():
-        maybe_init_pars(scenario)
+    # with scenario.transact():
+    maybe_init_pars(scenario)
 
     # Concatenate to one data frame per parameter
     return {par_name: pd.concat(dfs) for par_name, dfs in results.items()}
