0ce850f8ff814fa84e94fbcdc85e0b9ab7bca161
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-05-02 16:57:52 +0200

    Sort and rename contents in .transport.test_build
    - Remove redundant "_build_" from test names.

--- message_ix_models/tests/model/transport/test_build.py
+++ message_ix_models/tests/model/transport/test_build.py
@@ -47,168 +47,6 @@ if TYPE_CHECKING:
 log = logging.getLogger(__name__)
 
 
-@pytest.fixture
-def N_node(request) -> int:
-    """Expected number of nodes, by introspection of other parameter values."""
-    if "build_kw" in request.fixturenames:
-        build_kw = request.getfixturevalue("build_kw")
-
-        # NB This could also be done by len(.model.structure.get_codelist(…)), but hard-
-        #    coding is probably a little faster
-        return {"ISR": 1, "R11": 11, "R12": 12, "R14": 14}[build_kw["regions"]]
-    else:
-        raise NotImplementedError
-
-
-@pytest.mark.parametrize("years", [None, "A", "B"])
-@pytest.mark.parametrize(
-    "regions_arg, regions_exp",
-    [
-        ("R11", "R11"),
-        ("R12", "R12"),
-        ("R14", "R14"),
-        ("ISR", "ISR"),
-    ],
-)
-def test_make_spec(regions_arg, regions_exp, years):
-    # The spec can be generated
-    spec = structure.make_spec(regions_arg)
-
-    # The required elements of the "node" set match the configuration
-    nodes = get_codes(f"node/{regions_exp}")
-    expected = list(map(str, nodes[nodes.index("World")].child))
-    assert expected == spec["require"].set["node"]
-
-
-@MARK[10]
-@MARK[7]
-@build.get_computer.minimum_version
-@pytest.mark.parametrize(
-    "regions, years, dummy_LDV, nonldv, solve",
-    [
-        param("R11", "B", True, None, False, marks=MARK[1]),
-        param(  # 44s; 31 s with solve=False
-            "R11",
-            "A",
-            True,
-            None,
-            True,
-            marks=[
-                MARK[1],
-                pytest.mark.xfail(
-                    raises=ixmp.ModelError,
-                    reason="No supply of non-LDV commodities w/o IKARUS data",
-                ),
-            ],
-        ),
-        param("R11", "A", False, "IKARUS", False, marks=MARK[1]),  # 43 s
-        param("R11", "A", False, "IKARUS", True, marks=[mark.slow, MARK[1]]),  # 74 s
-        # R11, B
-        param("R11", "B", False, "IKARUS", False, marks=[mark.slow, MARK[1]]),
-        param("R11", "B", False, "IKARUS", True, marks=[mark.slow, MARK[1]]),
-        # R12, B
-        param("R12", "B", False, "IKARUS", True, marks=MARK["gh-337"]),
-        # R14, A
-        param(
-            "R14",
-            "A",
-            False,
-            "IKARUS",
-            False,
-            marks=[mark.slow, make_mark[2](genno.ComputationError)],
-        ),
-        # Pending iiasa/message_data#190
-        param("ISR", "A", True, None, False, marks=MARK[3]),
-    ],
-)
-def test_build_bare_res(
-    request, tmp_path, test_context, regions, years, dummy_LDV: bool, nonldv, solve
-):
-    """.transport.build() works on the bare RES, and the model solves."""
-    # Generate the relevant bare RES
-    ctx = test_context
-    ctx.update(regions=regions, years=years)
-    scenario = bare_res(request, ctx)
-
-    # Build succeeds without error
-    options = {
-        "data source": {"non-LDV": nonldv},
-        "dummy_LDV": dummy_LDV,
-        "dummy_supply": True,
-    }
-    build.main(ctx, scenario, options)
-
-    # dump_path = tmp_path / "scenario.xlsx"
-    # log.info(f"Dump contents to {dump_path}")
-    # scenario.to_excel(dump_path)
-
-    if solve:
-        scenario.solve(solve_options=dict(lpmethod=4, iis=1))
-
-        # commented: Appears to be giving a false negative
-        # # Use Reporting calculations to check the result
-        # result = report.check(scenario)
-        # assert result.all(), f"\n{result}"
-
-
-@pytest.mark.ece_db
-@pytest.mark.parametrize(
-    "url",
-    (
-        "ixmp://ene-ixmp/CD_Links_SSP2_v2/baseline",
-        "ixmp://ixmp-dev/ENGAGE_SSP2_v4.1.7/EN_NPi2020_1000f",
-        "ixmp://ixmp-dev/ENGAGE_SSP2_v4.1.7/baseline",
-        "ixmp://ixmp-dev/ENGAGE_SSP2_v4.1.7_ar5_gwp100/EN_NPi2020_1000_emif_new",
-        "ixmp://ixmp-dev/MESSAGEix-GLOBIOM_R12_CHN/baseline#17",
-        "ixmp://ixmp-dev/MESSAGEix-GLOBIOM_R12_CHN/baseline_macro#3",
-        # Local clones of the above
-        # "ixmp://clone-2021-06-09/ENGAGE_SSP2_v4.1.7/baseline",
-        # "ixmp://clone-2021-06-09/ENGAGE_SSP2_v4.1.7/EN_NPi2020_1000f",
-        # "ixmp://local/MESSAGEix-Transport on ENGAGE_SSP2_v4.1.7/baseline",
-    ),
-)
-def test_build_existing(tmp_path, test_context, url, solve=False):
-    """Test that model.transport.build works on certain existing scenarios.
-
-    These are the ones listed in the documenation, at :ref:`transport-base-scenarios`.
-    """
-    ctx = test_context
-
-    # Update the Context with the base scenario's `url`
-    ctx.handle_cli_args(url=url)
-
-    # Destination for built scenarios: uncomment one of
-    # the platform prepared by the text fixture…
-    ctx.dest_platform = copy(ctx.platform_info)
-    # # or, a specific, named platform.
-    # ctx.dest_platform = dict(name="local")
-
-    # New model name for the destination scenario
-    ctx.dest_scenario = copy(ctx.scenario_info)
-    ctx.dest_scenario["model"] = f"{ctx.dest_scenario['model']} +transport"
-
-    # Clone the base scenario to the test platform
-    scenario = ctx.clone_to_dest(create=False)
-    mp = scenario.platform
-
-    # Build succeeds without error
-    build.main(ctx, scenario)
-
-    # commented: slow
-    # dump_path = tmp_path / "scenario.xlsx"
-    # log.info(f"Dump contents to {dump_path}")
-    # scenario.to_excel(dump_path)
-
-    if solve:
-        scenario.solve(solve_options=dict(lpmethod=4))
-
-        # Use Reporting calculations to check the result
-        result = report.check(scenario)
-        assert result.all(), f"\n{result}"
-
-    del mp
-
-
 class LDV_PHEV_input(Check):
     """Check magnitudes of input energy intensities of LDV PHEV technologies.
 
@@ -429,6 +267,90 @@ CHECKS: dict["KeyLike", tuple[Check, ...]] = {
 }
 
 
+@pytest.fixture
+def N_node(request) -> int:
+    """Expected number of nodes, by introspection of other parameter values."""
+    if "build_kw" in request.fixturenames:
+        build_kw = request.getfixturevalue("build_kw")
+
+        # NB This could also be done by len(.model.structure.get_codelist(…)), but hard-
+        #    coding is probably a little faster
+        return {"ISR": 1, "R11": 11, "R12": 12, "R14": 14}[build_kw["regions"]]
+    else:
+        raise NotImplementedError
+
+
+@MARK[10]
+@MARK[7]
+@build.get_computer.minimum_version
+@pytest.mark.parametrize(
+    "regions, years, dummy_LDV, nonldv, solve",
+    [
+        param("R11", "B", True, None, False, marks=MARK[1]),
+        param(  # 44s; 31 s with solve=False
+            "R11",
+            "A",
+            True,
+            None,
+            True,
+            marks=[
+                MARK[1],
+                pytest.mark.xfail(
+                    raises=ixmp.ModelError,
+                    reason="No supply of non-LDV commodities w/o IKARUS data",
+                ),
+            ],
+        ),
+        param("R11", "A", False, "IKARUS", False, marks=MARK[1]),  # 43 s
+        param("R11", "A", False, "IKARUS", True, marks=[mark.slow, MARK[1]]),  # 74 s
+        # R11, B
+        param("R11", "B", False, "IKARUS", False, marks=[mark.slow, MARK[1]]),
+        param("R11", "B", False, "IKARUS", True, marks=[mark.slow, MARK[1]]),
+        # R12, B
+        param("R12", "B", False, "IKARUS", True, marks=MARK["gh-337"]),
+        # R14, A
+        param(
+            "R14",
+            "A",
+            False,
+            "IKARUS",
+            False,
+            marks=[mark.slow, make_mark[2](genno.ComputationError)],
+        ),
+        # Pending iiasa/message_data#190
+        param("ISR", "A", True, None, False, marks=MARK[3]),
+    ],
+)
+def test_bare_res(
+    request, tmp_path, test_context, regions, years, dummy_LDV: bool, nonldv, solve
+):
+    """.transport.build() works on the bare RES, and the model solves."""
+    # Generate the relevant bare RES
+    ctx = test_context
+    ctx.update(regions=regions, years=years)
+    scenario = bare_res(request, ctx)
+
+    # Build succeeds without error
+    options = {
+        "data source": {"non-LDV": nonldv},
+        "dummy_LDV": dummy_LDV,
+        "dummy_supply": True,
+    }
+    build.main(ctx, scenario, options)
+
+    # dump_path = tmp_path / "scenario.xlsx"
+    # log.info(f"Dump contents to {dump_path}")
+    # scenario.to_excel(dump_path)
+
+    if solve:
+        scenario.solve(solve_options=dict(lpmethod=4, iis=1))
+
+        # commented: Appears to be giving a false negative
+        # # Use Reporting calculations to check the result
+        # result = report.check(scenario)
+        # assert result.all(), f"\n{result}"
+
+
 @build.get_computer.minimum_version
 @MARK[10]
 @pytest.mark.parametrize(
@@ -505,3 +427,81 @@ def test_debug(
 
     assert result, "1 or more checks failed"
     del tmp
+
+
+@pytest.mark.ece_db
+@pytest.mark.parametrize(
+    "url",
+    (
+        "ixmp://ene-ixmp/CD_Links_SSP2_v2/baseline",
+        "ixmp://ixmp-dev/ENGAGE_SSP2_v4.1.7/EN_NPi2020_1000f",
+        "ixmp://ixmp-dev/ENGAGE_SSP2_v4.1.7/baseline",
+        "ixmp://ixmp-dev/ENGAGE_SSP2_v4.1.7_ar5_gwp100/EN_NPi2020_1000_emif_new",
+        "ixmp://ixmp-dev/MESSAGEix-GLOBIOM_R12_CHN/baseline#17",
+        "ixmp://ixmp-dev/MESSAGEix-GLOBIOM_R12_CHN/baseline_macro#3",
+        # Local clones of the above
+        # "ixmp://clone-2021-06-09/ENGAGE_SSP2_v4.1.7/baseline",
+        # "ixmp://clone-2021-06-09/ENGAGE_SSP2_v4.1.7/EN_NPi2020_1000f",
+        # "ixmp://local/MESSAGEix-Transport on ENGAGE_SSP2_v4.1.7/baseline",
+    ),
+)
+def test_existing(tmp_path, test_context, url, solve=False):
+    """Test that model.transport.build works on certain existing scenarios.
+
+    These are the ones listed in the documenation, at :ref:`transport-base-scenarios`.
+    """
+    ctx = test_context
+
+    # Update the Context with the base scenario's `url`
+    ctx.handle_cli_args(url=url)
+
+    # Destination for built scenarios: uncomment one of
+    # the platform prepared by the text fixture…
+    ctx.dest_platform = copy(ctx.platform_info)
+    # # or, a specific, named platform.
+    # ctx.dest_platform = dict(name="local")
+
+    # New model name for the destination scenario
+    ctx.dest_scenario = copy(ctx.scenario_info)
+    ctx.dest_scenario["model"] = f"{ctx.dest_scenario['model']} +transport"
+
+    # Clone the base scenario to the test platform
+    scenario = ctx.clone_to_dest(create=False)
+    mp = scenario.platform
+
+    # Build succeeds without error
+    build.main(ctx, scenario)
+
+    # commented: slow
+    # dump_path = tmp_path / "scenario.xlsx"
+    # log.info(f"Dump contents to {dump_path}")
+    # scenario.to_excel(dump_path)
+
+    if solve:
+        scenario.solve(solve_options=dict(lpmethod=4))
+
+        # Use Reporting calculations to check the result
+        result = report.check(scenario)
+        assert result.all(), f"\n{result}"
+
+    del mp
+
+
+@pytest.mark.parametrize("years", [None, "A", "B"])
+@pytest.mark.parametrize(
+    "regions_arg, regions_exp",
+    [
+        ("R11", "R11"),
+        ("R12", "R12"),
+        ("R14", "R14"),
+        ("ISR", "ISR"),
+    ],
+)
+def test_make_spec(regions_arg, regions_exp, years):
+    # The spec can be generated
+    spec = structure.make_spec(regions_arg)
+
+    # The required elements of the "node" set match the configuration
+    nodes = get_codes(f"node/{regions_exp}")
+    expected = list(map(str, nodes[nodes.index("World")].child))
+    assert expected == spec["require"].set["node"]
