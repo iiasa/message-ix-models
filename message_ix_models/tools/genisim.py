2c43b0ee4301f2a32df2464718766de0fdcdfde6
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-05-13 11:46:07 +0200

    Add upstream URLs for .tools.{bgs,genisim,iai}

--- message_ix_models/tools/genisim.py
+++ message_ix_models/tools/genisim.py
@@ -1,1 +1,7 @@
-"""Tools for GeniSim data."""
+"""Tools for GeniSim data.
+
+.. todo::
+
+   - Fetch data from https://www.genisim.com/aluminum/smelters/smelters.htm
+   - Transform data as required by :mod:`.model.material`.
+"""
