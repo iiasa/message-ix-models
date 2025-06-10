a289a725638ea3de98709a500559ef31b76c4fd0
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-06-10 10:00:00 +0200

    Handle "3.2.beta" release of SSP data
    - Add SSPDataSource.make_query() to collect logic.
    - Handle overlap at year=2020 or 2025 between scenario="Historical
      Reference" and projected values; varies by release.
    - Update docstrings.
    - Expand test code and cases.

--- message_ix_models/project/ssp/data.py
+++ message_ix_models/project/ssp/data.py
@@ -1,10 +1,18 @@
 import logging
+from collections.abc import Callable, Iterable
 from dataclasses import dataclass
-from typing import Union
+from itertools import filterfalse
+from typing import TYPE_CHECKING, Union
+
+from genno import Keys
 
 from message_ix_models.tools.exo_data import BaseOptions, ExoDataSource, register_source
 from message_ix_models.tools.iamc import iamc_like_data_for_query
 from message_ix_models.util import path_fallback
+
+if TYPE_CHECKING:
+    from genno import Computer, Key
+    from genno.types import AnyQuantity
 
 __all__ = [
     "SSPOriginal",
@@ -23,9 +31,10 @@ class SSPDataSource(ExoDataSource):
         model: str = ""
 
         #: Partial URN for a code in the SSP code list, e.g. "ICONICS:SSP(2017).1".
+        #: :attr:`ssp_id` **should** be preferred.
         source: str = ""
 
-        #: Short id of the SSP, e.g. "1".
+        #: Short ID of the SSP code, e.g. "1".
         ssp_id: str = ""
 
         def handle_source(self, prefix: str) -> None:
@@ -47,11 +56,14 @@ class SSPDataSource(ExoDataSource):
 
     options: Options
 
-    #: Alias from short measure IDs to IAMC 'variable'.
+    #: Alias from short measure IDs to IAMC 'variable'. See :meth:`make_query`.
     variable = {"GDP": "GDP|PPP", "POP": "Population"}
 
-    #: Replacements.
+    #: `replace` argument to :func:`iamc.to_quantity`.
     replace: dict[str, Union[str, dict[str, str]]] = {}
+
+    #: `unique` argument to :func:`iamc.to_quantity`.
+    unique: str = "MODEL SCENARIO VARIABLE UNIT"
 
     #: :py:`where` argument to :func:`path_fallback`. In order:
     #:
@@ -67,7 +79,49 @@ class SSPDataSource(ExoDataSource):
 
     def get(self):
         # Use prepared path, query, and replacements
-        return iamc_like_data_for_query(self.path, self.query, replace=self.replace)
+        return iamc_like_data_for_query(
+            self.path, self.query, replace=self.replace, unique=self.unique
+        )
+
+    def make_query(
+        self,
+        dim_case: Callable[[str], str],
+        model_scenario: Iterable[tuple[str, str]],
+        unit: str,
+    ) -> None:
+        """Assemble and store a :meth:`pandas.DataFrame.query` string.
+
+        Parameters
+        ----------
+        dim_case :
+            Function to apply to IAMC dimension IDs, for instance :meth:`str.upper` to
+            use "MODEL".
+        model_scenario :
+            Iterable of (`model_name`, `scenario_name`) pairs. `model_name` **may** be
+            an empty string.
+        unit :
+            Units. **May** be an empty string.
+        """
+        # Map the `measure` option to an IAMC 'variable' label appearing in the data
+        variable = self.variable[self.options.measure]
+
+        parts = [
+            f"{dim_case('variable')} == {variable!r} and",
+            f"{dim_case('unit')} == {unit!r} and" if unit else "",
+            "(False",
+        ]
+
+        # Add query pieces for desired combinations of (model name, scenario name)
+        for m, s in model_scenario:
+            parts.extend(
+                [
+                    f"or ({dim_case('scenario')} == {s!r}",
+                    f"and {dim_case('model')} == {m!r})" if m else ")",
+                ]
+            )
+
+        self.query = " ".join(parts + [")"])
+        log.debug(f"query: {self.query!s}")
 
 
 @register_source
@@ -80,31 +134,25 @@ class SSPOriginal(SSPDataSource):
 
     1. Read the general documentation for :mod:`.project.ssp.data`.
     2. If necessary, obtain copy of the original data file(s).
-    3. Call :func:`.exo_data.prepare_computer` with the arguments:
+    3. Call :meth:`.SSPOriginal.add_tasks` with keyword arguments corresponding to
+       :class:`SSPDataSource.Options`. In particular:
 
-       - `source`: Any value from :data:`.SSP_2017` or equivalent string, for instance
-         "ICONICS:SSP(2017).2". The specific SSP for which data is returned is
-         determined from the value.
-       - `source_kw` including:
+       - :attr:`~SSPDataSource.Options.model` **should** be one of:
 
-         - "model": one of:
+         - IIASA GDP
+         - IIASA-WiC POP
+         - NCAR
+         - OECD Env-Growth
+         - PIK GDP-32
 
-           - IIASA GDP
-           - IIASA-WiC POP
-           - NCAR
-           - OECD Env-Growth
-           - PIK GDP-32
-
-         - "measure": The measures available differ according to the model; see the
-           source data for details.
+      - :attr:`~SSPDataSource.Options.measure`: The measures available differ according
+        to the model; see the source data for details.
+      - :attr:`~SSPDataSource.Options.unit` is not recognized/has no effect.
 
     Example
     -------
-    >>> keys = prepare_computer(
-    ...     context,
-    ...     computer,
-    ...     source="ICONICS:SSP(2015).3",
-    ...     source_kw=dict(measure="POP", model="IIASA-WiC POP"),
+    >>> keys = SSPOriginal.add_tasks(
+    ...     computer, context, ssp_id="3", measure="POP", model="IIASA-WiC POP",
     ... )
     >>> result = computer.get(keys[0])
     """
@@ -129,23 +177,21 @@ class SSPOriginal(SSPDataSource):
         opt = self.options = self.Options.from_args(self, *args, **kwargs)
         opt.handle_source("ICONICS:SSP(2017)")
 
-        super().__init__()  # Create .key
-
-        # Map the `measure` option to an IAMC 'variable' label appearing in the data
-        v = self.variable[opt.measure]
-
-        # Determine the date based on the model ID. There is a 1:1 correspondence.
-        date = self.model_date[opt.model]
-
         # Identify input data path
         self.path = path_fallback("ssp", self.filename, where=self._where())
 
-        # Assemble a query string
-        extra = "d" if opt.ssp_id == "4" and opt.model == "IIASA-WiC POP" else ""
-        self.query = (
-            f"SCENARIO == 'SSP{opt.ssp_id}{extra}_v9_{date}' and VARIABLE == {v!r} and "
-            + (f"MODEL == {opt.model!r}" if opt.model else "True")
-        )
+        # Create .key
+        super().__init__()
+
+        # Extra pieces for scenario identifier
+        # - Determine a date based on the model ID. There is a 1:1 correspondence.
+        # - Append "d" in a certain case
+        s_extra = "d" if (opt.ssp_id == "4" and opt.model == "IIASA-WiC POP") else ""
+        date = self.model_date[opt.model]
+        scenario = f"SSP{opt.ssp_id}{s_extra}_v9_{date}"
+
+        # Assemble and store a query string
+        self.make_query(str.upper, [(opt.model, scenario)], "")
 
 
 @register_source
@@ -158,27 +204,39 @@ class SSPUpdate(SSPDataSource):
 
     1. Read the general documentation for :mod:`.project.ssp.data`.
     2. If necessary, obtain copy of the original data file(s).
-    3. Call :func:`.exo_data.prepare_computer` with the arguments:
+    3. Call :meth:`.SSPUpdate.add_tasks` with keyword arguments corresponding to
+       :class:`SSPUpdate.Options`. In particular:
 
-       - `source`: Any value from :data:`.SSP_2024` or equivalent string, for instance
-         "ICONICS:SSP(2024).2".
-       - `release`: One of "3.1", "3.0.1", "3.0", or "preview".
+       - For :attr:`~Options.release` up to "3.1" and :attr:`~Options.measure` "GDP",
+         :attr:`~Options.model` **must** be one of "IIASA GDP 2023" or
+         "OECD ENV-Growth 2023".
+       - For :attr:`~Options.release` "3.2.beta" and :attr:`~Options.measure` "GDP":
+
+         - :attr:`~Options.model` **must** be "OECD ENV-Growth 2025".
+         - :attr:`~Options.unit` **must** be given, with a value such as
+           "billion USD_2010/yr", "billion USD_2015/yr", or "billion USD_2017/yr".
+           Without these, keys are not unique.
 
     Example
     -------
-    >>> keys = prepare_computer(
-    ...     context,
+    >>> keys = SSPUpdate.add_tasks(
     ...     computer,
-    ...     source="ICONICS:SSP(2024).3",
-    ...     source_kw=dict(measure="GDP", model="IIASA GDP 2023"),
+    ...     context,
+    ...     release="3.1",
+    ...     ssp_id="3",
+    ...     measure="GDP"
+    ...     model="IIASA GDP 2023",
     ... )
     >>> result = computer.get(keys[0])
     """
 
     @dataclass
     class Options(SSPDataSource.Options):
-        #: Release.
+        #: Release: one of the keys of :attr:`filename`. **Required**.
         release: str = ""
+
+        #: Selector for the IAMC 'UNIT' dimension.
+        unit: str = ""
 
     options: Options
 
@@ -187,6 +245,7 @@ class SSPUpdate(SSPDataSource):
         "3.0": "1706548837040-ssp_basic_drivers_release_3.0_full.csv.gz",
         "3.0.1": "1710759470883-ssp_basic_drivers_release_3.0.1_full.csv.gz",
         "3.1": "1721734326790-ssp_basic_drivers_release_3.1_full.csv.gz",
+        "3.2.beta": "0000000000000-ssp_basic_drivers_release_3.2.beta_full.csv.xz",
         "preview": "SSP-Review-Phase-1.csv.gz",
     }
 
@@ -194,46 +253,118 @@ class SSPUpdate(SSPDataSource):
         opt = self.options = self.Options.from_args(self, *args, **kwargs)
         opt.handle_source("ICONICS:SSP(2024)")
 
-        super().__init__()  # Create .key
-
-        # Map the `measure` option to an IAMC 'variable' label appearing in the data
-        v = self.variable[opt.measure]
-
-        # Replacements to apply, if any
-        self.replace = {}
-
-        # Prepare query pieces
-        models: list[str] = []
-        scenarios = []
-
-        if opt.release in ("3.1", "3.0.1", "3.0"):
-            scenarios.append(f"SSP{opt.ssp_id}")
-
-            if opt.measure == "GDP":
-                # Configure to prepend (m="OECD…", s="Historical Reference")
-                # observations to series
-                models.extend({opt.model, "OECD ENV-Growth 2023"})
-                scenarios.append("Historical Reference")
-                self.replace.update(
-                    Model={"OECD ENV-Growth 2023": opt.model},
-                    Scenario={"Historical Reference": scenarios[0]},
-                )
-        elif opt.release == "preview":
-            models.extend([opt.model] if opt.model else [])
-            scenarios.append(f"SSP{opt.ssp_id} - Review Phase 1")
-        else:
-            log.error(
-                f"{opt.release = } invalid for {type(self)}; expected one of: "
-                f"{set(self.filename)}"
-            )
-            raise ValueError(opt.release)
-
         # Identify input data path
         self.path = path_fallback(
             "ssp", self.filename[opt.release], where=self._where()
         )
 
+        super().__init__()  # Create .key
+
+        # Replacements to apply, if any
+        self.replace = {}
+
+        # Prepare query pieces
+        m_s = []
+        scenario = f"SSP{opt.ssp_id}"
+        if (opt.release, opt.measure) == ("3.0", "GDP") or opt.release in (
+            "3.0.1",
+            "3.1",
+            "3.2.beta",
+        ):
+            # Configure to also load data for (m=…, s="Historical Reference")
+            # These data will be deduplicated later in .transform()
+
+            # Model name for historical data
+            m_hist = "OECD ENV-Growth" if opt.measure == "GDP" else "IIASA-WiC POP"
+            if opt.release in ("3.2.beta",):
+                m_hist += " 2025"
+                # Period in which historical and project data overlap
+                self.y_overlap = 2025
+            else:
+                m_hist += " 2023"
+                self.y_overlap = 2020
+
+            # Also retrieve data for this (model name, scenario name) pair
+            m_s.append((m_hist, "Historical Reference"))
+
+            # Map the model name for historical data to the same value
+            self.replace.update(Model={m_hist: opt.model})
+
+            # Result of iamc_like_data_for_query() will *not* have unique 'SCENARIO'
+            # This also signals to .transform() to deduplicate
+            self.unique = "MODEL VARIABLE UNIT"
+        elif opt.release == "preview":
+            # Add scenario name suffix
+            scenario += " - Review Phase 1"
+        elif opt.release not in self.filename:
+            msg = (
+                f"{opt.release = } invalid for {type(self)}; expected one of: "
+                f"{set(self.filename)}"
+            )
+            log.error(msg)
+            raise ValueError(msg)
+
+        # Select the indicated (model, scenario)
+        m_s.append((opt.model, scenario))
+
         # Assemble and store a query string
-        self.query = f"Scenario in {scenarios!r} and Variable == {v!r} and " + (
-            f"Model in {models!r}" if models else "True"
+        self.make_query(str.title, m_s, opt.unit)
+
+    def transform(self, c: "Computer", base_key: "Key") -> "Key":
+        """Add tasks to `c` to transform raw data from `base_key`.
+
+        If necessary, data for period 2020 or 2025 are deduplicated."""
+        k = Keys(
+            base=base_key,
+            # Use id of the class instance to avoid duplication in multiple use
+            idx=f"indexers:SCENARIO-y:SSPUpdate+{id(self)}",
+            result=base_key,
         )
+
+        if "SCENARIO" not in self.unique:
+            # Construct indexers; select only historical or projected data
+            # This discards data for (s="Historical Reference", y=2020 or 2025)
+            for tag, sense in ("hist", False), ("proj", True):
+                c.add(k.idx[tag], make_indexers, k.base, sense=sense, y0=self.y_overlap)
+                c.add(k.base[tag], "select", k.base, k.idx[tag])
+
+            # Concatenate again
+            k.result = k.base + "deduplicated"
+            c.add(k.result, "concat", k.base["hist"], k.base["proj"])
+
+        return super().transform(c, k.result)
+
+
+def make_indexers(qty: "AnyQuantity", *, sense: bool, y0: int) -> dict:
+    """Construct indexers for :func:`genno.operator.select`.
+
+    The indexers include subsets of the coords of `qty`:
+
+    - "SCENARIO" dimension: a single value, either "Historical Reference" or something
+      else.
+    - "y" dimension: a sorted :class:`list` of :class:`int`; either those less than
+      `y0`, or `y0` and greater.
+
+    Used in :meth:`SSPUpdate.transform`.
+
+    Parameters
+    ----------
+    sense :
+        if :any:`True`, return indexers for projected values; otherwise for
+        "Historical Reference" values.
+    """
+    func = filter if sense else filterfalse
+    result = {
+        dim: sorted(func(predicate, qty.coords[dim].data))
+        for dim, predicate in (
+            ("SCENARIO", lambda s: s != "Historical Reference"),
+            ("y", lambda y: y >= y0),
+        )
+    }
+    if 1 != len(result["SCENARIO"]):
+        raise ValueError(
+            f"Must yield exactly 1 scenario label; got {result['SCENARIO']} from "
+            f"{qty.coords['SCENARIO'].data}"
+        )
+    result.update(SCENARIO=result["SCENARIO"][0])
+    return result
