6b657b1196b7b50493cc3ac73030ed542ff4dd19
Jun Shepard <51372492+junukitashepard@users.noreply.github.com>
2025-06-26 10:25:27 +0200

    Simplify gas pipeline framework

---
+++ message_ix_models/project/newpathways/test.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Bilateralize trade flows
+"""
+# Import packages
+import os
+import sys
+import pandas as pd
+import logging
+import yaml
+import message_ix
+import ixmp
+import itertools
+
+from pathlib import Path
+from message_ix_models.util import package_data_path
+from message_ix_models.tools.bilateralize.bilateralize import *
+
+# Connect to ixmp
+mp = ixmp.Platform()
+
+# Get logger
+log = get_logger(__name__)
+
+# Generate bare sheets
+#generate_bare_sheets(log=log, mp=mp)
+
+# Read and inflate sheets based on model horizon
+trade_dict = build_parameter_sheets(log=log)
+
+# Update scenario
+clone_and_update(trade_dict=trade_dict,
+                 log=log,
+                 mp=mp, 
+                 solve = True)
