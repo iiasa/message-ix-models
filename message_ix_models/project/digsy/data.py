41320dd681b50a8334557ad7547d7d9c24733064
Florian Maczek <maczek@iiasa.ac.at>
2025-07-15 16:29:05 +0200

    Add digsy modifier computation

--- message_ix_models/project/digsy/data.py
+++ message_ix_models/project/digsy/data.py
@@ -4,80 +4,67 @@ import pandas as pd
 import pint_pandas  # noqa: F401
 from message_ix.util import make_df
 
+from message_ix_models.model.material.util import read_yaml_file
 from message_ix_models.util import private_data_path
 
 
-def read_industry_modifiers(scenario: Literal["BEST", "WORST"]) -> pd.DataFrame:
-    path = private_data_path("digsy", "DIGSY-MESSAGE_Industry_v4.xlsx")
+def read_config() -> dict:
+    config = read_yaml_file(private_data_path("digsy", "config.yaml"))
+    return config
 
-    df = pd.read_excel(path, sheet_name="Final_modifier")
 
-    subsector_message_map = {
-        "Other": {"commodity": ["i_therm", "i_spec"], "par": "demand"},
-        # other industry (do we treat intensity reduction correctly here?)
-        # iron and steel
-        "integrated cold rolling and finishing": {
-            "technology": "finishing_steel",
-            "par": "input",
-            "commodity": "electr",
-        },
-        "cold rolling": {
-            "technology": "finishing_steel",
-            "par": "input",
-            "commodity": "electr",
-        },
-        # chemicals
-        "ethylene production": {
-            "technology": "steam_cracker_petro",
-            "par": "input",
-            "commodity": ["ht_heat", "electr"],
-        },
-        # 'pure terephthalic acid (PTA) production': {"technology":
-        # "steam_cracker_petro", "par": "input""commodity":"comm"},
-        # aluminum
-        "calcination (alumina refinery)": {
-            "technology": "refining_aluminum",
-            "par": "input",
-            "commodity": "ht_heat",
-        },
-        # No energy representation in MESSAGE for this process
-        # (not even finishing at the moment)
-        # "die-casting (holding energy)": {
-        #     "technology": "",
-        #     "par": "input",
-        #     "commodity": "comm"},
-        "smelting (all plant types)": {
-            "technology": "prebake_aluminum",
-            "par": "input",
-            "commodity": "electr",
-        },
-        # cement
-        "vertical mill (dry process)": {
-            "technology": "raw_meal_prep_cement",
-            "par": "input",
-            "commodity": "electr",
-        },
-        "clinker production (all kilns)": {
-            "technology": "clinker_dry_cement",
-            "par": "input",
-            "commodity": ["ht_heat", "electr"],
-        },
-        "cement raw-mix blending": {
-            "technology": "raw_meal_prep_cement",
-            "par": "input",
-            "commodity": "electr",
-        },
-        "cement mill (grinding)": {
-            "technology": ["grinding_vertmill_cement", "grinding_ballmill_cement"],
-            # only for ballmill for now, vertmill seems not to be used by MESSAGE
-            "par": "input",
-            "commodity": "electr",
-        },
-    }
+def read_industry_file(config: dict) -> pd.DataFrame:
+    path = private_data_path("digsy", config["industry_input"]["file_name"])
+    df = pd.read_excel(path, sheet_name=config["industry_input"]["sheet_name"])
+    df.columns = [i.replace("TRModified_agg_", "") for i in df.columns]
+    df.columns = [i if not i.isdigit() else int(i) for i in df.columns]
+    return df
 
-    subsector_message_map = pd.DataFrame(subsector_message_map).T
-    df = df.set_index("subsector").join(subsector_message_map).reset_index()
-    return df[df["scenario"] == scenario]
+
+def get_industry_modifiers(scenario: Literal["BEST", "WORST"]) -> pd.DataFrame:
+    config = read_config()
+    df = read_industry_file(config)
+    df["subsector"] = df["subsector"] + (
+        df["Electric or thermal"].fillna("").astype(str)
+    )
+    df = df.drop(columns=["Electric or thermal"])
+    mapping = pd.DataFrame(config["subsector_message_map"]).T
+    df = df.set_index("subsector").join(mapping).reset_index()
+    df = df[df["scenario"] == scenario]
+    df = df[df["par"].notna()]
+    df = df.drop(columns=["subsector", "scenario", "Variable"])
+    df = df.explode("technology").explode("commodity")
+    return df
+
+
+def apply_industry_modifiers(mods: pd.DataFrame, pars: dict) -> dict:
+    for par, group in mods.groupby("par"):
+        par_data = pars[par]
+        par_node_col = [i for i in par_data.columns if "node" in i][0]
+        par_yr_col = [i for i in par_data.columns if "year" in i][0]
+        group["region"] = "R12_" + group["region"]
+        mod_tmp = (
+            group.rename(columns={"region": par_node_col})
+            .drop(columns=["sector", "par"])
+            .set_index(
+                [par_node_col, *[i for i in group.columns if i in par_data.columns]]
+            )
+            .melt(ignore_index=False, var_name=par_yr_col, value_name="value")
+            .set_index(par_yr_col, append=True)
+        )
+        mod_tmp["value"] = mod_tmp["value"].add(1)
+        par_data_modified = (
+            par_data.set_index(mod_tmp.index.names)
+            .join(mod_tmp, rsuffix="_mod")
+            .fillna(1)
+        )
+        par_data_modified["value"] = (
+            par_data_modified["value"] * par_data_modified["value_mod"]
+        )
+        par_data_modified.drop(columns=["value_mod"], inplace=True)
+        par_data_modified.reset_index(inplace=True)
+        pars[par] = par_data_modified
+    return pars
 
 
 def read_ict_demand() -> pd.DataFrame:
@@ -118,7 +105,3 @@ def read_ict_demand() -> pd.DataFrame:
         time="year",
     ).head()
     return df
-
-
-if __name__ == "__main__":
-    read_industry_modifiers("BEST")
