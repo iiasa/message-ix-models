2bffee9077f822650bef5aadd9a41310ac6b3a27
Vignesh Raghunathan <raghunathan@iiasa.ac.at>
2025-07-15 18:20:57 +0200

    Add test get_vintage_and_active_years

--- message_ix_models/tests/model/water/test_utils.py
+++ message_ix_models/tests/model/water/test_utils.py
@@ -1,9 +1,28 @@
 import pandas as pd
+import pytest
 
 from message_ix_models.model.water.utils import (
+    get_vintage_and_active_years,
     map_yv_ya_lt,
     read_config,
 )
+
+
+@pytest.fixture
+def mock_scenario_info():
+    """Mock ScenarioInfo with yv_ya property and year set."""
+
+    class MockScenarioInfo:
+        def __init__(self):
+            self.yv_ya = pd.DataFrame(
+                {
+                    "year_vtg": [2010, 2010, 2010, 2020, 2020, 2030],
+                    "year_act": [2010, 2020, 2030, 2020, 2030, 2030],
+                }
+            )
+            self.set = {"year": [2010, 2020, 2030]}
+
+    return MockScenarioInfo()
 
 
 def test_read_config(test_context):
@@ -16,6 +35,41 @@ def test_read_config(test_context):
         "cooltech_cost_and_shares_ssp_msg14",
         "tech_water_performance_ssp_msg",
     ]
+
+
+@pytest.mark.parametrize(
+    "technical_lifetime,expected_data",
+    [
+        (
+            10,
+            {
+                "year_vtg": [2010, 2010, 2020, 2020, 2030],
+                "year_act": [2010, 2020, 2020, 2030, 2030],
+            },
+        ),
+        (
+            20,
+            {
+                "year_vtg": [2010, 2010, 2010, 2020, 2020, 2030],
+                "year_act": [2010, 2020, 2030, 2020, 2030, 2030],
+            },
+        ),
+        (
+            None,
+            {
+                "year_vtg": [2010, 2010, 2010, 2020, 2020, 2030],
+                "year_act": [2010, 2020, 2030, 2020, 2030, 2030],
+            },
+        ),
+    ],
+)
+def test_get_vintage_and_active_years(
+    mock_scenario_info, technical_lifetime, expected_data
+):
+    """Test get_vintage_and_active_years function with different technical lifetimes."""
+    result = get_vintage_and_active_years(mock_scenario_info, technical_lifetime)
+    expected = pd.DataFrame(expected_data)
+    pd.testing.assert_frame_equal(result, expected)
 
 
 def test_map_yv_ya_lt():
