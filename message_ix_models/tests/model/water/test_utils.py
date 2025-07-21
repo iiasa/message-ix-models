aadf517bf6345e52da9539948ce0f42d4f410905
Vignesh Raghunathan <raghunathan@iiasa.ac.at>
2025-07-21 10:53:14 +0200

    Replace map_yv_ya_lt with get_vintage_and_active_years
    - map_yv_ya_lt in practice used historical vintages in active years. Due
      to incorrect input data piped to it.
      well.
    - The function itself was fine, new function uses info.ya_yv so its
      easier to validate.

--- message_ix_models/tests/model/water/test_utils.py
+++ message_ix_models/tests/model/water/test_utils.py
@@ -3,7 +3,6 @@ import pytest
 
 from message_ix_models.model.water.utils import (
     get_vintage_and_active_years,
-    map_yv_ya_lt,
     read_config,
 )
 
@@ -70,32 +69,3 @@ def test_get_vintage_and_active_years(
     result = get_vintage_and_active_years(mock_scenario_info, technical_lifetime)
     expected = pd.DataFrame(expected_data)
     pd.testing.assert_frame_equal(result, expected)
-
-
-def test_map_yv_ya_lt():
-    periods = (2010, 2020, 2030, 2040)
-    lt = 20
-    ya = 2020
-
-    expected = pd.DataFrame(
-        {
-            "year_vtg": [2010, 2010, 2020, 2020, 2020, 2030, 2030, 2040],
-            "year_act": [2020, 2030, 2020, 2030, 2040, 2030, 2040, 2040],
-        }
-    )
-
-    result = map_yv_ya_lt(periods, lt, ya).reset_index(drop=True)
-    # print(result)
-
-    pd.testing.assert_frame_equal(result, expected)
-
-    expected_no_ya = pd.DataFrame(
-        {
-            "year_vtg": [2010, 2010, 2010, 2020, 2020, 2020, 2030, 2030, 2040],
-            "year_act": [2010, 2020, 2030, 2020, 2030, 2040, 2030, 2040, 2040],
-        }
-    )
-
-    result_no_ya = map_yv_ya_lt(periods, lt).reset_index(drop=True)
-
-    pd.testing.assert_frame_equal(result_no_ya, expected_no_ya)
