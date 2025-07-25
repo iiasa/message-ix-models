78b391ceaaa199ee86635a440908dea0a22f2f7e
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-07-25 23:06:41 +0200

    Use PDT_CAP in .demand.prepare_computer()
    …for DIGSY and EDITS scenarios.

--- message_ix_models/model/transport/demand.py
+++ message_ix_models/model/transport/demand.py
@@ -13,6 +13,7 @@ from message_ix_models.report.key import GDP
 from message_ix_models.util import broadcast
 
 from . import factor
+from .data import PDT_CAP
 from .key import (
     cg,
     cost,
@@ -293,7 +294,11 @@ def prepare_computer(c: "Computer") -> None:
     --------
     TASKS
     """
-    config: "Config" = c.graph["context"].transport
+    from message_ix_models.project.digsy.structure import SCENARIO as DIGSY
+    from message_ix_models.project.edits.structure import SCENARIO as EDITS
+
+    context = c.graph["context"]
+    config: "Config" = context.transport
 
     # Compute total PDT per capita
     c.apply(pdt_per_capita)
@@ -304,13 +309,27 @@ def prepare_computer(c: "Computer") -> None:
     # Add other tasks for demand calculation
     c.add_queue(TASKS)
 
+    # Replace certain calculations for LED projected activity
+    exo_pdt_cap_scenario = ""
     if config.project.get("LED", False):
-        # Replace certain calculations for LED projected activity
+        exo_pdt_cap_scenario = "LED"
+    elif digsy_scenario := config.project.get("DIGSY", None):
+        # For DIGSY.BASE, preserve the standard calculation based on the respective SSP
+        # (fixed to SSP2 by .config.refresh_cl_scenario())
+        if digsy_scenario not in (DIGSY["_Z"], DIGSY["BASE"]):
+            exo_pdt_cap_scenario = f"DIGSY-{digsy_scenario.name}"
+    elif edits_scenario := config.project.get("EDITS", None):
+        if edits_scenario is not EDITS["_Z"]:
+            exo_pdt_cap_scenario = f"EDITS-{edits_scenario.name}"
 
-        # Select data from input file: projected PDT per capita
-        c.add(pdt_cap * "t", "select", exo.pdt_cap_proj, indexers=dict(scenario="LED"))
-
-        # Multiply by population for the total
-        c.add(pdt_nyt[0], "mul", pdt_cap * "t", pop)
+    if exo_pdt_cap_scenario:
+        log.info(f"Use exogenous PDT per capita for scenario={exo_pdt_cap_scenario!r}")
+        PDT_CAP.add_tasks(
+            c,
+            context=context,
+            strict=False,
+            nodes=context.model.regions,
+            scenario=exo_pdt_cap_scenario,
+        )
 
     c.add("transport_data", __name__, key="transport demand::ixmp")
