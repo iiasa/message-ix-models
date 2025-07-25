05a725e138fd76578cd52a7758b1caab17c7cae1
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-07-25 14:22:33 +0200

    Create .project.edits.structure submodule
    - Add get_cl_scenario(), SCENARIO enum.
    - Add tests.

--- message_ix_models/tests/project/test_edits.py
+++ message_ix_models/tests/project/test_edits.py
@@ -3,7 +3,10 @@ from shutil import copyfile
 import pytest
 
 from message_ix_models.project.edits import gen_demand, pasta_native_to_sdmx
+from message_ix_models.project.edits.structure import SCENARIO, get_cl_scenario
 from message_ix_models.util import package_data_path
+
+URN = "urn:sdmx:org.sdmx.infomodel.codelist.Code=IIASA_ECE:EDITS_MCE_SCENARIO(0.1).CA"
 
 
 @pytest.fixture
@@ -13,6 +16,26 @@ def test_pasta_data(test_context):
     target = test_context.get_local_path(*parts)
     target.parent.mkdir(parents=True, exist_ok=True)
     copyfile(package_data_path("test", *parts), target)
+
+
+class TestSCENARIO:
+    def test_members(self) -> None:
+        # Members can be accessed as attributes
+        SCENARIO.CA  # type: ignore [attr-defined]
+        SCENARIO.HA  # type: ignore [attr-defined]
+        SCENARIO._Z  # type: ignore [attr-defined]
+
+        # Members can be accessed by name
+        SCENARIO["CA"]
+        SCENARIO["HA"]
+        SCENARIO["_Z"]
+
+        # Members can be accessed by URN
+        assert SCENARIO["CA"] is SCENARIO.by_urn(URN)
+
+    def test_urn(self) -> None:
+        # Member URN can be retrieved
+        assert URN == SCENARIO["CA"].urn
 
 
 def test_pasta_native_to_sdmx(test_context, test_pasta_data):
@@ -30,3 +53,14 @@ def test_gen_demand(test_context, test_pasta_data):
 
     result = gen_demand(test_context)
     assert 3 == len(result)
+
+
+def test_get_cl_scenario() -> None:
+    # Code list can be generated
+    result = get_cl_scenario()
+
+    # Code list has expected number of members
+    assert 3 == len(result)
+
+    # Items have URNs that can be referenced
+    assert URN == result["CA"].urn
