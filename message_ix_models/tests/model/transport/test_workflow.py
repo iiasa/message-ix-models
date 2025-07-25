45207df584cc921351f8ff1ef1cfebaad3559402
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-07-25 15:35:53 +0200

    Add DIGSY-{BEST,WORST} to CL_TRANSPORT_SCENARIO
    - Add DIGSY-scenario-URN annotation to codes in this code list.
    - Rename EDITS-activity-id annotation to EDITS-scenario-URN.
    - Bump codelist version to 1.2.0.
    - Adjust tests.

--- message_ix_models/tests/model/transport/test_workflow.py
+++ message_ix_models/tests/model/transport/test_workflow.py
@@ -1,6 +1,8 @@
 import pytest
 
 from message_ix_models.model.transport.workflow import generate
+from message_ix_models.project.digsy.structure import SCENARIO as DIGSY
+from message_ix_models.project.edits.structure import SCENARIO as EDITS
 
 
 @generate.minimum_version
@@ -27,5 +29,14 @@ def test_generate(test_context, base_scenario) -> None:
     # Separate steps for tax and GHG pricing policies are included
     assert "SSP5 tax reported" in wf
     assert "SSP5 exo price reported" in wf
+
+    # WorkflowStep objects store expected configuration for certain projects
+    assert (
+        DIGSY["BEST"]
+        is wf.graph["DIGSY-BEST built"][0].kwargs["config"].project["DIGSY"]
+    )
+    assert (
+        EDITS["HA"] is wf.graph["EDITS-HA built"][0].kwargs["config"].project["EDITS"]
+    )
 
     # wf.run("LED-SSP1 reported")  # NB Only works with base_scenario="bare"
