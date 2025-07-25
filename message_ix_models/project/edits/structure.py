d97b3adb92af7a0e90198109cae752c062d9b590
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-07-25 14:22:33 +0200

    Create .project.edits.structure submodule
    - Add get_cl_scenario(), SCENARIO enum.
    - Add tests.

---
+++ message_ix_models/project/edits/structure.py
@@ -0,0 +1,30 @@
+import sdmx.urn
+from sdmx.model import common
+
+from message_ix_models.util.sdmx import ItemSchemeEnumType, URNLookupEnum, read
+
+
+def get_cl_scenario() -> "common.Codelist":
+    """Return a code list with the identifiers of EDITS MCE scenarios."""
+    cl: "common.Codelist" = common.Codelist(
+        id="EDITS_MCE_SCENARIO",
+        maintainer=read("IIASA_ECE:AGENCIES")["IIASA_ECE"],
+        version="0.1",
+        is_final=True,
+        is_external_reference=False,
+    )
+
+    for id_, name in (
+        ("CA", "Current ambition"),
+        ("HA", "High ambition"),
+        ("_Z", "Not applicable"),
+    ):
+        c = cl.setdefault(id=id_)
+        c.urn = sdmx.urn.make(c)
+
+    return cl
+
+
+class SCENARIO(URNLookupEnum, metaclass=ItemSchemeEnumType):
+    def _get_item_scheme(self) -> "common.Codelist":
+        return get_cl_scenario()
