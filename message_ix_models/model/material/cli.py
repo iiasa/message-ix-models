f175a3ff817a3b4192cc8b09becd6d4266cee103
yiyi1991 <juyiyi@iiasa.ac.at>
2025-07-27 14:49:24 +0200

    Add context-based cli build
    - build based on context.material
    - default attributes saved in config.py and passed to context.material
    - mix-models material cli options overwrites defaul attributes
    - should be compatible for both cli build and workflow build
    - commented out for now

--- message_ix_models/model/material/cli.py
+++ message_ix_models/model/material/cli.py
@@ -54,12 +54,14 @@ def build_scen(
     update_costs,
     power_sector,
 ):
-    # Collect CLI options and store themin context.material
-    context.material = {
-        "old_calib": old_calib,
-        "iea_data_path": iea_data_path,
-        "modify_existing_constraints": True,  # Default value from build() function
-    }
+    # # Collect CLI options and store them in context.material
+    # # This overwrites the default values from Config()
+    # from message_ix_models.model.material.config import Config
+    # context.material = Config(
+    #     old_calib=old_calib,
+    #     iea_data_path=iea_data_path,
+    #     modify_existing_constraints=True,
+    # ) # TODO: needs testing
 
     """Build a scenario.
 
