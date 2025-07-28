55ed16c3acb463d3bdcd6c9d5b006d11994923df
yiyi1991 <juyiyi@iiasa.ac.at>
2025-07-28 18:08:21 +0200

    Update autosummary in doc

---
+++ message_ix_models/tools/inter_pipe/__init__.py
@@ -0,0 +1,9 @@
+"""Inter-pipe tools for message-ix-models.
+
+This module provides tools for generating and building inter-pipe technologies
+in MESSAGE-IX models.
+"""
+
+from .generate_inter_pipe import inter_pipe_bare, inter_pipe_build
+
+__all__ = ["inter_pipe_bare", "inter_pipe_build"] 
