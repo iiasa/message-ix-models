ed1c10b0a9c2b7b8db9b92e2856590096781dc9c
SHEPARD Jun <shepard@iiasa.ac.at>
2025-08-08 13:49:34 +0200

    Update load_and_solve
    make sure config_path is pointing

--- message_ix_models/project/newpathways/load_and_solve.py
+++ message_ix_models/project/newpathways/load_and_solve.py
@@ -20,6 +20,16 @@ from message_ix_models.tools.bilateralize.historical_calibration import *
 from message_ix_models.tools.bilateralize.pull_gem import *
 from message_ix_models.tools.bilateralize.mariteam_calibration import *
 
+# Bring in configuration
+config, config_path = load_config(project_name = 'newpathways', 
+                                  config_name = 'config.yaml')
+
+covered_tec = config['covered_trade_technologies']
+message_regions = config['scenario']['regions']
+
+# Get logger
+log = get_logger(__name__)
+
 # Import pickle of parameter definitions
 tdf = os.path.join(os.path.dirname(config_path), 'scenario_parameters.pkl')
 trade_parameters = pd.read_pickle(tdf)
