3c800e924c653c3e855545a0ecf805f8a9693d2a
Florian Maczek <maczek@iiasa.ac.at>
2025-08-11 11:53:34 +0200

    Test new materials reporter config parser

---
+++ message_ix_models/tests/model/material/test_reporter.py
@@ -0,0 +1,27 @@
+import pytest
+from pytest import param
+
+from message_ix_models.model.material.report.run_reporting import load_config
+
+
+@pytest.mark.parametrize(
+    "config_name, exp_len",
+    [
+        param("fe", 705),
+        param("fe_methanol_ammonia", 74),
+        param("fs1", 46),
+        param("fs2", 38),
+        param("prod", 197),
+        param("ch4_emi", 60),
+    ],
+)
+def test_load_config(config_name, exp_len) -> None:
+    result = load_config(config_name)
+    df = result.mapping
+    # Data have the expected length
+    assert exp_len == len(df.index), f"Wrong length of {config_name} mapping table"
+
+    # Valid parameter data: no NaNs anywhere
+    assert not df.isna().any(axis=None), f"NaN entries in {config_name} mapping table"
+
+    # TODO Extend assertions
