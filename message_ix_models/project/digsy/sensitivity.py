1c5da35e596153714df6addbcc0598a692abfb60
Florian Maczek <maczek@iiasa.ac.at>
2025-08-28 14:00:47 +0200

    Add digsy fossil fuel sensitivity scenario

--- message_ix_models/project/digsy/sensitivity.py
+++ message_ix_models/project/digsy/sensitivity.py
@@ -15,8 +15,24 @@ def adjust_electrification_constraint(scenario: "Scenario"):
     return
 
 
-def adjust_fossil_fuels():
-    raise NotImplementedError
+def lower_unconv_cost(scenario: "Scenario"):
+    for par in ["var_cost", "inv_cost"]:
+        df = scenario.par(
+            par,
+            filters={
+                "technology": [
+                    "gas_extr_5",
+                    "gas_extr_6",
+                    "gas_extr_7",
+                    "oil_extr_5",
+                    "oil_extr_6",
+                    "oil_extr_7",
+                ]
+            },
+        )
+        df.value *= 0.9
+        with scenario.transact():
+            scenario.add_par(par, df)
 
 
 def adjust_rooftop_constraint(scenario: "Scenario", new_value: float = 0.4):
@@ -71,18 +87,41 @@ def query_ssp_pars(scens: list[str]):
     return df_list
 
 
+def create_sensitivity_scenarios(scens: list[tuple], suffix: str, modifier):
+    for model, scen in scens:
+        scenario = message_ix.Scenario(mp, model, scen)
+        clone = scenario.clone(
+            scenario.model, scenario.scenario + suffix, keep_solution=False
+        )
+        modifier(clone)
+    return
+
+
 if __name__ == "__main__":
     import ixmp
     import message_ix
-    import pandas as pd
+
+    sensitivity_map = {
+        "relaxed electrification growth constraint": {
+            "suffix": "_elec",
+            "modifier": adjust_electrification_constraint,
+        },
+        "lower shale cost": {
+            "suffix": "_shale_cost",
+            "modifier": lower_unconv_cost,
+        },
+    }
 
     mp = ixmp.Platform("ixmp_dev")
-    scen = message_ix.Scenario(mp, "DIGSY_SSP2", "baseline_elec")
-    for suffix in ["", "_BEST", "_WORST"]:
-        scen = message_ix.Scenario(mp, f"DIGSY_SSP2{suffix}", "baseline")
-        # scen = message_ix.Scenario(mp, "DIGSY_SSP2", "base_model_WORST")
-        clone = scen.clone(scen.model, scen.scenario + "_elec", keep_solution=False)
-        adjust_electrification_constraint(clone)
+    # create_sensitivity_scenarios(
+    #     [(f"DIGSY_SSP2{i}", "baseline") for i in ["", "_BEST", "_WORST"]],
+    #     **sensitivity_map["lower shale cost"],
+    # )
+    create_sensitivity_scenarios(
+        [(f"DIGSY_SSP2{i}", "baseline_1000f") for i in [""]],
+        **sensitivity_map["relaxed electrification growth constraint"],
+    )
+    # scen = message_ix.Scenario(mp, "DIGSY_SSP2", "baseline")
     # pars = query_ssp_pars(["SSP1", "SSP2", "SSP3", "SSP4", "SSP5", "LED"])
     # with pd.ExcelWriter("output.xlsx") as writer:
     #     for k,v in pars.items():
