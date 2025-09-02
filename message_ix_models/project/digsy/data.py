422e32e960b2da15b47d13b7c3b1f8afed8657ea
Florian Maczek <maczek@iiasa.ac.at>
2025-09-02 21:28:04 +0200

    Update digsy industry build
    - Use new modifier file version
    - Fix bug with multiple modifiers mapped to same technology

--- message_ix_models/project/digsy/data.py
+++ message_ix_models/project/digsy/data.py
@@ -27,15 +27,25 @@ def read_config() -> dict:
     return config
 
 
+def read_file(path, **kwargs) -> pd.DataFrame:
+    if path.suffix == ".csv":
+        df = pd.read_csv(path)
+    elif path.suffix in (".xls", ".xlsx"):
+        df = pd.read_excel(path, **kwargs)
+    else:
+        raise ValueError(f"File type of {path} not supported.")
+    return df
+
+
 def read_industry_file(config: dict) -> pd.DataFrame:
     path = private_data_path("projects", "digsy", config["industry_input"]["file_name"])
-    df = pd.read_excel(path, sheet_name=config["industry_input"]["sheet_name"])
+    df = read_file(path, sheet_name=config["industry_input"]["sheet_name"])
     df.columns = [i.replace("TRModified_agg_", "") for i in df.columns]
     df.columns = [i if not i.isdigit() else int(i) for i in df.columns]
     return df
 
 
-def get_industry_modifiers(scenario: Literal["BEST", "WORST"]) -> pd.DataFrame:
+def get_industry_modifiers(scenario: DIGSY_SCENS) -> pd.DataFrame:
     config = read_config()
     df = read_industry_file(config)
     df["subsector"] = df["subsector"] + (
@@ -43,6 +53,7 @@ def get_industry_modifiers(scenario: Literal["BEST", "WORST"]) -> pd.DataFrame:
     )
     df = df.drop(columns=["Electric or thermal"])
     mapping = pd.DataFrame(config["subsector_message_map"]).T
+    df["subsector"] = df["subsector"].str.strip()
     df = df.set_index("subsector").join(mapping).reset_index()
     df = df[df["scenario"] == scenario]
     df = df[df["par"].notna()]
@@ -77,6 +88,7 @@ def apply_industry_modifiers(mods: pd.DataFrame, pars: dict) -> dict:
             .set_index(par_yr_col, append=True)
         )
         mod_tmp["value"] = mod_tmp["value"].add(1)
+        mod_tmp = mod_tmp.groupby(mod_tmp.index.names).prod()
         par_data_modified = (
             par_data.set_index(mod_tmp.index.names)
             .join(mod_tmp, rsuffix="_mod")
