30d13f727fb0127962380ffaebeb653c1844b9b2
Florian Maczek <maczek@iiasa.ac.at>
2025-09-04 09:58:53 +0200

    Improve ICT-RC calibration

--- message_ix_models/project/digsy/data.py
+++ message_ix_models/project/digsy/data.py
@@ -313,6 +313,23 @@ def read_trp_materials(digsy_scenario) -> pd.DataFrame:
     return df
 
 
+def fe_to_ue(df: pd.DataFrame, scen: message_ix.Scenario) -> pd.DataFrame:
+    inp = scen.par("input", filters={"technology": "sp_el_RC"})
+    inp = (
+        inp[["node_loc", "year_act", "value"]]
+        .rename(columns={"node_loc": "node", "year_act": "year", "value": "efficiency"})
+        .set_index(["node", "year"])
+    )
+    df_new = (
+        df.set_index(["node", "year"])
+        .join(inp)
+        .assign(value=lambda x: x["value"] / x["efficiency"])
+        .reset_index()
+        .drop(columns=["efficiency"])
+    )
+    return df_new
+
+
 def adjust_mat_dem(
     total_demand: pd.DataFrame,
     sector_demand_base: pd.DataFrame,
@@ -339,3 +356,20 @@ def adjust_mat_dem(
         .reset_index()
     )
     return total_demand_adjusted
+
+
+def adjust_act_calib(ict: pd.DataFrame, scen: message_ix.Scenario):
+    for par in ["bound_activity_up", "bound_activity_lo"]:
+        bound = scen.par(par, filters={"technology": "sp_el_RC"})
+        ict_tot = (
+            ict.rename(columns={"node": "node_loc", "year": "year_act"})
+            .groupby(["node_loc", "year_act"])
+            .sum(numeric_only=True)
+        )
+        new_bound = (
+            bound.set_index([i for i in bound.columns if i != "value"])
+            .sub(ict_tot)
+            .reset_index()
+        ).dropna()
+        with scen.transact():
+            scen.add_par(par, new_bound)
