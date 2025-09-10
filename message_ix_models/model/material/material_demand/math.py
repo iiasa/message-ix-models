277cac7f3b808899455723c37a6e2c86989faec7
Florian Maczek <maczek@iiasa.ac.at>
2025-09-10 14:24:47 +0200

    Adjust typing in math.py for Python 3.9

--- message_ix_models/model/material/material_demand/math.py
+++ message_ix_models/model/material/material_demand/math.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import numpy as np
 import pandas as pd
 
@@ -5,16 +7,18 @@ GIGA = 10**9
 MEGA = 10**6
 
 
-def steel_function(x: pd.DataFrame | float, a: float, b: float, m: float):
+def steel_function(x: tuple[pd.Series, pd.Series], a: float, b: float, m: float):
     # cons_pcap ~ a * exp(b / gdp_pcap) * (1 - m) ** del_t
     gdp_pcap, del_t = x
     return a * np.exp(b / gdp_pcap) * (1 - m) ** del_t
 
 
-def cement_function(x: pd.DataFrame | float, a: float, b: float):
+def cement_function(x: tuple[pd.Series, pd.Series], a: float, b: float):
     gdp_pcap = x[0]
     return a * np.exp(b / gdp_pcap)
 
 
-def gompertz(phi: float, mu: float, y: pd.DataFrame | float, baseyear: int = 2020):
+def gompertz(
+    phi: float, mu: float, y: Union[pd.DataFrame, float], baseyear: int = 2020
+):
     return 1 - np.exp(-phi * np.exp(-mu * (y - baseyear)))
