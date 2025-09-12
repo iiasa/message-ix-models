e186a5b591be17869b5c1965c6fcbb165a057fe6
Florian Maczek <maczek@iiasa.ac.at>
2025-09-12 12:37:39 +0200

    Extend .material.data_generic test coverage

--- message_ix_models/tests/model/material/test_data_generic.py
+++ message_ix_models/tests/model/material/test_data_generic.py
@@ -2,6 +2,7 @@ import pytest
 from message_ix import make_df
 
 from message_ix_models.model.material.data_generic import (
+    get_furnace_inputs,
     get_thermal_industry_emi_coefficients,
 )
 from message_ix_models.testing import bare_res
@@ -49,3 +50,20 @@ def test_get_thermal_industry_emi_coefficients(
     df = get_thermal_industry_emi_coefficients(scenario)
     assert df["emi_factor"].iloc[0] == 0.1
     assert df["value_in"].iloc[0] == 1.0
+
+
+@pytest.mark.parametrize(
+    "regions, years, relations, solve",
+    [
+        pytest.param("R12", "B", "B", False),
+    ],
+)
+def test_get_furnace_inputs(
+    request, tmp_path, test_context, regions, years, relations, solve
+) -> None:
+    ctx = test_context
+    ctx.update(regions=regions, years=years, ssp="SSP2", relations=relations)
+    scenario = bare_res(request, ctx)
+    df = get_furnace_inputs(scenario, 2020).reset_index()
+
+    assert sorted(make_df("input").columns) == sorted(df.columns)
