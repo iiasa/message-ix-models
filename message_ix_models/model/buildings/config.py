31f746f07d2c8891d44235cd2d14a5090546445d
yiyi1991 <juyiyi@iiasa.ac.at>
2025-09-16 22:24:52 +0200

    Apply pre-commit auto-fixes

--- message_ix_models/model/buildings/config.py
+++ message_ix_models/model/buildings/config.py
@@ -1,10 +1,12 @@
 # message_ix_models/model/buildings/config.py
 from dataclasses import dataclass
 
+
 @dataclass
 class Config:
-    """Configuration for MESSAGEix-Buildings 
-    (moving cli options to context so that build buildings can be called in other workflows).
+    """Configuration for MESSAGEix-Buildings
+    (moving cli options to context so that build buildings can be called
+    in other workflows).
 
     This dataclass stores and documents all configuration settings required and used by
     :mod:`~message_ix_models.model.buildings`. It also handles (via
@@ -12,12 +14,12 @@ class Config:
     :file:`config.yaml`, while respecting higher-level configuration, for instance
     :attr:`.model.Config.regions`.
     """
-    
+
     with_materials: bool = True
-    
+
     # @classmethod
     # def from_context(cls, context, options=None):
-    #     config = cls() 
+    #     config = cls()
     #     if options:
     #         config = replace(config, **options) # type: ignore
     #     return config
--- message_ix_models/model/material/config.py
+++ message_ix_models/model/material/config.py
@@ -1,10 +1,12 @@
 # message_ix_models/model/material/config.py
 from dataclasses import dataclass
 
+
 @dataclass
 class Config:
-    """Configuration for MESSAGEix-Materials 
-    (moving cli options to context so that build material can be called in other workflows).
+    """Configuration for MESSAGEix-Materials
+    (moving cli options to context so that build material can be called
+    in other workflows).
 
     This dataclass stores and documents all configuration settings required and used by
     :mod:`~message_ix_models.model.material`. It also handles (via
@@ -12,17 +14,17 @@ class Config:
     :file:`config.yaml`, while respecting higher-level configuration, for instance
     :attr:`.model.Config.regions`.
     """
-    
+
     old_calib: bool = False
     iea_data_path: str = "P:ene.model\\IEA_database\\Florian\\"
-    modify_existing_constraints: bool = True # hardcoded to True
+    modify_existing_constraints: bool = True  # hardcoded to True
 
-    # examples: common knobs (e.g., SSP knobs) here such as 
-    # clinker ratio, reycling rate, etc. 
-    
+    # examples: common knobs (e.g., SSP knobs) here such as
+    # clinker ratio, reycling rate, etc.
+
     # @classmethod
     # def from_context(cls, context, options=None):
-    #     config = cls() 
+    #     config = cls()
     #     if options:
     #         config = replace(config, **options) # type: ignore
     #     return config
