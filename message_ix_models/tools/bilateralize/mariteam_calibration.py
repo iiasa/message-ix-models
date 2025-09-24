27be8369c15e633df29003df719a992f53ac3590
Jun Shepard <51372492+junukitashepard@users.noreply.github.com>
2025-09-24 16:06:32 +0200

    Add coal representation

--- message_ix_models/tools/bilateralize/mariteam_calibration.py
+++ message_ix_models/tools/bilateralize/mariteam_calibration.py
@@ -22,11 +22,14 @@ from message_ix_models.tools.bilateralize import bilateralize
 def calibrate_mariteam(covered_tec,
                        message_regions,
                        mtdict = {'LNG_shipped': {'astd_ship_type': 'Gas tankers',
-                                                 'flow_technology': 'LNG_tanker_LNG',
+                                                 'flow_technology': ['LNG_tanker_LNG', 'LNG_tanker_foil'],
                                                  'flow_input': 'LNG'},
                                  'crudeoil_shipped': {'astd_ship_type': 'Crude oil tankers',
-                                                      'flow_technology': 'crudoil_tanker_foil',
-                                                      'flow_input': 'foil'}},
+                                                      'flow_technology': ['crudoil_tanker_foil'],
+                                                      'flow_input': 'foil'},
+                                 'coal_shipped': {'astd_ship_type': 'Bulk carriers',
+                                                  'flow_technology': ['energy_bulk_carrier'],
+                                                  'flow_input': 'foil'}},
                        mt_output = "MariTEAM_output_2025-07-21.csv",
                        project_name: str = None,
                        config_name: str = None):
@@ -44,46 +47,56 @@ def calibrate_mariteam(covered_tec,
     
     for tec in [i for i in covered_tec if 'shipped' in i]:
         
-        basedf = mtdf[mtdf['astd_ship_type'] == mtdict[tec]['astd_ship_type']].copy()
-        basedf['node_loc'] = basedf[message_regions + '_origin']
-        basedf['technology'] = mtdict[tec]['flow_technology']
-        
-        # Fuel consumption (input)
-        mt_input = basedf.copy()
-        mt_input['mt_value'] = mt_input['intensity_MJ_tonne']/mt_input['distance_km_sum'] # MJ/t-km
-        mt_input['mt_value'] = mt_input['mt_value']*3.17e-11 # GWa/t-km
-        mt_input['mt_value'] = mt_input['mt_value']*1e6 # GWa/Mt-km
-        mt_input['unit'] = 'GWa' # denominator assumed in output
-        mt_input = mt_input.groupby(['node_loc', 'technology', 'unit'])['mt_value'].sum().reset_index()
-        
-        regavg = basedf.groupby(['node_loc'])[['energy_mj_sum', 'dwt', 'distance_km_sum']].sum().reset_index()
-        regavg['mt_value_reg'] = regavg['energy_mj_sum']/(regavg['dwt']*regavg['distance_km_sum'])
-        regavg = regavg[['node_loc', 'mt_value_reg']]
-        
-        inputdf = pd.read_csv(os.path.join(out_path, tec, "edit_files", "flow_technology", "input.csv"))
-        inputdf = inputdf.merge(mt_input, 
-                                left_on = ['node_loc', 'technology', 'unit'],
-                                right_on = ['node_loc', 'technology', 'unit'], how = 'left')
-        inputdf = inputdf.merge(regavg,
-                                left_on = ['node_loc'], right_on = ['node_loc'], how = 'left')
-        
-        inputdf['value'] = np.where(inputdf['mt_value'] > 0, inputdf['mt_value'], inputdf['value'])
-        inputdf['value'] = np.where((inputdf['value'].isnull()) & (inputdf['technology'].str.contains(mtdict[tec]['flow_technology'])), 
-                                    inputdf['mt_value_reg'], inputdf['value'])
-        
-        inputdf = inputdf[['node_origin', 'node_loc', 'technology', 'year_vtg', 'year_act', 'mode',
-                           'commodity', 'level', 'value', 'time', 'time_origin', 'unit']]
-        inputdf.to_csv(os.path.join(out_path, tec, "edit_files", "flow_technology", "input.csv"), index = False)
-        
+        for flow_fuel in mtdict[tec]['flow_technology']:
+            basedf = mtdf[mtdf['astd_ship_type'] == mtdict[tec]['astd_ship_type']].copy()
+            basedf['node_loc'] = basedf[message_regions + '_origin']
+            basedf['technology'] = flow_fuel
+            
+            # Fuel consumption (input)
+            mt_input = basedf.copy()
+            mt_input['mt_value'] = mt_input['intensity_MJ_tonne']/mt_input['distance_km_sum'] # MJ/t-km
+            mt_input['mt_value'] = mt_input['mt_value']*3.17e-11 # GWa/t-km
+            mt_input['mt_value'] = mt_input['mt_value']*1e6 # GWa/Mt-km
+            mt_input['unit'] = 'GWa' # denominator assumed in output
+            mt_input = mt_input.groupby(['node_loc', 'technology', 'unit'])['mt_value'].sum().reset_index()
+            
+            regavg = basedf.groupby(['node_loc'])[['energy_mj_sum', 'dwt', 'distance_km_sum']].sum().reset_index()
+            regavg['mt_value_reg'] = regavg['energy_mj_sum']/(regavg['dwt']*regavg['distance_km_sum'])
+            regavg = regavg[['node_loc', 'mt_value_reg']]
+            
+            inputdf = pd.read_csv(os.path.join(out_path, tec, "edit_files", "flow_technology", "input.csv"))
+            inputdf = inputdf.merge(mt_input, 
+                                    left_on = ['node_loc', 'technology', 'unit'],
+                                    right_on = ['node_loc', 'technology', 'unit'], how = 'left')
+            inputdf = inputdf.merge(regavg,
+                                    left_on = ['node_loc'], right_on = ['node_loc'], how = 'left')
+            
+            inputdf['value'] = np.where(inputdf['mt_value'] > 0, inputdf['mt_value'], inputdf['value'])
+            inputdf['value'] = np.where((inputdf['value'].isnull()) &\
+                                        (inputdf['technology'].str.contains(flow_fuel)), 
+                                        inputdf['mt_value_reg'], inputdf['value'])
+            
+            inputdf = inputdf[['node_origin', 'node_loc', 'technology', 'year_vtg', 'year_act', 'mode',
+                               'commodity', 'level', 'value', 'time', 'time_origin', 'unit']]
+            inputdf.to_csv(os.path.join(out_path, tec, "edit_files", "flow_technology", "input.csv"), index = False)
+            
         # Historical activity
-        histdf = basedf.copy()
-        histdf['value'] = histdf['distance_km_sum'] * histdf['dwt'] # t-km
-        histdf['value'] = histdf['value'] / 1e6 # Mt-km
-        histdf['unit'] = 'Mt-km'
-        histdf['year_act'] = 2025 # last historical year
-        histdf['mode'] = histdf['R12_origin'].str.replace(message_regions + '_', '') + '-' +\
-                            histdf['R12_destination'].str.replace(message_regions + '_', '')
-        histdf['time'] = 'year'
-        histdf = histdf[['node_loc', 'technology', 'year_act', 'value', 'unit', 'mode', 'time']]
-        
+        histdf = pd.DataFrame()
+        for flow_fuel in mtdict[tec]['flow_technology']:
+            basedf = mtdf[mtdf['astd_ship_type'] == mtdict[tec]['astd_ship_type']].copy()
+            basedf['node_loc'] = basedf[message_regions + '_origin']
+            basedf['technology'] = flow_fuel
+            basedf['value'] = basedf['distance_km_sum'] * basedf['dwt'] # t-km
+            basedf['value'] = basedf['value'] / 1e6 # Mt-km
+            basedf['unit'] = 'Mt-km'
+            basedf['year_act'] = 2025 # last historical year
+            basedf['mode'] = basedf['R12_origin'].str.replace(message_regions + '_', '') + '-' +\
+                             basedf['R12_destination'].str.replace(message_regions + '_', '')
+            basedf['time'] = 'year'
+            basedf = basedf[['node_loc', 'technology', 'year_act', 'value', 'unit', 'mode', 'time']]
+            
+            if flow_fuel == 'LNG_tanker_LNG': basedf['value'] *= 0.8 # Assume 80% of historical LNG tankers are propelled using LNG
+            if flow_fuel == 'LNG_tanker_foil': basedf['value'] *= 0.2 # Assume 20% of historical LNG tankers are propelled using diesel
+            histdf = pd.concat([histdf, basedf])
+            
         histdf.to_csv(os.path.join(out_path, tec, "edit_files", "flow_technology", "historical_activity.csv"), index = False)
