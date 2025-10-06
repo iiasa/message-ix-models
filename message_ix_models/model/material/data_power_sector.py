f88a0ea27e443cb87f310b15280d69ed678e959f
Florian Maczek <maczek@iiasa.ac.at>
2025-10-06 13:54:21 +0200

    Improve power sector material module
    - Convert xlsx mappings to csv and yaml
    - Move deprecated files to archive
    - Replace loops with vectorized computations
    - Generate parameter data dynamically based on vintage capacity of
       scenario

--- message_ix_models/model/material/data_power_sector.py
+++ message_ix_models/model/material/data_power_sector.py
@@ -1,363 +1,140 @@
-from collections import defaultdict
+from typing import TYPE_CHECKING
 
-import message_ix
+import numpy as np
 import pandas as pd
+import pint
+from message_ix import make_df
 
-from message_ix_models.util import package_data_path
+from message_ix_models import ScenarioInfo
+from message_ix_models.model.material.util import read_yaml_file
+from message_ix_models.util import package_data_path, same_node
+
+if TYPE_CHECKING:
+    from message_ix import Scenario
+
+    from message_ix_models.types import ParameterData
 
 
 def gen_data_power_sector(
-    scenario: message_ix.Scenario, dry_run: bool = False
-) -> dict[str, pd.DataFrame]:
+    scenario: "Scenario", dry_run: bool = False
+) -> "ParameterData":
     """Generate data for materials representation of power industry."""
-    # Load configuration
-
-    # paths to lca data
-    data_path = package_data_path("material", "power_sector")
-
-    # Information about scenario, e.g. node, year
-
-    # read inv.cost data
-    inv_cost = scenario.par("inv_cost")
-
-    # List of data frames, to be concatenated together at end
-    results = defaultdict(list)
-
-    # for p in set(param_name):
-    #     df = read_material_intensities(
-    #         p, str(data_path), node, year, technology, commodity, level, inv_cost
-    #     )
-    #     print("type df:", type(df))
-    #     print(df.head())
-    #
-    #     results[p].append(df)
-
-    int_dict = read_material_intensities(str(data_path), inv_cost)
-    for k, v in int_dict.items():
-        results[k].append(v)
+    info = ScenarioInfo(scenario)
+    int_dict = gen_cap_par_data(read_material_intensities(info), scenario, info)
 
     # create new parameters input_cap_new, output_cap_new, input_cap_ret,
     # output_cap_ret, input_cap and output_cap if they don't exist
     maybe_init_pars(scenario)
 
-    # Concatenate to one data frame per parameter
-    return {par_name: pd.concat(dfs) for par_name, dfs in results.items()}
+    return int_dict
 
 
-def read_material_intensities(
-    data_path: str, inv_cost: pd.DataFrame
-) -> dict[str, pd.DataFrame]:
-    ####################################################################
-    # read data
-    ####################################################################
-
+def read_material_intensities(s_info: "ScenarioInfo") -> pd.DataFrame:
     # read LCA data from ADVANCE LCA tool
-    data_path_lca = data_path + "/NTNU_LCA_coefficients.xlsx"
-    data_lca = pd.read_excel(data_path_lca, sheet_name="environmentalImpacts")
-
-    # For hydropower material intensity use "medium" from Kalt et al., 2021.
-    # Unit: t/MW
-
-    data_lca.loc[
-        (
-            (data_lca["technology"] == "Hydro")
-            & (data_lca["technology variant"] == "mix")
-            & (data_lca["phase"] == "Construction")
-            & (data_lca["impact"] == "Iron")
-        ),
-        2010,
-    ] = 45
-
-    data_lca.loc[
-        (
-            (data_lca["technology"] == "Hydro")
-            & (data_lca["technology variant"] == "mix")
-            & (data_lca["phase"] == "Construction")
-            & (data_lca["impact"] == "Iron")
-        ),
-        2030,
-    ] = 45
-
-    data_lca.loc[
-        (
-            (data_lca["technology"] == "Hydro")
-            & (data_lca["technology variant"] == "mix")
-            & (data_lca["phase"] == "Construction")
-            & (data_lca["impact"] == "Aluminium")
-        ),
-        2010,
-    ] = 0.572
-
-    data_lca.loc[
-        (
-            (data_lca["technology"] == "Hydro")
-            & (data_lca["technology variant"] == "mix")
-            & (data_lca["phase"] == "Construction")
-            & (data_lca["impact"] == "Aluminium")
-        ),
-        2030,
-    ] = 0.572
-
-    data_lca.loc[
-        (
-            (data_lca["technology"] == "Hydro")
-            & (data_lca["technology variant"] == "mix")
-            & (data_lca["phase"] == "Construction")
-            & (data_lca["impact"] == "Cement")
-        ),
-        2010,
-    ] = 787.5
-
-    data_lca.loc[
-        (
-            (data_lca["technology"] == "Hydro")
-            & (data_lca["technology variant"] == "mix")
-            & (data_lca["phase"] == "Construction")
-            & (data_lca["impact"] == "Cement")
-        ),
-        2030,
-    ] = 787.5
-
+    path = package_data_path("material", "power_sector")
     # read technology, region and commodity mappings
-    data_path_tec_map = data_path + "/MESSAGE_global_model_technologies.xlsx"
-    technology_mapping = pd.read_excel(data_path_tec_map, sheet_name="technology")
+    tec_map = (
+        pd.read_csv(
+            path.joinpath("MESSAGE_global_model_technologies.csv"),
+            usecols=[0, 7],
+        )
+        .dropna()
+        .groupby("LCA mapping")["Type of Technology"]
+        .apply(list)
+        .to_dict()
+    )
+    reg_map = read_yaml_file(
+        package_data_path("material", "power_sector", "themis_region_map.yaml")
+    )
+    comm_map = pd.read_csv(path.joinpath("lca_commodity_mapping.csv"), index_col=0)
 
-    data_path_reg_map = data_path + "/LCA_region_mapping.xlsx"
-    region_mapping = pd.read_excel(data_path_reg_map, sheet_name="region")
+    data = pd.read_excel(
+        path.joinpath("NTNU_LCA_coefficients.xlsx"), sheet_name="environmentalImpacts"
+    )
+    data = overwrite_hydro_intensities(data)
+    data = (
+        data.assign(technology=data["technology"].map(tec_map))
+        .explode("technology")
+        .assign(region=data["region"].map(reg_map))
+        .explode("region")
+        .rename(columns={"region": "node_loc"})
+    )
 
-    data_path_com_map = data_path + "/LCA_commodity_mapping.xlsx"
-    commodity_mapping = pd.read_excel(data_path_com_map, sheet_name="commodity")
-
-    ####################################################################
-    # process data
-    ####################################################################
+    data = data.set_index([i for i in data.columns if not isinstance(i, int)])
 
     # filter relevant scenario, technology variant (residue for biomass,
     # mix for others) and remove operation phase (and remove duplicates)
-    data_lca = data_lca.loc[
-        (
-            (data_lca["scenario"] == "Baseline")
-            & (data_lca["technology variant"].isin(["mix", "residue"]))
-            & (data_lca["phase"] != "Operation")
+    data = data.loc["THEMIS", "Baseline"].loc[
+        :,
+        "Environmental impacts",
+        :,
+        ["mix", "residue"],
+        :,
+        ["Construction", "End-of-life"],
+    ]
+    pre_2010 = [i for i in s_info.set["year"] if i < 2010]
+    data[pre_2010] = np.tile(data[[2010]].to_numpy(), (1, len(pre_2010)))
+    intermed_years = [2010, 2015, 2020, 2025, 2030, 2035, 2040, 2045, 2050]
+    data = data.reindex(columns=pre_2010 + intermed_years).interpolate(
+        axis=1, method="linear"
+    )
+    post_2050 = [i for i in s_info.Y if i > 2050]
+    data[post_2050] = np.tile(data[[2050]].to_numpy(), (1, len(post_2050)))
+    data = (
+        data.join(
+            comm_map,
+            how="inner",
         )
-    ]
-
-    data_lca[2015] = None
-    data_lca[2020] = None
-    data_lca[2025] = None
-    data_lca[2035] = None
-    data_lca[2040] = None
-    data_lca[2045] = None
-
-    # add intermediate time steps and turn into long table format
-    years = [2010, 2015, 2020, 2025, 2030, 2035, 2040, 2045]
-    data_lca = pd.melt(
-        data_lca,
-        id_vars=[
-            "source",
-            "scenario",
-            "region",
-            "variable",
-            "technology",
-            "technology variant",
-            "impact",
-            "phase",
-            "unit",
-        ],
-        value_vars=years,
-        var_name="year",
+        .dropna()
+        .set_index(["commodity", "level"], append=True)
+        .melt(ignore_index=False, var_name="year_vtg")
+        .reset_index()
+        .assign(unit="t/kW")
     )
-    # Make sure the values are numeric.
-    data_lca[["value"]] = data_lca[["value"]].astype(float)
+    data["value"] = pint.Quantity(data["value"].values, "kt").to("Mt").round(7)
+    return data
 
-    # apply technology, commodity/impact and region mappings to MESSAGEix
-    data_lca_merged_1 = pd.merge(
-        data_lca, region_mapping, how="inner", left_on="region", right_on="THEMIS"
+
+def gen_cap_par_data(data_lca: pd.DataFrame, scen, s_info) -> "ParameterData":
+    hist_cap = scen.par(
+        "historical_new_capacity",
+        filters={"technology": data_lca["technology"].unique()},
     )
-    data_lca_merged_2 = pd.merge(
-        data_lca_merged_1,
-        technology_mapping,
-        how="inner",
-        left_on="technology",
-        right_on="LCA mapping",
+    hist_cap = (
+        hist_cap[hist_cap["value"] != 0]
+        .set_index(["node_loc", "technology", "year_vtg"])
+        .drop("unit", axis=1)
     )
-    data_lca_merged_final = pd.merge(
-        data_lca_merged_2,
-        commodity_mapping,
-        how="inner",
-        left_on="impact",
-        right_on="impact",
-    )
+    inflow = data_lca.loc[data_lca["phase"] == "Construction"].copy()
+    outflow = data_lca.loc[data_lca["phase"] == "End-of-life"].copy()
+    input_cap_new = make_df(
+        "input_cap_new", **inflow[inflow["year_vtg"].ge(s_info.Y[0])], time="year"
+    ).pipe(same_node)
+    input_cap_ret = make_df("input_cap_ret", **inflow, time="year").pipe(same_node)
+    input_cap_ret_future = input_cap_ret[input_cap_ret["year_vtg"] > s_info.Y[0]]
+    input_cap_ret_hist = input_cap_ret.set_index(
+        ["node_loc", "technology", "year_vtg"]
+    )[
+        input_cap_ret.set_index(["node_loc", "technology", "year_vtg"]).index.isin(
+            hist_cap.index
+        )
+    ].reset_index()
+    input_cap_ret = pd.concat([input_cap_ret_hist, input_cap_ret_future])
 
-    data_lca = data_lca_merged_final[
-        ~data_lca_merged_final["MESSAGEix-GLOBIOM_1.1"].isnull()
-    ]
-
-    # Drop technology column that has LCA style names
-    # Instead MESSAGE technology names will be used in the technology column
-    data_lca = data_lca.drop(["technology"], axis=1)
-
-    data_lca.rename(
-        columns={
-            "MESSAGEix-GLOBIOM_1.1": "node",
-            "Type of Technology": "technology",
-        },
-        inplace=True,
-    )
-    keep_columns = [
-        "node",
-        "technology",
-        "phase",
-        "commodity",
-        "level",
-        "year",
-        "unit",
-        "value",
-    ]
-    data_lca = data_lca[keep_columns]
-
-    data_lca_final = pd.DataFrame()
-
-    for n in data_lca["node"].unique():
-        for t in data_lca["technology"].unique():
-            for c in data_lca["commodity"].unique():
-                for p in data_lca["phase"].unique():
-                    temp = data_lca.copy(deep=True).loc[
-                        (
-                            (data_lca["node"] == n)
-                            & (data_lca["technology"] == t)
-                            & (data_lca["commodity"] == c)
-                            & (data_lca["phase"] == p)
-                        )
-                    ]
-                    temp["value"] = temp["value"].interpolate(
-                        method="linear", limit_direction="forward", axis=0
-                    )
-                    data_lca_final = pd.concat([temp, data_lca_final])
-
-    # extract node, technology, commodity, level, and year list from LCA
-    # data set
-    node_list = data_lca_final["node"].unique()
-    year_list = data_lca_final["year"].unique()
-    tec_list = data_lca_final["technology"].unique()
-    com_list = data_lca_final["commodity"].unique()
-    lev_list = data_lca_final["level"].unique()
-    # add scrap as commodity level
-    lev_list = lev_list + ["end_of_life"]
-
-    ####################################################################
-    # create data frames for material intensity input/output parameters
-    ####################################################################
-
-    # new data frames for parameters
-    input_cap_new = pd.DataFrame()
-    input_cap_ret = pd.DataFrame()
-    output_cap_ret = pd.DataFrame()
-
-    for n in node_list:
-        for t in tec_list:
-            for c in com_list:
-                year_vtg_list = inv_cost.loc[
-                    ((inv_cost["node_loc"] == n) & (inv_cost["technology"] == t))
-                ]["year_vtg"].unique()
-                for y in year_vtg_list:
-                    # for years after maximum year in data set use
-                    # values for maximum year, similarly for years
-                    # before minimum year in data set use values for
-                    # minimum year
-                    if y > max(year_list):
-                        yeff = max(year_list)
-                    elif y < min(year_list):
-                        yeff = min(year_list)
-                    else:
-                        yeff = y
-                    val_cap_new = data_lca_final.loc[
-                        (data_lca_final["node"] == n)
-                        & (data_lca_final["technology"] == t)
-                        & (data_lca_final["phase"] == "Construction")
-                        & (data_lca_final["commodity"] == c)
-                        & (data_lca_final["year"] == yeff)
-                    ]["value"].values[0]
-                    val_cap_new = val_cap_new * 0.001
-
-                    input_cap_new = pd.concat(
-                        [
-                            input_cap_new,
-                            pd.DataFrame(
-                                {
-                                    "node_loc": n,
-                                    "technology": t,
-                                    "year_vtg": str(y),
-                                    "node_origin": n,
-                                    "commodity": c,
-                                    "level": "product",
-                                    "time_origin": "year",
-                                    "value": val_cap_new,
-                                    "unit": "t/kW",
-                                },
-                                index=[0],
-                            ),
-                        ]
-                    )
-
-                    val_cap_input_ret = data_lca_final.loc[
-                        (data_lca_final["node"] == n)
-                        & (data_lca_final["technology"] == t)
-                        & (data_lca_final["phase"] == "End-of-life")
-                        & (data_lca_final["commodity"] == c)
-                        & (data_lca_final["year"] == yeff)
-                    ]["value"].values[0]
-                    val_cap_input_ret = val_cap_input_ret * 0.001
-
-                    input_cap_ret = pd.concat(
-                        [
-                            input_cap_ret,
-                            pd.DataFrame(
-                                {
-                                    "node_loc": n,
-                                    "technology": t,
-                                    "year_vtg": str(y),
-                                    "node_origin": n,
-                                    "commodity": c,
-                                    "level": "product",
-                                    "time_origin": "year",
-                                    "value": val_cap_input_ret,
-                                    "unit": "t/kW",
-                                },
-                                index=[0],
-                            ),
-                        ]
-                    )
-
-                    val_cap_output_ret = data_lca_final.loc[
-                        (data_lca_final["node"] == n)
-                        & (data_lca_final["technology"] == t)
-                        & (data_lca_final["phase"] == "Construction")
-                        & (data_lca_final["commodity"] == c)
-                        & (data_lca_final["year"] == yeff)
-                    ]["value"].values[0]
-                    val_cap_output_ret = val_cap_output_ret * 0.001
-
-                    output_cap_ret = pd.concat(
-                        [
-                            output_cap_ret,
-                            pd.DataFrame(
-                                {
-                                    "node_loc": n,
-                                    "technology": t,
-                                    "year_vtg": str(y),
-                                    "node_dest": n,
-                                    "commodity": c,
-                                    "level": "end_of_life",
-                                    "time_dest": "year",
-                                    "value": val_cap_output_ret,
-                                    "unit": "t/kW",
-                                },
-                                index=[0],
-                            ),
-                        ]
-                    )
-
+    output_cap_ret = make_df(
+        "output_cap_ret",
+        **outflow.replace({"level": {"product": "end_of_life"}}),
+        time="year",
+    ).pipe(same_node)
+    output_cap_ret_future = output_cap_ret[output_cap_ret["year_vtg"] > s_info.Y[0]]
+    output_cap_ret_hist = output_cap_ret.set_index(
+        ["node_loc", "technology", "year_vtg"]
+    )[
+        output_cap_ret.set_index(["node_loc", "technology", "year_vtg"]).index.isin(
+            hist_cap.index
+        )
+    ].reset_index()
+    output_cap_ret = pd.concat([output_cap_ret_hist, output_cap_ret_future])
     return {
         "input_cap_new": input_cap_new,
         "input_cap_ret": input_cap_ret,
@@ -365,7 +142,21 @@ def read_material_intensities(
     }
 
 
-def maybe_init_pars(scenario: message_ix.Scenario) -> None:
+def overwrite_hydro_intensities(data_lca: pd.DataFrame) -> pd.DataFrame:
+    # For hydropower material intensity use "medium" from Kalt et al., 2021.
+    # Unit: t/MW
+    idx = (
+        (data_lca["technology"] == "Hydro")
+        & (data_lca["technology variant"] == "mix")
+        & (data_lca["phase"] == "Construction")
+    )
+    data_lca.loc[idx & (data_lca["impact"] == "Iron"), [2010, 2030]] = 45
+    data_lca.loc[idx & (data_lca["impact"] == "Aluminium"), [2010, 2030]] = 0.572
+    data_lca.loc[idx & (data_lca["impact"] == "Cement"), [2010, 2030]] = 787.5
+    return data_lca
+
+
+def maybe_init_pars(scenario: "Scenario") -> None:
     if not scenario.has_par("input_cap_new"):
         scenario.init_par(
             "input_cap_new",
