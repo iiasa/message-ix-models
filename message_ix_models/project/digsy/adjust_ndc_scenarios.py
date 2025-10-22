452dc7333daa0439af1df709817ff58f1db501e2
Florian Maczek <maczek@iiasa.ac.at>
2025-10-22 22:40:50 +0200

    Add scripts for digsy scenario updates

---
+++ message_ix_models/project/digsy/adjust_ndc_scenarios.py
@@ -0,0 +1,37 @@
+import ixmp
+import message_ix
+
+from message_ix_models.project.digsy.sensitivity import (
+    adjust_electrification_constraint,
+    adjust_rooftop_constraint,
+    lower_cv_cost,
+)
+
+if __name__ == "__main__":
+    mp = ixmp.Platform("ixmp_dev")
+    scen = message_ix.Scenario(mp, "DIGSY_SSP2_BEST", "INDC2030i_weak").clone(
+        "DIGSY_SSP2_BEST", "INDC2030i_weak", keep_solution=False
+    )
+    adjust_electrification_constraint(scen)
+    adjust_rooftop_constraint(scen, 0.4)
+    scen.solve("MESSAGE-MACRO")
+    del scen
+    scen = message_ix.Scenario(mp, "DIGSY_SSP2_BESTEST", "INDC2030i_weak").clone(
+        "DIGSY_SSP2_BESTEST",
+        "INDC2030i_weak",
+        keep_solution=False,
+    )
+    adjust_electrification_constraint(scen)
+    adjust_rooftop_constraint(scen, 0.45)
+    lower_cv_cost(scen, 0.666)
+    scen.solve("MESSAGE-MACRO")
+    del scen
+    scen = message_ix.Scenario(mp, "DIGSY_SSP2_WORSTEST", "INDC2030i_weak").clone(
+        "DIGSY_SSP2_WORSTEST",
+        "INDC2030i_weak",
+        keep_solution=False,
+    )
+    adjust_electrification_constraint(scen)
+    adjust_rooftop_constraint(scen, 0.5)
+    lower_cv_cost(scen, 1.25)
+    scen.solve("MESSAGE-MACRO")
---
+++ message_ix_models/project/digsy/adjust_npi_scenarios.py
@@ -0,0 +1,37 @@
+import ixmp
+import message_ix
+
+from message_ix_models.project.digsy.sensitivity import (
+    adjust_electrification_constraint,
+    adjust_rooftop_constraint,
+    lower_cv_cost,
+)
+
+if __name__ == "__main__":
+    mp = ixmp.Platform("ixmp_dev")
+    scen = message_ix.Scenario(mp, "DIGSY_SSP2_BEST", "NPiREF").clone(
+        "DIGSY_SSP2_BEST", "NPiREF", keep_solution=False
+    )
+    adjust_electrification_constraint(scen)
+    adjust_rooftop_constraint(scen, 0.4)
+    scen.solve("MESSAGE-MACRO")
+    del scen
+    scen = message_ix.Scenario(mp, "DIGSY_SSP2_BESTEST", "NPiREF").clone(
+        "DIGSY_SSP2_BESTEST",
+        "NPiREF",
+        keep_solution=False,
+    )
+    adjust_electrification_constraint(scen)
+    adjust_rooftop_constraint(scen, 0.45)
+    lower_cv_cost(scen, 0.666)
+    scen.solve("MESSAGE-MACRO")
+    del scen
+    scen = message_ix.Scenario(mp, "DIGSY_SSP2_WORSTEST", "NPiREF").clone(
+        "DIGSY_SSP2_WORSTEST",
+        "NPiREF",
+        keep_solution=False,
+    )
+    adjust_electrification_constraint(scen)
+    adjust_rooftop_constraint(scen, 0.5)
+    lower_cv_cost(scen, 1.25)
+    scen.solve("MESSAGE-MACRO")
