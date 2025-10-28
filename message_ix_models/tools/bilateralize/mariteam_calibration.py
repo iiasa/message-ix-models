48ce02c8ad5edd8b2fd057842dd6da3cbcf0946c
Jun Shepard <51372492+junukitashepard@users.noreply.github.com>
2025-10-28 16:51:28 +0100

    Split bilateralize into step code
    1) prepare_edit to generate edit files
    2) bare_to_scenario to generate dictionary
    3) load_and_solve to update scenario and optionally solve

--- message_ix_models/tools/bilateralize/mariteam_calibration.py
+++ message_ix_models/tools/bilateralize/mariteam_calibration.py
@@ -4,20 +4,13 @@ Calibration with MariTEAM output
 """
 # Import packages
 import os
-import sys
-import pandas as pd
-import logging
-import yaml
-import message_ix
-import ixmp
-import itertools
-import numpy as np
-import pickle
 
-from pathlib import Path
-from message_ix_models.util import package_data_path
-from message_ix_models.tools.iea import web
+import numpy as np
+import pandas as pd
+
 from message_ix_models.tools.bilateralize import bilateralize
+from message_ix_models.util import package_data_path
+
 
 def calibrate_mariteam(covered_tec,
                        message_regions,
@@ -46,24 +39,24 @@ def calibrate_mariteam(covered_tec,
     data_path = os.path.join(p_drive, "MESSAGE_trade")
     mt_path = os.path.join(data_path, "MariTEAM")
     out_path = os.path.join(os.path.dirname(package_data_path("bilateralize")), "bilateralize")
-    
+
     # Import MariTEAM outputs
     mtdf = pd.read_csv(os.path.join(mt_path, mt_output))
     mtdf = mtdf[mtdf[message_regions + '_origin'] != mtdf[message_regions + '_destination']] # no intraregional trade
-    
+
     for tec in [i for i in covered_tec if 'shipped' in i]:
-        
+
         # If a non-energy commodity, add to bulk carriers in mtdict
         if tec.replace('_shipped', '') in ['biomass', 'coal', 'crudeoil', 'eth', 'foil', 'lh2', 'loil', 'LNG', 'meth'] == False:
             mtdict_add = {tec: {'astd_ship_type': 'Bulk carriers',
                                 'flow_technology': ['energy_bulk_carrier_loil']}}
             mtdict.update(mtdict_add)
-        
+
         for flow_fuel in mtdict[tec]['flow_technology']:
             basedf = mtdf[mtdf['astd_ship_type'] == mtdict[tec]['astd_ship_type']].copy()
             basedf['node_loc'] = basedf[message_regions + '_origin']
             basedf['technology'] = flow_fuel
-            
+
             # Fuel consumption (input)
             mt_input = basedf.copy()
             mt_input['mt_value'] = mt_input['intensity_MJ_tonne']/mt_input['distance_km_sum'] # MJ/t-km
@@ -71,27 +64,27 @@ def calibrate_mariteam(covered_tec,
             mt_input['mt_value'] = mt_input['mt_value']*1e6 # GWa/Mt-km
             mt_input['unit'] = 'GWa' # denominator assumed in output
             mt_input = mt_input.groupby(['node_loc', 'technology', 'unit'])['mt_value'].sum().reset_index()
-            
+
             regavg = basedf.groupby(['node_loc'])[['energy_mj_sum', 'dwt', 'distance_km_sum']].sum().reset_index()
             regavg['mt_value_reg'] = regavg['energy_mj_sum']/(regavg['dwt']*regavg['distance_km_sum'])
             regavg = regavg[['node_loc', 'mt_value_reg']]
-            
+
             inputdf = pd.read_csv(os.path.join(out_path, tec, "edit_files", "flow_technology", "input.csv"))
-            inputdf = inputdf.merge(mt_input, 
+            inputdf = inputdf.merge(mt_input,
                                     left_on = ['node_loc', 'technology', 'unit'],
                                     right_on = ['node_loc', 'technology', 'unit'], how = 'left')
             inputdf = inputdf.merge(regavg,
                                     left_on = ['node_loc'], right_on = ['node_loc'], how = 'left')
-            
+
             inputdf['value'] = np.where(inputdf['mt_value'] > 0, inputdf['mt_value'], inputdf['value'])
             inputdf['value'] = np.where((inputdf['value'].isnull()) &\
-                                        (inputdf['technology'].str.contains(flow_fuel)), 
+                                        (inputdf['technology'].str.contains(flow_fuel)),
                                         inputdf['mt_value_reg'], inputdf['value'])
-            
+
             inputdf = inputdf[['node_origin', 'node_loc', 'technology', 'year_vtg', 'year_act', 'mode',
                                'commodity', 'level', 'value', 'time', 'time_origin', 'unit']]
             inputdf.to_csv(os.path.join(out_path, tec, "edit_files", "flow_technology", "input.csv"), index = False)
-            
+
             if not os.path.isfile(os.path.join(out_path, tec, "bare_files", "flow_technology", "input.csv")):
                 inputdf.to_csv(os.path.join(out_path, tec, "bare_files", "flow_technology", "input.csv"), index = False)
 
@@ -109,15 +102,15 @@ def calibrate_mariteam(covered_tec,
                              basedf['R12_destination'].str.replace(message_regions + '_', '')
             basedf['time'] = 'year'
             basedf = basedf[['node_loc', 'technology', 'year_act', 'value', 'unit', 'mode', 'time']]
-            
+
             shipping_fuel_dict = config['shipping_fuels']
-            
+
             if 'LNG_tanker' in flow_fuel:
                 basedf['value'] *= shipping_fuel_dict['LNG_tanker'][flow_fuel]
             if ('oil_tanker' in flow_fuel)&('crudeoil' not in flow_fuel):
                 basedf['value'] *= shipping_fuel_dict['oil_tanker'][flow_fuel]
 
             histdf = pd.concat([histdf, basedf])
-            
+
         histdf.to_csv(os.path.join(out_path, tec, "edit_files", "flow_technology", "historical_activity.csv"), index = False)
         histdf.to_csv(os.path.join(out_path, tec, "bare_files", "flow_technology", "historical_activity.csv"), index = False)
--- message_ix_models/tools/bilateralize/pull_gem.py
+++ message_ix_models/tools/bilateralize/pull_gem.py
@@ -4,20 +4,15 @@ Historical Calibration
 """
 # Import packages
 import os
-import sys
-import pandas as pd
-import logging
-import yaml
-import message_ix
-import ixmp
-import itertools
-import numpy as np
-import pickle
-
 from pathlib import Path
+
+import message_ix
+import numpy as np
+import pandas as pd
+import yaml
+
 from message_ix_models.tools.bilateralize import bilateralize
 from message_ix_models.util import package_data_path
-from message_ix_models.tools.iea import web
 
 oil_pipeline_file = 'GEM-GOIT-Oil-NGL-Pipelines-2025-03.xlsx'
 oil_pipeline_sheet = 'Pipelines'
@@ -26,34 +21,54 @@ gas_pipeline_file = 'GEM-GGIT-Gas-Pipelines-2024-12.xlsx'
 gas_pipeline_sheet = 'Gas Pipelines 2024-12-17'
 
 # Set up MESSAGE regions
-def gem_region(project_name = None, 
-               config_name = None):
+def gem_region(project_name: str | None = None,
+               config_name: str | None = None):
+    """
+    Set up MESSAGE regions
     
-    config, config_path = bilateralize.load_config(project_name = project_name, 
+    Args:
+        project_name: Name of project
+        config_name: Name of config file
+    """
+
+    config, config_path = bilateralize.load_config(project_name = project_name,
                                                    config_name = config_name)
     with open(config_path, "r") as f:
-        config = yaml.safe_load(f) 
+        config = yaml.safe_load(f)
     message_regions = config['scenario']['regions']
-    
+
     full_path = package_data_path("bilateralize", "node_lists", message_regions + "_node_list.yaml")
     with open(full_path, "r") as f:
-        message_regions = yaml.safe_load(f) 
+        message_regions = yaml.safe_load(f)
     message_regions_list = [r for r in message_regions.keys() if r not in ['World', 'GLB']]
-    
+
     return message_regions_list, message_regions
 
 # Import files
-def import_gem(input_file: str, 
-               input_sheet: str, 
-               trade_technology: str, 
+def import_gem(input_file: str | Path,
+               input_sheet: str,
+               trade_technology: str,
                flow_technology: str,
                flow_commodity: str,
-               project_name:str | None = None,
+               project_name: str | None = None,
                config_name: str | None = None,
                first_model_year: int = 2030):
+    """
+    Import Global Energy Monitor data
+    
+    Args:
+        input_file: Name of input file
+        input_sheet: Name of input sheet
+        trade_technology: Name of trade technology
+        flow_technology: Name of flow technology
+        flow_commodity: Name of flow commodity
+        project_name: Name of project
+        config_name: Name of config file
+        first_model_year: First model year
+    """
 
     # Pull in configuration
-    config, config_path = bilateralize.load_config(project_name = project_name, 
+    config, config_path = bilateralize.load_config(project_name = project_name,
                                                    config_name = config_name)
     p_drive = config['p_drive_location']
 
@@ -63,71 +78,71 @@ def import_gem(input_file: str,
 
     df = pd.read_excel(os.path.join(gem_path, input_file),
                        sheet_name = input_sheet)
-    
+
     df = df[df['StopYear'].isnull()] # Only continuing projects
-    
+
     df = df[['StartYear1', 'StartCountry', 'EndCountry',
              'CapacityBOEd', 'CostUSD', 'LengthMergedKm']].drop_duplicates()
 
-    # Clean up country codes   
+    # Clean up country codes
     cw = pd.read_csv(os.path.join(gem_path, 'country_crosswalk.csv'))
     for i in ['Start', 'End']:
         df = df.merge(cw, left_on = i + 'Country', right_on = 'GEM Country', how = 'left')
         df = df.rename(columns = {'ISO': i + 'ISO'})
-    
+
     # Add MESSAGE regions
     message_regions_list, message_regions = gem_region(project_name, config_name)
     df['EXPORTER'] = ''; df['IMPORTER'] = ''
     for r in message_regions_list:
         df['EXPORTER'] = np.where(df['StartISO'].isin(message_regions[r]['child']), r, df['EXPORTER'])
         df['IMPORTER'] = np.where(df['EndISO'].isin(message_regions[r]['child']), r, df['IMPORTER'])
-    
+
     # Collapse
     df['CapacityBOEd'] = df['CapacityBOEd'].replace('--', '0', regex = True).astype(float)
     df['CostUSD'] = df['CostUSD'].replace('--', '0', regex = True).astype(float)
     df['LengthMergedKm'] = df['LengthMergedKm'].replace('--', '0', regex = True).astype(float)
-    
+
     df = df[(df['CapacityBOEd'].isnull() == False) & (df['CostUSD'].isnull() == False)]
-    
+
     df = df[df['StartYear1'] < first_model_year] # No planned capacity
     df['YEAR'] = round(df['StartYear1'].astype(float)/5)*5 # Round year to the nearest 5
-    
+
     df_long = df.copy() # Keep for historical new capacity
     df = df.groupby(['EXPORTER', 'IMPORTER', 'YEAR'])[['CapacityBOEd', 'CostUSD', 'LengthMergedKm']].sum().reset_index()
-    
+
     # Convert units
     df['Capacity (BOEa)'] = df['CapacityBOEd']*365
     df['Capacity (TJ)'] = df['Capacity (BOEa)'] * 0.006 # BOEa to TJ
     df['Capacity (GWa)'] = df['Capacity (TJ)'] * (3.1712 * 1e-5) # TJ to GWa
-    
+
     # Generate investment costs
     df['InvCost (USD/km)'] = (df['CostUSD'])/df['Capacity (GWa)']
     #TODO: Add industry-specific deflators
-    
+
     # Generate capacity
     df['Capacity (GWa/km)'] = (df['Capacity (GWa)'])/df['LengthMergedKm']
-    
+
     # Cut down
     df = df[df['EXPORTER'] != df['IMPORTER']]
     df = df[(df['EXPORTER']!= "") & (df['IMPORTER'] != "")]
-    
+
     # Base file with all historical timesteps
     hist_base = pd.DataFrame()
     for y in list(range(2000, 2030, 5)):
         ydf = df[['EXPORTER', 'IMPORTER']].drop_duplicates().copy()
         ydf['YEAR'] = y
         hist_base = pd.concat([hist_base, ydf])
-    hist_base = hist_base[(hist_base['EXPORTER'] != '') & (hist_base['IMPORTER'] != '')]   
-    
-    df = hist_base.merge(df, 
-                         left_on = ['EXPORTER', 'IMPORTER', 'YEAR'], 
+    hist_base = hist_base[(hist_base['EXPORTER'] != '') & (hist_base['IMPORTER'] != '')]
+
+    df = hist_base.merge(df,
+                         left_on = ['EXPORTER', 'IMPORTER', 'YEAR'],
                          right_on = ['EXPORTER', 'IMPORTER', 'YEAR'], how = 'outer') # Set 2005 to 0 if missing
     for c in [c for c in df.columns if c not in ['EXPORTER', 'IMPORTER', 'YEAR']]:
         df[c] = np.where((df[c].isnull()) & (df['YEAR'] == 2000), 0, df[c])
 
     df['trade_technology'] = trade_technology
     df['flow_technology'] = flow_technology
-    
+
     # Output to trade_technology
     export_dir = package_data_path("bilateralize", trade_technology)
     trade_dir = os.path.join(os.path.dirname(export_dir), trade_technology, "edit_files")
@@ -137,9 +152,9 @@ def import_gem(input_file: str,
     export_dir = os.path.join(os.path.dirname(export_dir), trade_technology, "GEM")
     if not os.path.isdir(export_dir):
         os.makedirs(export_dir)
-    
+
     df.to_csv(os.path.join(export_dir, "GEM.csv"))
-    
+
     # Investment Costs
     inv_cost = df.groupby(['EXPORTER', 'IMPORTER'])[['CostUSD','Capacity (GWa)']].sum().reset_index()
     inv_cost['InvCost (USD/km)'] = (inv_cost['CostUSD'])/inv_cost['Capacity (GWa)']
@@ -149,7 +164,7 @@ def import_gem(input_file: str,
     inv_cost['value_update'] = inv_cost['InvCost (USD/km)']/1e6 # in MUSD/km
     inv_cost = inv_cost[['node_loc', 'technology', 'value_update']]
     inv_cost.to_csv(os.path.join(export_dir, "inv_cost_GEM.csv"), index = False)
-    
+
     basedf = pd.read_csv(os.path.join(flow_dir, "inv_cost.csv"))
     basedf['value'] = 100
     inv_cost = basedf.merge(inv_cost,
@@ -183,7 +198,7 @@ def import_gem(input_file: str,
     share_oil_pipelines = {'crudeoil_piped': 0.8,
                            'loil_piped': 0.1,
                            'foil_piped': 0.1} # The share of oil pipeline capacity used for given commodity (crude/light oil/fuel oil)
-    
+
     if trade_technology in ["crudeoil_piped", "loil_piped", "foil_piped"]:
         hist_tra = df[['EXPORTER', 'IMPORTER', 'YEAR', 'Capacity (GWa)']].drop_duplicates()
         hist_tra['Capacity (GWa)'] = np.where(hist_tra['Capacity (GWa)'].isnull(), 0, hist_tra['Capacity (GWa)'])
@@ -204,20 +219,20 @@ def import_gem(input_file: str,
     # Historical new capacity
     hist_cap = df[['EXPORTER', 'IMPORTER', 'YEAR', 'LengthMergedKm']]
     hist_cap = hist_cap.rename(columns = {'LengthMergedKm': 'CAPACITY_KM'})
-    
+
     hist_cap = hist_cap.groupby(['EXPORTER', 'IMPORTER', 'YEAR'])['CAPACITY_KM'].sum().reset_index()
     hist_cap = hist_cap.sort_values(by = ['EXPORTER', 'IMPORTER', 'YEAR'], ascending = True)
     #hist_cap['CAPACITY_KM'] = hist_cap.groupby(['EXPORTER', 'IMPORTER'])['CAPACITY_KM'].transform(pd.Series.cumsum)
     hist_cap = hist_cap[(hist_cap['YEAR'] < first_model_year) & (hist_cap['YEAR'] > 1999)]
-    
+
     hist_cap = hist_cap.sort_values(by = ['EXPORTER', 'IMPORTER', 'YEAR'], ascending = False)
     hist_cap = hist_cap[['EXPORTER', 'IMPORTER', 'YEAR', 'CAPACITY_KM']]
-    
+
     # hist_cap['CAPACITY_KM'] = np.where((hist_cap['YEAR'] == 2025) & (hist_cap['CAPACITY_KM'] == 0),
     #                                    1, hist_cap['CAPACITY_KM'])
-    
+
     hist_cap['CAPACITY_KM'] = hist_cap['CAPACITY_KM']/5  # Duration time is 5 years
-    
+
     hist_cap['node_loc'] = hist_cap['EXPORTER']
     hist_cap['technology'] = flow_technology + '_' + hist_cap['IMPORTER'].str.lower().str.split('_').str[-1]
     hist_cap['value'] = round(hist_cap['CAPACITY_KM'],0)
@@ -241,12 +256,12 @@ def import_gem(input_file: str,
     inputdf['value_update'] = round((1/inputdf['Capacity (GWa/km)']),0)
     inputdf['commodity'] = flow_commodity + '_' + inputdf['IMPORTER'].str.lower().str.split('_').str[-1]
     inputdf = inputdf[['node_loc', 'technology', 'value_update', 'commodity']] # km/GWa
-    
+
     basedf = pd.read_csv(os.path.join(trade_dir, "input.csv"))
     basedf['value'] = np.where(basedf['commodity'].str.contains(flow_commodity),
                                30, # The largest capacity pipelines have maximum 300,000GWh (~30bcm) annually
                                basedf['value'])
-    
+
     inputdf = basedf.merge(inputdf,
                            left_on = ['node_loc', 'technology', 'commodity'],
                            right_on = ['node_loc', 'technology', 'commodity'],
@@ -257,7 +272,7 @@ def import_gem(input_file: str,
                                 inputdf['value_update'],
                                 inputdf['value'])
     inputdf = inputdf.drop(['value_update'], axis = 1)
-    
+
     inputdf.to_csv(os.path.join(export_dir, "inputs_GEM.csv"), index = False)
     inputdf.to_csv(os.path.join(trade_dir, "input.csv"), index = False)
     inputdf.to_csv(os.path.join(trade_dir_out, "input.csv"), index = False)
