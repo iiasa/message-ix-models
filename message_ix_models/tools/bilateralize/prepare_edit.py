bf7ff85fe41e1c3cbcea51d6cf8718dfc7066544
Jun Shepard <51372492+junukitashepard@users.noreply.github.com>
2025-11-03 14:34:02 +0100

    Add more tests to test_bilateralize
    Increase coverage to GEM and MariTeam imports

--- message_ix_models/tools/bilateralize/prepare_edit.py
+++ message_ix_models/tools/bilateralize/prepare_edit.py
@@ -1270,13 +1270,14 @@ def generate_edit_files(
     export_edit_files(covered_tec, log, data_path, parameter_outputs)
 
 
-def prepare_edit_files(project_name: str | None = None, config_name: str | None = None):
+def prepare_edit_files(project_name: str | None = None, config_name: str | None = None, P_access: bool = True):
     """
     Prepare edit files for bilateralize tool
 
     Args:
         project_name: Name of the project (e.g., 'newpathways')
         config_name: Name of the config file (e.g., 'config.yaml')
+        P_access: User has access to P: drive
     """
     # Get logger
     log = get_logger(__name__)
@@ -1299,67 +1300,68 @@ def prepare_edit_files(project_name: str | None = None, config_name: str | None 
     generate_edit_files(log=log)
 
     # Import calibration files from Global Energy Monitor
-    import_gem(
-        input_file="GEM-GGIT-Gas-Pipelines-2024-12.xlsx",
-        input_sheet="Gas Pipelines 2024-12-17",
-        trade_technology="gas_piped",
-        flow_technology="gas_pipe",
-        flow_commodity="gas_pipeline_capacity",
-    )
-
-    for tradetec in ["crudeoil_piped", "foil_piped", "loil_piped"]:
+    if P_access is True:
         import_gem(
-            input_file="GEM-GOIT-Oil-NGL-Pipelines-2025-03.xlsx",
-            input_sheet="Pipelines",
-            trade_technology=tradetec,
-            flow_technology="oil_pipe",
-            flow_commodity="oil_pipeline_capacity",
+            input_file="GEM-GGIT-Gas-Pipelines-2024-12.xlsx",
+            input_sheet="Gas Pipelines 2024-12-17",
+            trade_technology="gas_piped",
+            flow_technology="gas_pipe",
+            flow_commodity="gas_pipeline_capacity",
         )
 
-    # Add MariTEAM calibration for maritime shipping
-    calibrate_mariteam(covered_tec, message_regions)
+        for tradetec in ["crudeoil_piped", "foil_piped", "loil_piped"]:
+            import_gem(
+                input_file="GEM-GOIT-Oil-NGL-Pipelines-2025-03.xlsx",
+                input_sheet="Pipelines",
+                trade_technology=tradetec,
+                flow_technology="oil_pipe",
+                flow_commodity="oil_pipeline_capacity",
+            )
 
-    # Add variable costs for shipped commodities
-    costdf = build_historical_price(message_regions)
-    costdf["technology"] = costdf["technology"].str.replace("ethanol_", "eth_")
-    costdf["technology"] = costdf["technology"].str.replace("fueloil_", "foil_")
+        # Add MariTEAM calibration for maritime shipping
+        calibrate_mariteam(covered_tec, message_regions)
 
-    for tec in [i for i in covered_tec if i != "gas_piped"]:
-        log.info("Add variable cost for " + tec)
+        # Add variable costs for shipped commodities
+        costdf = build_historical_price(message_regions)
+        costdf["technology"] = costdf["technology"].str.replace("ethanol_", "eth_")
+        costdf["technology"] = costdf["technology"].str.replace("fueloil_", "foil_")
 
-        if "piped" in tec:
-            tec_shipped = tec.replace("piped", "shipped")
-            add_df = costdf[costdf["technology"].str.contains(tec_shipped)].copy()
-            add_df["technology"] = add_df["technology"].str.replace("shipped", "piped")
-        else:
-            add_df = costdf[costdf["technology"].str.contains(tec)]
+        for tec in [i for i in covered_tec if i != "gas_piped"]:
+            log.info("Add variable cost for " + tec)
 
-        col_list = add_df.columns
-        mean_cost = add_df["value"].mean()
+            if "piped" in tec:
+                tec_shipped = tec.replace("piped", "shipped")
+                add_df = costdf[costdf["technology"].str.contains(tec_shipped)].copy()
+                add_df["technology"] = add_df["technology"].str.replace("shipped", "piped")
+            else:
+                add_df = costdf[costdf["technology"].str.contains(tec)]
 
-        input_df = pd.read_csv(os.path.join(data_path, tec, "edit_files", "input.csv"))
-        input_df = input_df[
-            ["node_loc", "technology", "year_act", "year_vtg", "mode", "time"]
-        ].drop_duplicates()
-        add_df = input_df.merge(
-            add_df,
-            left_on=["node_loc", "technology", "year_act", "year_vtg", "mode", "time"],
-            right_on=["node_loc", "technology", "year_act", "year_vtg", "mode", "time"],
-            how="left",
-        )
+            col_list = add_df.columns
+            mean_cost = add_df["value"].mean()
 
-        add_df["value"] = np.where(add_df["value"].isnull(), mean_cost, add_df["value"])
-        add_df["value"] = round(add_df["value"] / 5, 0)
+            input_df = pd.read_csv(os.path.join(data_path, tec, "edit_files", "input.csv"))
+            input_df = input_df[
+                ["node_loc", "technology", "year_act", "year_vtg", "mode", "time"]
+            ].drop_duplicates()
+            add_df = input_df.merge(
+                add_df,
+                left_on=["node_loc", "technology", "year_act", "year_vtg", "mode", "time"],
+                right_on=["node_loc", "technology", "year_act", "year_vtg", "mode", "time"],
+                how="left",
+            )
 
-        add_df["unit"] = "USD/GWa"
+            add_df["value"] = np.where(add_df["value"].isnull(), mean_cost, add_df["value"])
+            add_df["value"] = round(add_df["value"] / 5, 0)
 
-        add_df = add_df[col_list]
+            add_df["unit"] = "USD/GWa"
 
-        add_df = add_df[~add_df["technology"].str.contains("_imp")]
+            add_df = add_df[col_list]
 
-        add_df.to_csv(
-            os.path.join(data_path, tec, "edit_files", "var_cost.csv"), index=False
-        )
-        add_df.to_csv(
-            os.path.join(data_path, tec, "bare_files", "var_cost.csv"), index=False
-        )
+            add_df = add_df[~add_df["technology"].str.contains("_imp")]
+
+            add_df.to_csv(
+                os.path.join(data_path, tec, "edit_files", "var_cost.csv"), index=False
+            )
+            add_df.to_csv(
+                os.path.join(data_path, tec, "bare_files", "var_cost.csv"), index=False
+            )
