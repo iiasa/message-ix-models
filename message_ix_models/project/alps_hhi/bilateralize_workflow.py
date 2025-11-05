0e2734942a00e8cae29485b7bf681d42352f2f5d
Jun Shepard <51372492+junukitashepard@users.noreply.github.com>
2025-11-05 10:30:20 +0100

    Add other SSPs to config for bilat

--- message_ix_models/project/alps_hhi/bilateralize_workflow.py
+++ message_ix_models/project/alps_hhi/bilateralize_workflow.py
@@ -8,7 +8,7 @@ from message_ix_models.tools.bilateralize.bare_to_scenario import *
 from message_ix_models.tools.bilateralize.load_and_solve import *
 
 # Import scenario and models
-config, config_path = load_config(project_name = 'alps-hhi', config_name = 'config.yaml')
+config, config_path = load_config(project_name = 'alps_hhi', config_name = 'config.yaml')
 models_scenarios = config['models_scenarios']
 
 # Generate edit files
@@ -17,7 +17,7 @@ models_scenarios = config['models_scenarios']
 #                   P_access = True)
                    
 # Move data from bare files to a dictionary to update a MESSAGEix scenario
-trade_dict = bare_to_scenario(project_name = 'alps-hhi', 
+trade_dict = bare_to_scenario(project_name = 'alps_hhi', 
                               config_name = 'config.yaml')
 
 # Update base scenarios
@@ -30,9 +30,9 @@ for model_scen in models_scenarios.keys():
 
     load_and_solve(trade_dict = trade_dict,
                    solve = True,
-                   project_name = 'alps-hhi', 
+                   project_name = 'alps_hhi', 
                    config_name = 'config.yaml', 
                    start_model = base_model,
                    start_scen = base_scen,
                    target_model = 'alps_hhi',
-                   target_scen = base_scen)
+                   target_scen = model_scen)
