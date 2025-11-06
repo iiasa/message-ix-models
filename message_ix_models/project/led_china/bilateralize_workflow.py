c1323d487b916cabda7035521c0d53212f3ccd8e
Jun Shepard <51372492+junukitashepard@users.noreply.github.com>
2025-11-06 11:57:22 +0100

    Update project name in bilateralize_workflow

--- message_ix_models/project/led_china/bilateralize_workflow.py
+++ message_ix_models/project/led_china/bilateralize_workflow.py
@@ -8,16 +8,16 @@ from message_ix_models.tools.bilateralize.bare_to_scenario import *
 from message_ix_models.tools.bilateralize.load_and_solve import *
 
 # Import scenario and models
-config, config_path = load_config(project_name = 'led-china', config_name = 'config.yaml')
+config, config_path = load_config(project_name = 'led_china', config_name = 'config.yaml')
 models_scenarios = config['models_scenarios']
 
 # Generate edit files
-prepare_edit_files(project_name = 'led-china', 
+prepare_edit_files(project_name = 'led_china', 
                    config_name = 'config.yaml',
                    P_access = True)
                    
 # Move data from bare files to a dictionary to update a MESSAGEix scenario
-trade_dict = bare_to_scenario(project_name = 'led-china', 
+trade_dict = bare_to_scenario(project_name = 'led_china', 
                               config_name = 'config.yaml')
 
 # Update base scenarios
@@ -30,7 +30,7 @@ for model_scen in models_scenarios.keys():
 
     load_and_solve(trade_dict = trade_dict,
                    solve = True,
-                   project_name = 'led-china', 
+                   project_name = 'led_china', 
                    config_name = 'config.yaml', 
                    start_model = base_model,
                    start_scen = base_scen,
