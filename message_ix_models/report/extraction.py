902aa28553bb00617e6055ed1dfea3ea77347c5d
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-11-07 11:13:37 +0100

    Simplify .report.extraction using IAMCConversion

--- message_ix_models/report/extraction.py
+++ message_ix_models/report/extraction.py
@@ -1,20 +1,30 @@
 from functools import cache
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 
-import genno
-from genno import Keys, quote
+from genno import Keys
+
+from .util import IAMCConversion
 
 if TYPE_CHECKING:
     from genno import Computer
-    from genno.types import AnyQuantity
 
     from message_ix_models import Context
 
-UNIT = "EJ/yr"
+#: Some keys. Use the partial sum over 'g'.
+K = Keys(model="EXT:n-c-y", hist="historical_extraction:n-c-y", all="EXT:n-c-y:all")
+
+#: Describe the conversion to IAMC structure.
+CONV = IAMCConversion(
+    base=K.all[2],
+    var_parts=["Resource|Extraction", "c"],
+    sums=["c"],
+    unit="EJ/yr",
+    GLB_zeros=True,
+)
 
 
 @cache
-def get_commodity_groups() -> dict[str, list[str]]:
+def get_commodity_groups() -> dict[Literal["c"], dict[str, list[str]]]:
     """Return groups of commodities for reporting of extraction.
 
     Transcribed from
@@ -33,68 +43,31 @@ def get_commodity_groups() -> dict[str, list[str]]:
     result["Gas"] = result["Gas|Conventional"] + result["Gas|Unconventional"]
     result["Oil"] = result["Oil|Conventional"] + result["Oil|Unconventional"]
 
-    return result
-
-
-def zeros_for_glb(nodes: list[str], years: list[int]) -> "AnyQuantity":
-    """Return zeroes for a ``_GLB`` node.
-
-    This ensures that the values appear after conversion to IAMC-structure.
-    """
-    from message_ix_models.model.structure import get_codes
-
-    return genno.Quantity(0.0, coords=dict(n=["R11_GLB"]), units=UNIT).expand_dims(
-        c=list(get_commodity_groups()),
-        y=[int(c.id) for c in get_codes("year/B") if int(c.id) >= 1990],
-    )
+    return dict(c=result)
 
 
 def callback(c: "Computer", context: "Context") -> None:
-    from message_ix_models.report import iamc as handle_iamc
     from message_ix_models.report import util
-
-    # Some keys
-    k = Keys(
-        model="EXT:n-c-y",  # Partial sum over 'g'
-        hist="historical_extraction:n-c-y",  # Partial sum over 'g'
-        glb="EXT:n-c:glb",
-        all="EXT:n-c-y:all",
-    )
 
     # Discard "EXT|" prefix on constructed variable names
     # TODO Improve handle_iamc() or underlying code to make this unnecessary
     util.REPLACE_VARS[r"^EXT\|"] = ""
 
+    # TODO Generalize the following 3 blocks
+
     # Apply units
-    c.add(k.hist[0], "apply_units", k.hist, units="GWa/year")
-    c.add(k.model[0], "apply_units", k.model, units="GWa/year")
+    c.add(K.hist[0], "apply_units", K.hist, units="GWa/year")
+    c.add(K.model[0], "apply_units", K.model, units="GWa/year")
 
     # Add historical and current values together
-    c.add(k.all[0], "add", k.hist[0], k.model[0])
+    c.add(K.all[0], "add", K.hist[0], K.model[0])
 
     # Convert to target units
-    c.add(k.all[1], "convert_units", k.all[0], units=UNIT)
+    c.add(K.all[1], "convert_units", K.all[0], units=CONV.unit)
 
     # Aggregate on 'c' dimension
-    c.add(f"c::{__name__} agg", quote(dict(c=get_commodity_groups())))
-    c.add(k.all[2], "aggregate", k.all[1], f"c::{__name__} agg", keep=False)
-
-    # Combine with empty data for R11_GLB
-    # FIXME the "n" and "y" keys are empty lists in test_compare()
-    c.add(k.glb, zeros_for_glb, "n", "y")
-    c.add(k.all[3], "add", k.all[2], k.glb)
+    c.add(f"c::{__name__} agg", get_commodity_groups())
+    c.add(K.all[2], "aggregate", K.all[1], f"c::{__name__} agg", keep=False)
 
     # Transform to IAMC-structured data
-    handle_iamc(
-        c,
-        dict(
-            variable=__name__,
-            base=k.all[3],
-            var=["Resource|Extraction", "c"],
-            sums=["c"],
-            unit=UNIT,
-        ),
-    )
-
-    # Add to all::iamc
-    c.graph["all::iamc"] += (f"{__name__}::iamc",)
+    CONV.add_tasks(c)
