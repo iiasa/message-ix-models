16bea81546688d8a8491cdff5ae37fe011d85ad6
Jun Shepard <51372492+junukitashepard@users.noreply.github.com>
2025-11-07 13:31:48 +0100

    Set up reporting

---
+++ message_ix_models/project/led_china/reporting/pe_plot.py
@@ -0,0 +1,23 @@
+"""
+Plot primary energy in China by scenario
+"""
+# -*- coding: utf-8 -*-
+"""
+HHI reporting
+"""
+from typing import List
+
+import message_ix
+import numpy as np
+import pandas as pd
+import pyam
+import ixmp
+
+from message_ix.report import Reporter
+from message_ix_models.util import broadcast, package_data_path
+
+from message_ix_models.project.led_china.reporting.config import Config
+
+df = outdf.copy() # Change to input for function
+
+
