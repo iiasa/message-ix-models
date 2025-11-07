c0333c1cd0916ec3af14bd8155c2044aa74c337e
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-11-07 11:21:41 +0100

    Add .report.key.{all_iamc,groups.c}

--- message_ix_models/report/key.py
+++ message_ix_models/report/key.py
@@ -1,8 +1,16 @@
 """Keys for setting up reporting tasks."""
 
-from genno import Key
+from genno import Key, Keys
 
 GDP = Key("GDP", "ny")
 
 # NB genno ≤ 1.27.1 is sensitive to the order
 PRICE_COMMODITY = Key("PRICE_COMMODITY", "nclyh")
+
+#: All IAMC-structured data.
+all_iamc = Key("all", (), "iamc")
+
+#: Identifiers for grouping/aggregation mappings.
+groups = Keys(
+    c="c::groups",
+)
