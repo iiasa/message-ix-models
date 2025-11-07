8bcb3f13eccaf5a674321b38d6b76f04775f623c
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-11-07 12:29:15 +0100

    Uncomment 17 entries in commodity.yaml
    - {crude,gas}_[12345678] and uranium.
    - Add report-only aggregates with these codes as children.
    - Used in .report.extraction.
    - Adjust test expectations via .test_bare.SET_SIZE.

--- message_ix_models/tests/model/test_bare.py
+++ message_ix_models/tests/model/test_bare.py
@@ -6,7 +6,7 @@ from message_ix_models.model.bare import Config
 
 #: Number of items in the respective YAML files.
 SET_SIZE = dict(
-    commodity=18,
+    commodity=35,
     level=7,
     node=14 + 1,  # R14 is default, and 'World' exists automatically
     relation=20,
