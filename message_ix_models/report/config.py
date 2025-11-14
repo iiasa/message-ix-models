a90280a45ea5e15a9bb6106b5836ff02fe80e64c
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-11-14 22:43:59 +0100

    Add module .report.price
    - Replace reporting of prices to IAMC structure in report/global.yaml.
    - Add to default .report.Config.callback.

--- message_ix_models/report/config.py
+++ message_ix_models/report/config.py
@@ -24,13 +24,14 @@ Callback = Callable[[ComputerT, "Context"], None]
 
 
 def _default_callbacks() -> list[Callback]:
-    from . import defaults, extraction, gdp, plot
+    from . import defaults, extraction, gdp, plot, price
 
     # NB When updating this list, also update the docstring of Config.callback
     return [
         defaults,
         extraction.callback,
         gdp.callback,
+        price.callback,
         plot.callback,
     ]
 
@@ -57,7 +58,9 @@ class Config(ConfigHelper):
     #:
     #: 1. :func:`.report.defaults`
     #: 2. :func:`.report.extraction.callback`
-    #: 3. :func:`.report.plot.callback`
+    #: 3. :func:`.report.gdp.callback`
+    #: 4. :func:`.report.plot.callback`
+    #: 5. :func:`.report.price.callback`
     #:
     #: A callback function **must** take two arguments: the Computer/Reporter, and a
     #: :class:`.Context`:
