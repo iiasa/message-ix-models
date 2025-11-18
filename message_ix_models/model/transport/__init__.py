ab5db9af2459940a430e34f0e142ac57ee0c65d9
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-11-18 22:31:49 +0100

    Add .transport.SLURM_TEMPLATE
    …for use with `mix-models sbatch -m model.transport`.

--- message_ix_models/model/transport/__init__.py
+++ message_ix_models/model/transport/__init__.py
@@ -1,8 +1,10 @@
 """MESSAGEix-Transport."""
 
 from .config import Config, DataSourceConfig
+from .slurm import TEMPLATE as SLURM_TEMPLATE
 
 __all__ = [
+    "SLURM_TEMPLATE",
     "Config",
     "DataSourceConfig",
 ]
