9e754df73ee4603cb9dd61e9986ac4febafd3767
junukitashepard <51372492+junukitashepard@users.noreply.github.com>
2025-11-18 11:51:21 +0100

    Update testing and formatting to meet ruff/mypy

--- message_ix_models/tests/tools/bilateralize/test_pull_gem.py
+++ message_ix_models/tests/tools/bilateralize/test_pull_gem.py
@@ -10,20 +10,19 @@ def test_gem_region() -> None:
 @pytest.mark.xfail(
     raises=FileNotFoundError,
     reason="Input files not available for testing: "
-    "Global Energy Monitor/GEM-GOIT-*.xlsx",
+    "Global Energy Monitor/GEM-GOIT-*.csv",
 )
 @pytest.mark.parametrize(
-    "input_file, input_sheet",
+    "input_file",
     # Values appearing where the function is called in prepare_edit_files()
     [
-        ("GEM-GGIT-Gas-Pipelines-2024-12.xlsx", "Gas Pipelines 2024-12-17"),
-        ("GEM-GOIT-Oil-NGL-Pipelines-2025-03.xlsx", "Pipelines"),
+        ("GEM-GGIT-Gas-Pipelines-2024-12.csv"),
+        ("GEM-GOIT-Oil-NGL-Pipelines-2025-03.csv"),
     ],
 )
-def test_import_gem(input_file: str, input_sheet: str) -> None:
+def test_import_gem(input_file: str) -> None:
     import_gem(
-        input_file,
-        input_sheet,
+        input_file=input_file,
         trade_technology="foo",
         flow_technology="bar",
         flow_commodity="baz",
