0d6889ef0322cc5d43559f59cfc678ec27b0c9a2
junukitashepard <51372492+junukitashepard@users.noreply.github.com>
2025-11-20 08:26:59 +0100

    Update bilateralize_workflow.py

--- message_ix_models/project/alps_hhi/bilateralize_workflow.py
+++ message_ix_models/project/alps_hhi/bilateralize_workflow.py
@@ -1,21 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Bilateralize base scenarios for HHI analysis
+Bilateralize base scenarios for ALPS HHI Analysis
 """
 # Import packages
 from message_ix_models.tools.bilateralize.prepare_edit import *
 from message_ix_models.tools.bilateralize.bare_to_scenario import *
 from message_ix_models.tools.bilateralize.load_and_solve import *
 
+import os
+
 # Import scenario and models
 config, config_path = load_config(project_name = 'alps_hhi', config_name = 'config.yaml')
 models_scenarios = config['models_scenarios']
+data_path = package_data_path("bilateralize")
 
+# Clear bare files
+print("Clearing bare files")
+for tec in config['covered_trade_technologies']:
+    if os.path.exists(os.path.join(data_path, tec, "bare_files")):
+        for file in os.listdir(os.path.join(data_path, tec, "bare_files")):
+            if os.path.isfile(os.path.join(data_path, tec, "bare_files", file)):
+                os.remove(os.path.join(data_path, tec, "bare_files", file))
+    if os.path.exists(os.path.join(data_path, tec, "bare_files", "flow_technology")):
+        for file in os.listdir(os.path.join(data_path, tec, "bare_files", "flow_technology")):
+            if os.path.isfile(os.path.join(data_path, tec, "bare_files", "flow_technology", file)):
+                os.remove(os.path.join(data_path, tec, "bare_files", "flow_technology", file))
+    
 # Generate edit files
-#prepare_edit_files(project_name = 'alps-hhi', 
-#                   config_name = 'config.yaml',
-#                   P_access = True)
-                   
+prepare_edit_files(project_name = 'alps_hhi', 
+                   config_name = 'config.yaml',
+                   P_access = True)
+
 # Move data from bare files to a dictionary to update a MESSAGEix scenario
 trade_dict = bare_to_scenario(project_name = 'alps_hhi', 
                               config_name = 'config.yaml')
@@ -25,8 +40,8 @@ for model_scen in models_scenarios.keys():
     base_model = models_scenarios[model_scen]['model']
     base_scen = models_scenarios[model_scen]['scenario']
 
-    print(base_model)
-    print(base_scen)
+    print(f"Base model: {base_model}/{base_scen}")
+    print(f"Target model: alps_hhi/{model_scen}")
 
     load_and_solve(trade_dict = trade_dict,
                    solve = True,
