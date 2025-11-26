26d96c582212ec57b5f7f002a00fceb7e6a9cfef
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2025-11-26 15:44:22 +0100

    Add .model.workflow.STAGE enumeration

--- message_ix_models/model/workflow.py
+++ message_ix_models/model/workflow.py
@@ -3,6 +3,7 @@
 import logging
 from collections.abc import Collection
 from dataclasses import dataclass, field
+from enum import Enum, auto
 from typing import TYPE_CHECKING, Any
 
 from message_ix import Scenario
@@ -21,6 +22,14 @@ if TYPE_CHECKING:
 
 
 log = logging.getLogger(__name__)
+
+
+class STAGE(Enum):
+    """Enumeration of common workflow stages."""
+
+    BUILD = auto()
+    SOLVE = auto()
+    REPORT = auto()
 
 
 @dataclass
