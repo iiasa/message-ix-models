0d2d6a748aa8f261880081e16bea623a259adb71
Florian Maczek <maczek@iiasa.ac.at>
2025-12-16 17:32:36 +0100

    Add scenario list for premise workflow

--- message_ix_models/model/material/report/run_premise.py
+++ message_ix_models/model/material/report/run_premise.py
@@ -9,13 +9,32 @@ if __name__ == "__main__":
     import ixmp
     import message_ix
 
-    mp = ixmp.Platform("local3")
-    scen = message_ix.Scenario(mp, "SSP_SSP2_v6.2", "baseline_wo_GLOBIOM_ts")
-    rep = Reporter.from_scenario(scen)
-    prepare_reporter(ctx, reporter=rep)
-    df = premise.run(rep, scen, scen.model, scen.scenario)
-    df = df.filter(unit=["dimensionless", "", None], keep=False).rename(
-        mapping={"unit": {"Mt / a": "Mt/yr", "EJ / a": "EJ/yr"}}
-    )
-    df.to_excel("premise_out.xlsx")
+    med_and_medlow = [
+        ("SSP_SSP2_v6.5", "baseline_DEFAULT_step_14"),  # fmy = 2020
+        ("SSP_SSP2_v6.5", "NPi2030"),  # fmy = 2030
+        ("SSP_SSP2_v6.5", "NPiREF"),  # fmy = 2035; Medium Emissions is clone
+        ("SSP_SSP2_v6.5", "NPiREF_SSP2 - Medium-Low Emissionsf"),  # fmy = 2045
+    ]
+    low_emi = [
+        ("SSP_SSP2_v6.5", "INDC2030i_weak"),  # fmy = 2030
+        ("SSP_SSP2_v6.5", "INDC2030i_weak_SSP2 - Low Emissions"),  # fmy = 2035
+    ]
+    high = [
+        ("SSP_SSP3_v6.5", "baseline_DEFAULT_step_14"),  # fmy = 2020
+        ("SSP_SSP3_v6.5", "baseline"),  # fmy = 2030; High Emissions is clone
+        ("SSP_SSP5_v6.5", "baseline"),  # fmy = 2030; High Emissions is clone
+    ]
+    scens = med_and_medlow + low_emi + high
+    mp = ixmp.Platform("ixmp_dev")
+    for model_name, scen_name in scens:
+        print(f"Running premise for {model_name} - {scen_name}")
+        scen = message_ix.Scenario(mp, model_name, scen_name)
+        rep = Reporter.from_scenario(scen)
+        prepare_reporter(ctx, reporter=rep)
+        df = premise.run(rep, scen, scen.model, scen.scenario)
+        df = df.filter(unit=["dimensionless", "", None], keep=False).rename(
+            mapping={"unit": {"Mt / a": "Mt/yr", "EJ / a": "EJ/yr"}}
+        )
+        df.to_excel(f"{model_name}_{scen_name}.xlsx")
+        del scen
     print()
