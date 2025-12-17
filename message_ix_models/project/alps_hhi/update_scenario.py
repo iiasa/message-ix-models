d726ca8a8a113dfb3a015497617627f7aababa84
Jun Shepard <shepard@iiasa.ac.at>
2025-12-17 23:13:13 +0000

    Add chn/sas sensitivity on costs

--- message_ix_models/project/alps_hhi/update_scenario.py
+++ message_ix_models/project/alps_hhi/update_scenario.py
@@ -18,14 +18,17 @@ mp = ixmp.Platform()
 base_model_name = 'alps_hhi'
 base_scen_name = 'SSP2'
 target_model_name = 'alps_hhi'
-target_scen_name = 'SSP2u'
+target_scen_name = 'SSP2_pipecosts'
 
 base_scenario = message_ix.Scenario(mp, model=base_model_name, scenario=base_scen_name)
 hhi_scenario = base_scenario.clone(target_model_name, target_scen_name, 
                                    keep_solution = False)
 hhi_scenario.set_as_default()
 
-pardf = hhi_scenario.par('growth_activity_up', filters = {'technology': '
-hhi_scenario.solve()
+remdf = hhi_scenario.par('inv_cost', filters = {'technology': ['gas_pipe_chn', 'gas_pipe_sas']})
 
+with hhi_scenario.transact("Remove pipeline cost from CHN/SAS"):
+    hhi_scenario.remove_par('inv_cost', remdf)
+    
+hhi_scenario.solve()
 mp.close_db()
