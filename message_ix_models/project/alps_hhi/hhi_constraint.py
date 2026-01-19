8dbaf2189efdf25147cb5749d8cee6c18fecadb7
Jun Shepard <shepard@iiasa.ac.at>
2026-01-19 15:21:45 +0000

    Use scalind = 1 for HC

--- message_ix_models/project/alps_hhi/hhi_constraint.py
+++ message_ix_models/project/alps_hhi/hhi_constraint.py
@@ -124,6 +124,6 @@ def hhi_constraint_run(project_name: str,
 
     log.info(f"Solving HHI scenario {k}")
  
-    hhi_scenario.solve(solve_options = {"scaind":1}, gams_args = ['--HHI_CONSTRAINT=1'], quiet = False)
+    hhi_scenario.solve(solve_options = {"scaind":1, "predual":1}, gams_args = ['--HHI_CONSTRAINT=1'], quiet = False)
 
     mp.close_db()
