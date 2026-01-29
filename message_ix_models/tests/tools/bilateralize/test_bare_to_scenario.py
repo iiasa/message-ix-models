49c22022a44b0b38899d97faed2d145fe997c499
Jun Shepard <51372492+junukitashepard@users.noreply.github.com>
2026-01-29 10:04:32 +0100

    Add test coverage

--- message_ix_models/tests/tools/bilateralize/test_bare_to_scenario.py
+++ message_ix_models/tests/tools/bilateralize/test_bare_to_scenario.py
@@ -1,23 +1,24 @@
 import pytest
 
-from message_ix_models.tools.bilateralize.utils import get_logger, load_config
 from message_ix_models.tools.bilateralize.bare_to_scenario import (
     build_parameter_sheets,
-    calibrate_historical_shipping
+    calibrate_historical_shipping,
 )
+from message_ix_models.tools.bilateralize.utils import get_logger, load_config
 
 MARK = pytest.mark.xfail(
     raises=FileNotFoundError,
     reason="Input data files not available for testing.",
 )
 
-def test_calibrate_historical_shipping(project_name:str = None,
-                                       config_name:str = None) -> None:
 
+def test_calibrate_historical_shipping(
+    project_name: str = None, config_name: str = None
+) -> None:
     config, config_path, tec_config = load_config(
         project_name=project_name, config_name=config_name, load_tec_config=True
     )
-    
+
     covered_tec = config["covered_trade_technologies"]
 
     # Get logger
@@ -27,9 +28,11 @@ def test_calibrate_historical_shipping(project_name:str = None,
     trade_dict = build_parameter_sheets(
         log=log, project_name=project_name, config_name=config_name
     )
-    calibrate_historical_shipping(config=config,
-                                  trade_dict=trade_dict,
-                                  covered_tec=covered_tec,
-                                  project_name=project_name,
-                                  config_name=config_name)
+    calibrate_historical_shipping(
+        config=config,
+        trade_dict=trade_dict,
+        covered_tec=covered_tec,
+        project_name=project_name,
+        config_name=config_name,
+    )
     assert True
