9a5a47589443f863f7181fe271a299f8f432e0cd
Jun Shepard <shepard@iiasa.ac.at>
2026-02-04 13:55:03 +0000

    Add constraints to bilateralize workflow

--- message_ix_models/project/gas_security/bilateralize_workflow.py
+++ message_ix_models/project/gas_security/bilateralize_workflow.py
@@ -33,6 +33,41 @@ prepare_edit_files(project_name = 'gas_security',
                    config_name = 'config.yaml',
                    P_access = True)
 
+# Add scenario updates for project
+print("Add scenario updates for project")
+for tec in config['covered_trade_technologies']:
+    print(f"...{tec}")
+    if os.path.exists(package_data_path("gas_security", "scenario_updates", tec)):
+        for file in os.listdir(package_data_path("gas_security", "scenario_updates", tec)):
+            base_file = package_data_path("gas_security", "scenario_updates", tec, file)
+            if ".csv" in str(base_file):
+                dest_file = os.path.join(data_path, tec, "bare_files", file)
+                shutil.copy2(base_file, dest_file)
+                print(f"Copied file from scenario_updates to bare: {file}")
+                
+# Add constraints to the dictionary
+print("Add constraints to the dictionary")
+constraint_pars = ["initial_activity_lo", "initial_activity_up",
+                   "growth_activity_lo", "growth_activity_up",
+                   "soft_activity_up", "soft_activity_lo"]
+constraint_tec = config['constrained_tec']
+
+for con in constraint_pars:
+    print(f"...{con}")
+    for tec in constraint_tec:
+        print(f"......{tec}")
+        df = pd.read_csv(os.path.join(package_data_path("gas_security", "scenario_updates", tec), con + ".csv"))
+
+        #df['omit'] = 1
+        #df['omit'] = np.where((df['technology'].isin(['LNG_shipped_exp_weu', 'gas_piped_exp_weu', 
+        #                                             'LNG_shipped_exp_eeu', 'gas_piped_exp_eeu'])) |\
+        #                      (df['node_loc'].isin(['R12_WEU', 'R12_EEU'])),
+        #                     0, df['omit'])
+        #df = df[df['omit'] == 0]
+        #df = df.drop(columns = ['omit'])
+        
+        df.to_csv(os.path.join(data_path, tec, "bare_files", con + ".csv"), index = False)
+        
 # Move data from bare files to a dictionary to update a MESSAGEix scenario
 trade_dict = bare_to_scenario(project_name = 'gas_security', 
                               config_name = 'config.yaml',
@@ -52,7 +87,7 @@ for model_scen in models_scenarios.keys():
 
     print("Setting up scenario")
     load_and_solve(trade_dict = trade_dict,
-                   solve = False,
+                   solve = True,
                    project_name = 'gas_security', 
                    config_name = 'config.yaml', 
                    start_model = base_model,
@@ -60,29 +95,3 @@ for model_scen in models_scenarios.keys():
                    target_model = 'gas_security',
                    target_scen = model_scen,
                    extra_parameter_updates = liquefaction_parameters)
-
-    print("Updating extraction constraints")
-    mp = ixmp.Platform()
-    
-    base_scenario = message_ix.Scenario(mp, model='gas_security', scenario=model_scen)
-    out_scenario = base_scenario.clone('gas_security', model_scen)
-    out_scenario.set_as_default()
-
-    for g in ['growth_activity_up']:
-        updf = out_scenario.par(g)
-        updf = updf[(updf['technology'].str.contains('gas_extr_mpen'))]
-        updf = updf[updf['node_loc'].isin(['R12_WEU'])]
-    
-        remdf = updf.copy()
-        if g == 'growth_activity_up':
-            updf['value'] = 0.01
-        elif g == 'growth_activity_lo':
-            updf['value'] = -0.01
-            
-        with out_scenario.transact("update growth activity to gas_extr_mpen"):
-            out_scenario.remove_par(g, remdf)
-            out_scenario.add_par(g, updf)
-
-    print("Solve scenario")
-    out_scenario.solve()
-    mp.close_db()
