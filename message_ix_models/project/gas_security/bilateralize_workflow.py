60fcd7ee86640305b1088cd9ce1147b6a15ee670
junukitashepard <51372492+junukitashepard@users.noreply.github.com>
2026-02-04 11:40:34 +0100

    Initialize with bilateralization and liquefaction penalty

---
+++ message_ix_models/project/gas_security/bilateralize_workflow.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Bilateralize base scenarios for gas security analysis
+"""
+# Import packages
+from message_ix_models.tools.bilateralize.prepare_edit import *
+from message_ix_models.tools.bilateralize.bare_to_scenario import *
+from message_ix_models.tools.bilateralize.load_and_solve import *
+from message_ix_models.project.gas_security.liquefaction_calibration import *
+
+import os
+from ixmp import Platform
+
+# Import scenario and models
+config, config_path = load_config(project_name = 'gas_security', config_name = 'config.yaml')
+models_scenarios = config['models_scenarios']
+data_path = package_data_path("bilateralize")
+
+# Clear bare files
+print("Clearing bare files")
+for tec in config['covered_trade_technologies']:
+    if os.path.exists(os.path.join(data_path, tec, "bare_files")):
+       for file in os.listdir(os.path.join(data_path, tec, "bare_files")):
+            if os.path.isfile(os.path.join(data_path, tec, "bare_files", file)):
+               os.remove(os.path.join(data_path, tec, "bare_files", file))
+    if os.path.exists(os.path.join(data_path, tec, "bare_files", "flow_technology")):
+        for file in os.listdir(os.path.join(data_path, tec, "bare_files", "flow_technology")):
+            if os.path.isfile(os.path.join(data_path, tec, "bare_files", "flow_technology", file)):
+                os.remove(os.path.join(data_path, tec, "bare_files", "flow_technology", file))
+        
+# Generate edit files
+prepare_edit_files(project_name = 'gas_security', 
+                   config_name = 'config.yaml',
+                   P_access = True)
+
+# Move data from bare files to a dictionary to update a MESSAGEix scenario
+trade_dict = bare_to_scenario(project_name = 'gas_security', 
+                              config_name = 'config.yaml',
+                              p_drive_access = True)
+
+# Additional liquefaction calibration
+liquefaction_parameters = update_liquefaction_input(message_regions = "R12",
+                                                     project_name = 'gas_security',
+                                                     config_name = 'config.yaml')
+# Update base scenarios
+for model_scen in models_scenarios.keys():
+    base_model = models_scenarios[model_scen]['model']
+    base_scen = models_scenarios[model_scen]['scenario']
+
+    print(f"Base model: {base_model}/{base_scen}")
+    print(f"Target model: gas_security/{model_scen}")
+
+    print("Setting up scenario")
+    load_and_solve(trade_dict = trade_dict,
+                   solve = True,
+                   project_name = 'gas_security', 
+                   config_name = 'config.yaml', 
+                   start_model = base_model,
+                   start_scen = base_scen,
+                   target_model = 'gas_security',
+                   target_scen = model_scen,
+                   extra_parameter_updates = liquefaction_parameters)
---
+++ message_ix_models/project/gas_security/liquefaction_calibration.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Liquefaction calibration
+"""
+# Import packages
+import os
+import sys
+import pandas as pd
+import logging
+import yaml
+import message_ix
+import ixmp
+import itertools
+import numpy as np
+import pickle
+
+from pathlib import Path
+from message_ix_models.util import package_data_path
+from message_ix_models.tools.iea import web
+from message_ix_models.tools.bilateralize import bilateralize
+
+def update_liquefaction_input(message_regions: str = "R12", 
+                              project_name: str = None,
+                              config_name: str = None):
+    
+    # Pull in configuration
+    config, config_path = bilateralize.load_config(project_name = project_name, 
+                                                   config_name = config_name)
+    
+    # Pull in regionalization
+    dict_dir = package_data_path("node", message_regions + ".yaml")
+    with open(dict_dir, "r") as f:
+        dict_message_regions = yaml.safe_load(f) 
+    region_list = [i for i in list(dict_message_regions.keys()) if i != "World"]
+    
+    # Pull in dataset
+    datadir = os.path.dirname(package_data_path("bilateralize", "LNG Liquefaction.xlsx"))
+    liqdf = pd.read_excel(os.path.join(datadir, "LNG Liquefaction.xlsx"),
+                          sheet_name = 'Fuel Losses')
+    
+    # Collapse to regionalization
+    liqdf['node_loc'] = ''
+    for r in region_list:
+        liqdf['node_loc'] = np.where(liqdf['ISO'].isin(dict_message_regions[r]['child']),
+                                     r, liqdf['node_loc'])
+    liqdf = liqdf.groupby(['node_loc'])['Use Value'].mean().reset_index()
+    liqvalue = round(liqdf['Use Value'].mean(), 2)
+    
+    # Set up dataframe for all regions
+    outdf = pd.DataFrame.from_dict(dict(node_loc = region_list))
+    outdf = outdf.merge(liqdf, left_on = ['node_loc'], right_on = ['node_loc'], how = 'left')
+    outdf['Use Value'] = np.where(outdf['Use Value'].isnull(), liqvalue, outdf['Use Value'])
+    outdf = outdf.rename(columns = {'Use Value': 'value'})
+    outdf['value'] = round(outdf['value'], 2)
+    outdf['technology'] = 'LNG_prod'
+    outdf['commodity'] = 'gas'
+    outdf['level'] = 'primary'
+    
+    return dict(input = outdf)
