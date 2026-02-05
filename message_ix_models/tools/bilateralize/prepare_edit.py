a76c1282193608f540a3ccdae9f4cd06ef7eabea
Jun Shepard <shepard@iiasa.ac.at>
2026-02-05 13:50:02 +0000

    Fix pipeline gas usage

--- message_ix_models/tools/bilateralize/prepare_edit.py
+++ message_ix_models/tools/bilateralize/prepare_edit.py
@@ -634,6 +634,9 @@ def build_flow_input(
             )
         if "shipped" in tec:
             df_input_base["value"] = 1e-6  # Default is 1e-6 GWa
+        elif "piped" in tec:
+            df_input_base['value'] = 0.0015
+            
         df_input = pd.concat([df_input, df_input_base])
 
         # For shipped trade, set up bunker fuels
