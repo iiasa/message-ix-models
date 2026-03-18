f4de5ba2dbbc524cd6b0e4b72d14a80ae4562759
junukitashepard <51372492+junukitashepard@users.noreply.github.com>
2026-03-18 12:53:49 +0100

    Ensure all modes represented in updated fuel_bal tecs

--- message_ix_models/project/weu_security/adjust_reexports.py
+++ message_ix_models/project/weu_security/adjust_reexports.py
@@ -11,25 +11,37 @@ def adjust_reexports(base_scenario,
                      base_level:str):
     
     for trade_commodity in trade_commodity_list:
+
         # Output from domestic production should have updated level
         dom_prod = base_scenario.par('output', filters = {'commodity': trade_commodity,
                                                           'level': base_level})
         dom_prod = dom_prod[dom_prod['technology'].str.contains('_imp') == False]
         dom_prod_base = dom_prod.copy()
         dom_prod['level'] = 'primary' #base_level + '_1' # Update level
+        full_mode_list = dom_prod['mode'].unique()
     
         # Create fuel balancing to move level from base_level_1 to base_level
         fb_input = base_scenario.par('input', filters = {'technology': 'coal_bal'}) # use coal as basis
-        fb_input_base = fb_input.copy()
         fb_input['commodity'] = trade_commodity
         fb_input['level'] = 'primary' #base_level + '_1'
         fb_input['technology'] = trade_commodity + '_bal'
+
+        fb_input_out = pd.DataFrame()
+        for m in full_mode_list:
+            tdf = fb_input.copy()
+            tdf['mode'] = m
+            fb_input_out = pd.concat([fb_input_out, tdf])
     
         fb_output = base_scenario.par('output', filters = {'technology': 'coal_bal'})
-        fb_output_base = fb_output.copy()
         fb_output['commodity'] = trade_commodity
         fb_output['level'] = base_level
         fb_output['technology'] = trade_commodity + '_bal'
+    
+        fb_output_out = pd.DataFrame()
+        for m in full_mode_list:
+            tdf = fb_output.copy()
+            tdf['mode'] = m
+            fb_output_out = pd.concat([fb_output_out, tdf])
     
         # Add capacity factor for fuel balancing
         fb_cap = base_scenario.par('capacity_factor', filters = {'technology': 'coal_bal'})
@@ -52,8 +64,8 @@ def adjust_reexports(base_scenario,
             base_scenario.add_par('output', dom_prod)
     
         with base_scenario.transact("Update fuel balancing"):
-            base_scenario.add_par('input', fb_input)
-            base_scenario.add_par('output', fb_output)
+            base_scenario.add_par('input', fb_input_out)
+            base_scenario.add_par('output', fb_output_out)
             base_scenario.add_par('capacity_factor', fb_cap)
             
         with base_scenario.transact("Update export input"):
