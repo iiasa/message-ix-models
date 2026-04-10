8d26c38b3b7157063c93a920df10101bc180a8e8
Jun Shepard <shepard@iiasa.ac.at>
2026-04-10 10:42:01 +0000

    Rerun reporting with INDC2030

--- message_ix_models/project/weu_security/reporting/full_reporting.py
+++ message_ix_models/project/weu_security/reporting/full_reporting.py
@@ -11,18 +11,12 @@ from message_ix_models.tools.bilateralize.reporting.full_reporting import report
 
 # Call all scenarios for analysis
 mp = ixmp.Platform()
-for scen in ["SSP2", "FSU2040", "FSU2100",
-             #"SSP2_NAM10EJ", "FSU2040_NAM10EJ", "FSU2100_NAM10EJ",
-             #"SSP2_NAM15EJ", "FSU2040_NAM15EJ", "FSU2100_NAM15EJ",
-             #"SSP2_NAM20EJ", "FSU2040_NAM20EJ", "FSU2100_NAM20EJ",
-             #"SSP2_NAM25EJ", "FSU2040_NAM25EJ", "FSU2100_NAM25EJ",
-             "SSP2_NAM30EJ", "FSU2040_NAM30EJ", "FSU2100_NAM30EJ",
-             "SSP2_MEACON_1.0", "FSU2040_MEACON_1.0", "FSU2100_MEACON_1.0",]:
-             #"SSP2_MEACON_0.9", "FSU2040_MEACON_0.9", "FSU2100_MEACON_0.9",
-             #"SSP2_MEACON_0.8", "FSU2040_MEACON_0.8", "FSU2100_MEACON_0.8",
-             #"SSP2_MEACON_0.75", "FSU2040_MEACON_0.75", "FSU2100_MEACON_0.75",
-             #"SSP2_MEACON_0.5", "FSU2040_MEACON_0.5", "FSU2100_MEACON_0.5",
-             #"SSP2_MEACON_0.25", "FSU2040_MEACON_0.25", "FSU2100_MEACON_0.25"]:
+for scen in [#"SSP2", "FSU2040", "FSU2100",
+             #"SSP2_NAM30EJ", "FSU2040_NAM30EJ", "FSU2100_NAM30EJ",
+             #"SSP2_MEACON_1.0", "FSU2040_MEACON_1.0", "FSU2100_MEACON_1.0",
+             "INDC2030", "INDC2030_FSU2040", "INDC2030_FSU2100",
+             "INDC2030_NAM30EJ", "INDC2030_FSU2100_NAM30EJ",
+             "INDC2030_MEACON_1.0", "INDC2030_FSU2100_MEACON_1.0"]:
     report_combine(scenario = message_ix.Scenario(mp, model = "weu_security", scenario = scen),
                    legacy_config = package_data_path('weu_security', 'reporting', 'legacy', 'legacy_config.yaml'),
                    legacy_out_dir = package_data_path('weu_security', 'reporting', 'output', "legacy"),
