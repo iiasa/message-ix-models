78915c1783cb1a75ff81c5f552afa385b228eb64
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2026-05-05 12:36:58 +0200

    Add CL_SCENARIO_CIRCEULAR
    - Rename transport scenario code list to
      CL_SCENARIO_CIRCEULAR_TRANSPORT; adjust references.
    - Add and expand tests.

--- message_ix_models/tests/project/circeular/test_structure.py
+++ message_ix_models/tests/project/circeular/test_structure.py
@@ -1,10 +1,27 @@
-from message_ix_models.project.circeular.structure import CL_TRANSPORT_SCENARIO
+from message_ix_models.project.circeular.structure import (
+    CL_SCENARIO,
+    CL_SCENARIO_TRANSPORT,
+)
 
 
-class TestCL_TRANSPORT_SCENARIO:
+class TestCL_SCENARIO:
     def test_create(self) -> None:
         # Function runs without error
-        result = CL_TRANSPORT_SCENARIO.create()
+        result = CL_SCENARIO.get()
+
+        # Code list has expected number of items
+        assert 6 == len(result)
+
+        # An expected item is in the code list
+        item = result["N"]
+        # A short fragment is in the item URN
+        assert "Code=IIASA_ECE:CL_SCENARIO_CIRCEULAR(1.0.0).N" in item.urn
+
+
+class TestCL_SCENARIO_TRANSPORT:
+    def test_create(self) -> None:
+        # Function runs without error
+        result = CL_SCENARIO_TRANSPORT.get()
 
         # Code list has expected number of items
         assert 8 == len(result)
