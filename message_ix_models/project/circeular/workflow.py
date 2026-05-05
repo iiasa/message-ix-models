ca006066ba36353e43729844ee4e1375068bf279
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2026-05-05 13:31:46 +0200

    Add .circuelar.workflow.configure()
    - Remove usage of apply_bmt_config().

--- message_ix_models/project/circeular/workflow.py
+++ message_ix_models/project/circeular/workflow.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 from message_ix_models.workflow import Workflow
@@ -9,27 +10,57 @@ if TYPE_CHECKING:
 log = logging.getLogger(__name__)
 
 
+def configure(context: "Context") -> None:
+    """Configure for the CircEUlar BMT workflow.
+
+    This function **should** have an effect identical to
+    :func:`.bmt.config.apply_bmt_config`.
+    """
+    from message_ix_models.model.buildings.config import METHOD
+    from message_ix_models.model.buildings.config import Config as BuildingsConfig
+    from message_ix_models.model.transport.config import Config as TransportConfig
+
+    context.model.regions = "R12"
+
+    # Expected by .bmt.workflow.add_steps()
+    context.bmt = dict(model_name="MESSAGEix-GLOBIOM 2.1-BMT-R12 CircEUlar")
+
+    context.buildings = BuildingsConfig(
+        sturm_scenario="NONE",
+        data_paths=dict(
+            # Input file names under private_data_path("buildings")
+            prices=Path("input_prices_R12.csv"),
+            sturm_r=Path("report_MESSAGE_resid_SSP2_nopol_20250318.csv"),
+            sturm_c=Path("report_MESSAGE_comm_SSP2_nopol_20250318.csv"),
+            demand_static=Path("static_20251227.csv"),
+        ),
+        method=METHOD.B,
+        with_materials=True,
+    )
+
+    # Expected by .bmt.workflow.add_macro()
+    context.macro = "macro_calibration_input_SSP2_bmt.xlsx"
+
+    TransportConfig.from_context(context, options=dict(code="SSP2"))
+
+    # log.info(repr(context.asdict()))  # DEBUG
+
+
 def generate(context: "Context") -> Workflow:
     """Create the CircEUlar workflow."""
-    from message_ix_models.model.bmt.config import apply_bmt_config
     from message_ix_models.model.bmt.workflow import add_steps
 
     from .structure import CL_SCENARIO
 
-    ### Same as .bmt.workflow.generate() until ###
-    wf = Workflow(context)
-
     # Configure
-    context.ssp = "SSP2"
-    context.model.regions = "R12"
-    apply_bmt_config(context)
-    log.info(repr(context.asdict()))
+    configure(context)
+
+    # Create the workflow
+    wf = Workflow(context)
 
     # TODO Move this to a .Config.base_url setting on an appropriate config class
     #      (probably .model.workflow.Config).
     base_url = "ixmp://ixmp-dev/SSP_SSP2_v6.5/baseline_DEFAULT_step_14"
-
-    ###
 
     # Load the base scenario
     base_step = wf.add_step("M", None, target=base_url)
