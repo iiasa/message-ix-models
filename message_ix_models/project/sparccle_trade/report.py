2dce67c4238513dabe64190d9a3cb02b7b57b758
Jun Shepard <shepard@iiasa.ac.at>
2026-05-12 09:22:17 +0000

    Actually run the reporting in script

--- message_ix_models/project/sparccle_trade/report.py
+++ message_ix_models/project/sparccle_trade/report.py
@@ -25,3 +25,5 @@ def combined_legacy_trade_reporting():
                     legacy_out_dir = LEGACY_OUT_DIR,
                     trade_out_dir  = TRADE_OUT_DIR,
                     report_out_dir = REPORT_OUT_DIR)
+
+combined_legacy_trade_reporting()
