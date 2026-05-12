b30dab83ea9914905ac995bdf3d0da7148cc969b
junukitashepard <51372492+junukitashepard@users.noreply.github.com>
2026-05-12 11:07:32 +0200

    Create report.py

---
+++ message_ix_models/project/sparccle_trade/report.py
@@ -0,0 +1,27 @@
+"""Reporting workflow for SPARCCLE STS"""
+
+from pathlib import Path
+
+import ixmp
+import message_ix
+import message_data
+import pandas as pd
+
+from message_data.tools.post_processing.iamc_report_hackathon import report as legacy_report
+from message_ix_models.tools.bilateralize.reporting.full_reporting import report as report_combine
+from message_ix_models.util import package_data_path
+
+def combined_legacy_trade_reporting():
+    LEGACY_CONFIG  = package_data_path("bilateralize", "reporting", "legacy", "legacy_config.yaml")
+    LEGACY_OUT_DIR = package_data_path("sparccle_trade", "reporting", "legacy")
+    TRADE_OUT_DIR  = package_data_path("sparccle_trade", "reporting", "trade")
+    REPORT_OUT_DIR = package_data_path("sparccle_trade", "reporting")
+
+    # Call all scenarios for analysis
+    mp = ixmp.Platform()
+    for scen in ['SSP3_NPiREF', 'SSP3_STS3', 'SSP3_NPiREF_ins10', 'SSP3_STS3_ins10']:
+        report_combine(scenario    = message_ix.Scenario(mp, model="sparccle_trade", scenario=scen),
+                    legacy_config  = LEGACY_CONFIG,
+                    legacy_out_dir = LEGACY_OUT_DIR,
+                    trade_out_dir  = TRADE_OUT_DIR,
+                    report_out_dir = REPORT_OUT_DIR)
