490dc0c2882ecd380a0135164e526218931e9271
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2026-06-19 11:53:51 +0200

    Use transport input_cap_new per CircEUlar scenario
    - Add .transport.material.LABEL_SCENARIO.
    - Add tests.

--- message_ix_models/tests/model/transport/test_material.py
+++ message_ix_models/tests/model/transport/test_material.py
@@ -1,6 +1,24 @@
+import pytest
+
 from message_ix_models import Context
 from message_ix_models.model.transport import Config
-from message_ix_models.model.transport.material import get_groups
+from message_ix_models.model.transport.material import LABEL_SCENARIO, get_groups
+
+
+@pytest.mark.parametrize(
+    "value, exp",
+    (
+        ("SSP_2024.2", "*"),
+        ("CircEUlar-R", "*"),
+        ("CircEUlar-C", "CircEUlar-C"),
+        ("CircEUlar-N", "CircEUlar-N"),
+        ("CircEUlar-S", "*"),
+        ("CircEUlar-A", "CircEUlar-N"),
+        ("CircEUlar-E", "CircEUlar-N"),
+    ),
+)
+def test_LABEL_SCENARIO(value: str, exp: str) -> None:
+    assert exp == LABEL_SCENARIO(value)
 
 
 def test_get_groups(test_context: Context) -> None:
