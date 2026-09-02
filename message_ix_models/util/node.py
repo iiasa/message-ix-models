5a6645205ee20c012e352cdd51c2c85c6684327e
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2026-09-03 00:43:21 +0200

    Add .util.node.create_maps()
    - Add test.

--- message_ix_models/util/node.py
+++ message_ix_models/util/node.py
@@ -2,6 +2,7 @@
 
 import logging
 from collections.abc import Sequence
+from itertools import product
 
 from message_ix import Scenario
 from sdmx.model.v21 import Code
@@ -92,6 +93,91 @@ adapt_R11_R14 = MappingAdapter({d: R11_R14 for d in NODE_DIMS})
 #: The data is adapted using the mappings in :data:`R12_R11` for each of the dimensions
 #: in :data:`NODE_DIMS`.
 adapt_R12_R11 = MappingAdapter({d: R12_R11 for d in NODE_DIMS})
+
+
+def create_maps(
+    id_a: str, id_b: str, *, debug: bool = False
+) -> tuple[dict[str, str], dict[str, str]]:
+    """Create mappings between two node code lists with IDs `id_a` and `id_b`.
+
+    Two code lists, A and B, are loaded using :func:`.get_codelist`. Mappings are
+    determined by counting the common children of every pair of codes in A and B. For
+    each code in A, the code in B with the *most* common children is mapped.
+
+    For example:
+
+    - In code list A, code 'A1' has children 'AFG', 'BRA', 'CAN', and 'DEN'.
+    - In code list B:
+
+      - Code 'B1' has children 'AFG' and 'EGY'.
+      - Code 'B2' has children 'BRA', 'CAN', and 'FRA'.
+
+    - A1 and B1 have 1 common child; A1 and B2 have 2 common children.
+    - Thus A1 maps to B1.
+
+    The inverse mapping is computed using the same counts.
+
+    Parameters
+    ----------
+    id_a :
+        The IDs of a code list in :doc:`/pkg-data/node`, for instance "R12". The code
+        list must have a code "World" whose first-level children are regions or nodes.
+    id_b :
+        The ID of a second code list, different from `id_a`. It must also have a code
+        "World", and its second-level children must be a set with some overlap of the
+        second-level children of A.
+    debug :
+        If :any:`True`, log the full matrix of counts.
+
+    Returns
+    -------
+    tuple of (dict, dict)
+        The first :class:`dict` maps the ID of every region in code list A (keys) to one
+        in B (values). The second mapping does the opposite, mapping the ID of every
+        region in code list B to one in A.
+
+        As a consequence of the algorithm:
+
+        - Not every code of B  necessarily appears as a :class:`dict` value.
+        - If two possible mappings have the same count, the result is undefined.
+    """
+    import pandas as pd
+
+    from message_ix_models.model.structure import get_codelist
+
+    # Retrieve the code lists
+    cl_a = get_codelist(f"node/{id_a}")
+    cl_b = get_codelist(f"node/{id_b}")
+
+    # Count the common children of each pair of items from cl_a and cl_b
+    data = []
+    for a, b in product(cl_a["World"].child, cl_b["World"].child):
+        data.append((a.id, b.id, len(set(a.child) & set(b.child))))
+
+    # Convert to data frame
+    df = pd.DataFrame(data, columns=["a", "b", "count"])
+
+    # Create mappings
+    result = []
+    for orient in "a", "b":
+        # - Sort by "a", for instance, then by count, and group by "a".
+        # - The last row in each group contains the highest count and corresponding "b".
+        # - Reassamble a data frame. The grouping column appears first.
+        df_map = df.sort_values([orient, "count"]).groupby(orient).last().reset_index()
+        # Convert to dict
+        result.append({row.iloc[0]: row.iloc[1] for _, row in df_map.iterrows()})
+
+    if debug:
+        # Show debug output
+        log.debug(
+            "\n"
+            + df.pivot(columns="a", index="b", values="count")
+            .replace(0, pd.NA)
+            .to_string()
+            .replace("<NA>", "   -")
+        )
+
+    return result[0], result[1]
 
 
 def identify_nodes(scenario: Scenario) -> str:
